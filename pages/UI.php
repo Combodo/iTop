df98e257432fb53caee1a062dcdf763b7e4065e8
Denis Flaven <denis.flaven@combodo.com>
2010-05-04 14:39:43 +0000

    Preparing the integration of the Flash navigator
    SVN:trunk[397]

--- pages/UI.php
+++ pages/UI.php
@@ -1273,6 +1273,23 @@ try
 		$oBlock->Display($oP, 0);
 		$oObj->DisplayDetails($oP);
 		break;
+		
+		case 'swf_navigator':
+		$sClass = utils::ReadParam('class', '');
+		$id = utils::ReadParam('id', 0);
+		$sRelation = utils::ReadParam('relation', 'neighbours');
+		$width = 1000;
+		$height = 700;
+		$sParams = "pWidth=$width&pHeight=$height&drillUrl=".urlencode('../pages/UI.php?operation=details')."&displayController=false&xmlUrl=".urlencode("./xml.navigator.php")."&obj_class=$sClass&obj_id=$id&relation=$sRelation";
+		
+		$oP->add("<object classid=\"clsid:d27cdb6e-ae6d-11cf-96b8-444553540000\" codebase=\"http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=7,0,0,0\" width=\"$width\" height=\"$height\" id=\"navigator\" align=\"middle\">
+		<param name=\"allowScriptAccess\" value=\"sameDomain\" />
+		<param name=\"allowFullScreen\" value=\"false\" />
+		<param name=\"FlashVars\" value=\"$sParams\" />
+		<param name=\"movie\" value=\"../navigator/navigator.swf\" /><param name=\"quality\" value=\"high\" /><param name=\"bgcolor\" value=\"#ffffff\" />
+		<embed src=\"../navigator/navigator.swf\" flashVars=\"$sParams\" quality=\"high\" bgcolor=\"#ffffff\" width=\"$width\" height=\"$height\" name=\"navigator\" align=\"middle\" allowScriptAccess=\"sameDomain\" allowFullScreen=\"false\" type=\"application/x-shockwave-flash\" pluginspage=\"http://www.adobe.com/go/getflashplayer\" />
+		</object>\n");
+		break;
 	
 		default:
 		if (is_object($oActiveNode))
