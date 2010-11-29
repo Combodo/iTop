540eb5f272b00fb8c4faef00cfbd158e77e0fc91
Romain Quetiez <romain.quetiez@combodo.com>
2010-11-29 09:17:44 +0000

    Moved the standards menus into the "welcome" module
    SVN:trunk[985]

--- modules/itop-welcome-itil/model.itop-welcome-itil.php
+++ modules/itop-welcome-itil/model.itop-welcome-itil.php
@@ -14,7 +14,43 @@
 //   along with this program; if not, write to the Free Software
 //   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
+// Add the standard menus
+/*
+ * +--------------------+
+ * | Welcome            |
+ * +--------------------+
+ * 		Welcome To iTop
+ * +--------------------+
+ * | Tools              |
+ * +--------------------+
+ * 		CSV Import
+ * +--------------------+
+ * | Admin Tools        | << Only present if the user is an admin
+ * +--------------------+
+ *		User Accounts
+ *		Profiles
+ *		Notifications
+ *		Run Queries
+ *		Export
+ *		Data Model
+ *		Universal Search
+ */
+
 $oWelcomeMenu = new MenuGroup('WelcomeMenu', 10 /* fRank */);
 new TemplateMenuNode('WelcomeMenuPage', APPROOT.'modules/itop-welcome-itil/welcome_menu.html', $oWelcomeMenu->GetIndex() /* oParent */, 1 /* fRank */);
+
+$oToolsMenu = new MenuGroup('DataAdministration', 70 /* fRank */, 'Organization', UR_ACTION_MODIFY, UR_ALLOWED_YES|UR_ALLOWED_DEPENDS);
+new WebPageMenuNode('CSVImportMenu', '../pages/csvimport.php', $oToolsMenu->GetIndex(), 1 /* fRank */);
+
+// Add the admin menus
+$oAdminMenu = new MenuGroup('AdminTools', 80 /* fRank */);
+new OQLMenuNode('UserAccountsMenu', 'SELECT User', $oAdminMenu->GetIndex(), 1 /* fRank */);
+new OQLMenuNode('ProfilesMenu', 'SELECT URP_Profiles', $oAdminMenu->GetIndex(), 2 /* fRank */);
+new TemplateMenuNode('NotificationsMenu', '../application/templates/notifications_menu.html', $oAdminMenu->GetIndex(), 3 /* fRank */);
+new OQLMenuNode('AuditCategories', 'SELECT AuditCategory', $oAdminMenu->GetIndex(), 4 /* fRank */);
+new WebPageMenuNode('RunQueriesMenu', '../pages/run_query.php', $oAdminMenu->GetIndex(), 8 /* fRank */);
+new WebPageMenuNode('ExportMenu', '../webservices/export.php', $oAdminMenu->GetIndex(), 9 /* fRank */);
+new WebPageMenuNode('DataModelMenu', '../pages/schema.php', $oAdminMenu->GetIndex(), 10 /* fRank */);
+new WebPageMenuNode('UniversalSearchMenu', '../pages/UniversalSearch.php', $oAdminMenu->GetIndex(), 11 /* fRank */);
 
 ?>
