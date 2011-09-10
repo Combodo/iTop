17f00198d35f3482a933261f8df1cd4466122ca0
Denis Flaven <denis.flaven@combodo.com>
2011-09-10 16:20:50 +0000

    Fix for #404: context was lost when performing a drill-down in the flash navigator.
    SVN:trunk[1568]

--- pages/UI.php
+++ pages/UI.php
@@ -2032,7 +2032,8 @@ EOF
 		$oP->SetCurrentTab(Dict::S('UI:RelationshipGraph'));
 		$width = 1000;
 		$height = 700;
-		$sParams = "pWidth=$width&pHeight=$height&drillUrl=".urlencode(utils::GetAbsoluteUrlAppRoot().'pages/UI.php?operation=details')."&displayController=false&xmlUrl=".urlencode("./xml.navigator.php")."&obj_class=$sClass&obj_id=$id&relation=$sRelation";
+		$sDrillUrl = utils::GetAbsoluteUrlAppRoot().'pages/UI.php?operation=details&'.$oAppContext->GetForLink();
+		$sParams = "pWidth=$width&pHeight=$height&drillUrl=".urlencode($sDrillUrl)."&displayController=false&xmlUrl=".urlencode("./xml.navigator.php")."&obj_class=$sClass&obj_id=$id&relation=$sRelation";
 		
 		$oP->add("<div style=\"z-index:1;background:white\"><object style=\"z-index:2\" classid=\"clsid:d27cdb6e-ae6d-11cf-96b8-444553540000\" codebase=\"http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=7,0,0,0\" width=\"$width\" height=\"$height\" id=\"navigator\" align=\"middle\">
 		<param name=\"allowScriptAccess\" value=\"sameDomain\" />
