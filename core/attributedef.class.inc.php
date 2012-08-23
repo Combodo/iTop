9755af9b7bfb422b8a1e6abc4ccaa14d8680c425
Denis Flaven <denis.flaven@combodo.com>
2012-08-23 14:41:16 +0000

    Bug fix: properly export boolean attributes to XML (a value of false was creating an empty XML tag)
    SVN:1.2.1[2153]

--- core/attributedef.class.inc.php
+++ core/attributedef.class.inc.php
@@ -1085,6 +1085,11 @@ class AttributeBoolean extends AttributeInteger
 		if ($value) return 1;
 		return 0;
 	}
+
+	public function GetAsXML($sValue, $oHostObject = null)
+	{
+		return $sValue ? '1' : '0';
+	}
 }
 
 /**
