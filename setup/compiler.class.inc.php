a2e33634f5306f838646215ebea052c07cd03b51
Romain Quetiez <romain.quetiez@combodo.com>
2012-09-25 17:57:12 +0000

    Compiler: fixed issue with 'initial_state_path' (the real fix...)
    SVN:trunk[2215]

--- setup/compiler.class.inc.php
+++ setup/compiler.class.inc.php
@@ -768,7 +768,7 @@ EOF;
 				}
 
 				$sLifecycle .= "				),\n";
-				if (isset($oInitialStatePath))
+				if (!is_null($oInitialStatePath))
 				{
 					$sLifecycle .= "				\"initial_state_path\" => $sInitialStatePath,\n";
 				}
