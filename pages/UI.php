bd933f3b14fe46068400f0c20ac6219a1d9ae4d1
Denis Flaven <denis.flaven@combodo.com>
2012-10-17 14:13:22 +0000

    Properly handle all types of fields when entering values via the state transition "wizard" screen
    SVN:1.2.1[2281]

--- pages/UI.php
+++ pages/UI.php
@@ -1737,13 +1737,13 @@ EOF
 								{
 									$oAttDef = MetaModel::GetAttributeDef($sClass, $sAttCode);
 									$aErrors[] = Dict::Format('UI:AttemptingToSetASlaveAttribute_Name', $oAttDef->GetLabel());
-								}
-								else
-								{
-									$oObj->Set($sAttCode, $paramValue);
+									unset($aExpectedAttributes[$sAttCode]);
 								}
 							}
 						}
+						
+						$oObj->UpdateObjectFromPostedForm('', array_keys($aExpectedAttributes), $sTargetState);
+						
 						if (count($aErrors) == 0)
 						{
 							if ($oObj->ApplyStimulus($sStimulus))
@@ -1980,13 +1980,13 @@ EOF
 						{
 							$oAttDef = MetaModel::GetAttributeDef($sClass, $sAttCode);
 							$aErrors[] = Dict::Format('UI:AttemptingToChangeASlaveAttribute_Name', $oAttDef->GetLabel());
+							unset($aExpectedAttributes[$sAttCode]);
 						}
-						else
-						{
-						$oObj->Set($sAttCode, $paramValue);
 					}
 				}
-				}
+				
+				$oObj->UpdateObjectFromPostedForm('', array_keys($aExpectedAttributes), $sTargetState);
+				
 				if (count($aErrors) == 0)
 				{
 					if ($oObj->ApplyStimulus($sStimulus))
