ae1be5a53b7551aa77d6d31ccd4dbb480cccd8c8
Romain Quetiez <romain.quetiez@combodo.com>
2012-12-06 16:54:16 +0000

    New module to link storage and virtualization
    SVN:trunk[2542]

---
+++ datamodels/2.x/itop-bridge-virtualization-storage/module.itop-bridge-virtualization-storage.php
@@ -0,0 +1,46 @@
+<?php
+
+
+SetupWebPage::AddModule(
+	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
+	'itop-bridge-virtualization-storage/2.0.0',
+	array(
+		// Identification
+		//
+		'label' => 'Links between virtualization and storage',
+		'category' => 'business',
+
+		// Setup
+		//
+		'dependencies' => array(
+		),
+		'mandatory' => false,
+		'visible' => false,
+		'auto_select' => 'SetupInfo::ModuleIsSelected("itop-storage-mgmt") && SetupInfo::ModuleIsSelected("itop-virtualization-mgmt")',
+
+		// Components
+		//
+		'datamodel' => array(
+			'model.itop-bridge-virtualization-storage.php',
+		),
+		'data.struct' => array(
+			//'data.struct.itop-change-mgmt.xml',
+		),
+		'data.sample' => array(
+			//'data.sample.itop-change-mgmt.xml',
+		),
+		
+		// Documentation
+		//
+		'doc.manual_setup' => '',
+		'doc.more_information' => '',
+
+		// Default settings
+		//
+		'settings' => array(
+		),
+	)
+);
+
+
+?>
