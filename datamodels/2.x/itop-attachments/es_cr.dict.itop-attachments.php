14d0ebdd713f0f440cd26e457ff939615096c496
Erwan Taloc <erwan.taloc@combodo.com>
2013-04-16 11:36:52 +0000

    define correct file name for en , es_cr, fr dictionnaries
    SVN:trunk[2683]

---
+++ datamodels/2.x/itop-attachments/es_cr.dict.itop-attachments.php
@@ -0,0 +1,43 @@
+<?php
+// Copyright (C) 2010-2012 Combodo SARL
+//
+//   This file is part of iTop.
+//
+//   iTop is free software; you can redistribute it and/or modify	
+//   it under the terms of the GNU Affero General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//
+//   iTop is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU Affero General Public License for more details.
+//
+//   You should have received a copy of the GNU Affero General Public License
+//   along with iTop. If not, see <http://www.gnu.org/licenses/>
+
+
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2010-2012 Combodo SARL
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('ES CR', 'Spanish', 'Español, Castellano', array(
+	'Attachments:TabTitle_Count' => 'Anexos (%1$d)',
+	'Attachments:EmptyTabTitle' => 'Anexos',
+	'Attachments:FieldsetTitle' => 'Anexos',
+	'Attachments:DeleteBtn' => 'Borrar',
+	'Attachments:History_File_Added' => 'Anexo %1$s agregado.',
+	'Attachments:History_File_Removed' => 'Anexo %1$s removido.',
+	'Attachments:AddAttachment' => 'Agregar Anexo: ',
+	'Attachments:UploadNotAllowedOnThisSystem' => 'La carga de archivos NO está permitida en este sistema.',
+	'Attachment:Max_Go' => '(Tama&ntilde;o M&aacute;ximo de Archivo: %1$s Gb)',
+	'Attachment:Max_Mo' => '(Tama&ntilde;o M&aacute;ximo de Archivo: %1$s Mb)',
+	'Attachment:Max_Ko' => '(Tama&ntilde;o M&aacute;ximo de Archivo: %1$s Kb)',
+	'Attachments:NoAttachment' => 'No hay Anexo. ',
+	'Class:Attachment' => 'Anexo',
+	'Class:Attachment+' => 'Anexo',
+  ));
+?>
---
+++ datamodels/2.x/itop-attachments/fr.dict.itop-attachments.php
@@ -0,0 +1,40 @@
+<?php
+// Copyright (C) 2010-2012 Combodo SARL
+//
+//   This file is part of iTop.
+//
+//   iTop is free software; you can redistribute it and/or modify	
+//   it under the terms of the GNU Affero General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//
+//   iTop is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU Affero General Public License for more details.
+//
+//   You should have received a copy of the GNU Affero General Public License
+//   along with iTop. If not, see <http://www.gnu.org/licenses/>
+
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2010-2012 Combodo SARL
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('FR FR', 'French', 'Français', array(
+	'Attachments:TabTitle_Count' => 'Pièces jointes (%1$d)',
+	'Attachments:EmptyTabTitle' => 'Pièces jointes',
+	'Attachments:FieldsetTitle' => 'Pièces jointes',
+	'Attachments:DeleteBtn' => 'Supprimer',
+	'Attachments:History_File_Added' => 'Ajout de la pièce jointe: %1$s.',
+	'Attachments:History_File_Removed' => 'Suppression de la pièce jointe: %1$s.',
+	'Attachments:AddAttachment' => 'Ajouter une pièce jointe: ',
+	'Attachments:UploadNotAllowedOnThisSystem' => 'Le téléchargement de fichiers est interdit sur ce système.',
+	'Attachment:Max_Go' => '(Taille de fichier max.: %1$s Gb)',
+	'Attachment:Max_Mo' => '(Taille de fichier max.: %1$s Mb)',
+	'Attachment:Max_Ko' => '(Taille de fichier max.: %1$s Kb)',
+	'Attachments:NoAttachment' => 'Aucune pièce jointe.',	
+));
+?>
