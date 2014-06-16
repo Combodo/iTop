4fccf5c8158d656a544ce53880cff7ab44d0e44c
Denis Flaven <denis.flaven@combodo.com>
2014-06-16 15:14:40 +0000

    Add the ability to supply a default "from" email address for the "forgot password" feature, instead of using the same address as for the "to".
    SVN:trunk[3213]

--- application/loginwebpage.class.inc.php
+++ application/loginwebpage.class.inc.php
@@ -244,6 +244,11 @@ class LoginWebPage extends NiceWebPage
 
 			$oEmail = new Email();
 			$oEmail->SetRecipientTO($sTo);
+			$sFrom = MetaModel::GetConfig()->Get('forgot_password_from');
+			if ($sFrom == '')
+			{
+				$sFrom = $sTo;
+			}
 			$oEmail->SetRecipientFrom($sTo);
 			$oEmail->SetSubject(Dict::S('UI:ResetPwd-EmailSubject'));
 			$sResetUrl = utils::GetAbsoluteUrlAppRoot().'pages/UI.php?loginop=reset_pwd&auth_user='.urlencode($oUser->Get('login')).'&token='.urlencode($sToken);
