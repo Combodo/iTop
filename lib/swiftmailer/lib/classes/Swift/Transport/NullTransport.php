02e5d57998c04a7baca0558babbcada23dc42325
Denis Flaven <denis.flaven@combodo.com>
2014-06-26 12:07:35 +0000

    #949 Upgraded SwiftMailer to the latest version (5.2.1)
    SVN:trunk[3225]

--- lib/swiftmailer/lib/classes/Swift/Transport/NullTransport.php
+++ lib/swiftmailer/lib/classes/Swift/Transport/NullTransport.php
@@ -10,81 +10,85 @@
 
 /**
  * Pretends messages have been sent, but just ignores them.
+ *
  * @package Swift
  * @author  Fabien Potencier
  */
 class Swift_Transport_NullTransport implements Swift_Transport
 {
-  /** The event dispatcher from the plugin API */
-  private $_eventDispatcher;
+    /** The event dispatcher from the plugin API */
+    private $_eventDispatcher;
 
-  /**
-   * Constructor.
-   */
-  public function __construct(Swift_Events_EventDispatcher $eventDispatcher)
-  {
-    $this->_eventDispatcher = $eventDispatcher;
-  }
-  
-  /**
-   * Tests if this Transport mechanism has started.
-   *
-   * @return boolean
-   */
-  public function isStarted()
-  {
-    return true;
-  }
-  
-  /**
-   * Starts this Transport mechanism.
-   */
-  public function start()
-  {
-  }
-  
-  /**
-   * Stops this Transport mechanism.
-   */
-  public function stop()
-  {
-  }
-  
-  /**
-   * Sends the given message.
-   *
-   * @param Swift_Mime_Message $message
-   * @param string[] &$failedRecipients to collect failures by-reference
-   *
-   * @return int The number of sent emails
-   */
-  public function send(Swift_Mime_Message $message, &$failedRecipients = null)
-  {
-    if ($evt = $this->_eventDispatcher->createSendEvent($this, $message))
+    /**
+     * Constructor.
+     */
+    public function __construct(Swift_Events_EventDispatcher $eventDispatcher)
     {
-      $this->_eventDispatcher->dispatchEvent($evt, 'beforeSendPerformed');
-      if ($evt->bubbleCancelled())
-      {
-        return 0;
-      }
+        $this->_eventDispatcher = $eventDispatcher;
     }
-    
-    if ($evt)
+
+    /**
+     * Tests if this Transport mechanism has started.
+     *
+     * @return bool
+     */
+    public function isStarted()
     {
-      $evt->setResult(Swift_Events_SendEvent::RESULT_SUCCESS);
-      $this->_eventDispatcher->dispatchEvent($evt, 'sendPerformed');
+        return true;
     }
-    
-    return 0;
-  }
-  
-  /**
-   * Register a plugin.
-   *
-   * @param Swift_Events_EventListener $plugin
-   */
-  public function registerPlugin(Swift_Events_EventListener $plugin)
-  {
-    $this->_eventDispatcher->bindEventListener($plugin);
-  }
+
+    /**
+     * Starts this Transport mechanism.
+     */
+    public function start()
+    {
+    }
+
+    /**
+     * Stops this Transport mechanism.
+     */
+    public function stop()
+    {
+    }
+
+    /**
+     * Sends the given message.
+     *
+     * @param Swift_Mime_Message $message
+     * @param string[]           $failedRecipients An array of failures by-reference
+     *
+     * @return int     The number of sent emails
+     */
+    public function send(Swift_Mime_Message $message, &$failedRecipients = null)
+    {
+        if ($evt = $this->_eventDispatcher->createSendEvent($this, $message)) {
+            $this->_eventDispatcher->dispatchEvent($evt, 'beforeSendPerformed');
+            if ($evt->bubbleCancelled()) {
+                return 0;
+            }
+        }
+
+        if ($evt) {
+            $evt->setResult(Swift_Events_SendEvent::RESULT_SUCCESS);
+            $this->_eventDispatcher->dispatchEvent($evt, 'sendPerformed');
+        }
+
+        $count = (
+            count((array) $message->getTo())
+            + count((array) $message->getCc())
+            + count((array) $message->getBcc())
+            );
+
+        return $count;
+    }
+
+    /**
+     * Register a plugin.
+     *
+     * @param Swift_Events_EventListener $plugin
+     */
+    public function registerPlugin(Swift_Events_EventListener $plugin)
+    {
+        $this->_eventDispatcher->bindEventListener($plugin);
+    }
 }
