Combodo/iTop

commit 380acc1fd46f671296af354cba2dc9a3b9bb9a03

#932 Search form should be prefilled when running a search "shortcut" - very little progress: fixed the case when several criteria are given
git-svn-id: http://svn.combodo.net/subversion/itop.sf.net/trunk@3319 60fe80c7-c1f8-43be-bd32-c3e8feb7a3b0

core/expression.class.inc.php
@@ -301,12 +301,14 @@ class BinaryExpression extends Expression
 			}
 			else
 			{
-				$aResult = array_merge($this->m_oRightExpr->ListConstantFields(), $this->m_oLeftExpr->ListConstantFields()) ;
+				// Strictly, this should be removed
+				$aResult = array_merge_recursive($this->m_oRightExpr->ListConstantFields(), $this->m_oLeftExpr->ListConstantFields());
 			}
 		}
 		else
 		{
-			$aResult = array_merge($this->m_oRightExpr->ListConstantFields(), $this->m_oLeftExpr->ListConstantFields()) ;
+			// Strictly, this should be done only for the AND operator
+			$aResult = array_merge_recursive($this->m_oRightExpr->ListConstantFields(), $this->m_oLeftExpr->ListConstantFields());
 		}
 		return $aResult;
 	}

commit 82a8a0bba7ea8ee430942e472facc80246665a68

#932: fixed a regression introduced by [r3319]... in case a criteria is present several times.
SVN:trunk[3344]

core/dbobjectset.class.php
@@ -1098,8 +1098,11 @@ class DBObjectSet
 		{
 			foreach($aVals as $sCode => $oExpr)
 			{
-				$oScalarExpr = $oExpr->GetAsScalar($aScalarArgs);
-				$aConst[$sClassAlias][$sCode] = $oScalarExpr->GetValue();
+				if (is_object($oExpr)) // Array_merge_recursive creates an array when the same key is present multiple times... ignore them
+				{
+					$oScalarExpr = $oExpr->GetAsScalar($aScalarArgs);
+					$aConst[$sClassAlias][$sCode] = $oScalarExpr->GetValue();
+				}
 			}
 		}
 		return $aConst;		

core/expression.class.inc.php
@@ -299,13 +299,8 @@ class BinaryExpression extends Expression
 			{
 				$aResult[$this->m_oRightExpr->GetParent()][$this->m_oRightExpr->GetName()] = $this->m_oLeftExpr;
 			}
-			else
-			{
-				// Strictly, this should be removed
-				$aResult = array_merge_recursive($this->m_oRightExpr->ListConstantFields(), $this->m_oLeftExpr->ListConstantFields());
-			}
 		}
-		else
+		else if ($this->m_sOperator == 'AND')
 		{
 			// Strictly, this should be done only for the AND operator
 			$aResult = array_merge_recursive($this->m_oRightExpr->ListConstantFields(), $this->m_oLeftExpr->ListConstantFields());