8025dcbc3762d7b55bc791eee33d111405f090c4
Romain Quetiez <romain.quetiez@combodo.com>
2015-10-12 15:40:44 +0000

    Fixed regressions due to the recent code refactoring [3803]
    SVN:2.2.0[3808]

---
+++ core/expression.class.inc.php
@@ -0,0 +1,4 @@
+<?php
+// The file has been moved in iTop 2.2.0+ (revision 3803)
+// Preserve backward compatibility with some external tools (Cf. toolkit)
+require_once(APPROOT.'core/oql/expression.class.inc.php');
