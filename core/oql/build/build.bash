96b10baeeea673d6a3e718f33e82043c029dd5e5
Denis Flaven <denis.flaven@combodo.com>
2015-10-12 09:55:52 +0000

    Added a version number (arbitrary initialized to 2015-08-31 for iTop v2.2.0) to the OQL Lexer/parser.
    SVN:2.2.0[3802]

--- core/oql/build/build.bash
+++ core/oql/build/build.bash
@@ -1,4 +1,11 @@
 #!/bin/bash
-php PHP/LexerGenerator/cli.php oql-lexer.plex
-php PHP/ParserGenerator/cli.php oql-parser.y
+#
+# Rebuild the iTop Lexer / Parser
+# PEAR is required to build (really?)
+# Launch this batch from the core/oql/build directory
+# with ./build.bash
+#
+php PHP/LexerGenerator/cli.php ../oql-lexer.plex
+php PHP/ParserGenerator/cli.php ../oql-parser.y
+php -r "echo date('Y-m-d');" > ../version.txt
 
