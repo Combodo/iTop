57f0cce318b2a13f983ccba6fe8e9c532076f312
Guillaume Lajarige <guillaume.lajarige@combodo.com>
2016-01-11 15:32:14 +0000

    Added Silex framework
    SVN:trunk[3855]

---
+++ lib/silex/vendor/silex/silex/doc/intro.rst
@@ -0,0 +1,67 @@
+Introduction
+============
+
+Silex is a PHP microframework for PHP 5.3. It is built on the shoulders of
+`Symfony2`_ and `Pimple`_ and also inspired by `Sinatra`_.
+
+A microframework provides the guts for building simple single-file apps. Silex
+aims to be:
+
+* *Concise*: Silex exposes an intuitive and concise API that is fun to use.
+
+* *Extensible*: Silex has an extension system based around the Pimple micro
+  service-container that makes it even easier to tie in third party libraries.
+
+* *Testable*: Silex uses Symfony2's HttpKernel which abstracts request and
+  response. This makes it very easy to test apps and the framework itself. It
+  also respects the HTTP specification and encourages its proper use.
+
+In a nutshell, you define controllers and map them to routes, all in one step.
+
+Usage
+-----
+
+.. code-block:: php
+
+    <?php
+
+    // web/index.php
+    require_once __DIR__.'/../vendor/autoload.php';
+
+    $app = new Silex\Application();
+
+    $app->get('/hello/{name}', function ($name) use ($app) {
+        return 'Hello '.$app->escape($name);
+    });
+
+    $app->run();
+
+All that is needed to get access to the Framework is to include the
+autoloader.
+
+Next a route for ``/hello/{name}`` that matches for ``GET`` requests is defined.
+When the route matches, the function is executed and the return value is sent
+back to the client.
+
+Finally, the app is run. Visit ``/hello/world`` to see the result. It's really
+that easy!
+
+Installation
+------------
+
+Installing Silex is as easy as it can get: 
+
+.. code-block:: bash
+
+    composer require silex/silex
+
+Another way is to `download`_ the archive file and extract it.
+
+All examples in the documentation relies on a well-configured web server; read
+the :doc:`webserver documentation<web_servers>` to check yours.
+
+.. _Symfony2: http://symfony.com/
+.. _Pimple: http://pimple.sensiolabs.org/
+.. _Sinatra: http://www.sinatrarb.com/
+.. _Composer: http://getcomposer.org/
+.. _`download`: http://silex.sensiolabs.org/download
---
+++ lib/silex/vendor/silex/silex/tests/Silex/Tests/Provider/FormServiceProviderTest.php
@@ -0,0 +1,176 @@
+<?php
+
+/*
+ * This file is part of the Silex framework.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Silex\Tests\Provider;
+
+use Silex\Application;
+use Silex\Provider\FormServiceProvider;
+use Silex\Provider\TranslationServiceProvider;
+use Silex\Provider\ValidatorServiceProvider;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\Extension\Csrf\CsrfProvider\CsrfProviderInterface;
+use Symfony\Component\Form\FormTypeGuesserChain;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Translation\Exception\NotFoundResourceException;
+
+class FormServiceProviderTest extends \PHPUnit_Framework_TestCase
+{
+    public function testFormFactoryServiceIsFormFactory()
+    {
+        $app = new Application();
+        $app->register(new FormServiceProvider());
+        $this->assertInstanceOf('Symfony\Component\Form\FormFactory', $app['form.factory']);
+    }
+
+    public function testFormServiceProviderWillLoadTypes()
+    {
+        $app = new Application();
+
+        $app->register(new FormServiceProvider());
+
+        $app['form.types'] = $app->share($app->extend('form.types', function ($extensions) {
+            $extensions[] = new DummyFormType();
+
+            return $extensions;
+        }));
+
+        $form = $app['form.factory']->createBuilder('form', array())
+            ->add('dummy', 'dummy')
+            ->getForm();
+
+        $this->assertInstanceOf('Symfony\Component\Form\Form', $form);
+    }
+
+    public function testFormServiceProviderWillLoadTypeExtensions()
+    {
+        $app = new Application();
+
+        $app->register(new FormServiceProvider());
+
+        $app['form.type.extensions'] = $app->share($app->extend('form.type.extensions', function ($extensions) {
+            $extensions[] = new DummyFormTypeExtension();
+
+            return $extensions;
+        }));
+
+        $form = $app['form.factory']->createBuilder('form', array())
+            ->add('file', 'file', array('image_path' => 'webPath'))
+            ->getForm();
+
+        $this->assertInstanceOf('Symfony\Component\Form\Form', $form);
+    }
+
+    public function testFormServiceProviderWillLoadTypeGuessers()
+    {
+        $app = new Application();
+
+        $app->register(new FormServiceProvider());
+
+        $app['form.type.guessers'] = $app->share($app->extend('form.type.guessers', function ($guessers) {
+            $guessers[] = new FormTypeGuesserChain(array());
+
+            return $guessers;
+        }));
+
+        $this->assertInstanceOf('Symfony\Component\Form\FormFactory', $app['form.factory']);
+    }
+
+    public function testFormServiceProviderWillUseTranslatorIfAvailable()
+    {
+        $app = new Application();
+
+        $app->register(new FormServiceProvider());
+        $app->register(new TranslationServiceProvider());
+        $app['translator.domains'] = array(
+            'messages' => array(
+                'de' => array(
+                    'The CSRF token is invalid. Please try to resubmit the form.' => 'German translation',
+                ),
+            ),
+        );
+        $app['locale'] = 'de';
+
+        $app['form.csrf_provider'] = $app->share(function () {
+            return new FakeCsrfProvider();
+        });
+
+        $form = $app['form.factory']->createBuilder('form', array())
+            ->getForm();
+
+        $form->handleRequest($req = Request::create('/', 'POST', array('form' => array(
+            '_token' => 'the wrong token',
+        ))));
+
+        $this->assertFalse($form->isValid());
+        $this->assertContains('ERROR: German translation', $form->getErrorsAsString());
+    }
+
+    public function testFormServiceProviderWillNotAddNonexistentTranslationFiles()
+    {
+        $app = new Application(array(
+            'locale' => 'nonexistent',
+        ));
+
+        $app->register(new FormServiceProvider());
+        $app->register(new ValidatorServiceProvider());
+        $app->register(new TranslationServiceProvider(), array(
+            'locale_fallbacks' => array(),
+        ));
+
+        $app['form.factory'];
+        $translator = $app['translator'];
+
+        try {
+            $translator->trans('test');
+        } catch (NotFoundResourceException $e) {
+            $this->fail('Form factory should not add a translation resource that does not exist');
+        }
+    }
+}
+
+class DummyFormType extends AbstractType
+{
+    /**
+     * @return string The name of this type
+     */
+    public function getName()
+    {
+        return 'dummy';
+    }
+}
+
+class DummyFormTypeExtension extends AbstractTypeExtension
+{
+    public function getExtendedType()
+    {
+        return 'file';
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setOptional(array('image_path'));
+    }
+}
+
+class FakeCsrfProvider implements CsrfProviderInterface
+{
+    public function generateCsrfToken($intention)
+    {
+        return $intention.'123';
+    }
+
+    public function isCsrfTokenValid($intention, $token)
+    {
+        return $token === $this->generateCsrfToken($intention);
+    }
+}
