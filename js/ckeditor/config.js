3687657dd78316f4abd3bb97d7b119b045622409
Denis Flaven <denis.flaven@combodo.com>
2016-02-19 09:23:45 +0000

    Upgrading to CKEditor v4
    SVN:trunk[3914]

--- js/ckeditor/config.js
+++ js/ckeditor/config.js
@@ -1,64 +1,31 @@
-/*
-Copyright (c) 2003-2010, CKSource - Frederico Knabben. All rights reserved.
-For licensing, see LICENSE.html or http://ckeditor.com/license
-*/
+/**
+ * @license Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
 
-CKEDITOR.editorConfig = function( config )
-{
-	//config.customConfig // to ovveride config.js
+CKEDITOR.editorConfig = function( config ) {
+	config.toolbarGroups = [
+		{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
+		{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
+		{ name: 'links', groups: [ 'links' ] },
+		{ name: 'insert', groups: [ 'insert' ] },
+		{ name: 'forms', groups: [ 'forms' ] },
+		{ name: 'tools', groups: [ 'tools' ] },
+		{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
+		{ name: 'others', groups: [ 'others' ] },
+		'/',
+		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+		{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
+		{ name: 'styles', groups: [ 'styles' ] },
+		{ name: 'colors', groups: [ 'colors' ] },
+		{ name: 'about', groups: [ 'about' ] }
+	];
 
-	// The language will be set from within iTop, depending on the current user
-	// config.language = 'de'; // german
-	// config.contentsLanguage = 'de'; // german
-
-	// Make it more concise for iTop forms
+	config.removeButtons = 'Underline,Subscript,Superscript,Scayt,Anchor,Source,Outdent,Indent,Blockquote,About,PasteFromWord';
 	config.removePlugins = 'elementspath';
+	config.toolbarCanCollapse = true;
 	config.toolbarStartupExpanded = false;
-	config.toolbar = 'itop';
-	//config.toolbar_basic
-	//config.toolbar_Full
-	config.toolbar_itop =
-	[
-		['Source'],
-		['Cut','Copy','Paste','PasteText','PasteFromWord','-','Print'],
-		['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
-		'/',
-		['Bold','Italic','Underline','Strike'],
-		['NumberedList','BulletedList','-','Outdent','Indent'],
-		['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
-		['Link','Unlink','Anchor'],
-		['Image','Table','HorizontalRule','Smiley','SpecialChar','PageBreak'],
-		'/',
-		['Styles','Format','Font','FontSize'],
-		['TextColor','BGColor'],
-		['Maximize', 'ShowBlocks','-','About']
-	];
-	config.toolbar_Full =
-	[
-		['Source','-','Save','NewPage','Preview','-','Templates'],
-		['Cut','Copy','Paste','PasteText','PasteFromWord','-','Print', 'SpellChecker', 'Scayt'],
-		['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
-		['Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 'HiddenField'],
-		'/',
-		['Bold','Italic','Underline','Strike','-','Subscript','Superscript'],
-		['NumberedList','BulletedList','-','Outdent','Indent','Blockquote','CreateDiv'],
-		['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
-		['BidiLtr', 'BidiRtl' ],
-		['Link','Unlink','Anchor'],
-		['Image','Flash','Table','HorizontalRule','Smiley','SpecialChar','PageBreak'],
-		'/',
-		['Styles','Format','Font','FontSize'],
-		['TextColor','BGColor'],
-		['Maximize', 'ShowBlocks','-','About']
-	];
-
-	// Little value and even disturbing when it comes to describing very technical stuff
-	config.disableNativeSpellChecker = true;
-
-	// Handling of doc/image uploads
-	// config.filebrowserImageUploadUrl
-	// config.filebrowserUploadUrl
-
-	// + it is possible to define the supported tags (p, pre, etc) and the corresponding class.
-
 };
+
+
+
