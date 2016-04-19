Combodo/iTop

commit 384058c996bea9db2634776de8077a3497beff6b

#185 Navigation Breadcrumb - A beta version, based on the navigation history. Comments welcome!
git-svn-id: https://svn.code.sf.net/p/itop/code/trunk@4000 a333f486-631f-4898-b8df-5754b55c2be0

application/menunode.class.inc.php
@@ -1,5 +1,5 @@
 <?php
-// Copyright (C) 2010-2013 Combodo SARL
+// Copyright (C) 2010-2016 Combodo SARL
 //
 //   This file is part of iTop.
 //
@@ -20,7 +20,7 @@
 /**
  * Construction and display of the application's main menu
  *
- * @copyright   Copyright (C) 2010-2012 Combodo SARL
+ * @copyright   Copyright (C) 2010-2016 Combodo SARL
  * @license     http://opensource.org/licenses/AGPL-3.0
  */
 
@@ -297,16 +297,26 @@ class ApplicationMenu
 		$sMenuId = $oAppContext->GetCurrentValue('menu', null);		
 		if ($sMenuId  === null)
 		{
+			$sMenuId == self::GetDefaultMenuId();
+		}
+		return $sMenuId;
+	}	
+
+	static public function GetDefaultMenuId()
+	{
+		static $sDefaultMenuId = null;
+		if (is_null($sDefaultMenuId))
+		{
 			// Make sure the root menu is sorted on 'rank'
 			usort(self::$aRootMenus, array('ApplicationMenu', 'CompareOnRank'));
 			$oFirstGroup = self::GetMenuNode(self::$aRootMenus[0]['index']);
 			$aChildren = self::$aMenusIndex[$oFirstGroup->GetIndex()]['children'];
 			usort($aChildren, array('ApplicationMenu', 'CompareOnRank'));
 			$oMenuNode = self::GetMenuNode($aChildren[0]['index']);
-			$sMenuId = $oMenuNode->GetMenuId();
+			$sDefaultMenuId = $oMenuNode->GetMenuId();
 		}
-		return $sMenuId;
-	}	
+		return $sDefaultMenuId;
+	}
 }
 
 /**
@@ -339,6 +349,7 @@ abstract class MenuNode
 {
 	protected $sMenuId;
 	protected $index;
+	protected $iParentIndex;
 
 	/**
 	 * Properties reflecting how the node has been declared
@@ -379,6 +390,7 @@ abstract class MenuNode
 	public function __construct($sMenuId, $iParentIndex = -1, $fRank = 0, $sEnableClass = null, $iActionCode = null, $iAllowedResults = UR_ALLOWED_YES, $sEnableStimulus = null)
 	{
 		$this->sMenuId = $sMenuId;
+		$this->iParentIndex = $iParentIndex;
 		$this->aReflectionProperties = array();
 		if (strlen($sEnableClass) > 0)
 		{
@@ -411,7 +423,21 @@ abstract class MenuNode
 	
 	public function GetLabel()
 	{
-		return Dict::S("Menu:$this->sMenuId+", "");
+		$sRet = Dict::S("Menu:$this->sMenuId+", "");
+		if ($sRet === '')
+		{
+			if ($this->iParentIndex != -1)
+			{
+				$oParentMenu = ApplicationMenu::GetMenuNode($this->iParentIndex);
+				$sRet = $oParentMenu->GetTitle().' / '.$this->GetTitle();
+			}
+			else
+			{
+				$sRet = $this->GetTitle();
+			}
+			//$sRet = $this->GetTitle();
+		}
+		return $sRet;
 	}
 	
 	public function GetIndex()
@@ -647,11 +673,12 @@ class OQLMenuNode extends MenuNode
 			$this->bSearch, // Search pane
 			true, // Search open
 			$oPage, 
-			array_merge($this->m_aParams, $aExtraParams)
+			array_merge($this->m_aParams, $aExtraParams),
+			true
 		);
 	}
 
-	public static function RenderOQLSearch($sOql, $sTitle, $sUsageId, $bSearchPane, $bSearchOpen, WebPage $oPage, $aExtraParams = array())
+	public static function RenderOQLSearch($sOql, $sTitle, $sUsageId, $bSearchPane, $bSearchOpen, WebPage $oPage, $aExtraParams = array(), $bEnableBreadcrumb = false)
 	{
 		$sUsageId = utils::GetSafeId($sUsageId);
 		$oSearch = DBObjectSearch::FromOQL($sOql);
@@ -669,6 +696,15 @@ class OQLMenuNode extends MenuNode
 		$aParams = array_merge(array('table_id' => $sUsageId), $aExtraParams);
 		$oBlock = new DisplayBlock($oSearch, 'list', false /* Asynchronous */, $aParams);
 		$oBlock->Display($oPage, $sUsageId);
+
+		if ($bEnableBreadcrumb && ($oPage instanceof iTopWebPage))
+		{
+			// Breadcrumb
+			//$iCount = $oBlock->GetDisplayedCount();
+			$sPageId = "ui-search-".$oSearch->GetClass();
+			$sLabel = MetaModel::GetName($oSearch->GetClass());
+			$oPage->SetBreadCrumbEntry($sPageId, $sLabel, $sTitle, '', '../images/breadcrumb-search.png');
+		}
 	}
 }
 
@@ -920,6 +956,29 @@ EOF
 				$sId = addslashes($this->sMenuId);
 				$oPage->add_ready_script("EditDashboard('$sId');");
 			}
+			else
+			{
+				$oParentMenu = ApplicationMenu::GetMenuNode($this->iParentIndex);
+				$sParentTitle = $oParentMenu->GetTitle();
+				$sThisTitle = $this->GetTitle();
+				if ($sParentTitle != $sThisTitle)
+				{
+					$sDescription = $sParentTitle.' / '.$sThisTitle;
+				}
+				else
+				{
+					$sDescription = $sThisTitle;
+				}
+				if ($this->sMenuId == ApplicationMenu::GetDefaultMenuId())
+				{
+					$sIcon = '../images/breadcrumb_home.png';
+				}
+				else
+				{
+					$sIcon = '../images/breadcrumb-dashboard.png';
+				}
+				$oPage->SetBreadCrumbEntry("ui-dashboard-".$this->sMenuId, $this->GetTitle(), $sDescription, '', $sIcon);
+			}
 		}
 		else
 		{
@@ -950,7 +1009,7 @@ EOF
 		}
 		else
 		{
-			$oPage->p("Error: failed to load dashboard file: '{$this->sDashboardFile}'");
+			throw new Exception("Error: failed to load dashboard file: '{$this->sDashboardFile}'");
 		}
 	}
 	

application/displayblock.class.inc.php
@@ -1203,6 +1203,15 @@ EOF
 		$oNewCondition = Expression::FromOQL($sOQLCondition);
 		return $oNewCondition;		
 	}
+
+	/**
+	 * For the result to be meaningful, this function must be called AFTER GetRenderContent() (or Display())
+	 * @return int
+	 */
+	public function GetDisplayedCount()
+	{
+		return $this->m_oSet->Count();
+	}
 }
 
 /**

application/itopwebpage.class.inc.php
@@ -37,6 +37,11 @@ class iTopWebPage extends NiceWebPage implements iTabbedPage
 	private $m_sMessage;
 	private $m_sInitScript;
 	protected $m_oTabs;
+	protected $sBreadCrumbEntryId;
+	protected $sBreadCrumbEntryLabel;
+	protected $sBreadCrumbEntryDescription;
+	protected $sBreadCrumbEntryUrl;
+	protected $sBreadCrumbEntryIcon;
 
 	public function __construct($sTitle, $bPrintable = false)
 	{
@@ -44,6 +49,12 @@ class iTopWebPage extends NiceWebPage implements iTabbedPage
 		$this->m_oTabs = new TabManager();
 
 		ApplicationContext::SetUrlMakerClass('iTopStandardURLMaker');
+
+		$this->sBreadCrumbEntryId = null;
+		$this->sBreadCrumbEntryLabel = null;
+		$this->sBreadCrumbEntryDescription = null;
+		$this->sBreadCrumbEntryUrl = null;
+		$this->sBreadCrumbEntryIcon = '';
 
 		$this->m_sMenu = "";
 		$this->m_sMessage = '';
@@ -55,7 +66,7 @@ class iTopWebPage extends NiceWebPage implements iTabbedPage
 		$this->add_linked_stylesheet("../css/fg.menu.css");
 		$this->add_linked_stylesheet("../css/jquery.multiselect.css");
 		$this->add_linked_stylesheet("../css/magnific-popup.css");
-		
+
 		$this->add_linked_script('../js/jquery.layout.min.js');
 		$this->add_linked_script('../js/jquery.ba-bbq.min.js');
 		$this->add_linked_script("../js/jquery.treeview.js");
@@ -79,7 +90,8 @@ class iTopWebPage extends NiceWebPage implements iTabbedPage
 		$this->add_linked_script('../js/ajaxfileupload.js');
 		$this->add_linked_script('../js/jquery.mousewheel.js');
 		$this->add_linked_script('../js/jquery.magnific-popup.min.js');
-			
+		$this->add_linked_script('../js/breadcrumb.js');
+
 		
 		$sSearchAny = addslashes(Dict::S('UI:SearchValue:Any'));
 		$sSearchNbSelected = addslashes(Dict::S('UI:SearchValue:NbSelected'));
@@ -244,7 +256,7 @@ EOF;
 				});
 			}
 		});
-					
+
 		$('.resizable').filter(':visible').resizable();
 	}
 	catch(err)
@@ -517,7 +529,23 @@ EOF
 		}
 EOF
 		);
-	}	
+	}
+
+	/**
+	 * @param string $sId Identifies the item, to search after it in the current breadcrumb
+	 * @param string $sLabel Label of the breadcrumb item
+	 * @param string $sDescription More information, displayed as a tooltip
+	 * @param string $sUrl Specify a URL if the current URL as perceived on the browser side is not relevant
+	 * @param string $sIcon Icon (relative or absolute) path that will be displayed next to the label
+	 */
+	public function SetBreadCrumbEntry($sId, $sLabel, $sDescription, $sUrl = '', $sIcon = '')
+	{
+		$this->sBreadCrumbEntryId = $sId;
+		$this->sBreadCrumbEntryLabel = $sLabel;
+		$this->sBreadCrumbEntryDescription = $sDescription;
+		$this->sBreadCrumbEntryUrl = $sUrl;
+		$this->sBreadCrumbEntryIcon = $sIcon;
+	}
 
 	public function AddToMenu($sHtml)
 	{
@@ -643,7 +671,9 @@ EOF
 			'selectedList' => 1,
 		);
 		$sJSMultiselectOptions = json_encode($aMultiselectOptions);
-		
+
+		$siTopInstanceId = json_encode(APPROOT);
+		$sNewEntry = is_null($this->sBreadCrumbEntryId) ? 'null' : json_encode(array('id' => $this->sBreadCrumbEntryId, 'url' => $this->sBreadCrumbEntryUrl, 'label' => htmlentities($this->sBreadCrumbEntryLabel, ENT_QUOTES, 'UTF-8'), 'description' => htmlentities($this->sBreadCrumbEntryDescription, ENT_QUOTES, 'UTF-8'), 'icon' => $this->sBreadCrumbEntryIcon));
 		$this->add_ready_script(
 <<<EOF
 		// Since the event is only triggered when the hash changes, we need to trigger
@@ -654,6 +684,8 @@ EOF
 		$('table.listResults').each( function() { FixTableSorter($(this)); } );
 		
 		$('.multiselect').multiselect($sJSMultiselectOptions);
+
+		$('#itop-breadcrumb').breadcrumb({itop_instance_id: $siTopInstanceId, new_entry: $sNewEntry});
 
 		FixSearchFormsDisposition();
 
@@ -897,7 +929,7 @@ EOF
 			
 			if (!empty($sNorthPane))
 			{
-				$sNorthPane = '<div id="bottom-pane" class="ui-layout-north">'.$sNorthPane.'</div>';
+				$sNorthPane = '<div id="top-pane" class="ui-layout-north">'.$sNorthPane.'</div>';
 			}
 			
 			if (!empty($sSouthPane))
@@ -944,13 +976,27 @@ EOF
 			$sHtml .= '</div>';
 
 			$sHtml .= '<div class="ui-layout-center">';
-			$sHtml .= ' <div id="top-bar" style="width:100%">';
+			$sHtml .= ' <div id="top-bar" class="ui-helper-clearfix" style="width:100%">';
 			$sHtml .= self::FilterXSS($sApplicationBanner);
+
+			$sHtml .= ' <table id="top-bar-table">';
+			$sHtml .= ' <tr>';
+			$sHtml .= ' <td id="top-bar-table-breadcrumb">';
+			$sHtml .= ' <div id="itop-breadcrumb"></div>';
+			$sHtml .= ' </td>';
+			$sHtml .= ' <td id="top-bar-table-search">';
 			$sHtml .= '		<div id="global-search"><form action="'.utils::GetAbsoluteUrlAppRoot().'pages/UI.php"><table><tr><td></td><td><div id="global-search-area"><input id="global-search-input" type="text" name="text" placeholder="'.$sText.'"></input><div '.$sOnClick.' id="global-search-image"></div></div></td>';
-			//$sHtml .= '<td><input type="image" src="../images/searchBtn.png"/></a></td>';
-			$sHtml .= '<td><a id="help-link" href="'.$sOnlineHelpUrl.'" target="_blank"><img title="'.Dict::S('UI:Help').'" src="../images/help.png?itopversion='.ITOP_VERSION.'"/></td>';
-			$sHtml .= '<td>'.self::FilterXSS($sLogOffMenu).'</td><td><input type="hidden" name="operation" value="full_text"/></td></tr></table></form></div>';
-			//echo '<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="hidden" name="operation" value="full_text"/></td></tr></table></form></div>';
+			$sHtml .= '     <td><a id="help-link" href="'.$sOnlineHelpUrl.'" target="_blank"><img title="'.Dict::S('UI:Help').'" src="../images/help.png?itopversion='.ITOP_VERSION.'"/></td>';
+			$sHtml .= '     <td>'.self::FilterXSS($sLogOffMenu).'</td><td><input type="hidden" name="operation" value="full_text"/></td></tr></table></form></div>';
+			$sHtml .= ' </td>';
+			$sHtml .= ' </tr>';
+			$sHtml .= ' </table>';
+
+//			$sHtml .= '		<div id="global-search"><form action="'.utils::GetAbsoluteUrlAppRoot().'pages/UI.php"><table><tr><td></td><td><div id="global-search-area"><input id="global-search-input" type="text" name="text" placeholder="'.$sText.'"></input><div '.$sOnClick.' id="global-search-image"></div></div></td>';
+//			$sHtml .= '<td><a id="help-link" href="'.$sOnlineHelpUrl.'" target="_blank"><img title="'.Dict::S('UI:Help').'" src="../images/help.png?itopversion='.ITOP_VERSION.'"/></td>';
+//			$sHtml .= '<td>'.self::FilterXSS($sLogOffMenu).'</td><td><input type="hidden" name="operation" value="full_text"/></td></tr></table></form></div>';
+//			$sHtml .= ' <div id="itop-breadcrumb"></div>';
+
 			$sHtml .= ' </div>';
 			$sHtml .= ' <div class="ui-layout-content" style="overflow:auto;">';
 			$sHtml .= ' <!-- Beginning of page content -->';

application/shortcut.class.inc.php
@@ -1,5 +1,5 @@
 <?php
-// Copyright (C) 2010-2013 Combodo SARL
+// Copyright (C) 2010-2016 Combodo SARL
 //
 //   This file is part of iTop.
 //
@@ -21,7 +21,7 @@
  * Persistent class Shortcut and derived
  * Shortcuts of any kind
  *
- * @copyright   Copyright (C) 2010-2012 Combodo SARL
+ * @copyright   Copyright (C) 2010-2016 Combodo SARL
  * @license     http://opensource.org/licenses/AGPL-3.0
  */
 
@@ -200,7 +200,7 @@ class ShortcutOQL extends Shortcut
 		$bSearchOpen = false;
 		try
 		{
-			OQLMenuNode::RenderOQLSearch($this->Get('oql'), $this->Get('name'), 'shortcut_'.$this->GetKey(), $bSearchPane, $bSearchOpen, $oPage, $aExtraParams);
+			OQLMenuNode::RenderOQLSearch($this->Get('oql'), $this->Get('name'), 'shortcut_'.$this->GetKey(), $bSearchPane, $bSearchOpen, $oPage, $aExtraParams, true);
 		}
 		catch (Exception $e)
 		{

css/light-grey.css
@@ -1151,7 +1151,6 @@ div#logo div {
 
 
 #top-bar {
-  height: 55px;
   background: #f1f1f1;
   text-align: right;
 }
@@ -2198,6 +2197,74 @@ span.refresh-button {
 
 .history_entry_truncated .history_truncated_toggler {
   background-position: 0 -192px;
+}
+
+
+#top-bar-table {
+  width: 100%;
+}
+
+#top-bar-table #top-bar-table-search {
+  width: 347px;
+}
+
+
+#itop-breadcrumb {
+  overflow: hidden;
+  float: left;
+  background: #f1f1f1;
+  margin-left: -20px;
+}
+
+#itop-breadcrumb li {
+  list-style: none;
+  float: left;
+  margin: 0 22px 6px 0;
+}
+#itop-breadcrumb li .icon img {
+  height: 15px;
+  width: auto;
+  margin-right: 5px;
+  -webkit-filter: grayscale(100%);
+  filter: grayscale(100%);
+  filter: gray;
+  filter: url("data:image/svg+xml;utf8,<svg version='1.1' xmlns='http://www.w3.org/2000/svg' height='0'><filter id='greyscale'><feColorMatrix type='matrix' values='0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0 0 0 1 0' /></filter></svg>#greyscale");
+  opacity: 0.5;
+}
+#itop-breadcrumb li:hover .icon img {
+  -webkit-filter: none;
+  filter: none;
+  opacity: 1;
+}
+#itop-breadcrumb li a {
+  text-decoration: none;
+  color: #555555;
+  font-size: 9pt;
+  padding: 0;
+  background: none;
+}
+#itop-breadcrumb li a span.truncate {
+  max-width: 200px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: inline-block;
+}
+#itop-breadcrumb li a:hover {
+  text-decoration: none;
+  color: #e87c1e;
+}
+#itop-breadcrumb li a::after {
+  content: '';
+  position: absolute;
+  background-image: url(../images/breadcrumb-separator.png?v=v2.2.0);
+  background-repeat: no-repeat;
+  width: 8px;
+  height: 16px;
+  margin-left: 5px;
+}
+#itop-breadcrumb li:last-child a::after {
+  display: none;
 }
 
 

css/light-grey.scss
@@ -8,6 +8,10 @@ $popup-menu-highlight-color = $highlight-color;
 $popup-menu-text-color: #000;
 $popup-menu-background-color: #fff;
 $popup-menu-text-higlight-color: #fff;
+$breadcrumb-color: #555;
+$breadcrumb-text-color: #fff;
+$breadcrumb-highlight-color: $highlight-color;
+$breadcrumb-text-highlight-color: #fff;
 
 /* CSS Document */
 body {
@@ -868,7 +872,6 @@ div#logo div {
 	overflow: hidden;
 }
 #top-bar {
-	height: 55px;
 	background: $frame-background-color;
 	text-align: right;
 }
@@ -1615,4 +1618,80 @@ span.refresh-button {
 }
 .history_entry_truncated .history_truncated_toggler {
 	background-position: 0 -192px;
+}
+
+#top-bar-table {
+  width: 100%;
+  #top-bar-table-search{
+    width: 347px;
+  }
+}
+
+#itop-breadcrumb{
+  overflow: hidden;
+  float: left;
+  background: $frame-background-color;
+  margin-left: -20px;
+
+  li{
+	list-style: none;
+	float: left;
+	margin: 0 22px 6px 0;
+
+	.icon img{
+	  height: 15px;
+	  width: auto;
+	  margin-right: 5px;
+
+	  -webkit-filter: unquote("grayscale(100%)");
+	  filter: unquote("grayscale(100%)");
+	  filter: unquote("gray");
+	  filter: url("data:image/svg+xml;utf8,<svg version='1.1' xmlns='http://www.w3.org/2000/svg' height='0'><filter id='greyscale'><feColorMatrix type='matrix' values='0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0 0 0 1 0' /></filter></svg>#greyscale");
+
+	  // IE has no filter option: at least, have some effect when hovering...
+	  opacity: 0.5;
+	}
+
+	&:hover .icon img{
+	  -webkit-filter: none;
+	  filter: none;
+	  opacity: 1;
+	}
+
+	a{
+	  text-decoration: none;
+	  color: #555;
+	  font-size: 9pt;
+	  padding: 0;
+	  background: none;
+	  span.truncate
+	  {
+		// Ellipsis
+		max-width: 200px;
+		white-space: nowrap;
+		overflow: hidden;
+		text-overflow: ellipsis;
+		display: inline-block;
+	  }
+
+	  &:hover{
+		text-decoration: none;
+		color: $highlight-color;
+	  }
+
+	  &::after{
+		content:'';
+		position: absolute;
+		background-image: url(../images/breadcrumb-separator.png?v=#{$version});
+		background-repeat: no-repeat;
+		width: 8px;
+		height: 16px;
+		margin-left: 5px;
+	  }
+	}
+
+	&:last-child a::after{
+	  display: none;
+	}
+  }
 }

commit 0386c53a6a845319a3402381faca323e938734a7

#185 Navigation Breadcrumb - Fixed a regression introduced in [r4000]: default menu not displayed afer login
SVN:trunk[4005]

application/menunode.class.inc.php
@@ -297,7 +297,7 @@ class ApplicationMenu
 		$sMenuId = $oAppContext->GetCurrentValue('menu', null);		
 		if ($sMenuId  === null)
 		{
-			$sMenuId == self::GetDefaultMenuId();
+			$sMenuId = self::GetDefaultMenuId();
 		}
 		return $sMenuId;
 	}	