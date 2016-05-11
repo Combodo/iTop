c1a4c0185b65a6c01ff6cf9f86cbd057866d7dc6
Romain Quetiez <romain.quetiez@combodo.com>
2016-05-11 13:18:01 +0000

    Customer Portal: exit if 1) there is neither UR nor INC tickets installed 2) the current login has no contact associated to it
    SVN:trunk[4060]

--- datamodels/2.x/itop-portal/index.php
+++ datamodels/2.x/itop-portal/index.php
@@ -27,6 +27,16 @@ else
 }
 require_once APPROOT . '/application/startup.inc.php';
 
+if (!class_exists('UserRequest') && !class_exists('Incident'))
+{
+	die('iTop has neither been installed with User Request nor Incident tickets. Please contact your administrator.');
+}
+
+if (UserRights::GetContactId() == 0)
+{
+	die(Dict::S('Portal:ErrorNoContactForThisUser'));
+}
+
 $sDir = basename(__DIR__);
 define('PORTAL_MODULE_ID', $sDir);
 define('PORTAL_ID', $sDir);
