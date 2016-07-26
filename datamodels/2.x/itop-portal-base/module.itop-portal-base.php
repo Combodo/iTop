babcd6a92a4be655fdf320abee8460145d4cbc5a
Denis Flaven <denis.flaven@combodo.com>
2016-07-26 08:15:42 +0000

    Increased version number of the module to 1.0.1 to reflect the change related to revision 4300 (support of custom controller in a brick).
    SVN:trunk[4308]

--- datamodels/2.x/itop-portal-base/module.itop-portal-base.php
+++ datamodels/2.x/itop-portal-base/module.itop-portal-base.php
@@ -2,7 +2,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'itop-portal-base/1.0.0', array(
+	'itop-portal-base/1.0.1', array(
 	// Identification
 	'label' => 'Portal Development Library',
 		'category' => 'Portal',
