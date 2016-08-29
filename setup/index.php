dc9a6382f9ef266f535e61b68aef6deb98de369c
Denis Flaven <denis.flaven@combodo.com>
2016-08-29 12:52:51 +0000

    (retrofit from trunk) #1297: timezone configuration setting was inoperant.
    SVN:2.3[4356]

--- setup/index.php
+++ setup/index.php
@@ -36,6 +36,7 @@ ini_set('max_execution_time', max(240, ini_get('max_execution_time')));
 // While running the setup it is desirable to see any error that may happen
 ini_set('display_errors', true);
 ini_set('display_startup_errors', true);
+date_default_timezone_set('Europe/Paris'); // Just to avoid a warning if the timezone is not set in php.ini
 
 /////////////////////////////////////////////////////////////////////
 // Fake functions to protect the first run of the installer
