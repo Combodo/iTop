Combodo/iTop

commit 8a92b8953c3bd8ef9ad0675bda8eb6c147ff97a5

Rich text editor: the Maximize button icon is missing if  iTop is installed in a directory which name contains spaces
git-svn-id: https://svn.code.sf.net/p/itop/code/trunk@4384 a333f486-631f-4898-b8df-5754b55c2be0

core/inlineimage.class.inc.php
@@ -464,7 +464,7 @@ EOF
 			oEditor.on( 'instanceReady', function() {
 				if(!CKEDITOR.env.iOS && $('#'+oEditor.id+'_toolbox .editor_magnifier').length == 0)
 				{
-					$('#'+oEditor.id+'_toolbox').append('<span class="editor_magnifier" title="$sToggleFullScreen" style="display:block;width:12px;height:11px;border:1px #A6A6A6 solid;cursor:pointer; background-image:url($sAppRootUrl/images/full-screen.png)">&nbsp;</span>');
+					$('#'+oEditor.id+'_toolbox').append('<span class="editor_magnifier" title="$sToggleFullScreen" style="display:block;width:12px;height:11px;border:1px #A6A6A6 solid;cursor:pointer; background-image:url('$sAppRootUrl/images/full-screen.png')">&nbsp;</span>');
 					$('#'+oEditor.id+'_toolbox .editor_magnifier').on('click', function() {
 							oEditor.execCommand('maximize');
 							if ($(this).closest('.cke_maximized').length != 0)

commit 7141e0f4b0347115ada74a2b45dd117ef4ecdc74

Fix for regression introduced in [r4384] and causing a blank screen when attempting to edit an object
SVN:trunk[4390]

core/inlineimage.class.inc.php
@@ -464,7 +464,7 @@ EOF
 			oEditor.on( 'instanceReady', function() {
 				if(!CKEDITOR.env.iOS && $('#'+oEditor.id+'_toolbox .editor_magnifier').length == 0)
 				{
-					$('#'+oEditor.id+'_toolbox').append('<span class="editor_magnifier" title="$sToggleFullScreen" style="display:block;width:12px;height:11px;border:1px #A6A6A6 solid;cursor:pointer; background-image:url('$sAppRootUrl/images/full-screen.png')">&nbsp;</span>');
+					$('#'+oEditor.id+'_toolbox').append('<span class="editor_magnifier" title="$sToggleFullScreen" style="display:block;width:12px;height:11px;border:1px #A6A6A6 solid;cursor:pointer; background-image:url(\\'$sAppRootUrl/images/full-screen.png\\')">&nbsp;</span>');
 					$('#'+oEditor.id+'_toolbox .editor_magnifier').on('click', function() {
 							oEditor.execCommand('maximize');
 							if ($(this).closest('.cke_maximized').length != 0)