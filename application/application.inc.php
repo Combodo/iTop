2a38eb757d6115e99f0d4c3e1803569865e7dc3a
Romain Quetiez <romain.quetiez@combodo.com>
2016-09-27 12:41:52 +0000

    Code cleanup: remove unused code causing warnings with PHP7 (SQLBlock)
    SVN:trunk[4421]

--- application/application.inc.php
+++ application/application.inc.php
@@ -27,7 +27,6 @@
 require_once(APPROOT.'/application/applicationcontext.class.inc.php');
 require_once(APPROOT.'/application/cmdbabstract.class.inc.php');
 require_once(APPROOT.'/application/displayblock.class.inc.php');
-require_once(APPROOT.'/application/sqlblock.class.inc.php');
 require_once(APPROOT.'/application/audit.category.class.inc.php');
 require_once(APPROOT.'/application/audit.rule.class.inc.php');
 require_once(APPROOT.'/application/query.class.inc.php');
