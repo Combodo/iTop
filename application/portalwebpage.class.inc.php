af13b42eabd2b7836edf70697666f51a9f22d189
Guillaume Lajarige <guillaume.lajarige@combodo.com>
2016-12-21 13:10:15 +0000

    Legacy portal: Since iTop 2.3, plain text caselog entries can no longer be toggled due to a bad jQuery selector. Only HTML entries were working.
    SVN:trunk[4541]

--- application/portalwebpage.class.inc.php
+++ application/portalwebpage.class.inc.php
@@ -222,7 +222,7 @@ try
 	PrepareWidgets();
 
 	//$('.resizable').resizable(); // Make resizable everything that claims to be resizable !
-	$('.caselog_header').click( function () { $(this).toggleClass('open').next('.caselog_entry_html').toggle(); });
+	$('.caselog_header').click( function () { $(this).toggleClass('open').next('.caselog_entry,.caselog_entry_html').toggle(); });
 			
 	$(document).ajaxSend(function(event, jqxhr, options) {
 		jqxhr.setRequestHeader('X-Combodo-Ajax', 'true');
