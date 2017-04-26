9cc583c47bae16449d3d822e8b94914b1d9e080c
Romain Quetiez <romain.quetiez@combodo.com>
2017-04-26 10:27:25 +0000

    Archives: regression (warning during the setup)
    SVN:trunk[4693]

--- application/ajaxwebpage.class.inc.php
+++ application/ajaxwebpage.class.inc.php
@@ -54,9 +54,12 @@ class ajax_page extends WebPage implements iTabbedPage
 		$this->sContentDisposition = 'inline';
 		$this->m_sMenu = "";
 
-		$bArchiveMode = utils::IsArchiveMode();
-		DBSearch::SetArchiveModeDefault($bArchiveMode);
-		if ($bArchiveMode) MetaModel::DBSetReadOnly();
+		if (class_exists('DBSearch')) // This class does not exist in the contect of the setup page
+		{
+			$bArchiveMode = utils::IsArchiveMode();
+			DBSearch::SetArchiveModeDefault($bArchiveMode);
+			if ($bArchiveMode) MetaModel::DBSetReadOnly();
+		}
     }
 
 	public function AddTabContainer($sTabContainer, $sPrefix = '')
