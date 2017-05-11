6c6ad0a45bfbcfd00754a8bc0c7c14d98e1ce483
Guillaume Lajarige <guillaume.lajarige@combodo.com>
2017-05-11 08:36:22 +0000

    Portal: Fixed a regression introduced in r4724. User profile page could not be opened anymore.
    SVN:trunk[4731]

--- datamodels/2.x/itop-portal-base/portal/src/controllers/objectcontroller.class.inc.php
+++ datamodels/2.x/itop-portal-base/portal/src/controllers/objectcontroller.class.inc.php
@@ -515,7 +515,7 @@ class ObjectController extends AbstractController
                 }
 
                 // Hiding submit button or changing its label if necessary
-                if($aFormProperties['properties']['always_show_submit'] === false && !empty($aFormData['buttons']['transitions']))
+                if(!empty($aFormData['buttons']['transitions']) && isset($aFormProperties['properties']) &&$aFormProperties['properties']['always_show_submit'] === false)
                 {
                     unset($aFormData['buttons']['submit']);
                 }
