635e7cfeec5a0494299864e8ede2abddf6882282
Romain Quetiez <romain.quetiez@combodo.com>
2017-10-06 14:06:01 +0000

    Fixed integration issue (possibly a regression): if an extension implements iApplicationExtension::OnDBInsert, and it calls DBWrite, then a fatal error occurs (call a member function on a null value). The error occurs for several types of attributes such as ormCustomField, ormCaseLog, ormLinkSet. The fix consists in aligning the internal values of a DBObject as soon as it has been written into the Database.
    SVN:trunk[4993]

--- core/dbobject.class.php
+++ core/dbobject.class.php
@@ -1726,6 +1726,14 @@ abstract class DBObject implements iDisplay
 
 		$this->m_bIsInDB = true;
 		$this->m_bDirty = false;
+		foreach ($this->m_aCurrValues as $sAttCode => $value)
+		{
+			if (is_object($value))
+			{
+				$value = clone $value;
+			}
+			$this->m_aOrigValues[$sAttCode] = $value;
+		}
 
 		$this->AfterInsert();
 
