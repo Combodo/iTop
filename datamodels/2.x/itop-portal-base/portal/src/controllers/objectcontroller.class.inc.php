7f034f60d6c1b82a7b37828257c4e22201b8e8cf
Guillaume Lajarige <guillaume.lajarige@combodo.com>
2018-01-08 12:09:35 +0000

    N°1254 Portal: Add CSS/JS hooks on object forms for the current state - CSS class on <form> tag: form_object_state_<STATE_CODE> - HTML attribute on <form> tag: data-object-state="<STATE_CODE>"
    SVN:trunk[5209]

--- datamodels/2.x/itop-portal-base/portal/src/controllers/objectcontroller.class.inc.php
+++ datamodels/2.x/itop-portal-base/portal/src/controllers/objectcontroller.class.inc.php
@@ -647,6 +647,7 @@ class ObjectController extends AbstractController
 		$aFormData['formmanager_data'] = $oFormManager->ToJSON();
 		$aFormData['renderer'] = $oFormManager->GetRenderer();
 		$aFormData['object_name'] = $oFormManager->GetObject()->GetName();
+		$aFormData['object_state'] = $oFormManager->GetObject()->GetState();
 		$aFormData['fieldset'] = $aFieldSetData;
         $aFormData['display_mode'] = (isset($aFormProperties['properties'])) ? $aFormProperties['properties']['display_mode'] : ApplicationHelper::FORM_DEFAULT_DISPLAY_MODE;
 
