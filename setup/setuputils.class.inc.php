dcc4061261fb9ad55a9c363fa28e255501fc22b1
Pierre Goiffon <pierre.goiffon@combodo.com>
2018-01-08 15:41:25 +0000

    (Retrofit from trunk) N°942 setup : max version for PHP (r5213)
    SVN:2.4[5214]

--- setup/setuputils.class.inc.php
+++ setup/setuputils.class.inc.php
@@ -54,6 +54,8 @@ class SetupUtils
 	// versions that will be the minimum in next iTop major release (warning if not met)
 	const PHP_NEXT_MIN_VERSION = '5.6.32';
 	const MYSQL_NEXT_MIN_VERSION = '5.6.38';
+	// First recent version that is not yet validated by Combodo (warning)
+	const PHP_NOT_VALIDATED_VERSION = '7.1.9';
 
 	const MIN_MEMORY_LIMIT = 33554432; // = 32*1024*1024 Beware: Computations are not allowed in defining constants
 	const SUHOSIN_GET_MAX_VALUE_LENGTH = 2048;
@@ -362,6 +364,12 @@ class SetupUtils
 			{
 				$aResult[] = new CheckResult(CheckResult::WARNING,
 					"Error: The current PHP Version (".$sPhpVersion.") is lower than the minimum version required to run next ".ITOP_APPLICATION." release, which is (".self::PHP_NEXT_MIN_VERSION.")");
+			}
+
+			if (version_compare($sPhpVersion, self::PHP_NOT_VALIDATED_VERSION, '>='))
+			{
+				$aResult[] = new CheckResult(CheckResult::WARNING,
+					"Error: The current PHP Version (".$sPhpVersion.") is not yet validated by Combodo. You may experience some incompatibility issues.");
 			}
 		}
 		else
