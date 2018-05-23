Combodo/iTop

commit 953e3f6c3c97cd5c5c210b4ee46ff222bbb3b587

N°1418 Audits Perf optimization for AuditRule with valid_flag=true and lots of negative records Use a new helper method that don't parse values anymore on SELECT IN / NOT IN queries
git-svn-id: https://svn.code.sf.net/p/itop/code/trunk@5724 a333f486-631f-4898-b8df-5754b55c2be0

core/dbobjectsearch.class.php
@@ -1,5 +1,5 @@
 <?php
-// Copyright (c) 2010-2017 Combodo SARL
+// Copyright (c) 2010-2018 Combodo SARL
 //
 //   This file is part of iTop.
 //
@@ -357,10 +357,19 @@ class DBObjectSearch extends DBSearch
 		$this->AddConditionExpression($oNewCondition);
 	}
 
-	public function AddCondition($sFilterCode, $value, $sOpCode = null, $bParseSeachString = false)
+	/**
+	 * @param string $sFilterCode
+	 * @param mixed $value
+	 * @param string $sOpCode operator to use : 'IN', 'NOT IN', 'Contains',' Begins with', 'Finishes with', ...
+	 * @param bool $bParseSearchString
+	 *
+	 * @throws \CoreException
+	 *
+	 * @see AddConditionForInOperatorUsingParam for IN/NOT IN queries with lots of params
+	 */
+	public function AddCondition($sFilterCode, $value, $sOpCode = null, $bParseSearchString = false)
 	{
 		MyHelpers::CheckKeyInArray('filter code in class: '.$this->GetClass(), $sFilterCode, MetaModel::GetClassFilterDefs($this->GetClass()));
-		$oFilterDef = MetaModel::GetClassFilterDef($this->GetClass(), $sFilterCode);
 
 		$oField = new FieldExpression($sFilterCode, $this->GetClassAlias());
 		if (empty($sOpCode))
@@ -372,13 +381,13 @@ class DBObjectSearch extends DBSearch
 			else
 			{
 				$oAttDef = MetaModel::GetAttributeDef($this->GetClass(), $sFilterCode);
-				$oNewCondition = $oAttDef->GetSmartConditionExpression($value, $oField, $this->m_aParams, $bParseSeachString);
+				$oNewCondition = $oAttDef->GetSmartConditionExpression($value, $oField, $this->m_aParams, $bParseSearchString);
 				$this->AddConditionExpression($oNewCondition);
 				return;
 			}
 		}
 		// Parse search strings if needed and if the filter code corresponds to a valid attcode
-		if($bParseSeachString && MetaModel::IsValidAttCode($this->GetClass(), $sFilterCode))
+		if($bParseSearchString && MetaModel::IsValidAttCode($this->GetClass(), $sFilterCode))
 		{
 			$oAttDef = MetaModel::GetAttributeDef($this->GetClass(), $sFilterCode);
 			$value = $oAttDef->ParseSearchString($value);
@@ -398,14 +407,14 @@ class DBObjectSearch extends DBSearch
 			throw new CoreException('Deprecated operator, please consider using OQL (SQL) expressions like "(TO_DAYS(NOW()) - TO_DAYS(x)) AS AgeDays"', array('operator' => $sOpCode));
 			break;
 
-		case "IN":
+		case 'IN':
 			if (!is_array($value)) $value = array($value);
 			if (count($value) === 0) throw new Exception('AddCondition '.$sOpCode.': Value cannot be an empty array.');
 			$sListExpr = '('.implode(', ', CMDBSource::Quote($value)).')';
 			$sOQLCondition = $oField->Render()." IN $sListExpr";
 			break;
 
-		case "NOTIN":
+		case 'NOTIN':
 			if (!is_array($value)) $value = array($value);
             if (count($value) === 0) throw new Exception('AddCondition '.$sOpCode.': Value cannot be an empty array.');
 			$sListExpr = '('.implode(', ', CMDBSource::Quote($value)).')';
@@ -459,6 +468,8 @@ class DBObjectSearch extends DBSearch
 				break;
 			case "IN":
 			case "NOTIN":
+				// this will parse all of the values... Can take forever if there are lots of them !
+				// In this case using a parameter is far better : WHERE ... IN (:my_param)
 				$oNewCondition = Expression::FromOQL($sOQLCondition);
 				break;
 
@@ -471,6 +482,42 @@ class DBObjectSearch extends DBSearch
 		}
 
 		$this->AddConditionExpression($oNewCondition);
+	}
+
+	/**
+	 * @param string $sFilterCode attribute code to use
+	 * @param array $aValues
+	 * @param bool $bPositiveMatch if true will add a IN filter, else a NOT IN
+	 *
+	 * @throws \CoreException
+	 */
+	public function AddConditionForInOperatorUsingParam($sFilterCode, $aValues, $bPositiveMatch = true)
+	{
+		$oFieldExpression = new FieldExpression($sFilterCode, $this->GetClassAlias());
+
+		$sOperator = $bPositiveMatch ? 'IN' : 'NOT IN';
+
+		$sInParamName = $this->GenerateUniqueParamName();
+		$oParamExpression = new VariableExpression($sInParamName);
+		$this->SetInternalParams(array($sInParamName => $aValues));
+
+		$oInCondition = new BinaryExpression($oFieldExpression, $sOperator, $oParamExpression);
+		$this->AddConditionExpression($oInCondition);
+	}
+
+	/**
+	 * @return string a unique param name
+	 */
+	private function GenerateUniqueParamName() {
+		$iExistingParamsNb = count($this->m_aParams);
+		$iCurrentArrayParamNb = $iExistingParamsNb + 1;
+		$sParamName = 'param'.$iCurrentArrayParamNb;
+
+		if (isset($this->m_aParams[$sParamName])) {
+			$sParamName .= '_'.microtime(true) . '_' .rand(0,100);
+		}
+
+		return $sParamName;
 	}
 
 	/**

pages/audit.php
@@ -123,6 +123,7 @@ function GetRuleResultFilter($iRuleId, $oDefinitionFilter, $oAppContext)
 		{
 			$aValidIds[] = $aRow['id'];
 		}
+		/** @var \DBObjectSearch $oFilter */
 		$oFilter = $oDefinitionFilter->DeepClone();
 		if (count($aValidIds) > 0)
 		{
@@ -134,7 +135,7 @@ function GetRuleResultFilter($iRuleId, $oDefinitionFilter, $oAppContext)
 			$aInvalids = array_diff($aInDefSet, $aValidIds);
 			if (count($aInvalids) > 0)
 			{
-				$oFilter->AddCondition('id', $aInvalids, 'IN');
+				$oFilter->AddConditionForInOperatorUsingParam('id', $aInvalids, true);
 			}
 			else
 			{

commit 0a44f34c2c5bfca55de91a30d07b371e819052a7

N°1444 : fixed regression introduced in [r5724] & [r5773]
SVN:trunk[5809]

core/dbobjectsearch.class.php
@@ -508,7 +508,9 @@ class DBObjectSearch extends DBSearch
 		$oParamExpression = new VariableExpression($sInParamName);
 		$this->SetInternalParams(array($sInParamName => $aValues));
 
-		$oInCondition = new BinaryExpression($oFieldExpression, $sOperator, $oParamExpression);
+		$oListExpression = new ListExpression(array($oParamExpression));
+
+		$oInCondition = new BinaryExpression($oFieldExpression, $sOperator, $oListExpression);
 		$this->AddConditionExpression($oInCondition);
 	}
 

core/oql/expression.class.inc.php
@@ -303,6 +303,10 @@ class BinaryExpression extends Expression
 		{
 			throw new CoreException('Expecting an Expression object on the right hand', array('found_class' => get_class($oRightExpr)));
 		}
+		if ( (($sOperator == "IN") ||  ($sOperator == "NOT IN")) && !$oRightExpr instanceof ListExpression)
+		{
+			throw new CoreException("Expecting a List Expression object on the right hand for operator $sOperator", array('found_class' => get_class($oRightExpr)));
+		}
 		$this->m_oLeftExpr  = $oLeftExpr;
 		$this->m_oRightExpr = $oRightExpr;
 		$this->m_sOperator  = $sOperator;
@@ -1286,7 +1290,7 @@ class VariableExpression extends UnaryExpression
 			$res = CMDBSource::Quote($aArgs[$this->m_sName]);
 			if (is_array($res))
 			{
-				$res = '('.implode(', ', $res).')';
+				$res = implode(', ', $res);
 			}
 			return $res;
 		}