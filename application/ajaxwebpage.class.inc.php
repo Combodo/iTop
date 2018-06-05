9fe32614241e4b39d37f7e1812ac1446885bc5ad
Denis Flaven <denis.flaven@combodo.com>
2018-06-05 13:04:50 +0000

    Linked JS scripts can now be used in ajax pages. This is useful for IPopupMenu extensions which depend on a JS script and are loaded asynchronously when a list of objects changes (for example when changing the target class for a search)
    SVN:trunk[5826]

--- application/ajaxwebpage.class.inc.php
+++ application/ajaxwebpage.class.inc.php
@@ -244,9 +244,18 @@ EOF
         //echo $this->s_deferred_content;
         if (count($this->a_scripts) > 0)
         {
-            echo "<script type=\"text/javascript\">\n";
-            echo implode("\n", $this->a_scripts);
-            echo "\n</script>\n";
+        	echo "<script type=\"text/javascript\">\n";
+        	echo implode("\n", $this->a_scripts);
+        	echo "\n</script>\n";
+        }
+        if (count($this->a_linked_scripts) > 0)
+        {
+        	echo "<script type=\"text/javascript\">\n";
+        	foreach($this->a_linked_scripts as $sScriptUrl)
+        	{
+        		echo '$.getScript('.json_encode($sScriptUrl).");\n";
+        	}
+        	echo "\n</script>\n";
         }
         if (!empty($this->s_deferred_content))
         {
