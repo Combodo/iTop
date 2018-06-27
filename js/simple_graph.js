261bc83811f607226368ec461a4e9d9b7ffd45ae
Eric Espié <eric.espie@combodo.com>
2018-06-27 15:42:32 +0000

    N°1479 : Fixed a bug where impact analysis zoom would affect other tabs' size #jfb :)
    SVN:trunk[5918]

--- js/simple_graph.js
+++ js/simple_graph.js
@@ -801,7 +801,11 @@ $(function()
 			}
 			var aContexts = [];
 			$('#'+sId+'_contexts').multiselect('getChecked').each(function() { aContexts[$(this).val()] = me.options.additional_contexts[$(this).val()].oql; });
-			//this.element.closest('.ui-tabs').tabs({ heightStyle: "fill" });
+			// Don't set the height on the tabs widget, only on the current tab
+			// Adjust the height of the graph to the window size
+			var sTabHeight = parseInt($(window).height() - this.element.parent().offset().top - 50);
+			this.element.closest('.ui-tabs-panel').css({ height: sTabHeight + "px" });
+
 			this.adjust_height();
 			this._close_all_tooltips();
 			this.oPaper.rect(this.xPan, this.yPan, this.element.width(), this.element.height()).attr({fill: '#000', opacity: 0.4, 'stroke-width': 0});
