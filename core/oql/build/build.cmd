d40ffd944f894df0e688229b7f23593e1e44a74b
Pierre Goiffon <pierre.goiffon@combodo.com>
2018-08-28 13:55:40 +0000

    Revert "@@@@ OQL parsing"
    This reverts commit 0beafc1e9a7c4a629a458aec669aa5e11b27db49.
    
    SVN:b931[6027]

--- core/oql/build/build.cmd
+++ core/oql/build/build.cmd
@@ -1,6 +1,6 @@
 rem must be run with current directory = the directory of the batch
 rem PEAR is required to build
-php -d include_path=".;C:\Dev\wamp64\bin\php\php5.6.31\pear" ".\PHP\LexerGenerator\cli.php" ..\oql-lexer.plex
+php -d include_path=".;C:\iTop\PHP\PEAR" ".\PHP\LexerGenerator\cli.php" ..\oql-lexer.plex
 php ".\PHP\ParserGenerator\cli.php" ..\oql-parser.y
 php -r "echo date('Y-m-d');" > ..\version.txt
 pause
