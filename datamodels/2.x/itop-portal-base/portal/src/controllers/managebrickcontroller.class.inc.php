b84ac80aaaee96d9b75138db67b7c3b371570c1a
Molkobain <lajarige.guillaume@free.fr>
2018-10-08 13:05:45 +0200

    (Cherry pick from develop ab1715e) N°1576 Portal: Security hardening (missed one file in previous commit).

--- datamodels/2.x/itop-portal-base/portal/src/controllers/managebrickcontroller.class.inc.php
+++ datamodels/2.x/itop-portal-base/portal/src/controllers/managebrickcontroller.class.inc.php
@@ -577,7 +577,7 @@ class ManageBrickController extends BrickController
 						$oAttDef = MetaModel::GetAttributeDef($sCurrentClass, $sItemAttr);
 						if ($oAttDef->IsExternalKey())
 						{
-							$sValue = $oCurrentRow->Get($sItemAttr.'_friendlyname');
+							$sValue = $oCurrentRow->GetAsHTML($sItemAttr.'_friendlyname');
 
 							// Adding a view action on the external keys
 							if ($oCurrentRow->Get($sItemAttr) !== $oAttDef->GetNullValue())
@@ -595,13 +595,22 @@ class ManageBrickController extends BrickController
 								}
 							}
 						}
-						elseif ($oAttDef instanceof AttributeSubItem || $oAttDef instanceof AttributeDuration)
-						{
-							$sValue = $oAttDef->GetAsHTML($oCurrentRow->Get($sItemAttr));
-						}
+						elseif ($oAttDef instanceof AttributeImage)
+                        {
+                            $oOrmDoc = $oCurrentRow->Get($sItemAttr);
+                            if (is_object($oOrmDoc) && !$oOrmDoc->IsEmpty())
+                            {
+                                $sUrl = $oApp['url_generator']->generate('p_object_document_display', array('sObjectClass' => get_class($oCurrentRow), 'sObjectId' => $oCurrentRow->GetKey(), 'sObjectField' => $sItemAttr, 'cache' => 86400));
+                            }
+                            else
+                            {
+                                $sUrl = $oAttDef->Get('default_image');
+                            }
+                            $sValue = '<img src="' . $sUrl . '" />';
+                        }
 						else
 						{
-							$sValue = $oAttDef->GetValueLabel($oCurrentRow->Get($sItemAttr));
+                            $sValue = $oAttDef->GetAsHTML($oCurrentRow->Get($sItemAttr));
 						}
 						unset($oAttDef);
 
