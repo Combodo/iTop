Combodo/iTop

commit 10b7fa6014dfc05a06f9b25affb052c456706220

N°1647 Fix Excel web queries import. (JS script error popups)

webservices/export-v2.php
@@ -731,9 +731,17 @@ try
 		$sMimeType = $oExporter->GetMimeType();
 		if ($sMimeType == 'text/html')
 		{
-			$oP = new NiceWebPage('iTop export');
+			// Note: Using NiceWebPage only for HTML export as it includes JS scripts & files, which makes no sense in other export formats. More over, it breaks Excel spreadsheet import.
+			if($oExporter instanceof HTMLBulkExport)
+			{
+				$oP = new NiceWebPage('iTop export');
+				$oP->add_ready_script("$('table.listResults').tablesorter({widgets: ['MyZebra']});");
+			}
+			else
+			{
+				$oP = new WebPage('iTop export');
+			}
 			$oP->add_style("body { overflow: auto; }");
-			$oP->add_ready_script("$('table.listResults').tablesorter({widgets: ['MyZebra']});");
 		}
 		else
 		{

commit 81d82b4039326c8a6a047306404938117b3ba461

Fix regression introduced in commit 10b7fa6 (Web queries have issue with line breaks in cell value)

core/spreadsheetbulkexport.class.inc.php
@@ -234,7 +234,6 @@ EOF
 			}
 		}
 		$sData = '';
-		$sData .= '<style>table br {mso-data-placement:same-cell;}</style>'; // Trick for Excel: keep line breaks inside the same cell !
 		$sData .= "<table border=\"1\">\n";
 		$sData .= "<tr>\n";
 		foreach($aData as $sLabel)

webservices/export-v2.php
@@ -740,6 +740,7 @@ try
 			else
 			{
 				$oP = new WebPage('iTop export');
+                $oP->add_style("table br { mso-data-placement:same-cell; }"); // Trick for Excel: keep line breaks inside the same cell !
 			}
 			$oP->add_style("body { overflow: auto; }");
 		}