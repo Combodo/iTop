098b0531d811196aab98fb0673a789ab66a1910f
Eric <eric.espie@combodo.com>
2018-10-23 14:08:05 +0200

    Remove the generation of an additional class TagFieldData for AttributeDashboard

--- setup/compiler.class.inc.php
+++ setup/compiler.class.inc.php
@@ -1514,7 +1514,6 @@ EOF
 				}
 				elseif ($sAttType == 'AttributeDashboard')
 				{
-					$aTagFieldsInfo[] = $sAttCode;
 					$aParameters['is_user_editable'] = $this->GetPropBoolean($oField, 'is_user_editable', true);
 					$aParameters['definition_file'] = $this->GetPropString($oField, 'definition_file');
 
