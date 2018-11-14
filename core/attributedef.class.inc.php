Combodo/iTop

commit f9b2fda092a31e389df795d5c03b5f4332e67189

N°931 fix link for TagSet in portal lists - they were linking to a console search - now will filter the list using the label

core/attributedef.class.inc.php
@@ -9991,11 +9991,13 @@ class AttributeTagSet extends AttributeSet
 	 *
 	 * @param array $aValues
 	 * @param string $sCssClass
+	 * @param bool $bWithLink if true will generate a link, otherwise just a "a" tag without href
 	 *
 	 * @return string
 	 * @throws \CoreException
+	 * @throws \OQLException
 	 */
-	private function GenerateViewHtmlForValues($aValues, $sCssClass = '')
+	public function GenerateViewHtmlForValues($aValues, $sCssClass = '', $bWithLink = true)
 	{
 		if (empty($aValues)) {return '';}
 		$sHtml = '<span class="'.$sCssClass.' '.implode(' ', $this->aCSSClasses).'">';
@@ -10013,9 +10015,17 @@ class AttributeTagSet extends AttributeSet
 				$sContext = $oAppContext->GetForLink();
 				$sUIPage = cmdbAbstractObject::ComputeStandardUIPage($oFilter->GetClass());
 				$sFilter = rawurlencode($oFilter->serialize());
-				$sUrl = utils::GetAbsoluteUrlAppRoot()."pages/$sUIPage?operation=search&filter=".$sFilter."&{$sContext}";
 
-				$sHtml .= '<a href="'.$sUrl.'" class="attribute-set-item attribute-set-item-'.$sTagCode.'" data-code="'.$sTagCode.'" data-label="'.htmlentities($sTagLabel, ENT_QUOTES, 'UTF-8').'" data-description="'.htmlentities($sTagDescription, ENT_QUOTES, 'UTF-8').'">'.$sTagLabel.'</a>';
+				$sLink = '';
+				if ($bWithLink)
+				{
+					$sUrl = utils::GetAbsoluteUrlAppRoot()."pages/$sUIPage?operation=search&filter=".$sFilter."&{$sContext}";
+					$sLink = 'href="'.$sUrl.'"';
+				}
+
+				$sHtml .= '<a'.$sLink.' class="attribute-set-item attribute-set-item-'.$sTagCode.'" data-code="'.$sTagCode.'" data-label="'.htmlentities($sTagLabel,
+						ENT_QUOTES, 'UTF-8').'" data-description="'.htmlentities($sTagDescription, ENT_QUOTES,
+						'UTF-8').'">'.$sTagLabel.'</a>';
 			}
 			else
 			{

datamodels/2.x/itop-portal-base/portal/src/controllers/managebrickcontroller.class.inc.php
@@ -19,7 +19,6 @@
 
 namespace Combodo\iTop\Portal\Controller;
 
-use Exception;
 use AttributeDate;
 use AttributeDateTime;
 use AttributeDefinition;
@@ -29,12 +28,13 @@ use CMDBSource;
 use Combodo\iTop\Portal\Brick\AbstractBrick;
 use Combodo\iTop\Portal\Brick\ManageBrick;
 use Combodo\iTop\Portal\Helper\ApplicationHelper;
-use Combodo\iTop\Portal\Helper\SecurityHelper;
 use Combodo\iTop\Portal\Helper\ScopeValidatorHelper;
+use Combodo\iTop\Portal\Helper\SecurityHelper;
 use DBObject;
 use DBObjectSet;
 use DBSearch;
 use Dict;
+use Exception;
 use FieldExpression;
 use iPopupMenuExtension;
 use JSButtonItem;
@@ -615,6 +615,14 @@ class ManageBrickController extends BrickController
                             }
                             $sValue = '<img src="' . $sUrl . '" />';
                         }
+						elseif ($oAttDef instanceof \AttributeTagSet)
+						{
+							/** @var \ormTagSet $oSetValues */
+							$oSetValues = $oCurrentRow->Get($sItemAttr);
+							$aCodes = $oSetValues->GetTags();
+							/** @var \AttributeTagSet $oAttDef */
+							$sValue = $oAttDef->GenerateViewHtmlForValues($aCodes, '', false);
+						}
 						else
 						{
                             $sValue = $oAttDef->GetAsHTML($oCurrentRow->Get($sItemAttr));

datamodels/2.x/itop-portal-base/portal/src/views/bricks/browse/layout.html.twig
@@ -4,6 +4,12 @@
 
 {% block pPageBodyClass %}{{ parent() }} page_browse_brick page_browse_brick_as_{{ sBrowseMode }}{% endblock %}
 
+{% block pPageReadyScripts %}
+    {{ parent() }}
+    {% include 'itop-portal-base/portal/src/views/helpers/tagset_clic_handler.js.twig' %}
+{% endblock %}
+
+
 {% block pMainHeaderTitle %}
 	{{ oBrick.GetTitle()|dict_s }}
 {% endblock %}

datamodels/2.x/itop-portal-base/portal/src/views/bricks/manage/layout.html.twig
@@ -17,3 +17,7 @@
     {% endif %}
 {% endblock %}
 
+{% block pPageReadyScripts %}
+    {{ parent() }}
+    {% include 'itop-portal-base/portal/src/views/helpers/tagset_clic_handler.js.twig' %}
+{% endblock %}

datamodels/2.x/itop-portal-base/portal/src/views/helpers/tagset_clic_handler.js.twig
@@ -1,0 +1,23 @@
+/**
+ * When clicking on a TagSet item in a list (BrowseBrick or ManageBrick), filters the list using the item label
+ * @since 2.6 N°931 AttributeTagSet
+ */
+$("#main-content").on("click", ".table.dataTable .attribute.attribute-set.attribute-tag-set > .attribute-set-item", function () {
+	var $this = $(this),
+		$dataTableWrapper = $this.closest(".dataTables_wrapper"),
+		$searchField = $dataTableWrapper.find(".dataTables_filter input.form-control"),
+		iNbInputsFound = $searchField.length;
+
+	if (iNbInputsFound === 0)
+	{
+		return;
+	}
+	if (iNbInputsFound > 1)
+	{
+		return;
+	}
+
+	var sTagLabel = $this.text();
+	$searchField.prop("value", sTagLabel);
+	$searchField.trigger("keyup");
+});

commit 2ec1db55d90fe2e22626b0aad0d1f20269f41c2a

N°931 fix TagSet regression (not displayed correctly in the console) introduced in f9b2fda092a31e389df795d5c03b5f4332e67189

core/attributedef.class.inc.php
@@ -10045,7 +10045,7 @@ class AttributeTagSet extends AttributeSet
 				if ($bWithLink)
 				{
 					$sUrl = utils::GetAbsoluteUrlAppRoot()."pages/$sUIPage?operation=search&filter=".$sFilter."&{$sContext}";
-					$sLink = 'href="'.$sUrl.'"';
+					$sLink = ' href="'.$sUrl.'"';
 				}
 
 				$sHtml .= '<a'.$sLink.' class="attribute-set-item attribute-set-item-'.$sTagCode.'" data-code="'.$sTagCode.'" data-label="'.htmlentities($sTagLabel,