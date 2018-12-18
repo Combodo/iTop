b0af20ffff246ee7ca5ecc9a2129b2ccff93a3df
Purple Grape <purplegrape4@gmail.com>
2018-12-19 01:11:20 +0800

    🌐 update 2.6 chinese translation and corresponding licences (#45)
    * Update zh_cn.dict.authent-external.php
    
    * Update zh_cn.dict.authent-ldap.php
    
    * Update zh_cn.dict.authent-local.php
    
    * Update zh_cn.dict.itop-attachments.php
    
    * Update zh_cn.dict.itop-backup.php
    
    * Update zh_cn.dict.itop-change-mgmt-itil.php
    
    * Update zh_cn.dict.itop-change-mgmt.php
    
    * Update zh_cn.dict.itop-config-mgmt.php
    
    * Update zh_cn.dict.itop-config.php
    
    * Update zh_cn.dict.itop-datacenter-mgmt.php
    
    * Update zh.dict.itop-endusers-devices.php
    
    * Update zh_cn.dict.itop-hub-connector.php
    
    * Update zh_cn.dict.itop-incident-mgmt-itil.php
    
    * Update zh_cn.dict.itop-knownerror-mgmt.php
    
    * Update zh_cn.dict.itop-portal-base.php
    
    * Update zh_cn.dict.itop-portal-base.php
    
    * Update zh_cn.dict.itop-problem-mgmt.php
    
    * Update zh_cn.dict.itop-request-mgmt-itil.php
    
    * Update zh_cn.dict.itop-request-mgmt.php
    
    * Update zh_cn.dict.itop-welcome-itil.php
    
    * Update zh_cn.dict.itop-tickets.php
    
    * Update zh_cn.dict.itop-service-mgmt-provider.php
    
    * Update zh_cn.dict.itop-service-mgmt.php
    
    * Update zh_cn.dictionary.itop.core.php
    
    * Update zh_cn.dictionary.itop.ui.php

--- datamodels/2.x/itop-config/zh_cn.dict.itop-config.php
+++ datamodels/2.x/itop-config/zh_cn.dict.itop-config.php
@@ -2,9 +2,25 @@
 /**
  * Localized data
  *
- * @copyright   Copyright (C) 2013 Combodo
- * @license     http://opensource.org/licenses/AGPL-3.0
+ * @copyright Copyright (C) 2010-2018 Combodo SARL
+ * @license	http://opensource.org/licenses/AGPL-3.0
+ *
+ * This file is part of iTop.
+ *
+ * iTop is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * iTop is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with iTop. If not, see <http://www.gnu.org/licenses/>
  */
+
 Dict::Add('ZH CN', 'Chinese', '简体中文', array(
 
 	'Menu:ConfigEditor' => '配置文件编辑',
@@ -13,10 +29,10 @@ Dict::Add('ZH CN', 'Chinese', '简体中文', array(
 	'config-apply' => '应用',
 	'config-apply-title' => '应用 (Ctrl+S)',
 	'config-cancel' => '重置',
-    'config-saved' => '成功保存.',
+	'config-saved' => '成功保存.',
 	'config-confirm-cancel' => '您的修改将被丢弃.',
 	'config-no-change' => '没有变化: 配置文件将保持不变.',
-    'config-reverted' => '配置文件已恢复.',
+	'config-reverted' => '配置文件已恢复.',
 	'config-parse-error' => '第 %2$d 行: %1$s.<br/>配置文件尚未更新.',
 	'config-current-line' => '正在编辑第 %1$s 行',
 ));
