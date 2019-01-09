48c920f848b905f5a3fc0d7590b13f01d105c507
Stephen Abello <stephen.abello@combodo.com>
2019-01-09 11:02:21 +0100

    N°1934: Security hardening

--- setup/phpinfo.php
+++ setup/phpinfo.php
@@ -1,3 +1,9 @@
 <?php
+require_once('../approot.inc.php');
+
+require_once(APPROOT.'/application/startup.inc.php');
+
+require_once(APPROOT.'/application/loginwebpage.class.inc.php');
+LoginWebPage::DoLogin(true); // Check user rights and prompt if needed
 phpinfo();
 ?>
