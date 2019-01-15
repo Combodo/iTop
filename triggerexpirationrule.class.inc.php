6afbec050e4035026bd0baa8983cdbefd2a363b2
bruno DA SILVA <bruno.dasilva@combodo.com>
2019-01-15 17:22:36 +0100

    :page_facing_up: Add Combodo license

--- triggerexpirationrule.class.inc.php
+++ triggerexpirationrule.class.inc.php
@@ -1,21 +1,9 @@
 <?php
-// Copyright (C) 2012-2016 Combodo SARL
-//
-//   This program is free software; you can redistribute it and/or modify
-//   it under the terms of the GNU Lesser General Public License as published by
-//   the Free Software Foundation; version 3 of the License.
-//
-//   This program is distributed in the hope that it will be useful,
-//   but WITHOUT ANY WARRANTY; without even the implied warranty of
-//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//   GNU General Public License for more details.
-//
-//   You should have received a copy of the GNU General Public License
-//   along with this program; if not, write to the Free Software
-//   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 /**
- * @copyright   Copyright (C) 2012-2016 Combodo SARL
- * @license     http://opensource.org/licenses/AGPL-3.0
+ *
+ * @copyright   Copyright (C) 2010-2019 Combodo SARL
+ * @license     https://www.combodo.com/documentation/combodo-software-license.html
+ *
  */
 
 /**
