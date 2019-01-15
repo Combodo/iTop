b2bd5bac759db83d5d6d04cfc042dd9e35a7a422
bruno DA SILVA <bruno.dasilva@combodo.com>
2019-01-15 17:22:36 +0100

    :page_facing_up: Add Combodo license

--- en.dict.combodo-notify-on-expiration.php
+++ en.dict.combodo-notify-on-expiration.php
@@ -1,28 +1,16 @@
 <?php
-// Copyright (C) 2012-2018 Combodo SARL
-//
-//   This program is free software; you can redistribute it and/or modify
-//   it under the terms of the GNU General Public License as published by
-//   the Free Software Foundation; version 3 of the License.
-//
-//   This program is distributed in the hope that it will be useful,
-//   but WITHOUT ANY WARRANTY; without even the implied warranty of
-//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//   GNU General Public License for more details.
-//
-//   You should have received a copy of the GNU General Public License
-//   along with this program; if not, write to the Free Software
-//   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 /**
  * Localized data
+ *
+ * @copyright   Copyright (C) 2010-2019 Combodo SARL
+ * @license     https://www.combodo.com/documentation/combodo-software-license.html
  *
  * @author      Erwan Taloc <erwan.taloc@combodo.com>
  * @author      Romain Quetiez <romain.quetiez@combodo.com>
  * @author      Denis Flaven <denis.flaven@combodo.com>
  * @author      Guillaume Lajarige <guillaume.lajarige@combodo.com>
  * @author      Vincent Dumas <vincent.dumas@combodo.com>
- * @license     http://www.opensource.org/licenses/gpl-3.0.html LGPL
  */
 
 // ExpirationRule
