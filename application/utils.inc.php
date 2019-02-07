4800c70dbe979ca0b2711b40a43266dfe61b7c7d
Eric <eric.espie@combodo.com>
2019-02-07 17:28:00 +0100

    Add 'filename' as posted parameter sanitization filter type

--- application/utils.inc.php
+++ application/utils.inc.php
@@ -293,6 +293,11 @@ class utils
 			case 'string':
 			$retValue = filter_var($value, FILTER_SANITIZE_SPECIAL_CHARS);
 			break;
+
+			case 'filename':
+				// Check base file name (without path)
+				$retValue = filter_var($value, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"@^[^<>:;,?\"*|/\\\\]+$@")));
+				break;
 			
 			case 'context_param':
 			case 'parameter':
