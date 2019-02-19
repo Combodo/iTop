7cf7e55454be42d4ad2e8342d57c9efcb54c723d
Eric <eric.espie@combodo.com>
2019-02-19 10:16:45 +0100

    N°1823 - Fix tags not saved in case of error

--- application/cmdbabstract.class.inc.php
+++ application/cmdbabstract.class.inc.php
@@ -4221,6 +4221,7 @@ EOF
 								$oDummyObj->Set($sAttCode, $currValue);
 								/** @var ormTagSet $oTagSet */
 								$oTagSet = $oDummyObj->Get($sAttCode);
+								$oTagSet->SetDisplayPartial(true);
 								foreach($aKeys as $iIndex => $sValues)
 								{
 									if ($iIndex == 0)
