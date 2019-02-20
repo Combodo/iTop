b2919caafb58dd578635388be78852b72b26f5b9
Eric <eric.espie@combodo.com>
2019-02-20 14:57:39 +0100

    N°1975 - Fix: Change Menu rights to "Admin only" leads in crash test

--- core/expressioncache.class.inc.php
+++ core/expressioncache.class.inc.php
@@ -77,8 +77,8 @@ EOF;
 }
 EOF;
 
-				file_put_contents($sFilePath, $content);
-			}
+			SetupUtils::builddir(dirname($sFilePath));
+			file_put_contents($sFilePath, $content);
 		}
 		// Restore original user language
 		Dict::SetUserLanguage($sUserLang);
