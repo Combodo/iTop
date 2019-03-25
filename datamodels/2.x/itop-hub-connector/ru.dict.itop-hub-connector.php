3b8478c2f3de976ee1991aa923d60aad44c94573
Vladimir Kunin <v.b.kunin@gmail.com>
2019-03-25 18:24:43 +0300

    Changed license info

--- datamodels/2.x/itop-hub-connector/ru.dict.itop-hub-connector.php
+++ datamodels/2.x/itop-hub-connector/ru.dict.itop-hub-connector.php
@@ -5,7 +5,7 @@
  * @author      Vladimir Kunin <v.b.kunin@gmail.com>
  * @link        http://community.itop-itsm.ru  iTop Russian Community
  * @link        https://github.com/itop-itsm-ru/itop-rus
- * @license     http://www.opensource.org/licenses/gpl-3.0.html LGPL
+ * @license     http://opensource.org/licenses/AGPL-3.0
  *
  */
 Dict::Add('RU RU', 'Russian', 'Русский', array(
