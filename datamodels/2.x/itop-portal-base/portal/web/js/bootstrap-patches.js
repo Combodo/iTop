Combodo/iTop

commit b157fad0b646d636973c1a8748248277e5f2ea47

N°2166: Portal: fields in CKEditor modals are inactive

datamodels/2.x/itop-portal-base/portal/web/js/bootstrap-patches.js
@@ -1,0 +1,26 @@
+/*
+ * Patches for bootstrap 3 as it is no longer maintained by its editor
+ */
+
+//N°2166: Fix a boostrap/CKeditor incompatibility with their respective modals (source: https://stackoverflow.com/a/23667151)
+$.fn.modal.Constructor.prototype.enforceFocus = function () {
+	var $modalElement = this.$element;
+	$(document).on('focusin.modal', function (e) {
+		var $parent = $(e.target.parentNode);
+		if ($modalElement[0] !== e.target && !$modalElement.has(e.target).length
+			// add whatever conditions you need here:
+			&&
+			!$parent.hasClass('cke_dialog_ui_input_select') && !$parent.hasClass('cke_dialog_ui_input_text')) {
+			$modalElement.focus()
+		}
+	})
+};
+
+// Hack to enable multiple modals by making sure the .modal-open class is set to the <body> when there is at least one modal open left
+$(document).ready(function() {
+	$('body').on('hidden.bs.modal', function () {
+		if ($('.modal.in').length > 0) {
+			$('body').addClass('modal-open');
+		}
+	});
+});

datamodels/2.x/itop-portal-base/portal/src/views/layout.html.twig
@@ -94,6 +94,7 @@
 		<script type="text/javascript" src="{{ app['combodo.absolute_url'] ~ 'js/d3.min.js'|add_itop_version }}"></script>
 		<script type="text/javascript" src="{{ app['combodo.absolute_url'] ~ 'js/c3.min.js'|add_itop_version }}"></script>
 		<script type="text/javascript" src="{{ app['combodo.portal.base.absolute_url'] ~ 'lib/bootstrap/js/bootstrap.min.js'|add_itop_version }}"></script>
+		<script type="text/javascript" src="{{ app['combodo.portal.base.absolute_url'] ~ 'js/bootstrap-patches.js'|add_itop_version }}"></script>
 		<script type="text/javascript" src="{{ app['combodo.absolute_url'] ~ 'js/latinise/latinise.min.js'|add_itop_version }}"></script>
 		{# Visible.js to check if an element is visible on screen #}
 		<script type="text/javascript" src="{{ app['combodo.portal.base.absolute_url'] ~ 'lib/jquery-visible/js/jquery.visible.min.js'|add_itop_version }}"></script>
@@ -457,13 +458,6 @@
 					$('body').on('hidden.bs.modal', '.modal#modal-for-all', function () {
 						$(this).removeData('bs.modal');
 						$(this).find('.modal-content').html(GetContentLoaderTemplate());
-					});
-					// Hack to enable multiple modals by making sure the .modal-open class is set to the <body> when there is at least one modal open left
-					$('body').on('hidden.bs.modal', function () {
-						if($('.modal.in').length > 0)
-						{
-							$('body').addClass('modal-open');
-						}
 					});
 					// Hide tooltips when a modal is opening, otherwise it might be overlapping it
 					$('body').on('show.bs.modal', function () {

commit a93be39aeb968ef9a8501d1cc64ab349bb03fddf

N°2166: Fix regression introduced in b157fad

datamodels/2.x/itop-portal-base/portal/web/js/bootstrap-patches.js
@@ -2,18 +2,20 @@
  * Patches for bootstrap 3 as it is no longer maintained by its editor
  */
 
-//N°2166: Fix a boostrap/CKeditor incompatibility with their respective modals (source: https://stackoverflow.com/a/23667151)
-$.fn.modal.Constructor.prototype.enforceFocus = function () {
-	var $modalElement = this.$element;
-	$(document).on('focusin.modal', function (e) {
-		var $parent = $(e.target.parentNode);
-		if ($modalElement[0] !== e.target && !$modalElement.has(e.target).length
-			// add whatever conditions you need here:
-			&&
-			!$parent.hasClass('cke_dialog_ui_input_select') && !$parent.hasClass('cke_dialog_ui_input_text')) {
-			$modalElement.focus()
-		}
-	})
+//N°2166: Fix a boostrap/CKeditor incompatibility with their respective modals (source: https://stackoverflow.com/a/31679096)
+$.fn.modal.Constructor.prototype.enforceFocus = function() {
+	$( document )
+		.off( 'focusin.bs.modal' ) // guard against infinite focus loop
+		.on( 'focusin.bs.modal', $.proxy( function( e ) {
+			if (
+				this.$element[ 0 ] !== e.target && !this.$element.has( e.target ).length
+				// CKEditor compatibility fix start.
+				&& !$( e.target ).closest( '.cke_dialog, .cke' ).length
+			// CKEditor compatibility fix end.
+			) {
+				this.$element.trigger( 'focus' );
+			}
+		}, this ) );
 };
 
 // Hack to enable multiple modals by making sure the .modal-open class is set to the <body> when there is at least one modal open left