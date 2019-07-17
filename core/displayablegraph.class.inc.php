Combodo/iTop

commit a4743901a3098b8392fb70bca4ce1f165773f3b6

N°2269 Font Awesome : update iTop for new v5 icons * remove useless css in light-grey (weren't used) * change icons class names from v4 to v5

core/displayablegraph.class.inc.php
@@ -1446,7 +1446,8 @@ class DisplayableGraph extends SimpleGraph
 <<<EOF
  <div id="ds_flash" class="search_box">
 	<form id="dh_flash" class="search_form_handler closed">
-	<h2 class="sf_title"><span class="sft_long">$sSftShort</span><span class="sft_short">$sSftShort</span><span class="sft_toggler fa fa-caret-down pull-right" title="$sSearchToggle"></span></h2>
+	<h2 class="sf_title"><span class="sft_long">$sSftShort</span><span class="sft_short">$sSftShort</span><span class="sft_toggler fas 
+	fa-caret-down pull-right" title="$sSearchToggle"></span></h2>
 	<div id="dh_flash_criterion_outer" class="sf_criterion_area"><div class="sf_criterion_row">
 EOF
 		);

datamodels/2.x/itop-hub-connector/js/hub.js
@@ -84,7 +84,7 @@ $(function()
 		backup: function()
 		{
 			var me = this;
-			$('#hub-installation-progress-text').html('<i class="fa fa-database"></i> '+this.options.labels.database_backup);
+			$('#hub-installation-progress-text').html('<i class="fas fa-database"></i> '+this.options.labels.database_backup);
 			$.post(this.options.self_url, {operation: 'do_backup'}, function(data) { me._on_backup(data) }, 'json');						
 		},
 		_on_backup: function(data)
@@ -102,7 +102,7 @@ $(function()
 		},
 		compile: function()
 		{
-			$('#hub-installation-progress-text').html('<i class="fa fa-cogs"></i> '+this.options.labels.extensions_installation);
+			$('#hub-installation-progress-text').html('<i class="fas fa-cogs"></i> '+this.options.labels.extensions_installation);
 			var me = this;
 			var aExtensionCodes = [];
 			var aExtensionDirs = [];
@@ -124,7 +124,7 @@ $(function()
 		},
 		move_to_prod: function()
 		{
-			$('#hub-installation-progress-text').html('<i class="fa fa-cogs"></i> '+this.options.labels.extensions_installation);
+			$('#hub-installation-progress-text').html('<i class="fas fa-cogs"></i> '+this.options.labels.extensions_installation);
 			var me = this;
 			$.post(this.options.self_url, {operation: 'move_to_production', authent: this.options.authent}, function(data) { me._on_move_to_prod(data) }, 'json');
 		},
@@ -133,7 +133,7 @@ $(function()
 			if (data.code == 0)
 			{
 				this._reportSuccess(data.message);
-				$('#hub-installation-progress-text').html('<i class="fa fa-trophy"></i> '+this.options.labels.installation_successful);
+				$('#hub-installation-progress-text').html('<i class="fas fa-trophy"></i> '+this.options.labels.installation_successful);
 				// Report the installation status to iTop Hub
 				$('#at_the_end').append('<iframe style="border:0; width:200px; height: 20px;" src="'+this.options.iframe_url+'">Support of IFRAMES required, sorry</iframe>');
 				if (this.options.redirect_after_completion_url != '')
@@ -171,7 +171,8 @@ $(function()
 		},
 		_reportError: function(sMessage)
 		{
-			$('#hub-installation-progress-text').html('<span style="color:red; font-size:12pt; line-height:18pt;"><i class="fa fa-exclamation-triangle"></i> '+this.options.labels.rollback+'</span><br/><span style="color:#666; display:block; padding:10px;max-height:10em; overflow: auto;padding-top:0; margin-top:10px; text-align:left;">'+sMessage+'</span>');
+			$('#hub-installation-progress-text').html('<span style="color:red; font-size:12pt; line-height:18pt;"><i class="fas' +
+				' fa-exclamation-triangle"></i> '+this.options.labels.rollback+'</span><br/><span style="color:#666; display:block; padding:10px;max-height:10em; overflow: auto;padding-top:0; margin-top:10px; text-align:left;">'+sMessage+'</span>');
 			$('#hub_start_installation').val('Go Back to iTop');
 			$('#hub_start_installation').prop('disabled', false);
 			$('#hub-installation-progress').hide();

datamodels/2.x/itop-portal-base/portal/templates/errors/layout.html.twig
@@ -42,9 +42,11 @@
 			<p>{{ error_message }}</p>
 			<p>{{ 'Error:HTTP:GetHelp'|dict_format(constant('ITOP_APPLICATION_SHORT')) }}</p>
 			<p>
-				<a class="btn btn-default" href="#" onclick="history.back(); return false;"><span class="fa fa-arrow-left"></span> {{ 'Page:GoPreviousPage'|dict_s }}</a>
-				<a class="btn btn-default" href=""><span class="fa fa-repeat"></span> {{ 'Page:ReloadPage'|dict_s }}</a>
-				<a class="btn btn-default" href="{{ app.url_generator.generate('p_home') }}"><span class="fa fa-home"></span> {{ 'Page:GoPortalHome'|dict_s }}</a>
+				<a class="btn btn-default" href="#" onclick="history.back(); return false;"><span class="fas fa-arrow-left"></span> {{
+                    'Page:GoPreviousPage'|dict_s }}</a>
+				<a class="btn btn-default" href=""><span class="fas fa-redo"></span> {{ 'Page:ReloadPage'|dict_s }}</a>
+				<a class="btn btn-default" href="{{ app.url_generator.generate('p_home') }}"><span class="fas fa-home"></span> {{
+                    'Page:GoPortalHome'|dict_s }}</a>
 			</p>
 		</div>
 

datamodels/2.x/itop-portal-base/portal/templates/layout.html.twig
@@ -193,7 +193,7 @@
 								{% block pNavigationTopBricks %}
 									<li class="brick_menu_item {% if oBrick is not defined %}active{% endif %}">
 										<a href="{{ app.url_generator.generate('p_home') }}">
-											<span class="brick_icon fa fa-home fa-2x"></span>
+											<span class="brick_icon fas fa-home fa-2x"></span>
 											{{ 'Page:Home'|dict_s }}
 										</a>
 									</li>
@@ -210,18 +210,21 @@
 								{% endblock %}
 								{% if bUserConnected %}
 									<li role="separator" class="divider"></li>
-									<li><a href="{{ app.url_generator.generate('p_user_profile_brick') }}"><span class="brick_icon fa fa-user fa-2x fa-fw"></span>{{ 'Brick:Portal:UserProfile:Navigation:Dropdown:MyProfil'|dict_s }}</a></li>
+									<li><a href="{{ app.url_generator.generate('p_user_profile_brick') }}"><span class="brick_icon fas
+									fa-user fa-2x fa-fw"></span>{{ 'Brick:Portal:UserProfile:Navigation:Dropdown:MyProfil'|dict_s }}</a></li>
 									{% for aPortal in app['combodo.portal.instance.conf'].portals %}
 										{% if aPortal.id != app['combodo.portal.instance.conf'].properties.id %}
-											{% set sIconClass = (aPortal.id == 'backoffice') ? 'fa-list-alt' : 'fa-external-link' %}
-											<li><a href="{{ aPortal.url }}" target="_blank"><span class="brick_icon fa {{ sIconClass }} fa-2x fa-fw"></span>{{ aPortal.label|dict_s }}</a></li>
+											{% set sIconClass = (aPortal.id == 'backoffice') ? 'far fa-list-alt' : 'fas fa-external-link-alt' %}
+											<li><a href="{{ aPortal.url }}" target="_blank"><span class="brick_icon {{ sIconClass }} fa-2x fa-fw"></span>{{ aPortal.label|dict_s }}</a></li>
 										{% endif %}
 									{% endfor %}
 									{# We display the separator only if the user has more then 1 portal. Meaning default portal + console or several portal at least #}
 									{% if app['combodo.portal.instance.conf'].portals|length > 1 %}
 										<li role="separator" class="divider"></li>
 									{% endif %}
-									<li><a href="{{ app['combodo.absolute_url'] }}pages/logoff.php"><span class="brick_icon fa fa-sign-out fa-2x fa-fw"></span>{{ 'Brick:Portal:UserProfile:Navigation:Dropdown:Logout'|dict_s }}</a></li>
+									<li><a href="{{ app['combodo.absolute_url'] }}pages/logoff.php"><span class="brick_icon fas
+									fa-sign-out-alt fa-2x fa-fw"></span>{{ 'Brick:Portal:UserProfile:Navigation:Dropdown:Logout'|dict_s
+                                            }}</a></li>
 								{% endif %}
 							</ul>
 						</div>
@@ -249,18 +252,21 @@
 									<span class="caret"></span>
 								</a>
 								<ul class="dropdown-menu user_options" aria-labelledby="user_options">
-									<li><a href="{{ app.url_generator.generate('p_user_profile_brick') }}"><span class="brick_icon fa fa-user fa-lg fa-fw"></span>{{ 'Brick:Portal:UserProfile:Navigation:Dropdown:MyProfil'|dict_s }}</a></li>
+									<li><a href="{{ app.url_generator.generate('p_user_profile_brick') }}"><span class="brick_icon fas
+									fa-user fa-lg fa-fw"></span>{{ 'Brick:Portal:UserProfile:Navigation:Dropdown:MyProfil'|dict_s }}</a></li>
 									{% for aPortal in app['combodo.portal.instance.conf'].portals %}
 										{% if aPortal.id != app['combodo.portal.instance.conf'].properties.id %}
-											{% set sGlyphiconClass = (aPortal.id == 'backoffice') ? 'fa-list-alt' : 'fa-external-link' %}
-											<li><a href="{{ aPortal.url }}" {% if app['combodo.portal.instance.conf'].properties.allowed_portals.opening_mode == 'tab' %}target="_blank"{% endif %} title="{{ aPortal.label|dict_s }}"><span class="brick_icon fa {{ sGlyphiconClass }} fa-lg fa-fw"></span>{{ aPortal.label|dict_s }}</a></li>
+											{% set sGlyphiconClass = (aPortal.id == 'backoffice') ? 'far fa-list-alt' :
+                                                'fas fa-external-link-alt' %}
+											<li><a href="{{ aPortal.url }}" {% if app['combodo.portal.instance.conf'].properties.allowed_portals.opening_mode == 'tab' %}target="_blank"{% endif %} title="{{ aPortal.label|dict_s }}"><span class="brick_icon {{ sGlyphiconClass }} fa-lg fa-fw"></span>{{ aPortal.label|dict_s }}</a></li>
 										{% endif %}
 									{% endfor %}
 									{# We display the separator only if the user has more then 1 portal. Meaning default portal + console or several portal at least #}
 									{% if app['combodo.portal.instance.conf'].portals|length > 1 %}
 										<li role="separator" class="divider"></li>
 									{% endif %}
-									<li><a href="{{ app['combodo.absolute_url'] }}pages/logoff.php"><span class="brick_icon fa fa-sign-out fa-lg fa-fw"></span>{{ 'Brick:Portal:UserProfile:Navigation:Dropdown:Logout'|dict_s }}</a></li>
+									<li><a href="{{ app['combodo.absolute_url'] }}pages/logoff.php"><span class="brick_icon fas
+									fa-sign-out-alt fa-lg fa-fw"></span>{{ 'Brick:Portal:UserProfile:Navigation:Dropdown:Logout'|dict_s }}</a></li>
 								</ul>
 							</div>
 						</div>
@@ -270,7 +276,7 @@
 							<ul class="nav navbar-nav">
 								<li class="brick_menu_item {% if oBrick is not defined %}active{% endif %}">
 									<a href="{{ app.url_generator.generate('p_home') }}">
-										<span class="brick_icon fa fa-home fa-2x"></span>
+										<span class="brick_icon fas fa-home fa-2x"></span>
 										{{ 'Page:Home'|dict_s }}
 									</a>
 								</li>

js/search/search_form_criteria.js
@@ -379,7 +379,8 @@ $(function()
 
 			// Prepare base DOM structure
 			this.element
-				.append('<div class="sfc_header"><div class="sfc_title"></div><span class="sfc_toggle"><a class="fa fa-caret-down" href="#"></a></span></div>')
+				.append('<div class="sfc_header"><div class="sfc_title"></div><span class="sfc_toggle"><a class="fas fa-caret-down"' +
+					' href="#"></a></span></div>')
 				.append('<div class="sfc_form_group"><div class="sfc_fg_operators"></div><div class="sfc_fg_buttons"></div></div>');
 
 			// Bind events
@@ -422,7 +423,7 @@ $(function()
 			// Removable / locked decoration
 			if(this.options.is_removable === true)
 			{
-				this.element.find('.sfc_header').append('<span class="sfc_close"><a class="fa fa-times" href="#"></a></span>');
+				this.element.find('.sfc_header').append('<span class="sfc_close"><a class="fas fa-times" href="#"></a></span>');
 				this.element.find('.sfc_close').on('click', function(oEvent){
 					// Prevent anchor
 					oEvent.preventDefault();
@@ -433,7 +434,7 @@ $(function()
 			else
 			{
 				this.element.addClass('locked');
-				this.element.find('.sfc_header').append('<span class="sfc_locked"><span class="fa fa-lock"></span></span>');
+				this.element.find('.sfc_header').append('<span class="sfc_locked"><span class="fas fa-lock"></span></span>');
 			}
 
 			// Form group
@@ -489,8 +490,10 @@ $(function()
 				.append('<button type="button" name="search" class="sfc_fg_button sfc_fg_search">' + Dict.S('UI:Button:Search') + '</button>')
 				.append('<button type="button" name="apply" class="sfc_fg_button sfc_fg_apply">' + Dict.S('UI:Button:Apply') + '</button>')
 				.append('<button type="button" name="cancel" class="sfc_fg_button sfc_fg_cancel">' + Dict.S('UI:Button:Cancel') + '</button>')
-				.append('<button type="button" name="more" class="sfc_fg_button sfc_fg_more">' + Dict.S('UI:Button:More') + '<span class="fa fa-angle-double-down"></span></button>')
-				.append('<button type="button" name="less" class="sfc_fg_button sfc_fg_less">' + Dict.S('UI:Button:Less') + '<span class="fa fa-angle-double-up"></span></button>');
+				.append('<button type="button" name="more" class="sfc_fg_button sfc_fg_more">' + Dict.S('UI:Button:More') + '<span' +
+					' class="fas fa-angle-double-down"></span></button>')
+				.append('<button type="button" name="less" class="sfc_fg_button sfc_fg_less">' + Dict.S('UI:Button:Less') + '<span' +
+					' class="fas fa-angle-double-up"></span></button>');
 
 			// Events
 			this.element.find('.sfc_fg_button').on('click', function(oEvent){

pages/schema.php
@@ -233,7 +233,7 @@ function DisplayClassesList($oPage, $sContext)
 	$oPage->add("<h1>".Dict::S('UI:Schema:Title')."</h1>\n");
 
     $oPage->add("<label for='search-model'>" . Dict::S('UI:Schema:ClassFilter') ."</label><br/><input type=\"text\" id=\"search-model\" autofocus=\"autofocus\"/>");
-    $oPage->add("<div id=\"delDataModelSearch\"> <i class=\"fa fa-times-circle\"></i></div>");
+    $oPage->add("<div id=\"delDataModelSearch\"> <i class=\"fas fa-times-circle\"></i></div>");
 	$oPage->add("<ul id=\"ClassesList\" class=\"treeview fileview\">\n");
 	$oPage->add_ready_script(
 		<<<EOF
@@ -721,9 +721,11 @@ field.filter(function(d) {
 			.style("opacity","1");
 		divD3.style("left", (d3.event.pageX - 7*d['tooltip_data']['class'].length/2) + "px");
 		divD3.style("top", (d3.event.pageY - 65) + "px");
-		divD3.html( '<div id="tooltipD3_top">' + d['tooltip_data']['class'] + '</div><span id="tooltipD3_left"> <i class="fa fa-caret-left"></i> '
+		divD3.html( '<div id="tooltipD3_top">' + d['tooltip_data']['class'] + '</div><span id="tooltipD3_left"> <i class="fas 
+		fa-caret-left"></i> '
 		 			+  ( (d.related_position < 0) ? d['tooltip_data']['to_remote'] : d['tooltip_data']['to_me'] ) +  '</span><span id="tooltipD3_right"> <br/>'
-		 			+ ( (d.related_position < 0) ? d['tooltip_data']['to_me'] : d['tooltip_data']['to_remote'] ) + ' <i class="fa fa-caret-right"></i></span>');
+		 			+ ( (d.related_position < 0) ? d['tooltip_data']['to_me'] : d['tooltip_data']['to_remote'] ) + ' <i class="fas 
+		 			fa-caret-right"></i></span>');
 	})
 	.on('mouseout',function(d){
 		divD3.transition()

datamodels/2.x/itop-portal-base/portal/templates/bricks/user-profile/layout.html.twig
@@ -15,7 +15,7 @@
 {% block pMainContentHolder%}
 	{% if bDemoMode %}
 		<div class="alert alert-warning">
-			<span class="fa fa-info fa-2x" style="margin-right: 10px; vertical-align: sub;"></span>
+			<span class="fas fa-info fa-2x" style="margin-right: 10px; vertical-align: sub;"></span>
 			User profile edition is not available in demo mode.
 		</div>
 	{% endif %}
@@ -65,14 +65,14 @@
 											</span>
 											<span class="actions">
 												<span type="button" class="btn btn-default btn_edit">
-													<span class="fa fa-pencil fa-fw"></span>
+													<span class="fas fa-pencil-alt fa-fw"></span>
 													<input id="picture" type="file" name="picture" />
 												</span>
 												{#<button type="button" class="btn btn-default btn_undo" title="{{ 'UI:Button:ResetImage'|dict_s }}" disabled>
-													<span class="fa fa-undo fa-fw"></span>
+													<span class="fas fa-undo fa-fw"></span>
 												</button>
 												<button type="button" class="btn btn-default btn_reset" title="{{ 'UI:Button:RemoveImage'|dict_s }}">
-													<span class="fa fa-trash-o fa-fw"></span>
+													<span class="fas fa-trash-o fa-fw"></span>
 												</button>#}
 											</span>
 										</div>

application/cmdbabstract.class.inc.php
@@ -321,7 +321,7 @@ EOF
 				}
 				$sLabel = htmlentities(Dict::S('Tag:Synchronized'), ENT_QUOTES, 'UTF-8');
 				$sSynchroTagId = 'synchro_icon-'.$this->GetKey();
-				$aIcons[] = "<div class=\"tag\" id=\"$sSynchroTagId\"><span class=\"object-synchronized fa fa-lock fa-1x\">&nbsp;</span>&nbsp;$sLabel</div>";
+				$aIcons[] = "<div class=\"tag\" id=\"$sSynchroTagId\"><span class=\"object-synchronized fas fa-lock fa-1x\">&nbsp;</span>&nbsp;$sLabel</div>";
 				$sTip = addslashes($sTip);
 				$oPage->add_ready_script("$('#$sSynchroTagId').qtip( { content: '$sTip', show: 'mouseover', hide: { fixed: true }, style: { name: 'dark', tip: 'topLeft' }, position: { corner: { target: 'bottomMiddle', tooltip: 'topLeft' }} } );");
 			}
@@ -331,13 +331,13 @@ EOF
 		{
 			$sLabel = htmlentities(Dict::S('Tag:Archived'), ENT_QUOTES, 'UTF-8');
 			$sTitle = htmlentities(Dict::S('Tag:Archived+'), ENT_QUOTES, 'UTF-8');
-			$aIcons[] = "<div class=\"tag\" title=\"$sTitle\"><span class=\"object-archived fa fa-archive fa-1x\">&nbsp;</span>&nbsp;$sLabel</div>";
+			$aIcons[] = "<div class=\"tag\" title=\"$sTitle\"><span class=\"object-archived fas fa-archive fa-1x\">&nbsp;</span>&nbsp;$sLabel</div>";
 		}
 		elseif ($this->IsObsolete())
 		{
 			$sLabel = htmlentities(Dict::S('Tag:Obsolete'), ENT_QUOTES, 'UTF-8');
 			$sTitle = htmlentities(Dict::S('Tag:Obsolete+'), ENT_QUOTES, 'UTF-8');
-			$aIcons[] = "<div class=\"tag\" title=\"$sTitle\"><span class=\"object-obsolete fa fa-eye-slash fa-1x\">&nbsp;</span>&nbsp;$sLabel</div>";
+			$aIcons[] = "<div class=\"tag\" title=\"$sTitle\"><span class=\"object-obsolete fas fa-eye-slash fa-1x\">&nbsp;</span>&nbsp;$sLabel</div>";
 		}
 
 		$sObjectIcon = $this->GetIcon();

commit 9ed33f16dd66bce2d2a97263e671058826001dfb

N°2269 Font Awesome v5 : fix new lines breaking code :( Those regressions were introduced in a4743901 Saw with a JS error in schema.php

core/displayablegraph.class.inc.php
@@ -1446,8 +1446,7 @@ class DisplayableGraph extends SimpleGraph
 <<<EOF
  <div id="ds_flash" class="search_box">
 	<form id="dh_flash" class="search_form_handler closed">
-	<h2 class="sf_title"><span class="sft_long">$sSftShort</span><span class="sft_short">$sSftShort</span><span class="sft_toggler fas 
-	fa-caret-down pull-right" title="$sSearchToggle"></span></h2>
+	<h2 class="sf_title"><span class="sft_long">$sSftShort</span><span class="sft_short">$sSftShort</span><span class="sft_toggler fas fa-caret-down pull-right" title="$sSearchToggle"></span></h2>
 	<div id="dh_flash_criterion_outer" class="sf_criterion_area"><div class="sf_criterion_row">
 EOF
 		);

datamodels/2.x/itop-hub-connector/js/hub.js
@@ -171,8 +171,7 @@ $(function()
 		},
 		_reportError: function(sMessage)
 		{
-			$('#hub-installation-progress-text').html('<span style="color:red; font-size:12pt; line-height:18pt;"><i class="fas' +
-				' fa-exclamation-triangle"></i> '+this.options.labels.rollback+'</span><br/><span style="color:#666; display:block; padding:10px;max-height:10em; overflow: auto;padding-top:0; margin-top:10px; text-align:left;">'+sMessage+'</span>');
+			$('#hub-installation-progress-text').html('<span style="color:red; font-size:12pt; line-height:18pt;"><i class="fas'+' fa-exclamation-triangle"></i> '+this.options.labels.rollback+'</span><br/><span style="color:#666; display:block; padding:10px;max-height:10em; overflow: auto;padding-top:0; margin-top:10px; text-align:left;">'+sMessage+'</span>');
 			$('#hub_start_installation').val('Go Back to iTop');
 			$('#hub_start_installation').prop('disabled', false);
 			$('#hub-installation-progress').hide();

datamodels/2.x/itop-portal-base/portal/templates/errors/layout.html.twig
@@ -42,11 +42,9 @@
 			<p>{{ error_message }}</p>
 			<p>{{ 'Error:HTTP:GetHelp'|dict_format(constant('ITOP_APPLICATION_SHORT')) }}</p>
 			<p>
-				<a class="btn btn-default" href="#" onclick="history.back(); return false;"><span class="fas fa-arrow-left"></span> {{
-                    'Page:GoPreviousPage'|dict_s }}</a>
+				<a class="btn btn-default" href="#" onclick="history.back(); return false;"><span class="fas fa-arrow-left"></span> {{ 'Page:GoPreviousPage'|dict_s }}</a>
 				<a class="btn btn-default" href=""><span class="fas fa-redo"></span> {{ 'Page:ReloadPage'|dict_s }}</a>
-				<a class="btn btn-default" href="{{ app.url_generator.generate('p_home') }}"><span class="fas fa-home"></span> {{
-                    'Page:GoPortalHome'|dict_s }}</a>
+				<a class="btn btn-default" href="{{ app.url_generator.generate('p_home') }}"><span class="fas fa-home"></span> {{ 'Page:GoPortalHome'|dict_s }}</a>
 			</p>
 		</div>
 

datamodels/2.x/itop-portal-base/portal/templates/layout.html.twig
@@ -210,8 +210,7 @@
 								{% endblock %}
 								{% if bUserConnected %}
 									<li role="separator" class="divider"></li>
-									<li><a href="{{ app.url_generator.generate('p_user_profile_brick') }}"><span class="brick_icon fas
-									fa-user fa-2x fa-fw"></span>{{ 'Brick:Portal:UserProfile:Navigation:Dropdown:MyProfil'|dict_s }}</a></li>
+                                    <li><a href="{{ app.url_generator.generate('p_user_profile_brick') }}"><span class="brick_icon fas fa-user fa-2x fa-fw"></span>{{ 'Brick:Portal:UserProfile:Navigation:Dropdown:MyProfil'|dict_s }}</a></li>
 									{% for aPortal in app['combodo.portal.instance.conf'].portals %}
 										{% if aPortal.id != app['combodo.portal.instance.conf'].properties.id %}
 											{% set sIconClass = (aPortal.id == 'backoffice') ? 'far fa-list-alt' : 'fas fa-external-link-alt' %}
@@ -222,9 +221,7 @@
 									{% if app['combodo.portal.instance.conf'].portals|length > 1 %}
 										<li role="separator" class="divider"></li>
 									{% endif %}
-									<li><a href="{{ app['combodo.absolute_url'] }}pages/logoff.php"><span class="brick_icon fas
-									fa-sign-out-alt fa-2x fa-fw"></span>{{ 'Brick:Portal:UserProfile:Navigation:Dropdown:Logout'|dict_s
-                                            }}</a></li>
+									<li><a href="{{ app['combodo.absolute_url'] }}pages/logoff.php"><span class="brick_icon fas fa-sign-out-alt fa-2x fa-fw"></span>{{ 'Brick:Portal:UserProfile:Navigation:Dropdown:Logout'|dict_s }}</a></li>
 								{% endif %}
 							</ul>
 						</div>
@@ -252,12 +249,10 @@
 									<span class="caret"></span>
 								</a>
 								<ul class="dropdown-menu user_options" aria-labelledby="user_options">
-									<li><a href="{{ app.url_generator.generate('p_user_profile_brick') }}"><span class="brick_icon fas
-									fa-user fa-lg fa-fw"></span>{{ 'Brick:Portal:UserProfile:Navigation:Dropdown:MyProfil'|dict_s }}</a></li>
+									<li><a href="{{ app.url_generator.generate('p_user_profile_brick') }}"><span class="brick_icon fas fa-user fa-lg fa-fw"></span>{{ 'Brick:Portal:UserProfile:Navigation:Dropdown:MyProfil'|dict_s }}</a></li>
 									{% for aPortal in app['combodo.portal.instance.conf'].portals %}
 										{% if aPortal.id != app['combodo.portal.instance.conf'].properties.id %}
-											{% set sGlyphiconClass = (aPortal.id == 'backoffice') ? 'far fa-list-alt' :
-                                                'fas fa-external-link-alt' %}
+											{% set sGlyphiconClass = (aPortal.id == 'backoffice') ? 'far fa-list-alt' : 'fas fa-external-link-alt' %}
 											<li><a href="{{ aPortal.url }}" {% if app['combodo.portal.instance.conf'].properties.allowed_portals.opening_mode == 'tab' %}target="_blank"{% endif %} title="{{ aPortal.label|dict_s }}"><span class="brick_icon {{ sGlyphiconClass }} fa-lg fa-fw"></span>{{ aPortal.label|dict_s }}</a></li>
 										{% endif %}
 									{% endfor %}
@@ -265,8 +260,7 @@
 									{% if app['combodo.portal.instance.conf'].portals|length > 1 %}
 										<li role="separator" class="divider"></li>
 									{% endif %}
-									<li><a href="{{ app['combodo.absolute_url'] }}pages/logoff.php"><span class="brick_icon fas
-									fa-sign-out-alt fa-lg fa-fw"></span>{{ 'Brick:Portal:UserProfile:Navigation:Dropdown:Logout'|dict_s }}</a></li>
+									<li><a href="{{ app['combodo.absolute_url'] }}pages/logoff.php"><span class="brick_icon fas fa-sign-out-alt fa-lg fa-fw"></span>{{ 'Brick:Portal:UserProfile:Navigation:Dropdown:Logout'|dict_s }}</a></li>
 								</ul>
 							</div>
 						</div>

js/search/search_form_criteria.js
@@ -379,8 +379,7 @@ $(function()
 
 			// Prepare base DOM structure
 			this.element
-				.append('<div class="sfc_header"><div class="sfc_title"></div><span class="sfc_toggle"><a class="fas fa-caret-down"' +
-					' href="#"></a></span></div>')
+				.append('<div class="sfc_header"><div class="sfc_title"></div><span class="sfc_toggle"><a class="fas fa-caret-down"' + ' href="#"></a></span></div>')
 				.append('<div class="sfc_form_group"><div class="sfc_fg_operators"></div><div class="sfc_fg_buttons"></div></div>');
 
 			// Bind events
@@ -490,10 +489,8 @@ $(function()
 				.append('<button type="button" name="search" class="sfc_fg_button sfc_fg_search">' + Dict.S('UI:Button:Search') + '</button>')
 				.append('<button type="button" name="apply" class="sfc_fg_button sfc_fg_apply">' + Dict.S('UI:Button:Apply') + '</button>')
 				.append('<button type="button" name="cancel" class="sfc_fg_button sfc_fg_cancel">' + Dict.S('UI:Button:Cancel') + '</button>')
-				.append('<button type="button" name="more" class="sfc_fg_button sfc_fg_more">' + Dict.S('UI:Button:More') + '<span' +
-					' class="fas fa-angle-double-down"></span></button>')
-				.append('<button type="button" name="less" class="sfc_fg_button sfc_fg_less">' + Dict.S('UI:Button:Less') + '<span' +
-					' class="fas fa-angle-double-up"></span></button>');
+				.append('<button type="button" name="more" class="sfc_fg_button sfc_fg_more">' + Dict.S('UI:Button:More') + '<span' + ' class="fas fa-angle-double-down"></span></button>')
+				.append('<button type="button" name="less" class="sfc_fg_button sfc_fg_less">' + Dict.S('UI:Button:Less') + '<span' + ' class="fas fa-angle-double-up"></span></button>');
 
 			// Events
 			this.element.find('.sfc_fg_button').on('click', function(oEvent){

pages/schema.php
@@ -498,7 +498,7 @@ function DisplayRelatedClassesGraph($oPage, $sClass)
 EOF
         );
         $oPage->add_ready_script(
-            <<<EOF
+            <<<JS
 
 var data = $sData;
 var dataref = $sDataReferencing;
@@ -721,11 +721,9 @@ field.filter(function(d) {
 			.style("opacity","1");
 		divD3.style("left", (d3.event.pageX - 7*d['tooltip_data']['class'].length/2) + "px");
 		divD3.style("top", (d3.event.pageY - 65) + "px");
-		divD3.html( '<div id="tooltipD3_top">' + d['tooltip_data']['class'] + '</div><span id="tooltipD3_left"> <i class="fas 
-		fa-caret-left"></i> '
+		divD3.html( '<div id="tooltipD3_top">' + d['tooltip_data']['class'] + '</div><span id="tooltipD3_left"> <i class="fas fa-caret-left"></i> '
 		 			+  ( (d.related_position < 0) ? d['tooltip_data']['to_remote'] : d['tooltip_data']['to_me'] ) +  '</span><span id="tooltipD3_right"> <br/>'
-		 			+ ( (d.related_position < 0) ? d['tooltip_data']['to_me'] : d['tooltip_data']['to_remote'] ) + ' <i class="fas 
-		 			fa-caret-right"></i></span>');
+		 			+ ( (d.related_position < 0) ? d['tooltip_data']['to_me'] : d['tooltip_data']['to_remote'] ) + ' <i class="fas fa-caret-right"></i></span>');
 	})
 	.on('mouseout',function(d){
 		divD3.transition()
@@ -762,7 +760,7 @@ field.filter(function(d) {
     .attr("xlink:href", function(d, i) { return d.icon })
 	.attr("transform", "translate(-12, -24)");
 			
-EOF
+JS
         );
     }
     catch(Exception $e)