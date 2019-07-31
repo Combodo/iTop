661ecc57c539e98da5a58864a08e4ae872dd11c8
Molkobain <lajarige.guillaume@free.fr>
2019-07-31 11:43:28 +0200

    PHPDoc

--- datamodels/2.x/itop-portal-base/portal/public/js/bootstrap-patches.js
+++ datamodels/2.x/itop-portal-base/portal/public/js/bootstrap-patches.js
@@ -1,8 +1,31 @@
 /*
- * Patches for bootstrap 3 as it is no longer maintained by its editor
+ * Copyright (C) 2013-2019 Combodo SARL
+ *
+ * This file is part of iTop.
+ *
+ * iTop is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * iTop is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ *
+ *
  */
 
-//N°2166: Fix a boostrap/CKeditor incompatibility with their respective modals (source: https://stackoverflow.com/a/31679096)
+/*
+ * Patches for bootstrap 3 as it is no longer maintained by its editor
+ *
+ * @author Stephen Abello <stephen.abello@combodo.com>
+ * @since 2.6.2
+ */
+
+//N°2166: Fix a bootstrap/CKeditor incompatibility with their respective modals (source: https://stackoverflow.com/a/31679096)
 $.fn.modal.Constructor.prototype.enforceFocus = function() {
 	$( document )
 		.off( 'focusin.bs.modal' ) // guard against infinite focus loop
