fb1b730bd5f7cc7f9a66df42e34c0de96eed16e7
Molkobain <lajarige.guillaume@free.fr>
2019-08-16 10:37:39 +0200

    N°2435.8 Manage TCPDF lib using composer

--- lib/combodo/tcpdf/fonts/droidsansfallback.php
+++ lib/combodo/tcpdf/fonts/droidsansfallback.php
@@ -1,4 +1,22 @@
 <?php
+/**
+ * Copyright (C) 2013-2019 Combodo SARL
+ *
+ * This file is part of iTop.
+ *
+ * iTop is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * iTop is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ */
+
 $type='TrueTypeUnicode';
 $name='DroidSansFallback';
 $desc=array('Ascent'=>1043,'Descent'=>-266,'CapHeight'=>0,'Flags'=>96,'FontBBox'=>'[-90 -266 1121 1043]','ItalicAngle'=>-6.5,'StemV'=>70,'MissingWidth'=>600);
