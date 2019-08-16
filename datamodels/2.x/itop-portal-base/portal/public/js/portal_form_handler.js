a6ca282ff4a542ba86be96881e916eae578881aa
Molkobain <lajarige.guillaume@free.fr>
2019-08-16 17:05:37 +0200

    Internal: Remove unused function in portal form handler

--- datamodels/2.x/itop-portal-base/portal/public/js/portal_form_handler.js
+++ datamodels/2.x/itop-portal-base/portal/public/js/portal_form_handler.js
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2013-2019 Combodo SARL
+ *
+ * This file is part of iTop.
+ *
+ * iTop is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * iTop is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ */
+
 //iTop Portal Form handler
 //This extends itop.form_handler
 ;
@@ -300,14 +318,6 @@ $(function()
 			// Check all touched AFTER ajax is complete, otherwise the renderer will redraw the field in the mean time.
 			this.element.find('.form_fields').trigger('validate', {touched_fields_only: true});
 			this._enableFormAfterLoading();
-		},
-		_onFieldChange: function(oEvent, oData)
-		{
-			// Clear form help blocks
-			this.element.find('.form_alerts').removeClass('has-success has-warning has-error');
-			this.element.find('.form_alerts .alert').html('').hide();
-			
-			this._super(oEvent, oData);
 		},
 		// Place a field for which no container exists
 		_addField: function(sFieldId)
