443763de48238f5247dc4ac62f9b2f3ed84ffdd0
Pierre Goiffon <pierre.goiffon@combodo.com>
2019-11-26 12:08:55 +0100

    :green_heart: Jenkins : copy default config after unattended install The generated config file had too many default values, and not the wanted ones

--- .jenkins/bin/unattended_install/default_env.sh
+++ .jenkins/bin/unattended_install/default_env.sh
@@ -2,5 +2,12 @@
 
 set -x
 
+chmod 666 conf/production/config-itop.php
+
 cd toolkit
-php unattended_install.php default-params.xml
+php unattended_install.php --response_file=default-params.xml --clean=true
+
+cd ..
+chmod 666 conf/production/config-itop.php
+cp toolkit/default-config-itop.php conf/production/config-itop.php
+chmod 444 conf/production/config-itop.php
