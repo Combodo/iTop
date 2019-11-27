8ec6bb4758d91717176bf84fb38b9d5b90a0c5e9
Molkobain <lajarige.guillaume@free.fr>
2019-11-27 19:35:19 +0100

    N°1666 - Fix missing scroll bar missing in modal window "Create a new field" from Request Template

--- js/linksdirectwidget.js
+++ js/linksdirectwidget.js
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2013-2019 Combodo SARL
+ *
+ * This file is part of iTop.
+ *
+ * iTop is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * iTop is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ */
+
 // jQuery UI style "widget" for managing 1:n links "in-place"
 $(function()
 {
@@ -172,6 +190,7 @@ $(function()
 					modal: true,
 					width: 'auto',
 					height: 'auto',
+					maxHeight: $(window).height() - 50,
 					position: { my: "center", at: "center", of: window },
 					close: function() { me._onDlgClose(); }
 				});
