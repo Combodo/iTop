bd8144a67cee225103752b2ab9cb3c71c4974e4b
Pierre Goiffon <pierre.goiffon@combodo.com>
2019-11-27 11:45:30 +0100

    :construction_worker: Jenkins : config file is now picked by the unattended install The problem was that we had an absolute path in the XML : changing it to the file name is sufficient ! The copy introduced in 443763de was unecessary.

--- .jenkins/bin/unattended_install/default_env.sh
+++ .jenkins/bin/unattended_install/default_env.sh
@@ -6,8 +6,3 @@ chmod 666 conf/production/config-itop.php
 
 cd toolkit
 php unattended_install.php --response_file=default-params.xml --clean=true
-
-cd ..
-chmod 666 conf/production/config-itop.php
-cp toolkit/default-config-itop.php conf/production/config-itop.php
-chmod 444 conf/production/config-itop.php
