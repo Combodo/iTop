2bffa3cf1755f240f2cccb53c0ff30e45853c53a
Pierre Goiffon <pierre.goiffon@combodo.com>
2019-12-03 10:43:58 +0100

    N°2615 PHP versions setup requirements/warnings

--- setup/setuputils.class.inc.php
+++ setup/setuputils.class.inc.php
@@ -52,10 +52,10 @@ class SetupUtils
 	// -- Minimum versions (requirements : forbids installation if not met)
 	const PHP_MIN_VERSION = '5.6.0'; // 5.6 will be supported until the end of 2018 (see http://php.net/supported-versions.php)
 	const MYSQL_MIN_VERSION = '5.6.0'; // 5.6 to have fulltext on InnoDB for Tags fields (N°931)
-	const MYSQL_NOT_VALIDATED_VERSION = ''; // MySQL 8 is now OK (N°2010 in 2.7.0)
+	const MYSQL_NOT_VALIDATED_VERSION = ''; // MySQL 8 is now OK (N°2010 in 2.7.0) but has no query cache so mind the perf on large volumes !
 
 	// -- versions that will be the minimum in next iTop major release (warning if not met)
-	const PHP_NEXT_MIN_VERSION = ''; // no new PHP requirement for next iTop version
+	const PHP_NEXT_MIN_VERSION = '7.1.3'; // we are aiming on switching to Symfony 4 in iTop 2.8
 	const MYSQL_NEXT_MIN_VERSION = ''; // no new MySQL requirement for next iTop version
 	// -- First recent version that is not yet validated by Combodo (warning)
 	const PHP_NOT_VALIDATED_VERSION = '7.4.0';
