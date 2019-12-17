8fed7c7005959c74b37bb14ba4e10235d710223a
Pierre Goiffon <pierre.goiffon@combodo.com>
2019-12-17 09:16:10 +0100

    :lipstick: Setup : TLS message style was hardcoded, get back to standard CSS classes to be aligned with the rest

--- setup/setuputils.class.inc.php
+++ setup/setuputils.class.inc.php
@@ -946,7 +946,7 @@ class SetupUtils
 		$sTlsCaDisabled = $bTlsEnabled ? '' : ' disabled';
 		$oPage->add('<tbody id="tls_options" class="collapsable-options">');
 		$oPage->add('<tr><th colspan="3" style="text-align: left; background-color: transparent"><label style="margin: 6em; font-weight: normal; color: #696969"><img style="vertical-align:bottom" id="db_tls_img">Use TLS encrypted connection</label></th></tr>');
-		$oPage->add('<tr style="display:none"><td colspan="3" style="background-color: #f9e0df; padding: 1em; border: 1px solid #950303; color: #950303;">Before configuring MySQL with TLS encryption, read the documentation <a href="https://wiki.openitop.org/doku.php?id=2_5_0:install:php_and_mysql_tls" target="_blank">on Combodo\'s Wiki</a></td></tr>');
+		$oPage->add('<tr style="display:none"><td colspan="3" class="message message-error">Before configuring MySQL with TLS encryption, read the documentation <a href="https://wiki.openitop.org/doku.php?id=2_5_0:install:php_and_mysql_tls" target="_blank">on Combodo\'s Wiki</a></td></tr>');
 		$oPage->add('<tr style="display:none"><td colspan="3"><label><input id="db_tls_enabled" type="checkbox"'.$sTlsEnabledChecked.' name="db_tls_enabled" value="1"> Encrypted connection enabled</label></td></tr>');
 		$oPage->add('<tr style="display:none"><td>SSL CA:</td>');
 		$oPage->add('<td><input id="db_tls_ca" autocomplete="off" type="text" name="db_tls_ca" value="'.htmlentities($sTlsCA,
