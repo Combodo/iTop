f3f6adf2568241648a858e132c88f99efee21b9a
Vladimir Kunin <v.b.kunin@gmail.com>
2020-01-13 19:32:36 +0300

    :globe_with_meridians: Update Russian translations for 2.7.0-beta

--- datamodels/2.x/itop-core-update/ru.dict.itop-core-update.php
+++ datamodels/2.x/itop-core-update/ru.dict.itop-core-update.php
@@ -1,108 +1,96 @@
 <?php
 /**
- * Localized data
+ * Локализация интерфейса Combodo iTop подготовлена сообществом iTop по-русски http://community.itop-itsm.ru.
  *
- * @copyright Copyright (C) 2010-2018 Combodo SARL
- * @license	http://opensource.org/licenses/AGPL-3.0
+ * @author      Vladimir Kunin <v.b.kunin@gmail.com>
+ * @link        http://community.itop-itsm.ru  iTop Russian Community
+ * @link        https://github.com/itop-itsm-ru/itop-rus
+ * @license     http://opensource.org/licenses/AGPL-3.0
  *
- * This file is part of iTop.
- *
- * iTop is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * iTop is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with iTop. If not, see <http://www.gnu.org/licenses/>
  */
 
 Dict::Add('RU RU', 'Russian', 'Русский', array(
-	'iTopUpdate:UI:PageTitle' => 'Application Upgrade~~',
-    'itop-core-update:UI:SelectUpdateFile' => 'Application Upgrade~~',
-    'itop-core-update:UI:ConfirmUpdate' => 'Application Upgrade~~',
-    'itop-core-update:UI:UpdateCoreFiles' => 'Application Upgrade~~',
-    'itop-core-update:UI:UpdateDone' => 'Application Upgrade~~',
+	'iTopUpdate:UI:PageTitle' => 'Обновление приложения',
+	'itop-core-update:UI:SelectUpdateFile' => 'Обновление',
+	'itop-core-update:UI:ConfirmUpdate' => 'Обновление',
+	'itop-core-update:UI:UpdateCoreFiles' => 'Обновление',
+	'itop-core-update:UI:UpdateDone' => 'Обновление завершено',
 
-	'iTopUpdate:UI:SelectUpdateFile' => 'Select an upgrade file to upload~~',
-	'iTopUpdate:UI:CheckUpdate' => 'Verify upgrade file~~',
-	'iTopUpdate:UI:ConfirmInstallFile' => 'You are about to install %1$s~~',
-	'iTopUpdate:UI:DoUpdate' => 'Upgrade~~',
-	'iTopUpdate:UI:CurrentVersion' => 'Current installed version~~',
-    'iTopUpdate:UI:Back' => 'Back~~',
-    'iTopUpdate:UI:Cancel' => 'Cancel~~',
-    'iTopUpdate:UI:Continue' => 'Continue~~',
-    'iTopUpdate:UI:WithDBBackup' => 'Database backup~~',
-    'iTopUpdate:UI:WithFilesBackup' => 'Application files backup~~',
-    'iTopUpdate:UI:WithoutBackup' => 'No backup is planned~~',
-    'iTopUpdate:UI:Backup' => 'Backup generated before update~~',
-	'iTopUpdate:UI:DoFilesArchive' => 'Archive application files~~',
-	'iTopUpdate:UI:UploadArchive' => 'Select a package to upload~~',
-	'iTopUpdate:UI:ServerFile' => 'Path of a package already on the server~~',
+	'iTopUpdate:UI:SelectUpdateFile' => 'Выбор файла обновления',
+	'iTopUpdate:UI:CheckUpdate' => 'Проверить файл обновления',
+	'iTopUpdate:UI:ConfirmInstallFile' => 'Вы собираетесь установить %1$s',
+	'iTopUpdate:UI:DoUpdate' => 'Начать обновление',
+	'iTopUpdate:UI:CurrentVersion' => 'Текущая версия',
+	'iTopUpdate:UI:Back' => 'Назад',
+	'iTopUpdate:UI:Cancel' => 'Отменть',
+	'iTopUpdate:UI:Continue' => 'Продолжить',
+	'iTopUpdate:UI:WithDBBackup' => 'Резервная копия базы данных',
+	'iTopUpdate:UI:WithFilesBackup' => 'Архив файлов приложения',
+	'iTopUpdate:UI:WithoutBackup' => 'Без резервного копирования',
+	'iTopUpdate:UI:Backup' => 'Резервное копирование перед обновлением',
+	'iTopUpdate:UI:DoFilesArchive' => 'Создать архив файлов приложения',
+	'iTopUpdate:UI:UploadArchive' => 'Выбор пакета для загрузки',
+	'iTopUpdate:UI:ServerFile' => 'Путь к пакету на сервере',
 
-    'iTopUpdate:UI:Status' => 'Status~~',
-    'iTopUpdate:UI:Action' => 'Update~~',
-    'iTopUpdate:UI:History' => 'Versions History~~',
-    'iTopUpdate:UI:Progress' => 'Progress of the upgrade~~',
+	'iTopUpdate:UI:Status' => 'Статус',
+	'iTopUpdate:UI:Action' => 'Обновление',
+	'iTopUpdate:UI:History' => 'История версий',
+	'iTopUpdate:UI:Progress' => 'Ход обновления',
 
-    'iTopUpdate:UI:DoBackup:Label' => 'Backup files and database~~',
-    'iTopUpdate:UI:DoBackup:Warning' => 'Backup is not recommended due to limited available disk space~~',
+	'iTopUpdate:UI:DoBackup:Label' => 'Создать резервную копию базы данных',
+	'iTopUpdate:UI:DoBackup:Warning' => 'Резервное копирование не рекомендуется из-за ограниченного свободного места на диске',
 
-    'iTopUpdate:UI:DiskFreeSpace' => 'Disk free space~~',
-    'iTopUpdate:UI:ItopDiskSpace' => 'iTop disk space~~',
-    'iTopUpdate:UI:DBDiskSpace' => 'Database disk space~~',
-	'iTopUpdate:UI:FileUploadMaxSize' => 'File upload max size~~',
+	'iTopUpdate:UI:DiskFreeSpace' => 'Доступное дисковое пространство',
+	'iTopUpdate:UI:ItopDiskSpace' => 'Размер приложения',
+	'iTopUpdate:UI:DBDiskSpace' => 'Размер базы данных',
+	'iTopUpdate:UI:FileUploadMaxSize' => 'Максимальный размер загружаемого файла',
 
-	'iTopUpdate:UI:PostMaxSize' => 'PHP ini value post_max_size: %1$s~~',
-	'iTopUpdate:UI:UploadMaxFileSize' => 'PHP ini value upload_max_filesize: %1$s~~',
+	'iTopUpdate:UI:PostMaxSize' => 'Значение PHP ini post_max_size: %1$s~~',
+	'iTopUpdate:UI:UploadMaxFileSize' => 'Значение PHP ini upload_max_filesize: %1$s~~',
 
-    'iTopUpdate:UI:CanCoreUpdate:Loading' => 'Checking filesystem~~',
-    'iTopUpdate:UI:CanCoreUpdate:Error' => 'Checking filesystem failed (%1$s)~~',
-    'iTopUpdate:UI:CanCoreUpdate:ErrorFileNotExist' => 'Checking filesystem failed (File not exist %1$s)~~',
-    'iTopUpdate:UI:CanCoreUpdate:Failed' => 'Checking filesystem failed~~',
-    'iTopUpdate:UI:CanCoreUpdate:Yes' => 'Application can be updated~~',
-    'iTopUpdate:UI:CanCoreUpdate:No' => 'Application cannot be updated: %1$s~~',
+	'iTopUpdate:UI:CanCoreUpdate:Loading' => 'Проверка файловой системы',
+	'iTopUpdate:UI:CanCoreUpdate:Error' => 'Ошибка проверки файловой системы (%1$s)',
+	'iTopUpdate:UI:CanCoreUpdate:ErrorFileNotExist' => 'Ошибка проверки файловой системы (файл не существует %1$s)',
+	'iTopUpdate:UI:CanCoreUpdate:Failed' => 'Ошибка проверки файловой системы',
+	'iTopUpdate:UI:CanCoreUpdate:Yes' => 'Приложение может быть обновлено',
+	'iTopUpdate:UI:CanCoreUpdate:No' => 'Приложение не может быть обновлено: %1$s',
 
 	// Setup Messages
-    'iTopUpdate:UI:SetupMessage:Ready' => 'Ready to start~~',
-	'iTopUpdate:UI:SetupMessage:EnterMaintenance' => 'Entering maintenance mode~~',
-	'iTopUpdate:UI:SetupMessage:Backup' => 'Database backup~~',
-	'iTopUpdate:UI:SetupMessage:FilesArchive' => 'Archive application files~~',
-    'iTopUpdate:UI:SetupMessage:CopyFiles' => 'Copy new version files~~',
-    'iTopUpdate:UI:SetupMessage:Compile' => 'Upgrade application and database~~',
-	'iTopUpdate:UI:SetupMessage:ExitMaintenance' => 'Exiting maintenance mode~~',
-    'iTopUpdate:UI:SetupMessage:UpdateDone' => 'Upgrade completed~~',
+	'iTopUpdate:UI:SetupMessage:Ready' => 'Всё готово к началу',
+	'iTopUpdate:UI:SetupMessage:EnterMaintenance' => 'Переход в режим технического обслуживания',
+	'iTopUpdate:UI:SetupMessage:Backup' => 'Резервное копирование базы данных',
+	'iTopUpdate:UI:SetupMessage:FilesArchive' => 'Архивирование файлов приложения',
+	'iTopUpdate:UI:SetupMessage:CopyFiles' => 'Копирование файлов обновления',
+	'iTopUpdate:UI:SetupMessage:Compile' => 'Обновление приложения и базы данных',
+	'iTopUpdate:UI:SetupMessage:ExitMaintenance' => 'Выход из режима технического обслуживания',
+	'iTopUpdate:UI:SetupMessage:UpdateDone' => 'Обновление завершено',
 
 	// Errors
-	'iTopUpdate:Error:MissingFunction' => 'Impossible to start upgrade, missing function~~',
-	'iTopUpdate:Error:MissingFile' => 'Missing file: %1$s~~',
-	'iTopUpdate:Error:CorruptedFile' => 'File %1$s is corrupted~~',
-    'iTopUpdate:Error:BadFileFormat' => 'Upgrade file is not a zip file~~',
-    'iTopUpdate:Error:BadFileContent' => 'Upgrade file is not an application archive~~',
-    'iTopUpdate:Error:BadItopProduct' => 'Upgrade file is not compatible with your application~~',
-	'iTopUpdate:Error:Copy' => 'Error, cannot copy \'%1$s\' to \'%2$s\'~~',
-    'iTopUpdate:Error:FileNotFound' => 'File not found~~',
-    'iTopUpdate:Error:NoFile' => 'No file provided~~',
-	'iTopUpdate:Error:InvalidToken' => 'Invalid token~~',
-	'iTopUpdate:Error:UpdateFailed' => 'Upgrade failed ~~',
-	'iTopUpdate:Error:FileUploadMaxSizeTooSmall' => 'The upload max size seems too small for update. Please change the PHP configuration.~~',
+	'iTopUpdate:Error:MissingFunction' => 'Невозможно запустить обновление, функция отсутствует',
+	'iTopUpdate:Error:MissingFile' => 'Отсутствует файл: %1$s',
+	'iTopUpdate:Error:CorruptedFile' => 'Файл %1$s поврежден',
+	'iTopUpdate:Error:BadFileFormat' => 'Файл обновления не является zip-файлом',
+	'iTopUpdate:Error:BadFileContent' => 'Файл обновления не является архивом приложения',
+	'iTopUpdate:Error:BadItopProduct' => 'Файл обновления не совместим с вашим приложением',
+	'iTopUpdate:Error:Copy' => 'Ошибка, не удаётся скопировать \'%1$s\' в \'%2$s\'',
+	'iTopUpdate:Error:FileNotFound' => 'Файл не найден',
+	'iTopUpdate:Error:NoFile' => 'Нет архива',
+	'iTopUpdate:Error:InvalidToken' => 'Недопустимый токен',
+	'iTopUpdate:Error:UpdateFailed' => 'Ошибка обновления',
+	'iTopUpdate:Error:FileUploadMaxSizeTooSmall' => 'Максимальный размер загрузки недостаточный для обновления. Пожалуйста, измените конфигурацию PHP.',
 
-	'iTopUpdate:UI:RestoreArchive' => 'You can restore your application from the archive \'%1$s\'~~',
-	'iTopUpdate:UI:RestoreBackup' => 'You can restore the database from \'%1$s\'~~',
-	'iTopUpdate:UI:MaintenanceModeActive' => 'The application is currently under maintenance, no user can access the application. You have to run a setup or restore the application archive to return in normal mode.~~',
-	'iTopUpdate:UI:UpdateDone' => 'Upgrade successful~~',
-	'Menu:iTopUpdate' => 'Application Upgrade~~',
-	'Menu:iTopUpdate+' => 'Application Upgrade~~',
+	'iTopUpdate:UI:RestoreArchive' => 'Вы можете восстановить приложение из архива \'%1$s\'',
+	'iTopUpdate:UI:RestoreBackup' => 'Вы можете восстановить базу данных из резервной копии \'%1$s\'',
+	'iTopUpdate:UI:MaintenanceModeActive' => 'В настоящее время приложение находится в режиме технического обслуживания, пользователи не могут получить доступ к приложению. Вы должны запустить программу установки или восстановить архив приложения, чтобы вернуться к нормальному режиму.',
+	'iTopUpdate:UI:UpdateDone' => 'Обновление выполнено успешно',
+	'Menu:iTopUpdate' => 'Обновление приложения',
+	'Menu:iTopUpdate+' => 'Обновление приложения',
 
-    // Missing itop entries
-    'Class:ModuleInstallation/Attribute:installed' => 'Installed on~~',
-    'Class:ModuleInstallation/Attribute:name' => 'Name~~',
-    'Class:ModuleInstallation/Attribute:version' => 'Version~~',
-    'Class:ModuleInstallation/Attribute:comment' => 'Comment~~',
+	// Missing itop entries
+	'Class:ModuleInstallation/Attribute:installed' => 'Дата установки',
+	'Class:ModuleInstallation/Attribute:name' => 'Название',
+	'Class:ModuleInstallation/Attribute:version' => 'Версия',
+	'Class:ModuleInstallation/Attribute:comment' => 'Комментарий',
 ));
 
 
