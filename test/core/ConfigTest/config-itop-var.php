6e927114e00001ba271e3bbd753767595984fac8
bruno DA SILVA <bruno.dasilva@combodo.com>
2020-02-03 08:45:33 +0100

    N°2154 - :green_heart: fix tests  - the correct file is now versioned

---
+++ test/core/ConfigTest/config-itop-var.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ *
+ * Configuration file, generated for the unit tests
+ *
+ *
+ *
+ */
+$MySettings = array(
+
+
+
+	// app_root_url: Root URL used for navigating within the application, or from an email to the application (you can put $SERVER_NAME$ as a placeholder for the server's name)
+	//	default: ''
+	'app_root_url' => 'http://' . (isset($_SERVER['SERVER_NAME']) ? $_SERVER['SERVER_NAME'] : 'localhost') . '/itop/iTop/',
+
+
+);
+
+/**
+ *
+ * Modules specific settings
+ *
+ */
+$MyModuleSettings = array(
+);
+
+/**
+ *
+ * Data model modules to be loaded. Names are specified as relative paths
+ *
+ */
+$MyModules = array(
+	'addons' => array('user rights' => 'addons/userrights/userrightsprofile.class.inc.php'),
+);
+?>
