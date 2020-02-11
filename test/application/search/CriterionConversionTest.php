767bcdf117c2202e2c909efc905c6adbe5e9ec53
Eric <eric.espie@combodo.com>
2020-02-11 17:00:55 +0100

    N°2746 - Fix unit tests (typo)

--- test/application/search/CriterionConversionTest.php
+++ test/application/search/CriterionConversionTest.php
@@ -468,7 +468,7 @@ class CriterionConversionTest extends ItopDataTestCase
 				'ExpectedCriterion' => array(array('widget' => 'hierarchical_key')),
 			),
 			'IP range' => array(
-				'OQL' => "SELECT DatacenterDevice AS dev WHERE INET_ATON(dev.managementip_) > INET_ATON('10.22.32.224') AND INET_ATON(dev.managementip) < INET_ATON('10.22.32.255')",
+				'OQL' => "SELECT DatacenterDevice AS dev WHERE INET_ATON(dev.managementip) > INET_ATON('10.22.32.224') AND INET_ATON(dev.managementip) < INET_ATON('10.22.32.255')",
 				'ExpectedOQL' => "SELECT `dev` FROM DatacenterDevice AS `dev` WHERE ((INET_ATON(`dev`.`managementip`) < INET_ATON('10.22.32.255')) AND (INET_ATON(`dev`.`managementip`) > INET_ATON('10.22.32.224')))",
 				'ExpectedCriterion' => array(array('widget' => 'raw')),
 			),
