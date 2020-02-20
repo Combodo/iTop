19809249a26370ff021547b2f2db574487535ba5
Pierre Goiffon <pierre.goiffon@combodo.com>
2020-02-20 17:50:17 +0100

    :memo: Update PHPDoc for StopWatches interfaces

--- core/computing.inc.php
+++ core/computing.inc.php
@@ -1,35 +1,44 @@
 <?php
-// Copyright (C) 2010-2014 Combodo SARL
-//
-//   This file is part of iTop.
-//
-//   iTop is free software; you can redistribute it and/or modify	
-//   it under the terms of the GNU Affero General Public License as published by
-//   the Free Software Foundation, either version 3 of the License, or
-//   (at your option) any later version.
-//
-//   iTop is distributed in the hope that it will be useful,
-//   but WITHOUT ANY WARRANTY; without even the implied warranty of
-//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//   GNU Affero General Public License for more details.
-//
-//   You should have received a copy of the GNU Affero General Public License
-//   along with iTop. If not, see <http://www.gnu.org/licenses/>
+/**
+ * Copyright (C) 2010-2020 Combodo SARL
+ *
+ * This file is part of iTop.
+ *
+ * iTop is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * iTop is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with iTop. If not, see <http://www.gnu.org/licenses/>
+ */
 
 
 /**
- * Any extension to compute things like a stop watch deadline or working hours 
+ * Any extension to compute things like a stop watch deadline or working hours
  *
  * @copyright   Copyright (C) 2010-2012 Combodo SARL
  * @license     http://opensource.org/licenses/AGPL-3.0
  */
 
 /**
- * Metric computing for stop watches
+ * Metric computing for stop watches.
+ * Can be used for AttributeStopWatch goal (iTop XML node xpath: /itop_design/classes/class/fields/field/goal)
  */ 
 interface iMetricComputer
 {
 	public static function GetDescription();
+
+	/**
+	 * @param \DBObject $oObject
+	 *
+	 * @return float number of seconds for the time limit
+	 */
 	public function ComputeMetric($oObject);
 }
 
@@ -41,21 +50,21 @@ interface iWorkingTimeComputer
 	public static function GetDescription();
 
 	/**
-	 * Get the date/time corresponding to a given delay in the future from the present
-	 * considering only the valid (open) hours for a specified object
-	 * @param $oObject DBObject The object for which to compute the deadline
-	 * @param $iDuration integer The duration (in seconds) in the future
-	 * @param $oStartDate DateTime The starting point for the computation
-	 * @return DateTime The date/time for the deadline
+	 * @param DBObject $oObject The object for which to compute the deadline
+	 * @param integer $iDuration The duration (in seconds) in the future
+	 * @param DateTime $oStartDate The starting point for the computation
+	 *
+	 * @return DateTime The date/time corresponding to a given delay in the future from the present
+	 *      considering only the valid (open) hours for a specified object
 	 */
 	public function GetDeadline($oObject, $iDuration, DateTime $oStartDate);
 	
 	/**
-	 * Get duration (considering only open hours) elapsed bewteen two given DateTimes
-	 * @param $oObject DBObject The object for which to compute the duration
-	 * @param $oStartDate DateTime The starting point for the computation (default = now)
-	 * @param $oEndDate DateTime The ending point for the computation (default = now)
-	 * @return integer The duration (number of seconds) of open hours elapsed between the two dates
+	 * @param DBObject $oObject The object for which to compute the duration
+	 * @param DateTime $oStartDate The starting point for the computation (default = now)
+	 * @param DateTime $oEndDate The ending point for the computation (default = now)
+	 *
+	 * @return integer The duration (number of seconds) elapsed between two given dates, considering only open hours
 	 */
 	public function GetOpenDuration($oObject, DateTime $oStartDate, DateTime $oEndDate);
 }
@@ -87,12 +96,7 @@ class DefaultWorkingTimeComputer implements iWorkingTimeComputer
 	}
 
 	/**
-	 * Get the date/time corresponding to a given delay in the future from the present
-	 * considering only the valid (open) hours for a specified object
-	 * @param $oObject DBObject The object for which to compute the deadline
-	 * @param $iDuration integer The duration (in seconds) in the future
-	 * @param $oStartDate DateTime The starting point for the computation
-	 * @return DateTime The date/time for the deadline
+	 * @inheritDoc
 	 */
 	public function GetDeadline($oObject, $iDuration, DateTime $oStartDate)
 	{
@@ -113,11 +117,7 @@ class DefaultWorkingTimeComputer implements iWorkingTimeComputer
 	}
 	
 	/**
-	 * Get duration (considering only open hours) elapsed bewteen two given DateTimes
-	 * @param $oObject DBObject The object for which to compute the duration
-	 * @param $oStartDate DateTime The starting point for the computation (default = now)
-	 * @param $oEndDate DateTime The ending point for the computation (default = now)
-	 * @return integer The duration (number of seconds) of open hours elapsed between the two dates
+	 * @inheritDoc
 	 */
 	public function GetOpenDuration($oObject, DateTime $oStartDate, DateTime $oEndDate)
 	{
@@ -134,6 +134,3 @@ class DefaultWorkingTimeComputer implements iWorkingTimeComputer
 		return $iDuration;
 	}
 }
-
-
-?>
