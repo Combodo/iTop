Combodo/iTop

commit dba6e8ce1ac235ca8fce12e4afe976553d083659

Fix images being too wide in HTML fields and caselogs in the end-users portal
Regression introduced in a previous version of iTop.

datamodels/2.x/itop-portal-base/portal/public/css/portal.scss
@@ -1116,6 +1116,10 @@ table .group-actions .item-action-wrapper .panel-body > p:last-child{
 	color: $brand-primary;
 	font-size: 0.9em;
 }
+.form_field .form-control-static img{
+	max-width: 100% !important;
+	height: initial !important;
+}
 /* ExternalKey */
 .selectobject .input-group-addon{
 	cursor: pointer;
@@ -1343,11 +1347,6 @@ table .group-actions .item-action-wrapper .panel-body > p:last-child{
 /* BlobField */
 .form_fields .file_open_link{
 	margin-left: 10px;
-}
-
-.form_field .form-control-static img{
-	max-width: 100% !important;
-	height: initial !important;
 }
 
 .form_buttons{

sources/renderer/bootstrap/fieldrenderer/bsfieldrenderer.class.inc.php
@@ -39,6 +39,7 @@ class BsFieldRenderer extends FieldRenderer
 	public function Render()
 	{
 		$oOutput = parent::Render();
+		$oOutput->AddCssClass('form_field');
 		$oOutput->AddCssClass('form_field_'.$this->oField->GetDisplayMode());
 
 		return $oOutput;

commit 1ce5ec73eab5c92db1d9b1faaa33643ffa4616e3

N°2798 - Fix unable to submit portal forms (Regression from dba6e8ce)

datamodels/2.x/itop-portal-base/portal/public/css/portal.css
@@ -1,4 +1,4 @@
-/*
+/*!
  * Copyright (C) 2013-2020 Combodo SARL
  *
  * This file is part of iTop.
@@ -15,7 +15,23 @@
  *
  * You should have received a copy of the GNU Affero General Public License
  */
-
+/*!
+ * Copyright (C) 2013-2020 Combodo SARL
+ *
+ * This file is part of iTop.
+ *
+ * iTop is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * iTop is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ */
 /*!
  * Combodo portal template v1.0.0
  */
@@ -1035,6 +1051,11 @@ table .group-actions {
   color: #EA7D1E;
   font-size: 0.9em;
 }
+/* Note: We don't put the .form_field selector as it must work for read-only */
+.form-control-static img {
+  max-width: 100% !important;
+  height: initial !important;
+}
 /* ExternalKey */
 .selectobject .input-group-addon {
   cursor: pointer;
@@ -1248,10 +1269,6 @@ table .group-actions {
 /* BlobField */
 .form_fields .file_open_link {
   margin-left: 10px;
-}
-.form_field .form-control-static img {
-  max-width: 100% !important;
-  height: initial !important;
 }
 .form_buttons {
   padding-top: 20px;

datamodels/2.x/itop-portal-base/portal/public/css/portal.scss
@@ -1116,7 +1116,8 @@ table .group-actions .item-action-wrapper .panel-body > p:last-child{
 	color: $brand-primary;
 	font-size: 0.9em;
 }
-.form_field .form-control-static img{
+/* Note: We don't put the .form_field selector as it must work for read-only */
+.form-control-static img{
 	max-width: 100% !important;
 	height: initial !important;
 }

sources/renderer/bootstrap/fieldrenderer/bsfieldrenderer.class.inc.php
@@ -39,7 +39,6 @@ class BsFieldRenderer extends FieldRenderer
 	public function Render()
 	{
 		$oOutput = parent::Render();
-		$oOutput->AddCssClass('form_field');
 		$oOutput->AddCssClass('form_field_'.$this->oField->GetDisplayMode());
 
 		return $oOutput;