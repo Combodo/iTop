b5e69a112f78f851ea06e11de7965b5bb5e64c56
bruno DA SILVA <bruno.dasilva@combodo.com>
2020-03-21 00:26:29 +0100

    When the portal conf is removed, the kernel can now boot.
    To do so, comment Kernel.php :
    
    configureContainer()
     -> lines 160 to 165
    
    configureRoutes()
     - lines 184 to 188

--- sources/Kernel/Kernel.php
+++ sources/Kernel/Kernel.php
@@ -136,11 +136,15 @@ class Kernel extends BaseKernel
 	{
 		$confDir = APPCONF.utils::GetCurrentEnvironment();
 
+
 		$container->addResource(new FileResource($confDir.'/config-symfony-bundles.php'));
 		$container->setParameter('container.autowiring.strict_mode', true);
 		$container->setParameter('container.dumper.inline_class_loader', true);
 
-        $loader->load($confDir.'/{packages}/*'.self::CONFIG_EXTS, 'glob');
+		$loader->load("$confDir/config-symfony.yaml");
+
+
+		$loader->load($confDir.'/{packages}/*'.self::CONFIG_EXTS, 'glob');
         $loader->load($confDir.'/{packages}/'.$this->environment.'/**/*'.self::CONFIG_EXTS, 'glob');
         $loader->load($confDir.'/{services}'.self::CONFIG_EXTS, 'glob');
         $loader->load($confDir.'/{services}_'.$this->environment.self::CONFIG_EXTS, 'glob');
