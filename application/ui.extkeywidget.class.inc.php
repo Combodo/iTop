f683561cb29d38d75787a0bf314cd7b8d78605f3
Pierre Goiffon <pierre.goiffon@combodo.com>
2020-03-27 15:48:56 +0100

    Report PR #87 modifications in current develop branch

--- application/ui.extkeywidget.class.inc.php
+++ application/ui.extkeywidget.class.inc.php
@@ -640,10 +640,15 @@ HTML
 
 		$oPage->add('</td></tr></table>');
 		$oPage->add('</div>');
+
+		$oPage->add('<div class="treecontrol" id="treecontrolid"><a href="?#">'.Dict::S("UI:Button:CollapseAll").'</a> | <a href="?#">'.Dict::S("UI:Button:ExpandAll").'</a></div>');
+
 		$oPage->add("<input type=\"button\" id=\"btn_cancel_{$this->iId}\" value=\"".Dict::S('UI:Button:Cancel')."\" onClick=\"$('#dlg_tree_{$this->iId}').dialog('close');\">&nbsp;&nbsp;");
 		$oPage->add("<input type=\"button\" id=\"btn_ok_{$this->iId}\" value=\"".Dict::S('UI:Button:Ok')."\"  onClick=\"oACWidget_{$this->iId}.DoHKOk();\">");
 
 		$oPage->add('</div></div>');
+
+		$oPage->add_ready_script("\$('#tree_$this->iId ul').treeview({ control: '#treecontrolid',	persist: 'false'});\n");
 		$oPage->add_ready_script("\$('#tree_$this->iId ul').treeview();\n");
 		$oPage->add_ready_script("\$('#dlg_tree_$this->iId').dialog({ width: 'auto', height: 'auto', autoOpen: true, modal: true, title: '$sDialogTitle', resizeStop: oACWidget_{$this->iId}.OnHKResize, close: oACWidget_{$this->iId}.OnHKClose });\n");
 	}
@@ -728,7 +733,7 @@ HTML
 						$sSelect = '<input id="input_'.$fUniqueId.'_'.$aNodes[$id]->GetKey().'" type="radio" value="'.$aNodes[$id]->GetKey().'" name="selectObject" '.$sChecked.'>&nbsp;';
 					}
 				}
-				$oP->add('<li>'.$sSelect.'<label for="input_'.$fUniqueId.'_'.$aNodes[$id]->GetKey().'">'.$aNodes[$id]->GetName().'</label>');
+				$oP->add('<li class="closed">'.$sSelect.'<label for="input_'.$fUniqueId.'_'.$aNodes[$id]->GetKey().'">'.$aNodes[$id]->GetName().'</label>');
 				$this->DumpNodes($oP, $id, $aTree, $aNodes, $currValue);
 				$oP->add("</li>\n");
 			}
