Combodo/iTop

commit 503afb98316b012c595ad565cb22140ad169bc98

Make it possible to add return path for mails (#95)

core/email.class.inc.php
@@ -304,6 +304,10 @@ class EMail
 			$oHeaders = $this->m_oMessage->getHeaders();
 			switch(strtolower($sKey))
 			{
+				case 'return-path':
+					$this->m_oMessage->setReturnPath($sValue)
+					break;
+
 				default:
 				$oHeaders->addTextHeader($sKey, $sValue);
 			}

commit ff2e1a35071925a838456baa25673710fff48861

Fix syntax error in core/email.class.inc.php Missing ";" at the end of line :/ Introduced by 503afb98

core/email.class.inc.php
@@ -305,11 +305,11 @@ class EMail
 			switch(strtolower($sKey))
 			{
 				case 'return-path':
-					$this->m_oMessage->setReturnPath($sValue)
+					$this->m_oMessage->setReturnPath($sValue);
 					break;
 
 				default:
-				$oHeaders->addTextHeader($sKey, $sValue);
+					$oHeaders->addTextHeader($sKey, $sValue);
 			}
 		}
 	}