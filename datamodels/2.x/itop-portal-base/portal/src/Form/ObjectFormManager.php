c182b1a01f583dd62b3a9afe2631d863b72b0815
acognet <anne-catherine.cognet@combodo.com>
2020-04-02 11:50:01 +0200

    N°2848 - Align creation and update message on portal to console message

--- datamodels/2.x/itop-portal-base/portal/src/Form/ObjectFormManager.php
+++ datamodels/2.x/itop-portal-base/portal/src/Form/ObjectFormManager.php
@@ -1136,7 +1136,14 @@ class ObjectFormManager extends FormManager
 
 				if ($bWasModified)
 				{
-					$aData['messages']['success'] += array('_main' => array(Dict::Format('Brick:Portal:Object:Form:Message:ObjectSaved', $this->oObject->GetName())));
+					if(!$bActivateTriggers)
+					{
+						$aData['messages']['success'] += array(	'_main' => array(Dict::Format('UI:Title:Object_Of_Class_Created', $this->oObject->GetName(),MetaModel::GetName(get_class($this->oObject)))));
+					}
+					else
+					{
+						$aData['messages']['success'] += array('_main' => array(Dict::Format('UI:Class_Object_Updated', MetaModel::GetName(get_class($this->oObject)), $this->oObject->GetName())));
+					}
 				}
 			}
 			catch (Exception $e)
