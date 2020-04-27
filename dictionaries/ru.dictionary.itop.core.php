9ee7b16fb704ac5d9df4f582c66cdafae0a9cee7
Vladimir Kunin <v.b.kunin@gmail.com>
2020-04-28 00:02:30 +0300

    Add Russian translations for 2.7.0-1 (rebased)

--- dictionaries/ru.dictionary.itop.core.php
+++ dictionaries/ru.dictionary.itop.core.php
@@ -191,12 +191,12 @@ Dict::Add('RU RU', 'Russian', 'Русский', array(
 	'Core:AttributeTag' => 'Тег',
 	'Core:AttributeTag+' => 'Тег',
 	
-	'Core:Context=REST/JSON' => 'REST~~',
-	'Core:Context=Synchro' => 'Synchro~~',
-	'Core:Context=Setup' => 'Setup~~',
-	'Core:Context=GUI:Console' => 'Console~~',
-	'Core:Context=CRON' => 'cron~~',
-	'Core:Context=GUI:Portal' => 'Portal~~',
+	'Core:Context=REST/JSON' => 'REST',
+	'Core:Context=Synchro' => 'Synchro',
+	'Core:Context=Setup' => 'Setup',
+	'Core:Context=GUI:Console' => 'Console',
+	'Core:Context=CRON' => 'cron',
+	'Core:Context=GUI:Portal' => 'Portal',
 ));
 
 
@@ -529,8 +529,8 @@ Dict::Add('RU RU', 'Russian', 'Русский', array(
 	'Class:Trigger/Attribute:action_list+' => 'Действия, выполняемые при срабатывании триггера',
 	'Class:Trigger/Attribute:finalclass' => 'Тип',
 	'Class:Trigger/Attribute:finalclass+' => '',
-	'Class:Trigger/Attribute:context' => 'Context~~',
-	'Class:Trigger/Attribute:context+' => 'Context to allow the trigger to start~~',
+	'Class:Trigger/Attribute:context' => 'Контекст',
+	'Class:Trigger/Attribute:context+' => 'Контекст, в котором будет срабатывать триггер',
 ));
 
 //
