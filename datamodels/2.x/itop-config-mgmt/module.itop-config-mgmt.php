6523b34d584a064d679f4855adc0e792a5637e19
acognet <anne-catherine.cognet@combodo.com>
2020-06-23 11:19:44 +0200

    Update version number for 2.7.1

--- datamodels/2.x/itop-config-mgmt/module.itop-config-mgmt.php
+++ datamodels/2.x/itop-config-mgmt/module.itop-config-mgmt.php
@@ -3,7 +3,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'itop-config-mgmt/2.7.0',
+	'itop-config-mgmt/2.7.1',
 	array(
 		// Identification
 		//
