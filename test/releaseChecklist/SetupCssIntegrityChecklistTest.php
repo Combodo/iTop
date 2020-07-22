a53ce32e5425fc77e3af7e9774795338ba1af906
bruno DA SILVA <bruno.dasilva@combodo.com>
2020-07-22 17:07:12 +0200

    N°3062 - setup.css file integrity test
    this is for the task : "Generate setup.css at build time"

---
+++ test/releaseChecklist/SetupCssIntegrityChecklistTest.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Copyright (C) 2010-2020 Combodo SARL
+ *
+ *   This file is part of iTop.
+ *
+ *   iTop is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Affero General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   iTop is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Affero General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Affero General Public License
+ *   along with iTop. If not, see <http: *www.gnu.org/licenses/>
+ *
+ */
+
+namespace Combodo\iTop\Test\UnitTest\ReleaseChecklist;
+
+use Combodo\iTop\Test\UnitTest\ItopTestCase;
+use DOMDocument;
+use iTopDesignFormat;
+
+
+/**
+ * Class iTopDesignFormatChecklistTest
+ *
+ * @runTestsInSeparateProcesses
+ * @preserveGlobalState disabled
+ * @backupGlobals disabled
+ *
+ * @covers iTopDesignFormat
+ *
+ * @package Combodo\iTop\Test\UnitTest\Setup
+ */
+class SetupCssIntegrityChecklistTest extends ItopTestCase
+{
+
+	protected function setUp()
+	{
+		parent::setUp();
+	}
+
+	/**
+	 */
+	public function testSetupCssIntegrity()
+	{
+		$sSetupCssPath = APPROOT.'css/setup.css';
+		$sSetupCssContent = file_get_contents($sSetupCssPath);
+		$this->assertContains('/* integrityCheck: begin (do not remove/edit) */', $sSetupCssContent);
+		$this->assertContains('/* integrityCheck: end (do not remove/edit) */', $sSetupCssContent);
+		$this->assertGreaterThan(4000, strlen($sSetupCssContent)); //the value is totally arbitrary (at the time of the writing the file is 5660o  long
+	}
+
+}
