Combodo/iTop

commit a1f5d8041e64e64c3ea68a46963b725f7278c0c7

N°3251 (see also PR#152) - CSV data synchro allows "<NULL>" to be interpreted as "Let the attribute unchanged for the current row"

synchro/synchro_import.php
@@ -534,9 +534,9 @@ try
 				$aValues = array(); // Used to build the insert query
 				foreach ($aRow as $iCol => $value)
 				{
-					if ($value === null)
+					if ($value === null) // Source CSV: "<NULL>"
 					{
-						$aValues[] = 'NULL';
+						$aValues[] = null;
 					}
 					elseif ($aIsDateToTransform[$iCol] !== false)
 					{

commit 9319e0bdf947fab5d45fb08759c4c70d953dc05e

Fix SQL error introduced in a1f5d80

synchro/synchro_import.php
@@ -534,11 +534,7 @@ try
 				$aValues = array(); // Used to build the insert query
 				foreach ($aRow as $iCol => $value)
 				{
-					if ($value === null) // Source CSV: "<NULL>"
-					{
-						$aValues[] = null;
-					}
-					elseif ($aIsDateToTransform[$iCol] !== false)
+					if ($aIsDateToTransform[$iCol] !== false)
 					{
 						$bDateOnly = false;
 						$sFormat = $sDateTimeFormat;