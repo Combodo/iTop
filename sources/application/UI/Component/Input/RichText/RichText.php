fd3f972948123ef3e1dc67b7c7a04b7713bd8c01
Stephen Abello <stephen.abello@combodo.com>
2020-09-30 09:40:09 +0200

    N°2847 Add RichText input

---
+++ sources/application/UI/Component/Input/RichText/RichText.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @copyright   Copyright (C) 2010-2020 Combodo SARL
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+namespace Combodo\iTop\Application\UI\Component\Input\RichText;
+use Combodo\iTop\Application\UI\UIBlock;
+/**
+ * Class RichText
+ *
+ * @package Combodo\iTop\Application\UI\Component\RichText
+ */
+class RichText extends UIBlock
+{
+	// Overloaded constants
+	public const BLOCK_CODE = 'ibo-richtext';
+	public const HTML_TEMPLATE_REL_PATH = 'components/input/richtext/layout';
+	public const JS_TEMPLATE_REL_PATH = 'components/input/richtext/layout';
+}
