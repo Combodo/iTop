Combodo/iTop

commit 3b6a03d7037f01836f3deebbd34ace9f0f53e571

Fix warning if not countable

application/Html2Text.php
@@ -30,8 +30,11 @@ function mb_str_replace($search, $replace, $subject, &$count = 0) {
 		$replacements = array_pad($replacements, count($searches), '');
 		foreach ($searches as $key => $search) {
 			$parts = mb_split(preg_quote($search), $subject);
-			$count += count($parts) - 1;
-			$subject = implode($replacements[$key], $parts);
+			if (is_countable($parts))
+			{
+				$count += count($parts) - 1;
+				$subject = implode($replacements[$key], $parts);
+			}
 		}
 	} else {
 		// Call mb_str_replace for each subject in array, recursively

commit 3adcee6da8426e47976e93340e4ccb4d8adea95c

Fix regression introduced in 3b6a03d703

application/Html2Text.php
@@ -30,7 +30,7 @@ function mb_str_replace($search, $replace, $subject, &$count = 0) {
 		$replacements = array_pad($replacements, count($searches), '');
 		foreach ($searches as $key => $search) {
 			$parts = mb_split(preg_quote($search), $subject);
-			if (is_countable($parts))
+			if (is_array($parts))
 			{
 				$count += count($parts) - 1;
 				$subject = implode($replacements[$key], $parts);