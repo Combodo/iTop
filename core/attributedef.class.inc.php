Combodo/iTop

commit cfd9dba66e38f1c97b292165656b2d3b594f012c

N°2224 - Portal: Enable tooltips for object's attributes description

core/attributedef.class.inc.php
@@ -1026,6 +1026,13 @@ abstract class AttributeDefinition
 			$oFormField->AddValidator(new Validator($this->GetValidationPattern()));
 		}
 
+		// Description
+		$sAttDescription = $this->GetDescription();
+		if(!empty($sAttDescription))
+		{
+			$oFormField->SetDescription($this->GetDescription());
+		}
+
 		// Metadata
 		$oFormField->AddMetadata('attribute-code', $this->GetCode());
 		$oFormField->AddMetadata('attribute-type', get_class($this));

datamodels/2.x/itop-portal-base/portal/public/css/portal.scss
@@ -1113,6 +1113,17 @@ table .group-actions .item-action-wrapper .panel-body > p:last-child{
 /*********/
 /* Forms */
 /*********/
+.form_field_label > .control-label[data-tooltip-instanciated="true"] {
+	&::after {
+		content: "?";
+		padding-left: 3px;
+		vertical-align: top;
+
+		cursor: pointer;
+		color: $gray;
+		font-size: 0.85em;
+	}
+}
 .form_field .form_mandatory .control-label:after{
 	content: "\002a";
 	position: relative;

sources/Form/Field/Field.php
@@ -37,6 +37,7 @@ abstract class Field
     const ENUM_DISPLAY_MODE_DENSE = 'dense';        // Label and value side by side, closely
 
 	const DEFAULT_LABEL = '';
+	const DEFAULT_DESCRIPTION = '';
 	const DEFAULT_METADATA = array();
 	const DEFAULT_HIDDEN = false;
 	const DEFAULT_READ_ONLY = false;
@@ -48,6 +49,13 @@ abstract class Field
 	protected $sGlobalId;
 	protected $sFormPath;
 	protected $sLabel;
+	/**
+	 * Description text of the field, typically to bring more information to the user on the field purpose
+	 *
+	 * @var string
+	 * @since 3.0.0
+	 */
+	protected $sDescription;
 	protected $aMetadata;
 	protected $bHidden;
 	protected $bReadOnly;
@@ -71,6 +79,7 @@ abstract class Field
 		// No space in such an id, that could be used as a DOM node id
 		$this->sGlobalId = 'field_' . str_replace(' ', '_', $sId) . '_' . uniqid();
 		$this->sLabel = static::DEFAULT_LABEL;
+		$this->sDescription = static::DEFAULT_DESCRIPTION;
 		$this->aMetadata = static::DEFAULT_METADATA;
 		$this->bHidden = static::DEFAULT_HIDDEN;
 		$this->bReadOnly = static::DEFAULT_READ_ONLY;
@@ -119,6 +128,28 @@ abstract class Field
 	public function GetLabel()
 	{
 		return $this->sLabel;
+	}
+
+	/**
+	 * Return true if the field has a description. Note that an empty string is equivalent to no description.
+	 *
+	 * @see static::$sDescription
+	 * @return bool
+	 * @since 3.0.0
+	 */
+	public function HasDescription(): bool
+	{
+		return empty($this->sDescription) === false;
+	}
+
+	/**
+	 * @see static::$sDescription
+	 * @return string
+	 * @since 3.0.0
+	 */
+	public function GetDescription(): string
+	{
+		return $this->sDescription;
 	}
 
 	/**
@@ -245,6 +276,19 @@ abstract class Field
 	public function SetLabel($sLabel)
 	{
 		$this->sLabel = $sLabel;
+		return $this;
+	}
+
+	/**
+	 * @param string $sDescription
+	 * @see static::$sDescription
+	 *
+	 * @return $this
+	 * @since 3.0.0
+	 */
+	public function SetDescription(string $sDescription)
+	{
+		$this->sDescription = $sDescription;
 		return $this;
 	}
 

sources/Renderer/Bootstrap/FieldRenderer/BsFileUploadFieldRenderer.php
@@ -77,6 +77,7 @@ class BsFileUploadFieldRenderer extends BsFieldRenderer
 		$sCollapseTogglerClass = 'form_linkedset_toggler';
 		$sCollapseTogglerId = $sCollapseTogglerClass . '_' . $this->oField->GetGlobalId();
 		$sFieldWrapperId = 'form_upload_wrapper_' . $this->oField->GetGlobalId();
+		$sFieldDescriptionForHTMLTag = ($this->oField->HasDescription()) ? 'data-tooltip-content="'.utils::HtmlEntities($this->oField->GetDescription()).'"' : '';
 
 		// If collapsed
 		$sCollapseTogglerClass .= ' collapsed';
@@ -90,7 +91,7 @@ class BsFileUploadFieldRenderer extends BsFieldRenderer
 		{
 			$iAttachmentsCount = $this->oAttachmentsSet->Count();
 			$oOutput
-				->AddHtml('<label for="'.$this->oField->GetGlobalId().'" class="control-label">')
+				->AddHtml('<label for="'.$this->oField->GetGlobalId().'" class="control-label" '.$sFieldDescriptionForHTMLTag.'>')
 				->AddHtml('<a id="' . $sCollapseTogglerId . '" class="' . $sCollapseTogglerClass . '" data-toggle="collapse" href="#' . $sFieldWrapperId . '" aria-expanded="' . $sCollapseTogglerExpanded . '" aria-controls="' . $sFieldWrapperId . '">')
 				->AddHtml($this->oField->GetLabel(),true)
 				->AddHtml(' (<span class="attachments-count">'.$iAttachmentsCount.'</span>)')

sources/Renderer/Bootstrap/FieldRenderer/BsLinkedSetFieldRenderer.php
@@ -26,6 +26,7 @@ use Dict;
 use Exception;
 use IssueLog;
 use MetaModel;
+use utils;
 
 /**
  * Description of BsLinkedSetFieldRenderer
@@ -45,6 +46,7 @@ class BsLinkedSetFieldRenderer extends BsFieldRenderer
 	    $oOutput = parent::Render();
 
 		$sFieldMandatoryClass = ($this->oField->GetMandatory()) ? 'form_mandatory' : '';
+		$sFieldDescriptionForHTMLTag = ($this->oField->HasDescription()) ? 'data-tooltip-content="'.utils::HtmlEntities($this->oField->GetDescription()).'"' : '';
 		// Vars to build the table
 		$sAttributesToDisplayAsJson = json_encode($this->oField->GetAttributesToDisplay());
 		$sAttCodesToDisplayAsJson = json_encode($this->oField->GetAttributesToDisplay(true));
@@ -82,7 +84,7 @@ class BsLinkedSetFieldRenderer extends BsFieldRenderer
 			$oOutput->AddHtml('<div class="form-group ' . $sFieldMandatoryClass . '">');
 			if ($this->oField->GetLabel() !== '')
 			{
-				$oOutput->AddHtml('<label for="' . $this->oField->GetGlobalId() . '" class="control-label">')
+				$oOutput->AddHtml('<label for="' . $this->oField->GetGlobalId() . '" class="control-label" '.$sFieldDescriptionForHTMLTag.'>')
 					->AddHtml('<a id="' . $sCollapseTogglerId . '" class="' . $sCollapseTogglerClass . '" data-toggle="collapse" href="#' . $sFieldWrapperId . '" aria-expanded="' . $sCollapseTogglerExpanded . '" aria-controls="' . $sFieldWrapperId . '">')
 					->AddHtml($this->oField->GetLabel(), true)
 					->AddHtml('<span class="text">' . count($aItemIds) . '</span>')

sources/Renderer/Bootstrap/FieldRenderer/BsSelectObjectFieldRenderer.php
@@ -29,6 +29,7 @@ use Dict;
 use Exception;
 use IssueLog;
 use MetaModel;
+use utils;
 
 /**
  * Description of BsSelectObjectFieldRenderer
@@ -50,6 +51,7 @@ class BsSelectObjectFieldRenderer extends BsFieldRenderer
 		$sFieldValueClass = $this->oField->GetSearch()->GetClass();
 		$sFieldMandatoryClass = ($this->oField->GetMandatory()) ? 'form_mandatory' : '';
 		$iFieldControlType = $this->oField->GetControlType();
+		$sFieldDescriptionForHTMLTag = ($this->oField->HasDescription()) ? 'data-tooltip-content="'.utils::HtmlEntities($this->oField->GetDescription()).'"' : '';
 
 		// TODO : Remove this when hierarchical search supported
 		$this->oField->SetHierarchical(false);
@@ -71,7 +73,7 @@ class BsSelectObjectFieldRenderer extends BsFieldRenderer
 			$oOutput->AddHtml('<div class="form_field_label">');
 			if ($this->oField->GetLabel() !== '')
 			{
-				$oOutput->AddHtml('<label for="' . $this->oField->GetGlobalId() . '" class="control-label">')->AddHtml($this->oField->GetLabel(), true)->AddHtml('</label>');
+				$oOutput->AddHtml('<label for="' . $this->oField->GetGlobalId() . '" class="control-label" '.$sFieldDescriptionForHTMLTag.'>')->AddHtml($this->oField->GetLabel(), true)->AddHtml('</label>');
 			}
 			$oOutput->AddHtml('</div>');
 
@@ -341,7 +343,7 @@ EOF
 				$oOutput->AddHtml('<div class="form_field_label">');
 				if ($this->oField->GetLabel() !== '')
 				{
-					$oOutput->AddHtml('<label for="' . $this->oField->GetGlobalId() . '" class="control-label">')->AddHtml($this->oField->GetLabel(), true)->AddHtml('</label>');
+					$oOutput->AddHtml('<label for="' . $this->oField->GetGlobalId() . '" class="control-label" '.$sFieldDescriptionForHTMLTag.'>')->AddHtml($this->oField->GetLabel(), true)->AddHtml('</label>');
 				}
 				$oOutput->AddHtml('</div>');
 

sources/Renderer/Bootstrap/FieldRenderer/BsSetFieldRenderer.php
@@ -21,6 +21,7 @@
 namespace Combodo\iTop\Renderer\Bootstrap\FieldRenderer;
 
 use MetaModel;
+use utils;
 
 /**
  * Description of BsSetFieldRenderer
@@ -37,6 +38,7 @@ class BsSetFieldRenderer extends BsFieldRenderer
 	    $oOutput = parent::Render();
 
 		$sFieldMandatoryClass = ($this->oField->GetMandatory()) ? 'form_mandatory' : '';
+		$sFieldDescriptionForHTMLTag = ($this->oField->HasDescription()) ? 'data-tooltip-content="'.utils::HtmlEntities($this->oField->GetDescription()).'"' : '';
 		// Vars to build the table
 //		$sAttributesToDisplayAsJson = json_encode($this->oField->GetAttributesToDisplay());
 //		$sAttCodesToDisplayAsJson = json_encode($this->oField->GetAttributesToDisplay(true));
@@ -59,7 +61,7 @@ class BsSetFieldRenderer extends BsFieldRenderer
 			$oOutput->AddHtml('<div class="form_field_label">');
 			if ($this->oField->GetLabel() !== '')
 			{
-				$oOutput->AddHtml('<label for="' . $this->oField->GetGlobalId() . '" class="control-label">')
+				$oOutput->AddHtml('<label for="' . $this->oField->GetGlobalId() . '" class="control-label" '.$sFieldDescriptionForHTMLTag.'>')
 						->AddHtml($this->oField->GetLabel(), true)
 						->AddHtml('</label>');
 			}

commit b1e29cbd8198e258d26c95e858f30988a415d8ee

Fix call to Field::AddMetadata() to remove $sValue as null (regression introduced in cfd9dba6)

core/attributedef.class.inc.php
@@ -1053,7 +1053,7 @@ abstract class AttributeDefinition
 		// - Value raw
 		if ($this::IsScalar())
 		{
-			$oFormField->AddMetadata('value-raw', $oObject->Get($this->GetCode()));
+			$oFormField->AddMetadata('value-raw', (string) $oObject->Get($this->GetCode()));
 		}
 
 		return $oFormField;