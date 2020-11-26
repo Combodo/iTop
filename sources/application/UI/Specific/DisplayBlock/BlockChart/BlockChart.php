efbd249e7f4bf6eb430551ebe8dcfb6a057bd742
Eric <eric.espie@combodo.com>
2020-11-26 17:41:25 +0100

    N°3123 - Create specific block for DisplayBlock(type: Chart)

---
+++ sources/application/UI/Specific/DisplayBlock/BlockChart/BlockChart.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @copyright   Copyright (C) 2010-2020 Combodo SARL
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+namespace Combodo\iTop\Application\UI\Specific\DisplayBlock\BlockChart;
+
+
+use Combodo\iTop\Application\UI\tBlockParams;
+use Combodo\iTop\Application\UI\UIBlock;
+
+/**
+ * Class BlockChart
+ *
+ * @package Combodo\iTop\Application\UI\Specific\DisplayBlock\BlockChart
+ */
+class BlockChart extends UIBlock
+{
+	// Overloaded constants
+	public const BLOCK_CODE = 'ibo-blockchart';
+	public const HTML_TEMPLATE_REL_PATH = 'specific/displayblock/blockchart/layout';
+	public const JS_TEMPLATE_REL_PATH = 'specific/displayblock/blockchart/layout';
+
+	use tBlockParams;
+}
