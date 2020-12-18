2608c071d0c6c295de8123f46f05f072bf5e60fd
Eric <eric.espie@combodo.com>
2020-12-18 16:06:51 +0100

    N°3540 - Migrate printable version of an object (change some css classes)

--- application/cmdbabstract.class.inc.php
+++ application/cmdbabstract.class.inc.php
@@ -3200,7 +3200,7 @@ EOF
 			// - Fullscreen toggler for large fields
 			$sFullscreenTogglerTooltip = Dict::S('UI:ToggleFullScreen');
 			$sFullscreenTogglerHTML = (false === in_array($oAttDef->GetEditClass(), static::GetAttEditClassesToRenderAsLargeField())) ? '' : <<<HTML
-<a href="#" class="ibo-field--fullscreen-toggler no-print-preview" data-role="ibo-field--fullscreen-toggler" 
+<a href="#" class="ibo-field--fullscreen-toggler" data-role="ibo-field--fullscreen-toggler" 
 data-tooltip-content="{$sFullscreenTogglerTooltip}" data-fullscreen-toggler-target="$(this).closest('[data-role=\'ibo-field\']')"><span class="fas fa-fw fa-expand-arrows-alt"></span></a>
 HTML;
 
