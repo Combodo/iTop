91fd69e0bd937ac761edf9aadbb16fb3a4658a1a
Eric <eric.espie@combodo.com>
2021-02-10 10:53:01 +0100

    N°3563 - Migrate backoffice pages to new UIBlock system : Preferences (fix newsroom icon)

--- pages/preferences.php
+++ pages/preferences.php
@@ -242,7 +242,7 @@ JS
 		if ($iNewsroomDisplaySize < 1) $iNewsroomDisplaySize = 1;
 		if ($iNewsroomDisplaySize > 20) $iNewsroomDisplaySize = 20;
 		$sInput = '<input min="1" max="20" id="newsroom_display_size" type="number" size="2" name="newsroom_display_size" value="'.$iNewsroomDisplaySize.'">';
-		$sIcon = '<i id="newsroom_menu_icon" class="top-right-icon icon-additional-arrow fas fa-comment-dots" style="top: 0;"></i>';
+		$sIcon = '<i id="newsroom_menu_icon" class="top-right-icon icon-additional-arrow fas fa-bell" style="top: 0;"></i>';
 		$sNewsroomHtml .= Dict::Format('UI:Newsroom:DisplayAtMost_X_Messages', $sInput, $sIcon);
 		
 		/**
