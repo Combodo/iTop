68f7880d427f63017bbe60506364c433f646f83d
Eric <eric.espie@combodo.com>
2021-02-11 14:31:31 +0100

    Force theme compilation in development environment

--- setup/compiler.class.inc.php
+++ setup/compiler.class.inc.php
@@ -2912,6 +2912,9 @@ EOF;
 	 * @return string : file path of latest precompiled file to use for setup
 	 */
 	public function UseLatestPrecompiledFile(string $sTempTargetDir, string $sPrecompiledFileUri, $sPostCompilationLatestPrecompiledFile, $sThemeId) : ?string {
+		if (utils::IsDevelopmentEnvironment()) {
+			return null;
+		}
 		$bDataXmlPrecompiledFileExists = false;
 		clearstatcache();
 		if (!empty($sPrecompiledFileUri)){
