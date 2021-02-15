e2940150c37011bd77ca8c47ea9b09607115ef7a
odain <olivier.dain@combodo.com>
2021-02-15 17:23:58 +0100

    N°2982 - Speed up SCSS themes compilation during setup : create a php file dedicated to CompileCSSService/FindStylesheetObject

---
+++ application/compilecssservice.class.inc.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Copyright (C) 2013-2020 Combodo SARL
+ *
+ * This file is part of iTop.
+ *
+ * iTop is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * iTop is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ */
+
+/**
+ * Class CompileCSSService : used to ease testing ThemeHander class via mocks
+ *
+ * @author Olivier DAIN <olivier.dain@combodo.com>
+ * @since 3.0.0 N°2982
+ */
+class CompileCSSService
+{
+	/**
+	 * CompileCSSService constructor.
+	 */
+	public function __construct()
+	{
+	}
+
+	public function CompileCSSFromSASS($sSassContent, $aImportPaths =  [], $aVariables = []){
+		return utils::CompileCSSFromSASS($sSassContent, $aImportPaths, $aVariables);
+	}
+}
