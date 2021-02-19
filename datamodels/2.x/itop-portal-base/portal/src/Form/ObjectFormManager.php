71c5f47cd80938f4434b4800752c9b574143f119
Molkobain <lajarige.guillaume@free.fr>
2021-02-19 09:37:53 +0100

    PHPDoc

--- datamodels/2.x/itop-portal-base/portal/src/Form/ObjectFormManager.php
+++ datamodels/2.x/itop-portal-base/portal/src/Form/ObjectFormManager.php
@@ -1048,6 +1048,9 @@ class ObjectFormManager extends FormManager
 		$this->CancelAttachments();
 	}
 
+	/**
+	 * @inheritDoc
+	 */
 	public function CheckTransaction($aData)
 	{
 		$isTransactionValid = \utils::IsTransactionValid($this->oForm->GetTransactionId(), false); //The transaction token is kept in order to preserve BC with ajax forms (the second call would fail if the token is deleted). (The GC will take care of cleaning the token for us later on)
