925e45782c3197b1ed4c8f3da7e09cd2b598386e
Eric <eric.espie@combodo.com>
2021-02-26 14:05:26 +0100

    Fix indexation script and protection for concurrency with cron

--- datamodels/2.x/itop-core-update/index.php
+++ datamodels/2.x/itop-core-update/index.php
@@ -10,10 +10,12 @@ use Combodo\iTop\CoreUpdate\Controller\UpdateController;
 use ContextTag;
 
 require_once(APPROOT.'application/startup.inc.php');
-new ContextTag(ContextTag::TAG_SETUP);
+$oCtxCoreUpdate = new ContextTag(ContextTag::TAG_SETUP);
 
 $oUpdateController = new UpdateController(MODULESROOT.'itop-core-update/view', 'itop-core-update');
 $oUpdateController->DisableInDemoMode();
 $oUpdateController->AllowOnlyAdmin();
 $oUpdateController->SetDefaultOperation('SelectUpdateFile');
 $oUpdateController->HandleOperation();
+
+unset($oCtxCoreUpdate);
