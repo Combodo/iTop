6ff8101584be5237bd25dea0368d31043790b9f0
DudekArtur <80252112+DudekArtur@users.noreply.github.com>
2021-03-08 13:51:48 +0100

    Add files via upload

---
+++ datamodels/2.x/itop-config/dictionaries/pl.dict.itop-config.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright Copyright (C) 2010-2018 Combodo SARL
+ * @license    http://opensource.org/licenses/AGPL-3.0
+ *
+ * This file is part of iTop.
+ *
+ * iTop is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * iTop is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with iTop. If not, see <http://www.gnu.org/licenses/>
+ */
+
+Dict::Add('PL PL', 'Polish', 'Polski', array(
+
+	'Menu:ConfigEditor' => 'Konfiguracja ogólna',
+	'config-edit-title' => 'Edycja pliku konfiguracyjnego',
+	'config-edit-intro' => 'Zachowaj ostrożność podczas edycji pliku konfiguracyjnego.',
+	'config-apply' => 'Zastosuj',
+	'config-apply-title' => 'Zastosuj (Ctrl+S)',
+	'config-cancel' => 'Reset',
+	'config-saved' => 'Zapisano pomyślnie.',
+	'config-confirm-cancel' => 'Twoje zmiany zostaną utracone.',
+	'config-no-change' => 'Brak zmian: plik pozostał niezmieniony.',
+	'config-reverted' => 'Konfiguracja została przywrócona.',
+	'config-parse-error' => 'Linia %2$d: %1$s.<br/>Plik NIE został zaktualizowany.',
+	'config-current-line' => 'Edycja linii: %1$s',
+	'config-saved-warning-db-password' => 'Zapisano pomyślnie, ale kopia zapasowa nie działa z powodu nieobsługiwanych znaków w haśle bazy danych.',
+));
