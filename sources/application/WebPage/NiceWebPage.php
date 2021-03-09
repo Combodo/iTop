570b012000f52956cd0f55617eb4bdd203fd53d5
Pierre Goiffon <pierre.goiffon@combodo.com>
2021-03-09 11:15:49 +0100

    N°3750 fix body data-ready-scripts attribute not visible Was using .data() instead of .attr :( Was already changed in 8efc688f for other attributes, but I forgot this one :/

--- sources/application/WebPage/NiceWebPage.php
+++ sources/application/WebPage/NiceWebPage.php
@@ -239,14 +239,14 @@ EOF
 	protected function GetReadyScriptsStartedTrigger(): ?string
 	{
 		return <<<JS
-$("body").data("ready-scripts", "start");
+$("body").attr("data-ready-scripts", "start");
 JS;
 	}
 
 	protected function GetReadyScriptsFinishedTrigger(): ?string
 	{
 		return <<<JS
-$("body").data("ready-scripts", "done");
+$("body").attr("data-ready-scripts", "done");
 JS;
 	}
 }
