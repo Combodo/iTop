a27e8b67257e861e7d26970acfa41f5f08ebdc02
DudekArtur <80252112+DudekArtur@users.noreply.github.com>
2021-03-10 13:24:22 +0100

    Polish translation added

---
+++ datamodels/2.x/itop-welcome-itil/pl.dict.itop-welcome-itil.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright Copyright (C) 2010-2018 Combodo SARL
+ * @license    http://opensource.org/licenses/AGPL-3.0
+ *
+ * This file is part of iTop.
+ *
+ * iTop is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * iTop is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with iTop. If not, see <http://www.gnu.org/licenses/>
+ */
+
+Dict::Add('PL PL', 'Polish', 'Polski', array(
+	'Menu:DataSources' => 'Źródła danych synchronizacji',
+	'Menu:DataSources+' => 'Wszystkie źródła danych synchronizacji',
+	'Menu:WelcomeMenu' => 'Witaj',
+	'Menu:WelcomeMenu+' => 'Witaj w iTop',
+	'Menu:WelcomeMenuPage' => 'Witaj',
+	'Menu:WelcomeMenuPage+' => 'Witaj w iTop',
+	'Menu:AdminTools' => 'Administracja',
+	'Menu:AdminTools+' => 'Narzędzia administracyjne',
+	'Menu:AdminTools?' => 'Narzędzia dostępne tylko dla użytkowników posiadających profil administratora',
+	'Menu:DataModelMenu' => 'Model danych',
+	'Menu:DataModelMenu+' => 'Omówienie modelu danych',
+	'Menu:ExportMenu' => 'Eksport',
+	'Menu:ExportMenu+' => 'Eksportuj wyniki dowolnego zapytania w formacie HTML, CSV lub XML',
+	'Menu:NotificationsMenu' => 'Powiadomienia',
+	'Menu:NotificationsMenu+' => 'Konfiguracja powiadomień',
+	'Menu:AuditCategories' => 'Kategorie audytu',
+	'Menu:AuditCategories+' => 'Kategorie audytu',
+	'Menu:Notifications:Title' => 'Kategorie audytu',
+	'Menu:RunQueriesMenu' => 'Uruchom zapytania',
+	'Menu:RunQueriesMenu+' => 'Uruchom dowolne zapytanie',
+	'Menu:QueryMenu' => 'Słownik zapytań',
+	'Menu:QueryMenu+' => 'Słownik zapytań',
+	'Menu:UniversalSearchMenu' => 'Wyszukiwanie uniwersalne',
+	'Menu:UniversalSearchMenu+' => 'Szukaj wszędzie...',
+	'Menu:UserManagementMenu' => 'Zarządzanie użytkownikami',
+	'Menu:UserManagementMenu+' => 'Zarządzanie użytkownikami',
+	'Menu:ProfilesMenu' => 'Profile',
+	'Menu:ProfilesMenu+' => 'Profile',
+	'Menu:ProfilesMenu:Title' => 'Profile',
+	'Menu:UserAccountsMenu' => 'Konta użytkowników',
+	'Menu:UserAccountsMenu+' => 'Konta użytkowników',
+	'Menu:UserAccountsMenu:Title' => 'Konta użytkowników',
+	'Menu:MyShortcuts' => 'Moje skróty',
+	'Menu:UserManagement' => 'Zarządzanie użytkownikami',
+	'Menu:Queries' => 'Zapytania',
+	'Menu:ConfigurationTools' => 'Konfiguracja',
+));
+
+//
+// Class: AbstractResource
+//
+
+Dict::Add('PL PL', 'Polish', 'Polski', array(
+	'Class:AbstractResource' => 'Zasób abstrakcyjny',
+	'Class:AbstractResource+' => '',
+));
+
+//
+// Class: ResourceAdminMenu
+//
+
+Dict::Add('PL PL', 'Polish', 'Polski', array(
+	'Class:ResourceAdminMenu' => 'Zasób Menu administratora',
+	'Class:ResourceAdminMenu+' => '',
+));
+
+//
+// Class: ResourceRunQueriesMenu
+//
+
+Dict::Add('PL PL', 'Polish', 'Polski', array(
+	'Class:ResourceRunQueriesMenu' => 'Zasób Menu zapytań',
+	'Class:ResourceRunQueriesMenu+' => '',
+));
+
+//
+// Class: Action
+//
+
+Dict::Add('PL PL', 'Polish', 'Polski', array(
+	'Class:ResourceSystemMenu' => 'Zasób Menu systemowe',
+	'Class:ResourceSystemMenu+' => '',
+));
