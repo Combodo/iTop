Combodo/iTop

commit 370c08cd686c8b4d9c22f2be38ec886922b84a1e

N°3657 - Replace calls to deprecate jQuery methods (".click(callback)" to ".on('click', callback)")

setup/wizardsteps.class.inc.php
@@ -633,7 +633,7 @@ HTML
 
 			$oPage->add_ready_script(
 <<<EOF
-	$("#changes_summary .title").click(function() { $(this).parent().toggleClass('closed'); } );
+	$("#changes_summary .title").on('click', function() { $(this).parent().toggleClass('closed'); } );
 	$('input[name=upgrade_type]').bind('click change', function() { WizardUpdateButtons(); });
 EOF
 			);
@@ -726,7 +726,7 @@ EOF
 			$oPage->add('<li><b>'.$oLicense->product.'</b>, &copy; '.$oLicense->author.' is licensed under the <b>'.$oLicense->license_type.' license</b>. (<span class="toggle" id="toggle_'.$index.'">Details</span>)');
 			$oPage->add('<div id="license_'.$index.'" class="license_text" style="display:none;overflow:auto;max-height:10em;font-size:small;border:1px #696969 solid;margin-bottom:1em; margin-top:0.5em;padding:0.5em;">'.$oLicense->text.'</div>');
 			$oPage->add_ready_script('$(".license_text a").attr("target", "_blank").addClass("no-arrow");');
-			$oPage->add_ready_script('$("#toggle_'.$index.'").click( function() { $("#license_'.$index.'").toggle(); } );');
+			$oPage->add_ready_script('$("#toggle_'.$index.'").on('click', function() { $("#license_'.$index.'").toggle(); } );');
             $index++;
 		}
 		$oPage->add('</ul>');
@@ -1012,7 +1012,7 @@ class WizStepMiscParams extends WizardStep
 <<<EOF
 		$('#application_url').bind('change keyup', function() { WizardUpdateButtons(); } );
 		$('#graphviz_path').bind('change keyup init', function() { WizardUpdateButtons();  WizardAsyncAction('check_graphviz', { graphviz_path: $('#graphviz_path').val(), authent: $('#authent_token').val()}); } ).trigger('init');
-		$('#btn_next').click(function() {
+		$('#btn_next').on('click', function() {
 			bRet = true;
 			if ($(this).attr('data-graphviz') != 'ok')
 			{
@@ -1151,7 +1151,7 @@ class WizStepUpgradeMiscParams extends WizardStep
 <<<EOF
 		$('#application_url').bind('change keyup', function() { WizardUpdateButtons(); } );
 		$('#graphviz_path').bind('change keyup init', function() { WizardUpdateButtons();  WizardAsyncAction('check_graphviz', { graphviz_path: $('#graphviz_path').val(), authent: $('#authent_token').val() }); } ).trigger('init');
-		$('#btn_next').click(function() {
+		$('#btn_next').on('click', function() {
 			bRet = true;
 			if ($(this).attr('data-graphviz') != 'ok')
 			{
@@ -2318,7 +2318,7 @@ CSS
 		$oPage->add_ready_script(
 			<<<JS
 	$("#params_summary div").addClass('closed');
-	$("#params_summary .title").click(function() { $(this).parent().toggleClass('closed'); } );
+	$("#params_summary .title").on('click', function() { $(this).parent().toggleClass('closed'); } );
 	$("#btn_next").bind("click.install", function(event) {
 			$('#summary').hide();
 			$('#installation_progress').show();

application/cmdbabstract.class.inc.php
@@ -1936,7 +1936,7 @@ HTML
 oACWidget_{$iId} = new ExtKeyWidget('$iId', 'QueryOQL', 'SELECT QueryOQL WHERE is_template = \'yes\'', '$sSearchQueryLbl', true, null, null, true, true, 'oql');
 // noinspection JSAnnotator
 oACWidget_{$iId}.emptyHtml = "<div style=\"background: #fff; border:0; text-align:center; vertical-align:middle;\"><p>Use the search form above to search for objects to be added.</p></div>";
-$("#$sPredefinedBtnId").click(function () {
+$("#$sPredefinedBtnId").on('click', function () {
 	oACWidget_{$iId}.Search();
 });
 
@@ -1970,7 +1970,7 @@ JS
 						);
 						$oTestQueryButton->AddCSSClass('ibo-action-button');
 						$oPage->add_ready_script(<<<JS
-$("#$sTestResId").click(function () {
+$("#$sTestResId").on('click', function () {
 	var sQueryRaw = $("#$iId").val(),
 		sQueryEncoded = encodeURI(sQueryRaw);
 	window.open('$sBaseUrl' + sQueryEncoded, '_blank');
@@ -2713,7 +2713,7 @@ EOF
 
 		// Hook the cancel button via jQuery so that it can be unhooked easily as well if needed
 		$sDefaultUrl = utils::GetAbsoluteUrlAppRoot().'pages/UI.php?operation=cancel&'.$oAppContext->GetForLink();
-		$oPage->add_ready_script("$('#form_{$this->m_iFormId} button.cancel').click( function() { BackToDetails('$sClass', $iKey, '$sDefaultUrl', $sJSToken)} );");
+		$oPage->add_ready_script("$('#form_{$this->m_iFormId} button.cancel').on('click', function() { BackToDetails('$sClass', $iKey, '$sDefaultUrl', $sJSToken)} );");
 
 		$iFieldsCount = count($aFieldsMap);
 		$sJsonFieldsMap = json_encode($aFieldsMap);

application/dashboard.class.inc.php
@@ -1190,7 +1190,7 @@ EOF
 
 		$oPage->add_ready_script(
 <<<EOF
-	$('#select_layout input').click( function() {
+	$('#select_layout input').on('click', function() {
 		var sLayoutClass = $(this).val();
 		$('.itop-dashboard').runtimedashboard('option', {layout_class: sLayoutClass});
 	} );

core/attributedef.class.inc.php
@@ -12320,7 +12320,7 @@ class AttributeRedundancySettings extends AttributeDBField
 						$sEditValue = $bSelected ? $iCurrentValue : '';
 						$sValue = '<input class="redundancy-min-up-count" type="string" size="3" name="'.$sName.'" value="'.$sEditValue.'">';
 						// To fix an issue on Firefox: focus set to the option (because the input is within the label for the option)
-						$oPage->add_ready_script("\$('[name=\"$sName\"]').click(function(){var me=this; setTimeout(function(){\$(me).focus();}, 100);});");
+						$oPage->add_ready_script("\$('[name=\"$sName\"]').on('click', function(){var me=this; setTimeout(function(){\$(me).focus();}, 100);});");
 					}
 					else
 					{
@@ -12335,7 +12335,7 @@ class AttributeRedundancySettings extends AttributeDBField
 						$sEditValue = $bSelected ? $iCurrentValue : '';
 						$sValue = '<input class="redundancy-min-up-percent" type="string" size="3" name="'.$sName.'" value="'.$sEditValue.'">';
 						// To fix an issue on Firefox: focus set to the option (because the input is within the label for the option)
-						$oPage->add_ready_script("\$('[name=\"$sName\"]').click(function(){var me=this; setTimeout(function(){\$(me).focus();}, 100);});");
+						$oPage->add_ready_script("\$('[name=\"$sName\"]').on('click', function(){var me=this; setTimeout(function(){\$(me).focus();}, 100);});");
 					}
 					else
 					{

core/displayablegraph.class.inc.php
@@ -1464,7 +1464,7 @@ EOF
 		);
 		$oP->add_ready_script(
 <<<EOF
-	$("#dh_flash > .sf_title").click( function() {
+	$("#dh_flash > .sf_title").on('click', function() {
 		$("#dh_flash").toggleClass('closed');
 	});
     $('#ReloadMovieBtn').button().button('disable');

datamodels/2.x/itop-hub-connector/land.php
@@ -270,7 +270,7 @@ function DoInstall(WebPage $oPage)
     $sWidgetParams = json_encode($aWidgetParams);
     
     $oPage->add_ready_script("$('#hub_installation_widget').hub_installation($sWidgetParams);");
-    $oPage->add_ready_script("$('#hub_start_installation').click(function() { $('#hub_installation_widget').hub_installation('start_installation');} );");
+    $oPage->add_ready_script("$('#hub_start_installation').on('click', function() { $('#hub_installation_widget').hub_installation('start_installation');} );");
     $oPage->add_ready_script("$('#hub_installation_widget').hub_installation('check_before_backup');");
     $oPage->add('<div id="debug"></div>');
 }

datamodels/2.x/itop-portal-base/portal/templates/bricks/browse/mode_list.html.twig
@@ -351,7 +351,7 @@
 			});
 			
 			// Auto collapse item actions popup
-			$('body').click(function(){
+			$('body').on('click', function(){
 				$('table .item-action-wrapper.collapse.in').collapse('hide');
 			});
 		});

datamodels/2.x/itop-portal-base/portal/templates/bricks/browse/mode_mosaic.html.twig
@@ -400,7 +400,7 @@
 		
 		$(document).ready(function(){
 			// Auto collapse item actions popup
-			$('body').click(function(){
+			$('body').on('click', function(){
 				$('#brick_content_mosaic .item-action-wrapper.collapse.in').collapse('hide');
 			});
 			

commit 21321c0599537c125a020885f33bc2ebad9b135e

Fix regression introduced in 370c08cd

setup/wizardsteps.class.inc.php
@@ -726,7 +726,7 @@ EOF
 			$oPage->add('<li><b>'.$oLicense->product.'</b>, &copy; '.$oLicense->author.' is licensed under the <b>'.$oLicense->license_type.' license</b>. (<span class="toggle" id="toggle_'.$index.'">Details</span>)');
 			$oPage->add('<div id="license_'.$index.'" class="license_text" style="display:none;overflow:auto;max-height:10em;font-size:small;border:1px #696969 solid;margin-bottom:1em; margin-top:0.5em;padding:0.5em;">'.$oLicense->text.'</div>');
 			$oPage->add_ready_script('$(".license_text a").attr("target", "_blank").addClass("no-arrow");');
-			$oPage->add_ready_script('$("#toggle_'.$index.'").on('click', function() { $("#license_'.$index.'").toggle(); } );');
+			$oPage->add_ready_script('$("#toggle_'.$index.'").on("click", function() { $("#license_'.$index.'").toggle(); } );');
             $index++;
 		}
 		$oPage->add('</ul>');