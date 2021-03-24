b6b6d11abc063570589342a6ea3560c8294a6442
azana38 <61279140+azana38@users.noreply.github.com>
2021-03-24 08:40:03 +0100

    N°3852 - Translate new keys in FR

--- dictionaries/ui/layouts/fr.dictionary.itop.activity-panel.php
+++ dictionaries/ui/layouts/fr.dictionary.itop.activity-panel.php
@@ -22,37 +22,37 @@ Dict::Add('FR FR', 'French', 'Français', array(
 	'UI:Layout:ActivityPanel:SizeToggler:Expand:Tooltip' => 'Déplier',
 	'UI:Layout:ActivityPanel:SizeToggler:Reduce:Tooltip' => 'Replier',
 	'UI:Layout:ActivityPanel:DisplayToggler:Close:Tooltip' => 'Fermer',
-	'UI:Layout:ActivityPanel:LoadMoreEntries:Tooltip' => 'Load previous entries~~',
+	'UI:Layout:ActivityPanel:LoadMoreEntries:Tooltip' => 'Charger les entrées précédentes',
 
 	// Tabs
-	'UI:Layout:ActivityPanel:Tab:Activity:Title' => 'Activity~~',
-	'UI:Layout:ActivityPanel:Tab:Log:DraftIndicator:Tooltip' => 'Draft entry~~',
-	'UI:Layout:ActivityPanel:Tab:Toolbar:Filter:Logs:Title' => 'Logs~~',
-	'UI:Layout:ActivityPanel:Tab:Toolbar:Filter:Logs:Tooltip' => 'Show / hide logs entries on this tab~~',
-	'UI:Layout:ActivityPanel:Tab:Toolbar:Filter:Transitions:Title' => 'State changes~~',
-	'UI:Layout:ActivityPanel:Tab:Toolbar:Filter:Transitions:Tooltip' => 'Show / hide state changes on this tab~~',
-	'UI:Layout:ActivityPanel:Tab:Toolbar:Filter:Edits:Title' => 'Edits~~',
-	'UI:Layout:ActivityPanel:Tab:Toolbar:Filter:Edits:Tooltip' => 'Show / hide fields edits on this tab~~',
-	'UI:Layout:ActivityPanel:Tab:Toolbar:Action:OpenAll:Tooltip' => 'Open all messages~~',
-	'UI:Layout:ActivityPanel:Tab:Toolbar:Action:CloseAll:Tooltip' => 'Close all messages~~',
-	'UI:Layout:ActivityPanel:Tab:Toolbar:Info:AuthorsCount:Tooltip' => 'Number of persons interacting in the visible entries~~',
-	'UI:Layout:ActivityPanel:Tab:Toolbar:Info:MessagesCount:Tooltip' => 'Number of messages in the visible log(s)~~',
+	'UI:Layout:ActivityPanel:Tab:Activity:Title' => 'Activité',
+	'UI:Layout:ActivityPanel:Tab:Log:DraftIndicator:Tooltip' => 'Brouillon',
+	'UI:Layout:ActivityPanel:Tab:Toolbar:Filter:Logs:Title' => 'Journal',
+	'UI:Layout:ActivityPanel:Tab:Toolbar:Filter:Logs:Tooltip' => 'Afficher / masquer les entrées de journaux sur cet onglet',
+	'UI:Layout:ActivityPanel:Tab:Toolbar:Filter:Transitions:Title' => 'Changement de statut',
+	'UI:Layout:ActivityPanel:Tab:Toolbar:Filter:Transitions:Tooltip' => 'Afficher / masquer les changements de statut sur cet onglet',
+	'UI:Layout:ActivityPanel:Tab:Toolbar:Filter:Edits:Title' => 'Modifications',
+	'UI:Layout:ActivityPanel:Tab:Toolbar:Filter:Edits:Tooltip' => 'Afficher / masquer les modifications de statut sur cet onglet',
+	'UI:Layout:ActivityPanel:Tab:Toolbar:Action:OpenAll:Tooltip' => 'Ouvrir tous les messages',
+	'UI:Layout:ActivityPanel:Tab:Toolbar:Action:CloseAll:Tooltip' => 'Fermer tous les messages',
+	'UI:Layout:ActivityPanel:Tab:Toolbar:Info:AuthorsCount:Tooltip' => 'Nombre de personnes ayant contribué aux entrées affichées',
+	'UI:Layout:ActivityPanel:Tab:Toolbar:Info:MessagesCount:Tooltip' => 'Nombre de messages dans le(s) journal / journaux affichés',
 
 	// Compose button
-	'UI:Layout:ActivityPanel:ComposeButton:Tooltip' => 'Compose a new log entry~~',
+	'UI:Layout:ActivityPanel:ComposeButton:Tooltip' => 'Nouvelle entrée de journal',
 //	'UI:Layout:ActivityPanel:ComposeButton:Tooltip' => 'Nouvelle entrée de journal', // attention label du tooltip utilisé dans UI:Preferences:ActivityPanel:EntryFormOpened+ (dictionaries/ui/pages/fr.dictionary.itop.preferences.php)
 
 	// Case log entry
-	'UI:Layout:ActivityPanel:MultipleEntriesSaveConfirmation:Title' => 'Multiple logs save~~',
-	'UI:Layout:ActivityPanel:MultipleEntriesSaveConfirmation:Explanation' => 'By pressing the "save" button, you will submit entries for all the edited logs at once.~~',
+	'UI:Layout:ActivityPanel:MultipleEntriesSaveConfirmation:Title' => 'Enregistrement de plusieurs journaux',
+	'UI:Layout:ActivityPanel:MultipleEntriesSaveConfirmation:Explanation' => 'En cliquant sur le bouton "Envoyer", vous allez enregistrer en une seule fois toutes les entrées de journaux mises à jour.',
 
 	// Notification entry
 	'UI:Layout:ActivityPanel:NotificationEntry:MessageLink:Tooltip' => 'Click to open the notifications tab and get more information~~',
 
 	// Placeholder
-	'UI:Layout:ActivityPanel:NoEntry:Placeholder:Hint' => 'It\'s calm up here, no activity yet~~',
+	'UI:Layout:ActivityPanel:NoEntry:Placeholder:Hint' => 'Temps calme, il n''y a pas encore d''activité',
 
 	// Closed cover
-	'UI:Layout:ActivityPanel:ClosedCover:Title' => 'Activity panel~~',
-	'UI:Layout:ActivityPanel:ClosedCover:Tooltip' => 'Click to open the activity panel~~',
+	'UI:Layout:ActivityPanel:ClosedCover:Title' => 'Panneau d''activité',
+	'UI:Layout:ActivityPanel:ClosedCover:Tooltip' => 'Cliquer pour ouvrir le panneau d''activité',
 ));
