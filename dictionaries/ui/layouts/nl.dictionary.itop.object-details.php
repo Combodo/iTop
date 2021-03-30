9c139093603b22419e54c62a8aeeb7bbe6ea7c67
Thomas Casteleyn <thomas.casteleyn@super-visions.com>
2021-03-30 11:20:21 +0200

    Update UI layouts

--- dictionaries/ui/layouts/nl.dictionary.itop.object-details.php
+++ dictionaries/ui/layouts/nl.dictionary.itop.object-details.php
@@ -5,8 +5,8 @@
  */
 
 Dict::Add('NL NL', 'Dutch', 'Nederlands', [
-	'UI:Layout:ObjectDetails:KeyboardShortcut:EditObject' => 'Edit displayed object~~',
-	'UI:Layout:ObjectDetails:KeyboardShortcut:DeleteObject' => 'Delete displayed object~~',
+	'UI:Layout:ObjectDetails:KeyboardShortcut:EditObject' => 'Pas huidig object aan',
+	'UI:Layout:ObjectDetails:KeyboardShortcut:DeleteObject' => 'Verwijder huidig object',
 	'UI:Layout:ObjectDetails:KeyboardShortcut:NewObject' => 'Create a new object (with same class as displayed object)~~',
-	'UI:Layout:ObjectDetails:KeyboardShortcut:SaveObject' => 'Save displayed object~~',
+	'UI:Layout:ObjectDetails:KeyboardShortcut:SaveObject' => 'Bewaar huidig object',
 ]);
