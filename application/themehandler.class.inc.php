Combodo/iTop

commit e2f301827aa51ab61554f3406d30571814f21f88

N°3936 - Add protection to avoid crashes in certain conditions since d6695d3a

application/themehandler.class.inc.php
@@ -96,7 +96,8 @@ class ThemeHandler
 		$sThemeId = null;
 
 		try {
-			if (true === MetaModel::GetConfig()->Get('user_preferences.allow_backoffice_theme_override')) {
+			$oConf = MetaModel::GetConfig();
+			if ((!is_null($oConf)) && (true === $oConf->Get('user_preferences.allow_backoffice_theme_override'))) {
 				$sThemeId = appUserPreferences::GetPref('backoffice_theme', null);
 			}
 		}

commit c0f5906dcec0fc02a80047a216fd8a7dc5a37095

Fix crash when accessing a backoffice page in maintenance mode (regression from e2f30182)

application/themehandler.class.inc.php
@@ -73,10 +73,7 @@ class ThemeHandler
 	public static function GetApplicationThemeId(): string
 	{
 		try {
-			if (is_null(MetaModel::GetConfig())) {
-				throw new CoreException('no config');
-			}
-			$sThemeId = MetaModel::GetConfig()->Get('backoffice_default_theme');
+			$sThemeId = utils::GetConfig()->Get('backoffice_default_theme');
 		}
 		catch (CoreException $oCompileException) {
 			// Fallback on our default theme in case the config. is not available yet
@@ -96,8 +93,7 @@ class ThemeHandler
 		$sThemeId = null;
 
 		try {
-			$oConf = MetaModel::GetConfig();
-			if ((!is_null($oConf)) && (true === $oConf->Get('user_preferences.allow_backoffice_theme_override'))) {
+			if (true === utils::GetConfig()->Get('user_preferences.allow_backoffice_theme_override')) {
 				$sThemeId = appUserPreferences::GetPref('backoffice_theme', null);
 			}
 		}
@@ -188,7 +184,8 @@ class ThemeHandler
 	{
 		try {
 			// Try to compile theme defined in the configuration
-			$sThemeId = static::GetCurrentUserThemeId();
+			// Note: In maintenance mode we should stick to the app theme (also we don't have access to many PHP classes, including the user preferences)
+			$sThemeId = SetupUtils::IsInMaintenanceMode() ? static::GetApplicationThemeId() : static::GetCurrentUserThemeId();
 			static::CompileTheme($sThemeId);
 		}
 		catch (CoreException $oCompileException) {