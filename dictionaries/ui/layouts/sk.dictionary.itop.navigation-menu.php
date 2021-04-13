f3cf1549695e06789e74235da74fe0dc316d1bf5
Molkobain <lajarige.guillaume@free.fr>
2021-04-13 11:28:00 +0200

    N°3899 - Navigation menu: Filter hot key text now corresponds to the one defined in the user preferences

--- dictionaries/ui/layouts/sk.dictionary.itop.navigation-menu.php
+++ dictionaries/ui/layouts/sk.dictionary.itop.navigation-menu.php
@@ -24,7 +24,6 @@ Dict::Add('SK SK', 'Slovak', 'Slovenčina', array(
 	'UI:Layout:NavigationMenu:Toggler:TooltipWithSiloLabel' => 'Expand / Collapse (Filtered on %1$s)~~',
 	'UI:Layout:NavigationMenu:MenuFilter:Input:Placeholder' => 'Filter...~~',
 	'UI:Layout:NavigationMenu:MenuFilter:Input:Tooltip' => 'Type your keywords to filter menus~~',
-	'UI:Layout:NavigationMenu:MenuFilter:Input:Hotkey' => 'Alt + M',
 	'UI:Layout:NavigationMenu:MenuFilter:Input:Hint' => 'Matches from all menu groups will be displayed~~',
 	'UI:Layout:NavigationMenu:MenuFilter:Placeholder:Hint' => 'No result for this menu filter~~',
 	'UI:Layout:NavigationMenu:UserInfo:WelcomeMessage:Text' => 'Hi %1$s!~~',
