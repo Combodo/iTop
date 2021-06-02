7168860a0b47d506ed7592b96922985463a7dc0c
Purple Grape <purplegrape4@gmail.com>
2021-06-02 14:06:16 +0800

    :globe_with_meridians: Improved chinese for 2.7 (#218)

--- datamodels/2.x/itop-attachments/zh_cn.dict.itop-attachments.php
+++ datamodels/2.x/itop-attachments/zh_cn.dict.itop-attachments.php
@@ -30,10 +30,10 @@ Dict::Add('ZH CN', 'Chinese', '简体中文', array(
 	'Attachment:Max_Ko' => '(最大文件尺寸: %1$s KB)',
 	'Attachments:NoAttachment' => '没有附件. ',
 	'Attachments:PreviewNotAvailable' => '该附件类型不支持预览.',
-	'Attachments:Error:FileTooLarge' => '上传的文件过大. %1$s~~',
-	'Attachments:Error:UploadedFileEmpty' => 'The received file is empty and cannot be attached.
-Either you have pushed an empty file,
-or ask your iTop administrator if the iTop server disk is full.~~',
+	'Attachments:Error:FileTooLarge' => '上传的文件过大. %1$s',
+	'Attachments:Error:UploadedFileEmpty' => '收到的文件为空,无法添加.
+可能是因为您上传了一个空文件,,
+或者咨询iTop 管理员确认iTop 服务器磁盘空间是否已满.',
 	'Attachments:Render:Icons' => '显示为图标',
 	'Attachments:Render:Table' => '显示为列表',
 ));
@@ -47,11 +47,11 @@ Dict::Add('ZH CN', 'Chinese', '简体中文', array(
 	'Class:Attachment+' => '',
 	'Class:Attachment/Attribute:expire' => '过期',
 	'Class:Attachment/Attribute:expire+' => '~~',
-	'Class:Attachment/Attribute:temp_id' => '临时 id',
+	'Class:Attachment/Attribute:temp_id' => '临时id',
 	'Class:Attachment/Attribute:temp_id+' => '~~',
 	'Class:Attachment/Attribute:item_class' => 'Item class~~',
 	'Class:Attachment/Attribute:item_class+' => '~~',
-	'Class:Attachment/Attribute:item_id' => 'Item~~',
+	'Class:Attachment/Attribute:item_id' => '项目',
 	'Class:Attachment/Attribute:item_id+' => '~~',
 	'Class:Attachment/Attribute:item_org_id' => 'Item organization~~',
 	'Class:Attachment/Attribute:item_org_id+' => '',
