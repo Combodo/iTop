215dcfb46532bbf4cece36a224a6ce10f25b9657
Molkobain <lajarige.guillaume@free.fr>
2021-06-12 18:47:32 +0200

    Dictionaries: Group files by block for an easier navigation through them

--- dictionaries/ui/layouts/tab-container/de.dictionary.itop.tab-container.php
+++ dictionaries/ui/layouts/tab-container/de.dictionary.itop.tab-container.php
@@ -1,10 +1,10 @@
 <?php
-/**
+/*
  * @copyright   Copyright (C) 2010-2021 Combodo SARL
  * @license     http://opensource.org/licenses/AGPL-3.0
  */
 
-Dict::Add('RU RU', 'Russian', 'Русский', [
+Dict::Add('DE DE', 'German', 'Deutsch', [
 	'UI:Layout:TabContainer:ExtraTabsListToggler:Label' => 'Other tabs~~',
 	'UIBlock:Error:AddBlockNotTabForbidden' => 'Cannot add block %1$s to %2$s (only Tab blocks are allowed)~~',
 	'UIBlock:TabContainer:RemoteTabLoad' => 'Click to load this tab~~',
