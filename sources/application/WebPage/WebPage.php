Combodo/iTop

commit 9048d09bf60ea6c978c96eefc23c1cb750fc8316

Fix JS dictionaries not being load in \WebPage

sources/application/WebPage/WebPage.php
@@ -1087,6 +1087,11 @@ class WebPage implements Page
 			header($sHeader);
 		}
 
+		// Compute and add dict entries for JS
+		if ($this->bAddJSDict) {
+			$this->output_dict_entries();
+		}
+
 		$s_captured_output = $this->ob_get_clean_safe();
 
 		$aData = [];
@@ -1129,16 +1134,6 @@ class WebPage implements Page
 			$aData['aPage']['sFaviconUrl'] = $this->GetFaviconAbsoluteUrl();
 		}
 
-		// Dict entries for JS
-		//		if ($this->bAddJSDict) {
-		//			$this->output_dict_entries();
-		//		}
-
-
-		//		if (trim($s_captured_output) != "") {
-		//			echo "<div class=\"raw_output\">".utils::FilterXSS($s_captured_output)."</div>\n";
-		//		}
-
 		$oTwigEnv = TwigHelper::GetTwigEnvironment(BlockRenderer::TWIG_BASE_PATH, BlockRenderer::TWIG_ADDITIONAL_PATHS);
 		// Render final TWIG into global HTML
 		$oKpi = new ExecutionKPI();
@@ -1149,7 +1144,6 @@ class WebPage implements Page
 		$oKpi = new ExecutionKPI();
 		echo $sHtml;
 		$oKpi->ComputeAndReport('Echoing ('.round(strlen($sHtml) / 1024).' Kb)');
-
 
 		if (class_exists('DBSearch')) {
 			DBSearch::RecordQueryTrace();

commit 5d1d6d07a6bd58ca8b5bc1fb3737668133581fce

Fix regression introduced in 9048d09bf

sources/application/WebPage/WebPage.php
@@ -710,7 +710,7 @@ var aDictEntries = {$sEntriesAsJson};
 
 // Check if Dict._entries already exists in order to complete, this is for async calls only.
 // Note: We should not overload the WebPage::get_dict_file_content() in AjaxPage to put the part below as the same dict file can be consumed either by a regular page or an async page.
-if (typeof Dict._entries != "undefined") {
+if ((typeof Dict != "undefined") && (typeof Dict._entries != "undefined")) {
 	$.extend(Dict._entries, aDictEntries);
 }
 JS;
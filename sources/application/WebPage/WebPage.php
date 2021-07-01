5d1d6d07a6bd58ca8b5bc1fb3737668133581fce
Molkobain <lajarige.guillaume@free.fr>
2021-07-01 10:55:34 +0200

    Fix regression introduced in 9048d09bf

--- sources/application/WebPage/WebPage.php
+++ sources/application/WebPage/WebPage.php
@@ -710,7 +710,7 @@ var aDictEntries = {$sEntriesAsJson};
 
 // Check if Dict._entries already exists in order to complete, this is for async calls only.
 // Note: We should not overload the WebPage::get_dict_file_content() in AjaxPage to put the part below as the same dict file can be consumed either by a regular page or an async page.
-if (typeof Dict._entries != "undefined") {
+if ((typeof Dict != "undefined") && (typeof Dict._entries != "undefined")) {
 	$.extend(Dict._entries, aDictEntries);
 }
 JS;
