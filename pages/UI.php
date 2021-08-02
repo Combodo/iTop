Combodo/iTop

commit 344cce9fdde45788b61e1b58b48540975f7c1bb0

N°4076 - Allow block parameters to change the behaviour of blocks on the page

pages/UI.php
@@ -547,31 +547,24 @@ try
 				// Save search to history
 				// - Prepare icon
 				$sQueryIconUrl = null;
-				if(!empty($sClassName))
-				{
+				if(!empty($sClassName)) {
 					$sQueryIconUrl = MetaModel::GetClassIcon($sClassName, false);
 				}
 				// - Prepare label
 				$sQueryLabel = null;
-				if($sQuery !== $sFullText)
-				{
+				if ($sQuery !== $sFullText) {
 					$sQueryLabel = $sFullText;
 				}
 				GlobalSearchHelper::AddQueryToHistory($sQuery, $sQueryIconUrl, $sQueryLabel);
-				$oP->GetTopBarLayout()
-					->GetGlobalSearch()
-					->SetQuery($sQuery)
-					->SetLastQueries(GlobalSearchHelper::GetLastQueries());
+				$oP->AddBlockParam('global_search.query', $sQuery);
 
 				// Check the needle length
 				$iMinLenth = MetaModel::GetConfig()->Get('full_text_needle_min');
-				foreach ($aFullTextNeedles as $sNeedle)
-				{
-					if (strlen($sNeedle) < $iMinLenth)
-					{
+				foreach ($aFullTextNeedles as $sNeedle) {
+					if (strlen($sNeedle) < $iMinLenth) {
 						$oP->p(Dict::Format('UI:Search:NeedleTooShort', $sNeedle, $iMinLenth));
 						$key = array_search($sNeedle, $aFullTextNeedles);
-						if($key!== false)
+						if ($key !== false)
 						{
 							unset($aFullTextNeedles[$key]);
 						}

templates/base/components/global-search/layout.html.twig
@@ -1,7 +1,7 @@
 <div id="{{ oUIBlock.GetId() }}"
 	 {# Note: The "ibo-is-opened" class is put there and not through the JS widget to avoid visual glitches #}
 	 {# Otherwise it would open only when the DOM is ready and the JS widget instantiated #}
-     class="ibo-global-search {% if oUIBlock.HasQuery() %}ibo-is-opened{% endif %}"
+     class="ibo-global-search"
      data-role="ibo-global-search">
 	<form action="{{ oUIBlock.GetEndpoint() }}" method="get" class="ibo-global-search--head" data-role="ibo-global-search--head">
 		<a href="#" class="ibo-global-search--icon" data-role="ibo-global-search--icon"

templates/base/components/global-search/layout.js.twig
@@ -1,3 +1,2 @@
 $('#{{ oUIBlock.GetId() }}').global_search({
-	init_opened: {{ oUIBlock.HasQuery()|var_export }}
 });

sources/application/TwigBase/Controller/Controller.php
@@ -65,6 +65,7 @@ abstract class Controller
 	private $m_aLinkedStylesheets;
 	private $m_aSaas;
 	private $m_aAjaxTabs;
+	private $m_aBlockParams;
 	/** @var string */
 	private $m_sAccessTokenConfigParamId = null;
 
@@ -76,21 +77,19 @@ abstract class Controller
 	 */
 	public function __construct($sViewPath, $sModuleName = 'core')
 	{
-		$this->m_aLinkedScripts = array();
-		$this->m_aLinkedStylesheets = array();
-		$this->m_aSaas = array();
-		$this->m_aAjaxTabs = array();
-		$this->m_aDefaultParams = array();
+		$this->m_aLinkedScripts = [];
+		$this->m_aLinkedStylesheets = [];
+		$this->m_aSaas = [];
+		$this->m_aAjaxTabs = [];
+		$this->m_aDefaultParams = [];
+		$this->m_aBlockParams = [];
 		$this->SetViewPath($sViewPath);
 		$this->SetModuleName($sModuleName);
-		if ($sModuleName != 'core')
-		{
-			try
-			{
-				$this->m_aDefaultParams = array('sIndexURL' => utils::GetAbsoluteUrlModulePage($this->m_sModule, 'index.php'));
+		if ($sModuleName != 'core') {
+			try {
+				$this->m_aDefaultParams = ['sIndexURL' => utils::GetAbsoluteUrlModulePage($this->m_sModule, 'index.php')];
 			}
-			catch (Exception $e)
-			{
+			catch (Exception $e) {
 				IssueLog::Error($e->getMessage());
 			}
 		}
@@ -392,6 +391,9 @@ abstract class Controller
 		foreach ($this->m_aSaas as $sSaasRelPath) {
 			$this->AddSaasToPage($sSaasRelPath);
 		}
+		foreach ($this->m_aBlockParams as $sKey => $value) {
+			$this->SetBlockParamToPage($sKey, $value);
+		}
 		$this->OutputPage();
 	}
 
@@ -550,11 +552,18 @@ abstract class Controller
 	 */
 	public function AddAjaxTab($sCode, $sURL, $bCache = true, $sLabel = null)
 	{
-		if (is_null($sLabel))
-		{
+		if (is_null($sLabel)) {
 			$sLabel = Dict::S($sCode);
 		}
 		$this->m_aAjaxTabs[$sCode] = array('label' => $sLabel, 'url' => $sURL, 'cache' => $bCache);
+	}
+
+	/**
+	 * @param array $aBlockParams
+	 */
+	public function AddBlockParams(array $aBlockParams)
+	{
+		$this->m_aBlockParams = $aBlockParams;
 	}
 
 	/**
@@ -595,7 +604,7 @@ abstract class Controller
 		switch ($sPageType)
 		{
 			case self::ENUM_PAGE_TYPE_HTML:
-				$this->m_oPage = new iTopWebPage($this->GetOperationTitle());
+				$this->m_oPage = new iTopWebPage($this->GetOperationTitle(), false);
 				$this->m_oPage->add_xframe_options();
 				break;
 
@@ -677,6 +686,11 @@ abstract class Controller
 	private function AddAjaxTabToPage($sCode, $sTitle, $sURL, $bCache)
 	{
 		$this->m_oPage->AddAjaxTab($sCode, $sURL, $bCache, $sTitle);
+	}
+
+	public function SetBlockParamToPage(string $sKey, $value)
+	{
+		$this->m_oPage->SetBlockParam($sKey, $value);
 	}
 
 	/**

sources/application/UI/Base/Component/GlobalSearch/GlobalSearch.php
@@ -70,8 +70,8 @@ class GlobalSearch extends UIBlock implements iKeyboardShortcut
 		$this->SetEndpoint(static::DEFAULT_ENDPOINT_REL_URL);
 		$this->SetQuery('');
 		$this->SetLastQueries($aLastQueries);
-		$this->bShowHistory = (bool) MetaModel::GetConfig()->Get('global_search.show_history');
-		$this->iMaxHistoryResults = (int) MetaModel::GetConfig()->Get('global_search.max_history_results');
+		$this->bShowHistory = (bool)MetaModel::GetConfig()->Get('global_search.show_history');
+		$this->iMaxHistoryResults = (int)MetaModel::GetConfig()->Get('global_search.max_history_results');
 	}
 
 	/**

sources/application/UI/Base/Layout/TopBar/TopBarFactory.php
@@ -41,6 +41,7 @@ class TopBarFactory
 	 * @param array|null $aBreadcrumbsEntry Current breadcrumbs entry to add
 	 *
 	 * @return \Combodo\iTop\Application\UI\Base\Layout\TopBar\TopBar
+	 * @throws \ConfigException
 	 * @throws \CoreException
 	 * @throws \CoreUnexpectedValue
 	 * @throws \MySQLException
@@ -49,18 +50,15 @@ class TopBarFactory
 	{
 		$oTopBar = new TopBar(TopBar::BLOCK_CODE);
 
-		if (utils::GetConfig()->Get('quick_create.enabled') === true)
-		{
+		if (utils::GetConfig()->Get('quick_create.enabled') === true) {
 			$oTopBar->SetQuickCreate(QuickCreateFactory::MakeFromUserHistory());
 		}
 
-		if (utils::GetConfig()->Get('global_search.enabled') === true)
-		{
+		if (utils::GetConfig()->Get('global_search.enabled') === true) {
 			$oTopBar->SetGlobalSearch(GlobalSearchFactory::MakeFromUserHistory());
 		}
 
-		if(utils::GetConfig()->Get('breadcrumb.enabled') === true)
-		{
+		if (utils::GetConfig()->Get('breadcrumb.enabled') === true) {
 			$oBreadcrumbs = new Breadcrumbs($aBreadcrumbsEntry, Breadcrumbs::BLOCK_CODE);
 
 			$oTopBar->SetBreadcrumbs($oBreadcrumbs);

sources/application/WebPage/AjaxPage.php
@@ -177,24 +177,26 @@ class AjaxPage extends WebPage implements iTabbedPage
 
 		$aData['aPage'] = [
 			'sAbsoluteUrlAppRoot' => addslashes(utils::GetAbsoluteUrlAppRoot()),
-			'sTitle' => $this->s_title,
-			'aMetadata' => [
+			'sTitle'              => $this->s_title,
+			'aMetadata'           => [
 				'sCharset' => static::PAGES_CHARSET,
 				'sLang' => $this->GetLanguageForMetadata(),
 			],
-			'aCssFiles' => $this->a_linked_stylesheets,
-			'aCssInline' => $this->a_styles,
-			'aJsFiles' => $this->a_linked_scripts,
-			'aJsInlineLive' => $this->a_scripts,
+			'aCssFiles'           => $this->a_linked_stylesheets,
+			'aCssInline'          => $this->a_styles,
+			'aJsFiles'            => $this->a_linked_scripts,
+			'aJsInlineLive'       => $this->a_scripts,
 			'aJsInlineOnDomReady' => $this->GetReadyScripts(),
-			'aJsInlineOnInit' => $this->a_init_scripts,
-			'bEscapeContent' => ($this->sContentType == 'text/html') && ($this->sContentDisposition == 'inline'),
+			'aJsInlineOnInit'     => $this->a_init_scripts,
+			'bEscapeContent'      => ($this->sContentType == 'text/html') && ($this->sContentDisposition == 'inline'),
 			// TODO 3.0.0: TEMP, used while developping, remove it.
-			'sSanitizedContent' => utils::FilterXSS($this->s_content),
-			'sDeferredContent' => utils::FilterXSS(addslashes(str_replace("\n", '', $this->s_deferred_content))),
-			'sCapturedOutput' => utils::FilterXSS($s_captured_output),
-			'sPromiseId' => $this->sPromiseId
+			'sSanitizedContent'   => utils::FilterXSS($this->s_content),
+			'sDeferredContent'    => utils::FilterXSS(addslashes(str_replace("\n", '', $this->s_deferred_content))),
+			'sCapturedOutput'     => utils::FilterXSS($s_captured_output),
+			'sPromiseId'          => $this->sPromiseId,
 		];
+
+		$aData['aBlockParams'] = $this->GetBlockParams();
 
 		$oTwigEnv = TwigHelper::GetTwigEnvironment(BlockRenderer::TWIG_BASE_PATH, BlockRenderer::TWIG_ADDITIONAL_PATHS);
 		// Render final TWIG into global HTML

sources/application/WebPage/NiceWebPage.php
@@ -30,7 +30,7 @@ class NiceWebPage extends WebPage
 		$this->m_sRootUrl = $this->GetAbsoluteUrlAppRoot();
 		parent::__construct($s_title, $bPrintable);
 
-	    $this->LoadTheme();
+		$this->LoadTheme();
 	}
 
 	/**

commit e28f704f3e1d5951f87f307bcc20748afea1f5c1

N°4076 - Revert regression in global search from 344cce9fd
Current query was not visible when reaching the results page

pages/UI.php
@@ -556,7 +556,7 @@ try
 					$sQueryLabel = $sFullText;
 				}
 				GlobalSearchHelper::AddQueryToHistory($sQuery, $sQueryIconUrl, $sQueryLabel);
-				$oP->AddBlockParam('global_search.query', $sQuery);
+				$oP->SetBlockParam('ibo-global-search.sQuery', $sQuery);
 
 				// Check the needle length
 				$iMinLenth = MetaModel::GetConfig()->Get('full_text_needle_min');

templates/base/components/global-search/layout.html.twig
@@ -1,7 +1,7 @@
 <div id="{{ oUIBlock.GetId() }}"
 	 {# Note: The "ibo-is-opened" class is put there and not through the JS widget to avoid visual glitches #}
 	 {# Otherwise it would open only when the DOM is ready and the JS widget instantiated #}
-     class="ibo-global-search"
+     class="ibo-global-search {% if oUIBlock.HasQuery() %}ibo-is-opened{% endif %}"
      data-role="ibo-global-search">
 	<form action="{{ oUIBlock.GetEndpoint() }}" method="get" class="ibo-global-search--head" data-role="ibo-global-search--head">
 		<a href="#" class="ibo-global-search--icon" data-role="ibo-global-search--icon"

templates/base/components/global-search/layout.js.twig
@@ -1,2 +1,3 @@
 $('#{{ oUIBlock.GetId() }}').global_search({
+	init_opened: {{ oUIBlock.HasQuery()|var_export }}
 });