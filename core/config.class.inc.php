70b07721e6a82704831f182c383b77513ac8f5a1
Molkobain <lajarige.guillaume@free.fr>
2021-08-09 10:46:42 +0200

    N°2875 - Fix typo in config example

--- core/config.class.inc.php
+++ core/config.class.inc.php
@@ -1287,7 +1287,7 @@ class Config
 		],
 		'mentions.allowed_classes' => [
 			'type' => 'array',
-			'description' => 'Classes which can be mentioned through the autocomplete in the caselogs. Key of the array must be a single character that will trigger the autocomplete, value can be either a DM class or a valid OQL (eg. "@" => "Person", "?" => "SELECT FAQ WHERE status = \'published\')',
+			'description' => 'Classes which can be mentioned through the autocomplete in the caselogs. Key of the array must be a single character that will trigger the autocomplete, value can be either a DM class or a valid OQL (eg. "@" => "Person", "?" => "SELECT FAQ WHERE status = \'published\'")',
 			'default' => [
 				'@' => 'SELECT Person WHERE status = \'active\'',
 			],
