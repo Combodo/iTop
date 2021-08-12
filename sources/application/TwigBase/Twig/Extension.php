Combodo/iTop

commit 7af10e519774d78b02a181bbe22ab23614feda81

N°3918 - Fix wiki syntax "[[CLASS:ID]]" not working in logs

sources/application/TwigBase/Twig/Extension.php
@@ -8,6 +8,7 @@ namespace Combodo\iTop\Application\TwigBase\Twig;
 
 
 use AttributeDateTime;
+use AttributeText;
 use Combodo\iTop\Application\UI\Base\iUIBlock;
 use Combodo\iTop\Renderer\BlockRenderer;
 use Dict;
@@ -95,6 +96,17 @@ class Extension
 		 */
 		$oTwigEnv->addFilter(new Twig_SimpleFilter('sanitize', function (string $sString, string $sFilter) {
 				return utils::Sanitize($sString, '', $sFilter);
+			})
+		);
+
+		/**
+		 * Filter to transform the wiki syntax into HTML
+		 *
+		 * @uses \AttributeText::RenderWikiHtml()
+		 * @since 3.0.0
+		 */
+		$oTwigEnv->addFilter(new Twig_SimpleFilter('render_wiki_to_html', function ($sString) {
+				return AttributeText::RenderWikiHtml($sString);
 			})
 		);
 

templates/base/layouts/activity-panel/activity-entry/layout.html.twig
@@ -28,7 +28,7 @@
 				<div class="ibo-activity-entry--main-information-content" data-role="ibo-activity-entry--main-information-content">
 					{% block iboActivityEntryMainInformationContent %}
 						{# Content is printed as raw because it is stored as HTML in the database and should have been sanitized before storage, so we can assume it is safe #}
-						{{ oUIBlock.GetContent()|raw }}
+						{{ oUIBlock.GetContent()|render_wiki_to_html|raw }}
 					{% endblock %}
 				</div>
 			</div>

commit 0c668829904e6a8600830cef776e9e66099383ad

N°3918 - Fix regression on mentions display introduced by 7af10e51

sources/application/TwigBase/Twig/Extension.php
@@ -100,13 +100,13 @@ class Extension
 		);
 
 		/**
-		 * Filter to transform the wiki syntax into HTML
+		 * Filter to transform the wiki syntax ONLY into HTML.
 		 *
 		 * @uses \AttributeText::RenderWikiHtml()
 		 * @since 3.0.0
 		 */
 		$oTwigEnv->addFilter(new Twig_SimpleFilter('render_wiki_to_html', function ($sString) {
-				return AttributeText::RenderWikiHtml($sString);
+				return AttributeText::RenderWikiHtml($sString, true /* Important, otherwise hyperlinks will be tranformed as well */);
 			})
 		);
 