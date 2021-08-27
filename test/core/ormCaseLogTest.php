cf556de76e1f0b10d34dacf5b1c3f59e84cee796
Molkobain <lajarige.guillaume@free.fr>
2021-08-27 15:57:23 +0200

    Add method to get number of entries of an ormCaseLog

---
+++ test/core/ormCaseLogTest.php
@@ -0,0 +1,40 @@
+<?php
+/*
+ * @copyright   Copyright (C) 2010-2021 Combodo SARL
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+
+namespace Combodo\iTop\Test\UnitTest\Core;
+
+use Combodo\iTop\Test\UnitTest\ItopDataTestCase;
+use ormCaseLog;
+
+
+/**
+ * Tests of the ormCaseLog class
+ *
+ * @covers \ormCaseLog
+ * @runTestsInSeparateProcesses
+ * @preserveGlobalState disabled
+ * @backupGlobals disabled
+ */
+class ormCaseLogTest extends ItopDataTestCase
+{
+	/**
+	 * @covers \ormCaseLog::GetEntryCount()
+	 * @throws \ArchivedObjectException
+	 * @throws \CoreException
+	 * @throws \OQLException
+	 */
+	public function testGetEntryCount()
+	{
+		// New log, with no entry
+		$oLog = new ormCaseLog();
+		$this->assertEquals($oLog->GetEntryCount(), 0, 'Should be no entry yet, returned '.$oLog->GetEntryCount());
+
+		// Add an entry
+		$oLog->AddLogEntry('First entry');
+		$this->assertEquals($oLog->GetEntryCount(), 1, 'Should be 1 entry, returned '.$oLog->GetEntryCount());
+	}
+}
