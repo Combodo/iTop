Combodo/iTop

commit 7c7386afc71a9191f5fdd66888fe3ec8248ceaf0

N°3851 Update Emogrifier to a version supporting iTop PHP versions range

lib/composer/ClassLoader.php
@@ -42,6 +42,8 @@ namespace Composer\Autoload;
  */
 class ClassLoader
 {
+    private $vendorDir;
+
     // PSR-4
     private $prefixLengthsPsr4 = array();
     private $prefixDirsPsr4 = array();
@@ -56,6 +58,13 @@ class ClassLoader
     private $classMapAuthoritative = false;
     private $missingClasses = array();
     private $apcuPrefix;
+
+    private static $registeredLoaders = array();
+
+    public function __construct($vendorDir = null)
+    {
+        $this->vendorDir = $vendorDir;
+    }
 
     public function getPrefixes()
     {
@@ -300,6 +309,17 @@ class ClassLoader
     public function register($prepend = false)
     {
         spl_autoload_register(array($this, 'loadClass'), true, $prepend);
+
+        if (null === $this->vendorDir) {
+            return;
+        }
+
+        if ($prepend) {
+            self::$registeredLoaders = array($this->vendorDir => $this) + self::$registeredLoaders;
+        } else {
+            unset(self::$registeredLoaders[$this->vendorDir]);
+            self::$registeredLoaders[$this->vendorDir] = $this;
+        }
     }
 
     /**
@@ -308,13 +328,17 @@ class ClassLoader
     public function unregister()
     {
         spl_autoload_unregister(array($this, 'loadClass'));
+
+        if (null !== $this->vendorDir) {
+            unset(self::$registeredLoaders[$this->vendorDir]);
+        }
     }
 
     /**
      * Loads the given class or interface.
      *
      * @param  string    $class The name of the class
-     * @return bool|null True if loaded, null otherwise
+     * @return true|null True if loaded, null otherwise
      */
     public function loadClass($class)
     {
@@ -323,6 +347,8 @@ class ClassLoader
 
             return true;
         }
+
+        return null;
     }
 
     /**
@@ -365,6 +391,16 @@ class ClassLoader
         }
 
         return $file;
+    }
+
+    /**
+     * Returns the currently registered loaders indexed by their corresponding vendor directories.
+     *
+     * @return self[]
+     */
+    public static function getRegisteredLoaders()
+    {
+        return self::$registeredLoaders;
     }
 
     private function findFileWithExtension($class, $ext)

commit 6150881154c68092c0596f7b143f66291840e14a

Re-dump autoloader since 7c7386a broke it

lib/composer/ClassLoader.php
@@ -42,30 +42,75 @@ namespace Composer\Autoload;
  */
 class ClassLoader
 {
+    /** @var ?string */
     private $vendorDir;
 
     // PSR-4
+    /**
+     * @var array[]
+     * @psalm-var array<string, array<string, int>>
+     */
     private $prefixLengthsPsr4 = array();
+    /**
+     * @var array[]
+     * @psalm-var array<string, array<int, string>>
+     */
     private $prefixDirsPsr4 = array();
+    /**
+     * @var array[]
+     * @psalm-var array<string, string>
+     */
     private $fallbackDirsPsr4 = array();
 
     // PSR-0
+    /**
+     * @var array[]
+     * @psalm-var array<string, array<string, string[]>>
+     */
     private $prefixesPsr0 = array();
+    /**
+     * @var array[]
+     * @psalm-var array<string, string>
+     */
     private $fallbackDirsPsr0 = array();
 
+    /** @var bool */
     private $useIncludePath = false;
+
+    /**
+     * @var string[]
+     * @psalm-var array<string, string>
+     */
     private $classMap = array();
+
+    /** @var bool */
     private $classMapAuthoritative = false;
+
+    /**
+     * @var bool[]
+     * @psalm-var array<string, bool>
+     */
     private $missingClasses = array();
+
+    /** @var ?string */
     private $apcuPrefix;
 
+    /**
+     * @var self[]
+     */
     private static $registeredLoaders = array();
 
+    /**
+     * @param ?string $vendorDir
+     */
     public function __construct($vendorDir = null)
     {
         $this->vendorDir = $vendorDir;
     }
 
+    /**
+     * @return string[]
+     */
     public function getPrefixes()
     {
         if (!empty($this->prefixesPsr0)) {
@@ -75,28 +120,47 @@ class ClassLoader
         return array();
     }
 
+    /**
+     * @return array[]
+     * @psalm-return array<string, array<int, string>>
+     */
     public function getPrefixesPsr4()
     {
         return $this->prefixDirsPsr4;
     }
 
+    /**
+     * @return array[]
+     * @psalm-return array<string, string>
+     */
     public function getFallbackDirs()
     {
         return $this->fallbackDirsPsr0;
     }
 
+    /**
+     * @return array[]
+     * @psalm-return array<string, string>
+     */
     public function getFallbackDirsPsr4()
     {
         return $this->fallbackDirsPsr4;
     }
 
+    /**
+     * @return string[] Array of classname => path
+     * @psalm-var array<string, string>
+     */
     public function getClassMap()
     {
         return $this->classMap;
     }
 
     /**
-     * @param array $classMap Class to filename map
+     * @param string[] $classMap Class to filename map
+     * @psalm-param array<string, string> $classMap
+     *
+     * @return void
      */
     public function addClassMap(array $classMap)
     {
@@ -111,9 +175,11 @@ class ClassLoader
      * Registers a set of PSR-0 directories for a given prefix, either
      * appending or prepending to the ones previously set for this prefix.
      *
-     * @param string       $prefix  The prefix
-     * @param array|string $paths   The PSR-0 root directories
-     * @param bool         $prepend Whether to prepend the directories
+     * @param string          $prefix  The prefix
+     * @param string[]|string $paths   The PSR-0 root directories
+     * @param bool            $prepend Whether to prepend the directories
+     *
+     * @return void
      */
     public function add($prefix, $paths, $prepend = false)
     {
@@ -156,11 +222,13 @@ class ClassLoader
      * Registers a set of PSR-4 directories for a given namespace, either
      * appending or prepending to the ones previously set for this namespace.
      *
-     * @param string       $prefix  The prefix/namespace, with trailing '\\'
-     * @param array|string $paths   The PSR-4 base directories
-     * @param bool         $prepend Whether to prepend the directories
+     * @param string          $prefix  The prefix/namespace, with trailing '\\'
+     * @param string[]|string $paths   The PSR-4 base directories
+     * @param bool            $prepend Whether to prepend the directories
      *
      * @throws \InvalidArgumentException
+     *
+     * @return void
      */
     public function addPsr4($prefix, $paths, $prepend = false)
     {
@@ -204,8 +272,10 @@ class ClassLoader
      * Registers a set of PSR-0 directories for a given prefix,
      * replacing any others previously set for this prefix.
      *
-     * @param string       $prefix The prefix
-     * @param array|string $paths  The PSR-0 base directories
+     * @param string          $prefix The prefix
+     * @param string[]|string $paths  The PSR-0 base directories
+     *
+     * @return void
      */
     public function set($prefix, $paths)
     {
@@ -220,10 +290,12 @@ class ClassLoader
      * Registers a set of PSR-4 directories for a given namespace,
      * replacing any others previously set for this namespace.
      *
-     * @param string       $prefix The prefix/namespace, with trailing '\\'
-     * @param array|string $paths  The PSR-4 base directories
+     * @param string          $prefix The prefix/namespace, with trailing '\\'
+     * @param string[]|string $paths  The PSR-4 base directories
      *
      * @throws \InvalidArgumentException
+     *
+     * @return void
      */
     public function setPsr4($prefix, $paths)
     {
@@ -243,6 +315,8 @@ class ClassLoader
      * Turns on searching the include path for class files.
      *
      * @param bool $useIncludePath
+     *
+     * @return void
      */
     public function setUseIncludePath($useIncludePath)
     {
@@ -265,6 +339,8 @@ class ClassLoader
      * that have not been registered with the class map.
      *
      * @param bool $classMapAuthoritative
+     *
+     * @return void
      */
     public function setClassMapAuthoritative($classMapAuthoritative)
     {
@@ -285,6 +361,8 @@ class ClassLoader
      * APCu prefix to use to cache found/not-found classes, if the extension is enabled.
      *
      * @param string|null $apcuPrefix
+     *
+     * @return void
      */
     public function setApcuPrefix($apcuPrefix)
     {
@@ -305,6 +383,8 @@ class ClassLoader
      * Registers this instance as an autoloader.
      *
      * @param bool $prepend Whether to prepend the autoloader or not
+     *
+     * @return void
      */
     public function register($prepend = false)
     {
@@ -324,6 +404,8 @@ class ClassLoader
 
     /**
      * Unregisters this instance as an autoloader.
+     *
+     * @return void
      */
     public function unregister()
     {
@@ -403,6 +485,11 @@ class ClassLoader
         return self::$registeredLoaders;
     }
 
+    /**
+     * @param  string       $class
+     * @param  string       $ext
+     * @return string|false
+     */
     private function findFileWithExtension($class, $ext)
     {
         // PSR-4 lookup
@@ -474,6 +561,10 @@ class ClassLoader
  * Scope isolated include.
  *
  * Prevents access to $this/self from included files.
+ *
+ * @param  string $file
+ * @return void
+ * @private
  */
 function includeFile($file)
 {

lib/composer/autoload_classmap.php
@@ -36,6 +36,7 @@ return array(
     'AttributeBoolean' => $baseDir . '/core/attributedef.class.inc.php',
     'AttributeCaseLog' => $baseDir . '/core/attributedef.class.inc.php',
     'AttributeClass' => $baseDir . '/core/attributedef.class.inc.php',
+    'AttributeClassAttCodeSet' => $baseDir . '/core/attributedef.class.inc.php',
     'AttributeClassState' => $baseDir . '/core/attributedef.class.inc.php',
     'AttributeCustomFields' => $baseDir . '/core/attributedef.class.inc.php',
     'AttributeDBField' => $baseDir . '/core/attributedef.class.inc.php',
@@ -50,6 +51,7 @@ return array(
     'AttributeEmailAddress' => $baseDir . '/core/attributedef.class.inc.php',
     'AttributeEncryptedString' => $baseDir . '/core/attributedef.class.inc.php',
     'AttributeEnum' => $baseDir . '/core/attributedef.class.inc.php',
+    'AttributeEnumSet' => $baseDir . '/core/attributedef.class.inc.php',
     'AttributeExternalField' => $baseDir . '/core/attributedef.class.inc.php',
     'AttributeExternalKey' => $baseDir . '/core/attributedef.class.inc.php',
     'AttributeFinalClass' => $baseDir . '/core/attributedef.class.inc.php',
@@ -72,12 +74,14 @@ return array(
     'AttributePercentage' => $baseDir . '/core/attributedef.class.inc.php',
     'AttributePhoneNumber' => $baseDir . '/core/attributedef.class.inc.php',
     'AttributePropertySet' => $baseDir . '/core/attributedef.class.inc.php',
+    'AttributeQueryAttCodeSet' => $baseDir . '/core/attributedef.class.inc.php',
     'AttributeRedundancySettings' => $baseDir . '/core/attributedef.class.inc.php',
     'AttributeSet' => $baseDir . '/core/attributedef.class.inc.php',
     'AttributeStopWatch' => $baseDir . '/core/attributedef.class.inc.php',
     'AttributeString' => $baseDir . '/core/attributedef.class.inc.php',
     'AttributeSubItem' => $baseDir . '/core/attributedef.class.inc.php',
     'AttributeTable' => $baseDir . '/core/attributedef.class.inc.php',
+    'AttributeTagSet' => $baseDir . '/core/attributedef.class.inc.php',
     'AttributeTemplateHTML' => $baseDir . '/core/attributedef.class.inc.php',
     'AttributeTemplateString' => $baseDir . '/core/attributedef.class.inc.php',
     'AttributeTemplateText' => $baseDir . '/core/attributedef.class.inc.php',
@@ -384,9 +388,17 @@ return array(
     'DashboardMenuNode' => $baseDir . '/application/menunode.class.inc.php',
     'Dashlet' => $baseDir . '/application/dashlet.class.inc.php',
     'DashletBadge' => $baseDir . '/application/dashlet.class.inc.php',
+    'DashletEmptyCell' => $baseDir . '/application/dashlet.class.inc.php',
+    'DashletGroupBy' => $baseDir . '/application/dashlet.class.inc.php',
+    'DashletGroupByBars' => $baseDir . '/application/dashlet.class.inc.php',
+    'DashletGroupByPie' => $baseDir . '/application/dashlet.class.inc.php',
     'DashletGroupByTable' => $baseDir . '/application/dashlet.class.inc.php',
     'DashletHeaderDynamic' => $baseDir . '/application/dashlet.class.inc.php',
     'DashletHeaderStatic' => $baseDir . '/application/dashlet.class.inc.php',
+    'DashletObjectList' => $baseDir . '/application/dashlet.class.inc.php',
+    'DashletPlainText' => $baseDir . '/application/dashlet.class.inc.php',
+    'DashletProxy' => $baseDir . '/application/dashlet.class.inc.php',
+    'DashletUnknown' => $baseDir . '/application/dashlet.class.inc.php',
     'DataTable' => $baseDir . '/application/datatable.class.inc.php',
     'DataTableConfig' => $baseDir . '/sources/application/UI/Base/Component/DataTable/DataTableConfig/DataTableConfig.php',
     'Datamatrix' => $vendorDir . '/combodo/tcpdf/include/barcodes/datamatrix.php',
@@ -398,9 +410,21 @@ return array(
     'DeleteException' => $baseDir . '/application/exceptions/DeleteException.php',
     'DeletionPlan' => $baseDir . '/core/deletionplan.class.inc.php',
     'DeprecatedCallsLog' => $baseDir . '/core/log.class.inc.php',
+    'DesignerBooleanField' => $baseDir . '/application/forms.class.inc.php',
+    'DesignerComboField' => $baseDir . '/application/forms.class.inc.php',
     'DesignerForm' => $baseDir . '/application/forms.class.inc.php',
+    'DesignerFormField' => $baseDir . '/application/forms.class.inc.php',
+    'DesignerFormSelectorField' => $baseDir . '/application/forms.class.inc.php',
+    'DesignerHiddenField' => $baseDir . '/application/forms.class.inc.php',
+    'DesignerIconSelectionField' => $baseDir . '/application/forms.class.inc.php',
+    'DesignerIntegerField' => $baseDir . '/application/forms.class.inc.php',
+    'DesignerLabelField' => $baseDir . '/application/forms.class.inc.php',
+    'DesignerLongTextField' => $baseDir . '/application/forms.class.inc.php',
+    'DesignerSortableField' => $baseDir . '/application/forms.class.inc.php',
     'DesignerStaticTextField' => $baseDir . '/application/forms.class.inc.php',
     'DesignerSubFormField' => $baseDir . '/application/forms.class.inc.php',
+    'DesignerTabularForm' => $baseDir . '/application/forms.class.inc.php',
+    'DesignerTextField' => $baseDir . '/application/forms.class.inc.php',
     'Dict' => $baseDir . '/core/dict.class.inc.php',
     'DictException' => $baseDir . '/application/exceptions/dict/DictException.php',
     'DictExceptionMissingString' => $baseDir . '/application/exceptions/dict/DictExceptionMissingString.php',
@@ -843,6 +867,8 @@ return array(
     'RowStatus_Modify' => $baseDir . '/core/bulkchange.class.inc.php',
     'RowStatus_NewObj' => $baseDir . '/core/bulkchange.class.inc.php',
     'RowStatus_NoChange' => $baseDir . '/core/bulkchange.class.inc.php',
+    'RunTimeIconSelectionField' => $baseDir . '/application/forms.class.inc.php',
+    'RuntimeDashboard' => $baseDir . '/application/dashboard.class.inc.php',
     'SQLExpression' => $baseDir . '/core/oql/expression.class.inc.php',
     'SQLObjectQuery' => $baseDir . '/core/sqlobjectquery.class.inc.php',
     'SQLObjectQueryBuilder' => $baseDir . '/core/sqlobjectquerybuilder.class.inc.php',

lib/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit0018331147de7601e7552f7da8e3bb8b
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit0018331147de7601e7552f7da8e3bb8b', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit0018331147de7601e7552f7da8e3bb8b', 'loadClassLoader'));

lib/composer/autoload_static.php
@@ -266,6 +266,7 @@ class ComposerStaticInit0018331147de7601e7552f7da8e3bb8b
         'AttributeBoolean' => __DIR__ . '/../..' . '/core/attributedef.class.inc.php',
         'AttributeCaseLog' => __DIR__ . '/../..' . '/core/attributedef.class.inc.php',
         'AttributeClass' => __DIR__ . '/../..' . '/core/attributedef.class.inc.php',
+        'AttributeClassAttCodeSet' => __DIR__ . '/../..' . '/core/attributedef.class.inc.php',
         'AttributeClassState' => __DIR__ . '/../..' . '/core/attributedef.class.inc.php',
         'AttributeCustomFields' => __DIR__ . '/../..' . '/core/attributedef.class.inc.php',
         'AttributeDBField' => __DIR__ . '/../..' . '/core/attributedef.class.inc.php',
@@ -280,6 +281,7 @@ class ComposerStaticInit0018331147de7601e7552f7da8e3bb8b
         'AttributeEmailAddress' => __DIR__ . '/../..' . '/core/attributedef.class.inc.php',
         'AttributeEncryptedString' => __DIR__ . '/../..' . '/core/attributedef.class.inc.php',
         'AttributeEnum' => __DIR__ . '/../..' . '/core/attributedef.class.inc.php',
+        'AttributeEnumSet' => __DIR__ . '/../..' . '/core/attributedef.class.inc.php',
         'AttributeExternalField' => __DIR__ . '/../..' . '/core/attributedef.class.inc.php',
         'AttributeExternalKey' => __DIR__ . '/../..' . '/core/attributedef.class.inc.php',
         'AttributeFinalClass' => __DIR__ . '/../..' . '/core/attributedef.class.inc.php',
@@ -302,12 +304,14 @@ class ComposerStaticInit0018331147de7601e7552f7da8e3bb8b
         'AttributePercentage' => __DIR__ . '/../..' . '/core/attributedef.class.inc.php',
         'AttributePhoneNumber' => __DIR__ . '/../..' . '/core/attributedef.class.inc.php',
         'AttributePropertySet' => __DIR__ . '/../..' . '/core/attributedef.class.inc.php',
+        'AttributeQueryAttCodeSet' => __DIR__ . '/../..' . '/core/attributedef.class.inc.php',
         'AttributeRedundancySettings' => __DIR__ . '/../..' . '/core/attributedef.class.inc.php',
         'AttributeSet' => __DIR__ . '/../..' . '/core/attributedef.class.inc.php',
         'AttributeStopWatch' => __DIR__ . '/../..' . '/core/attributedef.class.inc.php',
         'AttributeString' => __DIR__ . '/../..' . '/core/attributedef.class.inc.php',
         'AttributeSubItem' => __DIR__ . '/../..' . '/core/attributedef.class.inc.php',
         'AttributeTable' => __DIR__ . '/../..' . '/core/attributedef.class.inc.php',
+        'AttributeTagSet' => __DIR__ . '/../..' . '/core/attributedef.class.inc.php',
         'AttributeTemplateHTML' => __DIR__ . '/../..' . '/core/attributedef.class.inc.php',
         'AttributeTemplateString' => __DIR__ . '/../..' . '/core/attributedef.class.inc.php',
         'AttributeTemplateText' => __DIR__ . '/../..' . '/core/attributedef.class.inc.php',
@@ -614,9 +618,17 @@ class ComposerStaticInit0018331147de7601e7552f7da8e3bb8b
         'DashboardMenuNode' => __DIR__ . '/../..' . '/application/menunode.class.inc.php',
         'Dashlet' => __DIR__ . '/../..' . '/application/dashlet.class.inc.php',
         'DashletBadge' => __DIR__ . '/../..' . '/application/dashlet.class.inc.php',
+        'DashletEmptyCell' => __DIR__ . '/../..' . '/application/dashlet.class.inc.php',
+        'DashletGroupBy' => __DIR__ . '/../..' . '/application/dashlet.class.inc.php',
+        'DashletGroupByBars' => __DIR__ . '/../..' . '/application/dashlet.class.inc.php',
+        'DashletGroupByPie' => __DIR__ . '/../..' . '/application/dashlet.class.inc.php',
         'DashletGroupByTable' => __DIR__ . '/../..' . '/application/dashlet.class.inc.php',
         'DashletHeaderDynamic' => __DIR__ . '/../..' . '/application/dashlet.class.inc.php',
         'DashletHeaderStatic' => __DIR__ . '/../..' . '/application/dashlet.class.inc.php',
+        'DashletObjectList' => __DIR__ . '/../..' . '/application/dashlet.class.inc.php',
+        'DashletPlainText' => __DIR__ . '/../..' . '/application/dashlet.class.inc.php',
+        'DashletProxy' => __DIR__ . '/../..' . '/application/dashlet.class.inc.php',
+        'DashletUnknown' => __DIR__ . '/../..' . '/application/dashlet.class.inc.php',
         'DataTable' => __DIR__ . '/../..' . '/application/datatable.class.inc.php',
         'DataTableConfig' => __DIR__ . '/../..' . '/sources/application/UI/Base/Component/DataTable/DataTableConfig/DataTableConfig.php',
         'Datamatrix' => __DIR__ . '/..' . '/combodo/tcpdf/include/barcodes/datamatrix.php',
@@ -628,9 +640,21 @@ class ComposerStaticInit0018331147de7601e7552f7da8e3bb8b
         'DeleteException' => __DIR__ . '/../..' . '/application/exceptions/DeleteException.php',
         'DeletionPlan' => __DIR__ . '/../..' . '/core/deletionplan.class.inc.php',
         'DeprecatedCallsLog' => __DIR__ . '/../..' . '/core/log.class.inc.php',
+        'DesignerBooleanField' => __DIR__ . '/../..' . '/application/forms.class.inc.php',
+        'DesignerComboField' => __DIR__ . '/../..' . '/application/forms.class.inc.php',
         'DesignerForm' => __DIR__ . '/../..' . '/application/forms.class.inc.php',
+        'DesignerFormField' => __DIR__ . '/../..' . '/application/forms.class.inc.php',
+        'DesignerFormSelectorField' => __DIR__ . '/../..' . '/application/forms.class.inc.php',
+        'DesignerHiddenField' => __DIR__ . '/../..' . '/application/forms.class.inc.php',
+        'DesignerIconSelectionField' => __DIR__ . '/../..' . '/application/forms.class.inc.php',
+        'DesignerIntegerField' => __DIR__ . '/../..' . '/application/forms.class.inc.php',
+        'DesignerLabelField' => __DIR__ . '/../..' . '/application/forms.class.inc.php',
+        'DesignerLongTextField' => __DIR__ . '/../..' . '/application/forms.class.inc.php',
+        'DesignerSortableField' => __DIR__ . '/../..' . '/application/forms.class.inc.php',
         'DesignerStaticTextField' => __DIR__ . '/../..' . '/application/forms.class.inc.php',
         'DesignerSubFormField' => __DIR__ . '/../..' . '/application/forms.class.inc.php',
+        'DesignerTabularForm' => __DIR__ . '/../..' . '/application/forms.class.inc.php',
+        'DesignerTextField' => __DIR__ . '/../..' . '/application/forms.class.inc.php',
         'Dict' => __DIR__ . '/../..' . '/core/dict.class.inc.php',
         'DictException' => __DIR__ . '/../..' . '/application/exceptions/dict/DictException.php',
         'DictExceptionMissingString' => __DIR__ . '/../..' . '/application/exceptions/dict/DictExceptionMissingString.php',
@@ -1073,6 +1097,8 @@ class ComposerStaticInit0018331147de7601e7552f7da8e3bb8b
         'RowStatus_Modify' => __DIR__ . '/../..' . '/core/bulkchange.class.inc.php',
         'RowStatus_NewObj' => __DIR__ . '/../..' . '/core/bulkchange.class.inc.php',
         'RowStatus_NoChange' => __DIR__ . '/../..' . '/core/bulkchange.class.inc.php',
+        'RunTimeIconSelectionField' => __DIR__ . '/../..' . '/application/forms.class.inc.php',
+        'RuntimeDashboard' => __DIR__ . '/../..' . '/application/dashboard.class.inc.php',
         'SQLExpression' => __DIR__ . '/../..' . '/core/oql/expression.class.inc.php',
         'SQLObjectQuery' => __DIR__ . '/../..' . '/core/sqlobjectquery.class.inc.php',
         'SQLObjectQueryBuilder' => __DIR__ . '/../..' . '/core/sqlobjectquerybuilder.class.inc.php',