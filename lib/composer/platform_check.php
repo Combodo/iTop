Combodo/iTop

commit 7c7386afc71a9191f5fdd66888fe3ec8248ceaf0

N°3851 Update Emogrifier to a version supporting iTop PHP versions range

lib/composer/platform_check.php
@@ -1,26 +1,0 @@
-<?php
-
-// platform_check.php @generated by Composer
-
-$issues = array();
-
-if (!(PHP_VERSION_ID >= 70103)) {
-    $issues[] = 'Your Composer dependencies require a PHP version ">= 7.1.3". You are running ' . PHP_VERSION . '.';
-}
-
-if ($issues) {
-    if (!headers_sent()) {
-        header('HTTP/1.1 500 Internal Server Error');
-    }
-    if (!ini_get('display_errors')) {
-        if (PHP_SAPI === 'cli' || PHP_SAPI === 'phpdbg') {
-            fwrite(STDERR, 'Composer detected issues in your platform:' . PHP_EOL.PHP_EOL . implode(PHP_EOL, $issues) . PHP_EOL.PHP_EOL);
-        } elseif (!headers_sent()) {
-            echo 'Composer detected issues in your platform:' . PHP_EOL.PHP_EOL . str_replace('You are running '.PHP_VERSION.'.', '', implode(PHP_EOL, $issues)) . PHP_EOL.PHP_EOL;
-        }
-    }
-    trigger_error(
-        'Composer detected issues in your platform: ' . implode(' ', $issues),
-        E_USER_ERROR
-    );
-}

core/email.class.inc.php
@@ -24,6 +24,10 @@
  * @license     http://opensource.org/licenses/AGPL-3.0
  */
 
+use Pelago\Emogrifier\CssInliner;
+use Pelago\Emogrifier\HtmlProcessor\CssToAttributeConverter;
+use Pelago\Emogrifier\HtmlProcessor\HtmlPruner;
+
 Swift_Preferences::getInstance()->setCharset('UTF-8');
 
 
@@ -335,8 +339,9 @@ class EMail
 	{
 		if (($sMimeType === 'text/html') && ($sCustomStyles !== null))
 		{
-			$emogrifier = new \Pelago\Emogrifier($sBody, $sCustomStyles);
-			$sBody = $emogrifier->emogrify(); // Adds html/body tags if not already present
+			$oDomDocument = CssInliner::fromHtml($sBody)->inlineCss($sCustomStyles)->getDomDocument();
+			HtmlPruner::fromDomDocument($oDomDocument)->removeElementsWithDisplayNone();
+			$sBody = CssToAttributeConverter::fromDomDocument($oDomDocument)->convertCssToVisualAttributes()->render(); // Adds html/body tags if not already present
 		}
 		$this->m_aData['body'] = array('body' => $sBody, 'mimeType' => $sMimeType);
 		$this->m_oMessage->setBody($sBody, $sMimeType);

lib/composer/ClassLoader.php
@@ -42,6 +42,8 @@ namespace Composer\Autoload;
  */
 class ClassLoader
 {
+    private $vendorDir;
+
     // PSR-4
     private $prefixLengthsPsr4 = array();
     private $prefixDirsPsr4 = array();
@@ -56,6 +58,13 @@ class ClassLoader
     private $classMapAuthoritative = false;
     private $missingClasses = array();
     private $apcuPrefix;
+
+    private static $registeredLoaders = array();
+
+    public function __construct($vendorDir = null)
+    {
+        $this->vendorDir = $vendorDir;
+    }
 
     public function getPrefixes()
     {
@@ -300,6 +309,17 @@ class ClassLoader
     public function register($prepend = false)
     {
         spl_autoload_register(array($this, 'loadClass'), true, $prepend);
+
+        if (null === $this->vendorDir) {
+            return;
+        }
+
+        if ($prepend) {
+            self::$registeredLoaders = array($this->vendorDir => $this) + self::$registeredLoaders;
+        } else {
+            unset(self::$registeredLoaders[$this->vendorDir]);
+            self::$registeredLoaders[$this->vendorDir] = $this;
+        }
     }
 
     /**
@@ -308,13 +328,17 @@ class ClassLoader
     public function unregister()
     {
         spl_autoload_unregister(array($this, 'loadClass'));
+
+        if (null !== $this->vendorDir) {
+            unset(self::$registeredLoaders[$this->vendorDir]);
+        }
     }
 
     /**
      * Loads the given class or interface.
      *
      * @param  string    $class The name of the class
-     * @return bool|null True if loaded, null otherwise
+     * @return true|null True if loaded, null otherwise
      */
     public function loadClass($class)
     {
@@ -323,6 +347,8 @@ class ClassLoader
 
             return true;
         }
+
+        return null;
     }
 
     /**
@@ -365,6 +391,16 @@ class ClassLoader
         }
 
         return $file;
+    }
+
+    /**
+     * Returns the currently registered loaders indexed by their corresponding vendor directories.
+     *
+     * @return self[]
+     */
+    public static function getRegisteredLoaders()
+    {
+        return self::$registeredLoaders;
     }
 
     private function findFileWithExtension($class, $ext)

lib/composer/autoload_classmap.php
@@ -36,7 +36,6 @@ return array(
     'AttributeBoolean' => $baseDir . '/core/attributedef.class.inc.php',
     'AttributeCaseLog' => $baseDir . '/core/attributedef.class.inc.php',
     'AttributeClass' => $baseDir . '/core/attributedef.class.inc.php',
-    'AttributeClassAttCodeSet' => $baseDir . '/core/attributedef.class.inc.php',
     'AttributeClassState' => $baseDir . '/core/attributedef.class.inc.php',
     'AttributeCustomFields' => $baseDir . '/core/attributedef.class.inc.php',
     'AttributeDBField' => $baseDir . '/core/attributedef.class.inc.php',
@@ -51,7 +50,6 @@ return array(
     'AttributeEmailAddress' => $baseDir . '/core/attributedef.class.inc.php',
     'AttributeEncryptedString' => $baseDir . '/core/attributedef.class.inc.php',
     'AttributeEnum' => $baseDir . '/core/attributedef.class.inc.php',
-    'AttributeEnumSet' => $baseDir . '/core/attributedef.class.inc.php',
     'AttributeExternalField' => $baseDir . '/core/attributedef.class.inc.php',
     'AttributeExternalKey' => $baseDir . '/core/attributedef.class.inc.php',
     'AttributeFinalClass' => $baseDir . '/core/attributedef.class.inc.php',
@@ -74,14 +72,12 @@ return array(
     'AttributePercentage' => $baseDir . '/core/attributedef.class.inc.php',
     'AttributePhoneNumber' => $baseDir . '/core/attributedef.class.inc.php',
     'AttributePropertySet' => $baseDir . '/core/attributedef.class.inc.php',
-    'AttributeQueryAttCodeSet' => $baseDir . '/core/attributedef.class.inc.php',
     'AttributeRedundancySettings' => $baseDir . '/core/attributedef.class.inc.php',
     'AttributeSet' => $baseDir . '/core/attributedef.class.inc.php',
     'AttributeStopWatch' => $baseDir . '/core/attributedef.class.inc.php',
     'AttributeString' => $baseDir . '/core/attributedef.class.inc.php',
     'AttributeSubItem' => $baseDir . '/core/attributedef.class.inc.php',
     'AttributeTable' => $baseDir . '/core/attributedef.class.inc.php',
-    'AttributeTagSet' => $baseDir . '/core/attributedef.class.inc.php',
     'AttributeTemplateHTML' => $baseDir . '/core/attributedef.class.inc.php',
     'AttributeTemplateString' => $baseDir . '/core/attributedef.class.inc.php',
     'AttributeTemplateText' => $baseDir . '/core/attributedef.class.inc.php',
@@ -388,17 +384,9 @@ return array(
     'DashboardMenuNode' => $baseDir . '/application/menunode.class.inc.php',
     'Dashlet' => $baseDir . '/application/dashlet.class.inc.php',
     'DashletBadge' => $baseDir . '/application/dashlet.class.inc.php',
-    'DashletEmptyCell' => $baseDir . '/application/dashlet.class.inc.php',
-    'DashletGroupBy' => $baseDir . '/application/dashlet.class.inc.php',
-    'DashletGroupByBars' => $baseDir . '/application/dashlet.class.inc.php',
-    'DashletGroupByPie' => $baseDir . '/application/dashlet.class.inc.php',
     'DashletGroupByTable' => $baseDir . '/application/dashlet.class.inc.php',
     'DashletHeaderDynamic' => $baseDir . '/application/dashlet.class.inc.php',
     'DashletHeaderStatic' => $baseDir . '/application/dashlet.class.inc.php',
-    'DashletObjectList' => $baseDir . '/application/dashlet.class.inc.php',
-    'DashletPlainText' => $baseDir . '/application/dashlet.class.inc.php',
-    'DashletProxy' => $baseDir . '/application/dashlet.class.inc.php',
-    'DashletUnknown' => $baseDir . '/application/dashlet.class.inc.php',
     'DataTable' => $baseDir . '/application/datatable.class.inc.php',
     'DataTableConfig' => $baseDir . '/sources/application/UI/Base/Component/DataTable/DataTableConfig/DataTableConfig.php',
     'Datamatrix' => $vendorDir . '/combodo/tcpdf/include/barcodes/datamatrix.php',
@@ -410,21 +398,9 @@ return array(
     'DeleteException' => $baseDir . '/application/exceptions/DeleteException.php',
     'DeletionPlan' => $baseDir . '/core/deletionplan.class.inc.php',
     'DeprecatedCallsLog' => $baseDir . '/core/log.class.inc.php',
-    'DesignerBooleanField' => $baseDir . '/application/forms.class.inc.php',
-    'DesignerComboField' => $baseDir . '/application/forms.class.inc.php',
     'DesignerForm' => $baseDir . '/application/forms.class.inc.php',
-    'DesignerFormField' => $baseDir . '/application/forms.class.inc.php',
-    'DesignerFormSelectorField' => $baseDir . '/application/forms.class.inc.php',
-    'DesignerHiddenField' => $baseDir . '/application/forms.class.inc.php',
-    'DesignerIconSelectionField' => $baseDir . '/application/forms.class.inc.php',
-    'DesignerIntegerField' => $baseDir . '/application/forms.class.inc.php',
-    'DesignerLabelField' => $baseDir . '/application/forms.class.inc.php',
-    'DesignerLongTextField' => $baseDir . '/application/forms.class.inc.php',
-    'DesignerSortableField' => $baseDir . '/application/forms.class.inc.php',
     'DesignerStaticTextField' => $baseDir . '/application/forms.class.inc.php',
     'DesignerSubFormField' => $baseDir . '/application/forms.class.inc.php',
-    'DesignerTabularForm' => $baseDir . '/application/forms.class.inc.php',
-    'DesignerTextField' => $baseDir . '/application/forms.class.inc.php',
     'Dict' => $baseDir . '/core/dict.class.inc.php',
     'DictException' => $baseDir . '/application/exceptions/dict/DictException.php',
     'DictExceptionMissingString' => $baseDir . '/application/exceptions/dict/DictExceptionMissingString.php',
@@ -451,6 +427,7 @@ return array(
     'EventWebService' => $baseDir . '/core/event.class.inc.php',
     'ExcelBulkExport' => $baseDir . '/core/excelbulkexport.class.inc.php',
     'ExcelExporter' => $baseDir . '/application/excelexporter.class.inc.php',
+    'ExceptionLog' => $baseDir . '/core/log.class.inc.php',
     'ExecAsyncTask' => $baseDir . '/core/asynctask.class.inc.php',
     'ExecutionKPI' => $baseDir . '/core/kpi.class.inc.php',
     'Expression' => $baseDir . '/core/oql/expression.class.inc.php',
@@ -569,11 +546,13 @@ return array(
     'Page' => $baseDir . '/sources/application/WebPage/Page.php',
     'ParseError' => $vendorDir . '/symfony/polyfill-php70/Resources/stubs/ParseError.php',
     'Pelago\\Emogrifier' => $vendorDir . '/pelago/emogrifier/src/Emogrifier.php',
-    'Pelago\\Emogrifier\\CssConcatenator' => $vendorDir . '/pelago/emogrifier/src/Emogrifier/CssConcatenator.php',
     'Pelago\\Emogrifier\\CssInliner' => $vendorDir . '/pelago/emogrifier/src/Emogrifier/CssInliner.php',
     'Pelago\\Emogrifier\\HtmlProcessor\\AbstractHtmlProcessor' => $vendorDir . '/pelago/emogrifier/src/Emogrifier/HtmlProcessor/AbstractHtmlProcessor.php',
     'Pelago\\Emogrifier\\HtmlProcessor\\CssToAttributeConverter' => $vendorDir . '/pelago/emogrifier/src/Emogrifier/HtmlProcessor/CssToAttributeConverter.php',
     'Pelago\\Emogrifier\\HtmlProcessor\\HtmlNormalizer' => $vendorDir . '/pelago/emogrifier/src/Emogrifier/HtmlProcessor/HtmlNormalizer.php',
+    'Pelago\\Emogrifier\\HtmlProcessor\\HtmlPruner' => $vendorDir . '/pelago/emogrifier/src/Emogrifier/HtmlProcessor/HtmlPruner.php',
+    'Pelago\\Emogrifier\\Utilities\\ArrayIntersector' => $vendorDir . '/pelago/emogrifier/src/Emogrifier/Utilities/ArrayIntersector.php',
+    'Pelago\\Emogrifier\\Utilities\\CssConcatenator' => $vendorDir . '/pelago/emogrifier/src/Emogrifier/Utilities/CssConcatenator.php',
     'PhpParser\\Builder' => $vendorDir . '/nikic/php-parser/lib/PhpParser/Builder.php',
     'PhpParser\\BuilderFactory' => $vendorDir . '/nikic/php-parser/lib/PhpParser/BuilderFactory.php',
     'PhpParser\\BuilderHelpers' => $vendorDir . '/nikic/php-parser/lib/PhpParser/BuilderHelpers.php',
@@ -864,8 +843,6 @@ return array(
     'RowStatus_Modify' => $baseDir . '/core/bulkchange.class.inc.php',
     'RowStatus_NewObj' => $baseDir . '/core/bulkchange.class.inc.php',
     'RowStatus_NoChange' => $baseDir . '/core/bulkchange.class.inc.php',
-    'RunTimeIconSelectionField' => $baseDir . '/application/forms.class.inc.php',
-    'RuntimeDashboard' => $baseDir . '/application/dashboard.class.inc.php',
     'SQLExpression' => $baseDir . '/core/oql/expression.class.inc.php',
     'SQLObjectQuery' => $baseDir . '/core/sqlobjectquery.class.inc.php',
     'SQLObjectQueryBuilder' => $baseDir . '/core/sqlobjectquerybuilder.class.inc.php',

lib/composer/autoload_real.php
@@ -22,10 +22,8 @@ class ComposerAutoloaderInit0018331147de7601e7552f7da8e3bb8b
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInit0018331147de7601e7552f7da8e3bb8b', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit0018331147de7601e7552f7da8e3bb8b', 'loadClassLoader'));
 
         $includePaths = require __DIR__ . '/include_paths.php';

lib/composer/autoload_static.php
@@ -266,7 +266,6 @@ class ComposerStaticInit0018331147de7601e7552f7da8e3bb8b
         'AttributeBoolean' => __DIR__ . '/../..' . '/core/attributedef.class.inc.php',
         'AttributeCaseLog' => __DIR__ . '/../..' . '/core/attributedef.class.inc.php',
         'AttributeClass' => __DIR__ . '/../..' . '/core/attributedef.class.inc.php',
-        'AttributeClassAttCodeSet' => __DIR__ . '/../..' . '/core/attributedef.class.inc.php',
         'AttributeClassState' => __DIR__ . '/../..' . '/core/attributedef.class.inc.php',
         'AttributeCustomFields' => __DIR__ . '/../..' . '/core/attributedef.class.inc.php',
         'AttributeDBField' => __DIR__ . '/../..' . '/core/attributedef.class.inc.php',
@@ -281,7 +280,6 @@ class ComposerStaticInit0018331147de7601e7552f7da8e3bb8b
         'AttributeEmailAddress' => __DIR__ . '/../..' . '/core/attributedef.class.inc.php',
         'AttributeEncryptedString' => __DIR__ . '/../..' . '/core/attributedef.class.inc.php',
         'AttributeEnum' => __DIR__ . '/../..' . '/core/attributedef.class.inc.php',
-        'AttributeEnumSet' => __DIR__ . '/../..' . '/core/attributedef.class.inc.php',
         'AttributeExternalField' => __DIR__ . '/../..' . '/core/attributedef.class.inc.php',
         'AttributeExternalKey' => __DIR__ . '/../..' . '/core/attributedef.class.inc.php',
         'AttributeFinalClass' => __DIR__ . '/../..' . '/core/attributedef.class.inc.php',
@@ -304,14 +302,12 @@ class ComposerStaticInit0018331147de7601e7552f7da8e3bb8b
         'AttributePercentage' => __DIR__ . '/../..' . '/core/attributedef.class.inc.php',
         'AttributePhoneNumber' => __DIR__ . '/../..' . '/core/attributedef.class.inc.php',
         'AttributePropertySet' => __DIR__ . '/../..' . '/core/attributedef.class.inc.php',
-        'AttributeQueryAttCodeSet' => __DIR__ . '/../..' . '/core/attributedef.class.inc.php',
         'AttributeRedundancySettings' => __DIR__ . '/../..' . '/core/attributedef.class.inc.php',
         'AttributeSet' => __DIR__ . '/../..' . '/core/attributedef.class.inc.php',
         'AttributeStopWatch' => __DIR__ . '/../..' . '/core/attributedef.class.inc.php',
         'AttributeString' => __DIR__ . '/../..' . '/core/attributedef.class.inc.php',
         'AttributeSubItem' => __DIR__ . '/../..' . '/core/attributedef.class.inc.php',
         'AttributeTable' => __DIR__ . '/../..' . '/core/attributedef.class.inc.php',
-        'AttributeTagSet' => __DIR__ . '/../..' . '/core/attributedef.class.inc.php',
         'AttributeTemplateHTML' => __DIR__ . '/../..' . '/core/attributedef.class.inc.php',
         'AttributeTemplateString' => __DIR__ . '/../..' . '/core/attributedef.class.inc.php',
         'AttributeTemplateText' => __DIR__ . '/../..' . '/core/attributedef.class.inc.php',
@@ -618,17 +614,9 @@ class ComposerStaticInit0018331147de7601e7552f7da8e3bb8b
         'DashboardMenuNode' => __DIR__ . '/../..' . '/application/menunode.class.inc.php',
         'Dashlet' => __DIR__ . '/../..' . '/application/dashlet.class.inc.php',
         'DashletBadge' => __DIR__ . '/../..' . '/application/dashlet.class.inc.php',
-        'DashletEmptyCell' => __DIR__ . '/../..' . '/application/dashlet.class.inc.php',
-        'DashletGroupBy' => __DIR__ . '/../..' . '/application/dashlet.class.inc.php',
-        'DashletGroupByBars' => __DIR__ . '/../..' . '/application/dashlet.class.inc.php',
-        'DashletGroupByPie' => __DIR__ . '/../..' . '/application/dashlet.class.inc.php',
         'DashletGroupByTable' => __DIR__ . '/../..' . '/application/dashlet.class.inc.php',
         'DashletHeaderDynamic' => __DIR__ . '/../..' . '/application/dashlet.class.inc.php',
         'DashletHeaderStatic' => __DIR__ . '/../..' . '/application/dashlet.class.inc.php',
-        'DashletObjectList' => __DIR__ . '/../..' . '/application/dashlet.class.inc.php',
-        'DashletPlainText' => __DIR__ . '/../..' . '/application/dashlet.class.inc.php',
-        'DashletProxy' => __DIR__ . '/../..' . '/application/dashlet.class.inc.php',
-        'DashletUnknown' => __DIR__ . '/../..' . '/application/dashlet.class.inc.php',
         'DataTable' => __DIR__ . '/../..' . '/application/datatable.class.inc.php',
         'DataTableConfig' => __DIR__ . '/../..' . '/sources/application/UI/Base/Component/DataTable/DataTableConfig/DataTableConfig.php',
         'Datamatrix' => __DIR__ . '/..' . '/combodo/tcpdf/include/barcodes/datamatrix.php',
@@ -640,21 +628,9 @@ class ComposerStaticInit0018331147de7601e7552f7da8e3bb8b
         'DeleteException' => __DIR__ . '/../..' . '/application/exceptions/DeleteException.php',
         'DeletionPlan' => __DIR__ . '/../..' . '/core/deletionplan.class.inc.php',
         'DeprecatedCallsLog' => __DIR__ . '/../..' . '/core/log.class.inc.php',
-        'DesignerBooleanField' => __DIR__ . '/../..' . '/application/forms.class.inc.php',
-        'DesignerComboField' => __DIR__ . '/../..' . '/application/forms.class.inc.php',
         'DesignerForm' => __DIR__ . '/../..' . '/application/forms.class.inc.php',
-        'DesignerFormField' => __DIR__ . '/../..' . '/application/forms.class.inc.php',
-        'DesignerFormSelectorField' => __DIR__ . '/../..' . '/application/forms.class.inc.php',
-        'DesignerHiddenField' => __DIR__ . '/../..' . '/application/forms.class.inc.php',
-        'DesignerIconSelectionField' => __DIR__ . '/../..' . '/application/forms.class.inc.php',
-        'DesignerIntegerField' => __DIR__ . '/../..' . '/application/forms.class.inc.php',
-        'DesignerLabelField' => __DIR__ . '/../..' . '/application/forms.class.inc.php',
-        'DesignerLongTextField' => __DIR__ . '/../..' . '/application/forms.class.inc.php',
-        'DesignerSortableField' => __DIR__ . '/../..' . '/application/forms.class.inc.php',
         'DesignerStaticTextField' => __DIR__ . '/../..' . '/application/forms.class.inc.php',
         'DesignerSubFormField' => __DIR__ . '/../..' . '/application/forms.class.inc.php',
-        'DesignerTabularForm' => __DIR__ . '/../..' . '/application/forms.class.inc.php',
-        'DesignerTextField' => __DIR__ . '/../..' . '/application/forms.class.inc.php',
         'Dict' => __DIR__ . '/../..' . '/core/dict.class.inc.php',
         'DictException' => __DIR__ . '/../..' . '/application/exceptions/dict/DictException.php',
         'DictExceptionMissingString' => __DIR__ . '/../..' . '/application/exceptions/dict/DictExceptionMissingString.php',
@@ -681,6 +657,7 @@ class ComposerStaticInit0018331147de7601e7552f7da8e3bb8b
         'EventWebService' => __DIR__ . '/../..' . '/core/event.class.inc.php',
         'ExcelBulkExport' => __DIR__ . '/../..' . '/core/excelbulkexport.class.inc.php',
         'ExcelExporter' => __DIR__ . '/../..' . '/application/excelexporter.class.inc.php',
+        'ExceptionLog' => __DIR__ . '/../..' . '/core/log.class.inc.php',
         'ExecAsyncTask' => __DIR__ . '/../..' . '/core/asynctask.class.inc.php',
         'ExecutionKPI' => __DIR__ . '/../..' . '/core/kpi.class.inc.php',
         'Expression' => __DIR__ . '/../..' . '/core/oql/expression.class.inc.php',
@@ -799,11 +776,13 @@ class ComposerStaticInit0018331147de7601e7552f7da8e3bb8b
         'Page' => __DIR__ . '/../..' . '/sources/application/WebPage/Page.php',
         'ParseError' => __DIR__ . '/..' . '/symfony/polyfill-php70/Resources/stubs/ParseError.php',
         'Pelago\\Emogrifier' => __DIR__ . '/..' . '/pelago/emogrifier/src/Emogrifier.php',
-        'Pelago\\Emogrifier\\CssConcatenator' => __DIR__ . '/..' . '/pelago/emogrifier/src/Emogrifier/CssConcatenator.php',
         'Pelago\\Emogrifier\\CssInliner' => __DIR__ . '/..' . '/pelago/emogrifier/src/Emogrifier/CssInliner.php',
         'Pelago\\Emogrifier\\HtmlProcessor\\AbstractHtmlProcessor' => __DIR__ . '/..' . '/pelago/emogrifier/src/Emogrifier/HtmlProcessor/AbstractHtmlProcessor.php',
         'Pelago\\Emogrifier\\HtmlProcessor\\CssToAttributeConverter' => __DIR__ . '/..' . '/pelago/emogrifier/src/Emogrifier/HtmlProcessor/CssToAttributeConverter.php',
         'Pelago\\Emogrifier\\HtmlProcessor\\HtmlNormalizer' => __DIR__ . '/..' . '/pelago/emogrifier/src/Emogrifier/HtmlProcessor/HtmlNormalizer.php',
+        'Pelago\\Emogrifier\\HtmlProcessor\\HtmlPruner' => __DIR__ . '/..' . '/pelago/emogrifier/src/Emogrifier/HtmlProcessor/HtmlPruner.php',
+        'Pelago\\Emogrifier\\Utilities\\ArrayIntersector' => __DIR__ . '/..' . '/pelago/emogrifier/src/Emogrifier/Utilities/ArrayIntersector.php',
+        'Pelago\\Emogrifier\\Utilities\\CssConcatenator' => __DIR__ . '/..' . '/pelago/emogrifier/src/Emogrifier/Utilities/CssConcatenator.php',
         'PhpParser\\Builder' => __DIR__ . '/..' . '/nikic/php-parser/lib/PhpParser/Builder.php',
         'PhpParser\\BuilderFactory' => __DIR__ . '/..' . '/nikic/php-parser/lib/PhpParser/BuilderFactory.php',
         'PhpParser\\BuilderHelpers' => __DIR__ . '/..' . '/nikic/php-parser/lib/PhpParser/BuilderHelpers.php',
@@ -1094,8 +1073,6 @@ class ComposerStaticInit0018331147de7601e7552f7da8e3bb8b
         'RowStatus_Modify' => __DIR__ . '/../..' . '/core/bulkchange.class.inc.php',
         'RowStatus_NewObj' => __DIR__ . '/../..' . '/core/bulkchange.class.inc.php',
         'RowStatus_NoChange' => __DIR__ . '/../..' . '/core/bulkchange.class.inc.php',
-        'RunTimeIconSelectionField' => __DIR__ . '/../..' . '/application/forms.class.inc.php',
-        'RuntimeDashboard' => __DIR__ . '/../..' . '/application/dashboard.class.inc.php',
         'SQLExpression' => __DIR__ . '/../..' . '/core/oql/expression.class.inc.php',
         'SQLObjectQuery' => __DIR__ . '/../..' . '/core/sqlobjectquery.class.inc.php',
         'SQLObjectQueryBuilder' => __DIR__ . '/../..' . '/core/sqlobjectquerybuilder.class.inc.php',

commit 92e315e2c7211a26de18648cbd442f0588b616ea

Re-dump autoloader since 7c7386a broke it -- with platform check enabled

lib/composer/platform_check.php
@@ -1,0 +1,42 @@
+<?php
+
+// platform_check.php @generated by Composer
+
+$issues = array();
+
+if (!(PHP_VERSION_ID >= 70103)) {
+    $issues[] = 'Your Composer dependencies require a PHP version ">= 7.1.3". You are running ' . PHP_VERSION . '.';
+}
+
+$missingExtensions = array();
+extension_loaded('ctype') || $missingExtensions[] = 'ctype';
+extension_loaded('dom') || $missingExtensions[] = 'dom';
+extension_loaded('gd') || $missingExtensions[] = 'gd';
+extension_loaded('iconv') || $missingExtensions[] = 'iconv';
+extension_loaded('json') || $missingExtensions[] = 'json';
+extension_loaded('libxml') || $missingExtensions[] = 'libxml';
+extension_loaded('mysqli') || $missingExtensions[] = 'mysqli';
+extension_loaded('soap') || $missingExtensions[] = 'soap';
+extension_loaded('tokenizer') || $missingExtensions[] = 'tokenizer';
+extension_loaded('xml') || $missingExtensions[] = 'xml';
+
+if ($missingExtensions) {
+    $issues[] = 'Your Composer dependencies require the following PHP extensions to be installed: ' . implode(', ', $missingExtensions) . '.';
+}
+
+if ($issues) {
+    if (!headers_sent()) {
+        header('HTTP/1.1 500 Internal Server Error');
+    }
+    if (!ini_get('display_errors')) {
+        if (PHP_SAPI === 'cli' || PHP_SAPI === 'phpdbg') {
+            fwrite(STDERR, 'Composer detected issues in your platform:' . PHP_EOL.PHP_EOL . implode(PHP_EOL, $issues) . PHP_EOL.PHP_EOL);
+        } elseif (!headers_sent()) {
+            echo 'Composer detected issues in your platform:' . PHP_EOL.PHP_EOL . str_replace('You are running '.PHP_VERSION.'.', '', implode(PHP_EOL, $issues)) . PHP_EOL.PHP_EOL;
+        }
+    }
+    trigger_error(
+        'Composer detected issues in your platform: ' . implode(' ', $issues),
+        E_USER_ERROR
+    );
+}