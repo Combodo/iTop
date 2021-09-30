Combodo/iTop

commit f2ff5a4e834ca18495fa059823ef58eb8a92bb05

N°3203 - Datamodel: Add semantic for image & state attributes Part. II

core/metamodel.class.php
@@ -853,8 +853,13 @@ abstract class MetaModel
 	}
 
 	/**
-	 * Return true if the $sClass has a state attribute defined
+	 * Return true if the $sClass has a state attribute defined.
 	 *
+	 * Note that having a state attribute does NOT mean having a lifecycle!
+	 * - A Person with active/inactive state won't have transitions and therefore no lifecycle
+	 * - A UserRequest will have transitions between its states and so a lifecycle
+	 *
+	 * @see self::HasLifecycle($sClass)
 	 * @param string $sClass Datamodel class to check
 	 *
 	 * @return bool
@@ -2372,6 +2377,22 @@ abstract class MetaModel
 		{
 			return self::$m_aStates[$sClass];
 		}
+		elseif (self::HasStateAttributeCode($sClass))
+		{
+			$sStateAttCode = self::GetStateAttributeCode($sClass);
+			$oAttDef = self::GetAttributeDef($sClass, $sStateAttCode);
+
+			$aStates = [];
+			foreach($oAttDef->GetAllowedValues() as $sStateCode => $sStateLabel)
+			{
+				$aStates[$sStateCode] = [
+					'attribute_inherit' => '',
+					'attribute_list' => [],
+				];
+			}
+
+			return $aStates;
+		}
 		else
 		{
 			return array();
@@ -2427,6 +2448,21 @@ abstract class MetaModel
 		{
 			return array();
 		}
+	}
+
+	/**
+	 * Return true if $sClass has a lifecycle, which means that it has a state attribute AND stimuli
+	 *
+	 * @param string $sClass
+	 *
+	 * @return bool
+	 * @throws \CoreException
+	 * @since 3.0.0
+	 * @see   self::HasStateAttributeCode($sClass)
+	 */
+	public static function HasLifecycle(string $sClass)
+	{
+		return self::HasStateAttributeCode($sClass) && !empty(self::EnumStimuli($sClass));
 	}
 
 	/**
@@ -2517,17 +2553,13 @@ abstract class MetaModel
 	public static function GetAttributeFlags($sClass, $sState, $sAttCode)
 	{
 		$iFlags = 0; // By default (if no life cycle) no flag at all
-		$sStateAttCode = self::GetStateAttributeCode($sClass);
-		if (!empty($sStateAttCode))
-		{
+		if (self::HasLifecycle($sClass)) {
 			$aStates = MetaModel::EnumStates($sClass);
-			if (!array_key_exists($sState, $aStates))
-			{
+			if (!array_key_exists($sState, $aStates)) {
 				throw new CoreException("Invalid state '$sState' for class '$sClass', expecting a value in {".implode(', ', array_keys($aStates))."}");
 			}
 			$aCurrentState = $aStates[$sState];
-			if ((array_key_exists('attribute_list', $aCurrentState)) && (array_key_exists($sAttCode, $aCurrentState['attribute_list'])))
-			{
+			if ((array_key_exists('attribute_list', $aCurrentState)) && (array_key_exists($sAttCode, $aCurrentState['attribute_list']))) {
 				$iFlags = $aCurrentState['attribute_list'][$sAttCode];
 			}
 		}
@@ -2547,18 +2579,14 @@ abstract class MetaModel
 	public static function GetTransitionFlags($sClass, $sState, $sStimulus, $sAttCode)
 	{
 		$iFlags = 0; // By default (if no lifecycle) no flag at all
-		$sStateAttCode = self::GetStateAttributeCode($sClass);
-		if (!empty($sStateAttCode))
-		{
+		if (self::HasLifecycle($sClass)) {
 			$aTransitions = MetaModel::EnumTransitions($sClass, $sState);
-			if (!array_key_exists($sStimulus, $aTransitions))
-			{
+			if (!array_key_exists($sStimulus, $aTransitions)) {
 				throw new CoreException("Invalid transition '$sStimulus' for class '$sClass', expecting a value in {".implode(', ', array_keys($aTransitions))."}");
 			}
 
 			$aCurrentTransition = $aTransitions[$sStimulus];
-			if ((array_key_exists('attribute_list', $aCurrentTransition)) && (array_key_exists($sAttCode, $aCurrentTransition['attribute_list'])))
-			{
+			if ((array_key_exists('attribute_list', $aCurrentTransition)) && (array_key_exists($sAttCode, $aCurrentTransition['attribute_list']))) {
 				$iFlags = $aCurrentTransition['attribute_list'][$sAttCode];
 			}
 		}
@@ -2637,37 +2665,30 @@ abstract class MetaModel
 	public static function GetInitialStateAttributeFlags($sClass, $sState, $sAttCode)
 	{
 		$iFlags = self::GetAttributeFlags($sClass, $sState, $sAttCode); // Be default set the same flags as the 'target' state
-		$sStateAttCode = self::GetStateAttributeCode($sClass);
-		if (!empty($sStateAttCode))
-		{
+		if (self::HasLifecycle($sClass)) {
 			$aStates = MetaModel::EnumInitialStates($sClass);
-			if (array_key_exists($sState, $aStates))
-			{
+			if (array_key_exists($sState, $aStates)) {
 				$bReadOnly = (($iFlags & OPT_ATT_READONLY) == OPT_ATT_READONLY);
 				$bHidden = (($iFlags & OPT_ATT_HIDDEN) == OPT_ATT_HIDDEN);
-				foreach($aStates[$sState] as $sPrevState)
-				{
+				foreach($aStates[$sState] as $sPrevState) {
 					$iPrevFlags = self::GetAttributeFlags($sClass, $sPrevState, $sAttCode);
-					if (($iPrevFlags & OPT_ATT_HIDDEN) != OPT_ATT_HIDDEN)
-					{
+					if (($iPrevFlags & OPT_ATT_HIDDEN) != OPT_ATT_HIDDEN) {
 						$bReadOnly = $bReadOnly && (($iPrevFlags & OPT_ATT_READONLY) == OPT_ATT_READONLY); // if it is/was not readonly => then it's not
 					}
 					$bHidden = $bHidden && (($iPrevFlags & OPT_ATT_HIDDEN) == OPT_ATT_HIDDEN); // if it is/was not hidden => then it's not
 				}
-				if ($bReadOnly)
-				{
+
+				if ($bReadOnly) {
 					$iFlags = $iFlags | OPT_ATT_READONLY;
 				}
-				else
-				{
+				else {
 					$iFlags = $iFlags & ~OPT_ATT_READONLY;
 				}
-				if ($bHidden)
-				{
+
+				if ($bHidden) {
 					$iFlags = $iFlags | OPT_ATT_HIDDEN;
 				}
-				else
-				{
+				else {
 					$iFlags = $iFlags & ~OPT_ATT_HIDDEN;
 				}
 			}
@@ -2876,6 +2897,25 @@ abstract class MetaModel
 							$sObsolescence = self::$m_aClassParams[$sParent]['obsolescence_expression'];
 						}
 						self::$m_aClassParams[$sPHPClass]['obsolescence_expression'] = $sObsolescence;
+
+						// Inherit fields semantic
+						// - State attribute
+						$bParentHasStateAttribute = (isset(self::$m_aClassParams[$sParent]['state_attcode']) && !empty(self::$m_aClassParams[$sParent]['state_attcode']));
+						$bHasStateAttribute = (isset(self::$m_aClassParams[$sPHPClass]['state_attcode']) && !empty(self::$m_aClassParams[$sPHPClass]['state_attcode']));
+						if($bParentHasStateAttribute && !$bHasStateAttribute) {
+							// Set attribute code
+							self::$m_aClassParams[$sPHPClass]['state_attcode'] = self::$m_aClassParams[$sParent]['state_attcode'];
+
+							// Set states
+							self::$m_aStates[$sPHPClass] = self::$m_aStates[$sParent];
+						}
+						// - Image attribute
+						$bParentHasImageAttribute = (isset(self::$m_aClassParams[$sParent]['image_attcode']) && !empty(self::$m_aClassParams[$sParent]['image_attcode']));
+						$bHasImageAttribute = (isset(self::$m_aClassParams[$sPHPClass]['image_attcode']) && !empty(self::$m_aClassParams[$sPHPClass]['image_attcode']));
+						if($bParentHasImageAttribute && !$bHasImageAttribute) {
+							// Set attribute code
+							self::$m_aClassParams[$sPHPClass]['image_attcode'] = self::$m_aClassParams[$sParent]['image_attcode'];
+						}
 
 						foreach(MetaModel::EnumPlugins('iOnClassInitialization') as $sPluginClass => $oClassInit)
 						{

application/cmdbabstract.class.inc.php
@@ -834,7 +834,7 @@ EOF
 							if ((!$oAttDef->IsLinkSet()) && (($iFlags & OPT_ATT_HIDDEN) == 0) && !($oAttDef instanceof AttributeDashboard)) {
 								$sInputId = $this->m_iFormId.'_'.$sAttCode;
 								if ($oAttDef->IsWritable()) {
-									if ($sStateAttCode == $sAttCode) {
+									if (($sStateAttCode === $sAttCode) && (MetaModel::HasLifecycle($sClass))) {
 										// State attribute is always read-only from the UI
 										$sHTMLValue = $this->GetStateLabel();
 										$val = array(
@@ -4565,7 +4565,7 @@ HTML
 							//echo "<p>current value for $sAttCode : $currValue</p>";
 							$oDummyObj->Set($sAttCode, $currValue);
 							$aComments[$sAttCode] = '';
-							if ($sAttCode != MetaModel::GetStateAttributeCode($sClass))
+							if ($sAttCode != MetaModel::GetStateAttributeCode($sClass) || !MetaModel::HasLifecycle($sClass))
 							{
 								$aComments[$sAttCode] .= '<input type="checkbox" checked id="enable_'.$iFormId.'_'.$sAttCode.'"  onClick="ToggleField(this.checked, \''.$iFormId.'_'.$sAttCode.'\')"/>';
 							}
@@ -4625,7 +4625,7 @@ HTML
 								$oDummyObj->Set($sAttCode, null);
 							}
 							$aComments[$sAttCode] = '';
-							if ($sAttCode != MetaModel::GetStateAttributeCode($sClass))
+							if ($sAttCode != MetaModel::GetStateAttributeCode($sClass) || !MetaModel::HasLifecycle($sClass))
 							{
 								$aComments[$sAttCode] .= '<input type="checkbox" id="enable_'.$iFormId.'_'.$sAttCode.'" onClick="ToggleField(this.checked, \''.$iFormId.'_'.$sAttCode.'\')"/>';
 							}
@@ -4636,11 +4636,11 @@ HTML
 				}
 			}
 
-			$sStateAttCode = MetaModel::GetStateAttributeCode($sClass);
-			if (($sStateAttCode != '') && ($oDummyObj->GetState() == ''))
+			if (MetaModel::HasLifecycle($sClass) && ($oDummyObj->GetState() == ''))
 			{
 				// Hmmm, it's not gonna work like this ! Set a default value for the "state"
 				// Maybe we should use the "state" that is the most common among the objects...
+				$sStateAttCode = MetaModel::GetStateAttributeCode($sClass);
 				$aMultiValues = $aValues[$sStateAttCode];
 				uasort($aMultiValues, 'MyComparison');
 				foreach($aMultiValues as $sCurrValue => $aVal)

core/dbobject.class.php
@@ -1673,6 +1673,7 @@ abstract class DBObject implements iDisplay
 	public function GetAttributeFlags($sAttCode, &$aReasons = array(), $sTargetState = '')
 	{
 		$iFlags = 0; // By default (if no life cycle) no flag at all
+		$sClass = get_class($this);
 
 		$aReadOnlyAtts = $this->GetReadOnlyAttributes();
 		if (($aReadOnlyAtts != null) && (in_array($sAttCode, $aReadOnlyAtts)))
@@ -1680,16 +1681,16 @@ abstract class DBObject implements iDisplay
 			return OPT_ATT_READONLY;
 		}
 
-		$sStateAttCode = MetaModel::GetStateAttributeCode(get_class($this));
-		if (!empty($sStateAttCode))
+		if (MetaModel::HasLifecycle($sClass))
 		{
 			if ($sTargetState != '')
 			{
-				$iFlags = MetaModel::GetAttributeFlags(get_class($this), $sTargetState, $sAttCode);			
+				$iFlags = MetaModel::GetAttributeFlags($sClass, $sTargetState, $sAttCode);
 			}
 			else
 			{
-				$iFlags = MetaModel::GetAttributeFlags(get_class($this), $this->Get($sStateAttCode), $sAttCode);
+				$sStateAttCode = MetaModel::GetStateAttributeCode($sClass);
+				$iFlags = MetaModel::GetAttributeFlags($sClass, $this->Get($sStateAttCode), $sAttCode);
 			}
 		}
 		$aReasons = array();
@@ -1742,10 +1743,10 @@ abstract class DBObject implements iDisplay
     public function GetTransitionFlags($sAttCode, $sStimulus, &$aReasons = array(), $sOriginState = '')
     {
         $iFlags = 0; // By default (if no lifecycle) no flag at all
+	    $sClass = get_class($this);
 
-        $sStateAttCode = MetaModel::GetStateAttributeCode(get_class($this));
         // If no state attribute, there is no lifecycle
-        if (empty($sStateAttCode))
+        if (!MetaModel::HasLifecycle($sClass))
         {
             return $iFlags;
         }
@@ -1753,6 +1754,7 @@ abstract class DBObject implements iDisplay
         // Retrieving current state if necessary
         if ($sOriginState === '')
         {
+	        $sStateAttCode = MetaModel::GetStateAttributeCode($sClass);
             $sOriginState = $this->Get($sStateAttCode);
         }
 
@@ -1760,7 +1762,7 @@ abstract class DBObject implements iDisplay
         $iAttributeFlags = $this->GetAttributeFlags($sAttCode, $aReasons, $sOriginState);
 
         // Retrieving transition flags
-        $iTransitionFlags = MetaModel::GetTransitionFlags(get_class($this), $sOriginState, $sStimulus, $sAttCode);
+        $iTransitionFlags = MetaModel::GetTransitionFlags($sClass, $sOriginState, $sStimulus, $sAttCode);
 
         // Merging transition flags with attribute flags
         $iFlags = $iTransitionFlags | $iAttributeFlags;
@@ -1811,10 +1813,12 @@ abstract class DBObject implements iDisplay
 	public function GetInitialStateAttributeFlags($sAttCode, &$aReasons = array())
 	{
 		$iFlags = 0;
-		$sStateAttCode = MetaModel::GetStateAttributeCode(get_class($this));
-		if (!empty($sStateAttCode))
+		$sClass = get_class($this);
+
+		if (MetaModel::HasLifecycle($sClass))
 		{
-			$iFlags = MetaModel::GetInitialStateAttributeFlags(get_class($this), $this->Get($sStateAttCode), $sAttCode);
+			$sStateAttCode = MetaModel::GetStateAttributeCode($sClass);
+			$iFlags = MetaModel::GetInitialStateAttributeFlags($sClass, $this->Get($sStateAttCode), $sAttCode);
 		}
 		return $iFlags; // No need to care about the synchro flags since we'll be creating a new object anyway
 	}
@@ -3631,11 +3635,12 @@ abstract class DBObject implements iDisplay
      */
 	public function EnumTransitions()
 	{
-		$sStateAttCode = MetaModel::GetStateAttributeCode(get_class($this));
-		if (empty($sStateAttCode)) return array();
+		$sClass = get_class($this);
+		if (!MetaModel::HasLifecycle($sClass)) return array();
 
+		$sStateAttCode = MetaModel::GetStateAttributeCode($sClass);
 		$sState = $this->Get($sStateAttCode);
-		return MetaModel::EnumTransitions(get_class($this), $sState);
+		return MetaModel::EnumTransitions($sClass, $sState);
 	}
 
     /**
@@ -3681,14 +3686,14 @@ abstract class DBObject implements iDisplay
 	public function ApplyStimulus($sStimulusCode, $bDoNotWrite = false)
 	{
 		$sClass = get_class($this);
-		$sStateAttCode = MetaModel::GetStateAttributeCode($sClass);
-		if (empty($sStateAttCode))
+		if (!MetaModel::HasLifecycle($sClass))
 		{
 			throw new CoreException('No lifecycle for the class '.$sClass);
 		}
 
 		MyHelpers::CheckKeyInArray('object lifecycle stimulus', $sStimulusCode, MetaModel::EnumStimuli($sClass));
 
+		$sStateAttCode = MetaModel::GetStateAttributeCode($sClass);
 		$aStateTransitions = $this->EnumTransitions();
 		if (!array_key_exists($sStimulusCode, $aStateTransitions))
 		{
@@ -5338,10 +5343,10 @@ abstract class DBObject implements iDisplay
      */
 	public static function MakeDefaultInstance($sClass)
 	{
-		$sStateAttCode = MetaModel::GetStateAttributeCode($sClass);
 		$oObj = MetaModel::NewObject($sClass);
-		if (!empty($sStateAttCode))
+		if (MetaModel::HasLifecycle($sClass))
 		{
+			$sStateAttCode = MetaModel::GetStateAttributeCode($sClass);
 			$sTargetState = MetaModel::GetDefaultState($sClass);
 			$oObj->Set($sStateAttCode, $sTargetState);
 		}

datamodels/2.x/itop-change-mgmt-itil/datamodel.itop-change-mgmt-itil.xml
@@ -21,6 +21,9 @@
             <attribute id="ref"/>
           </attributes>
         </naming>
+        <fields_semantic>
+            <state_attribute>status</state_attribute>
+        </fields_semantic>
         <icon>images/change.png</icon>
         <reconciliation>
           <attributes>
@@ -199,7 +202,6 @@
         </field>
       </fields>
       <lifecycle>
-        <attribute>status</attribute>
         <highlight_scale>
           <item id="approved">
             <rank>1</rank>

datamodels/2.x/itop-change-mgmt/datamodel.itop-change-mgmt.xml
@@ -21,6 +21,9 @@
                         <attribute id="ref"/>
                     </attributes>
                 </naming>
+                <fields_semantic>
+                    <state_attribute>status</state_attribute>
+                </fields_semantic>
                 <icon>images/change.png</icon>
                 <reconciliation>
                     <attributes>
@@ -160,7 +163,6 @@
                         <icon>images/change-closed.png</icon>
                     </item>
                 </highlight_scale>
-                <attribute>status</attribute>
                 <stimuli>
                     <stimulus id="ev_assign" xsi:type="StimulusUserAction"/>
                     <stimulus id="ev_plan" xsi:type="StimulusUserAction"/>

datamodels/2.x/itop-config-mgmt/datamodel.itop-config-mgmt.xml
@@ -256,6 +256,9 @@
 						<attribute id="name"/>
 					</attributes>
 				</naming>
+				<fields_semantic>
+					<state_attribute>status</state_attribute>
+				</fields_semantic>
 				<icon>images/server.png</icon>
 				<reconciliation>
 					<attributes>
@@ -1516,6 +1519,9 @@
 						<attribute id="name"/>
 					</attributes>
 				</naming>
+				<fields_semantic>
+					<state_attribute>status</state_attribute>
+				</fields_semantic>
 				<icon>images/solution.png</icon>
 				<reconciliation>
 					<attributes>
@@ -1657,6 +1663,9 @@
 						<attribute id="name"/>
 					</attributes>
 				</naming>
+				<fields_semantic>
+					<state_attribute>status</state_attribute>
+				</fields_semantic>
 				<icon>images/business-process.png</icon>
 				<reconciliation>
 					<attributes>
@@ -1768,6 +1777,9 @@
 						<attribute id="system_name"/>
 					</attributes>
 				</naming>
+				<fields_semantic>
+					<state_attribute>status</state_attribute>
+				</fields_semantic>
 				<icon>images/application.png</icon>
 				<reconciliation>
 					<attributes>
@@ -5972,6 +5984,9 @@
 						<attribute id="name"/>
 					</attributes>
 				</naming>
+				<fields_semantic>
+					<state_attribute>status</state_attribute>
+				</fields_semantic>
 				<icon>images/group.png</icon>
 				<reconciliation>
 					<attributes>

datamodels/2.x/itop-incident-mgmt-itil/datamodel.itop-incident-mgmt-itil.xml
@@ -21,6 +21,9 @@
 						<attribute id="ref"/>
 					</attributes>
 				</naming>
+                <fields_semantic>
+                    <state_attribute>status</state_attribute>
+                </fields_semantic>
 				<icon>images/incident.png</icon>
 				<reconciliation>
 					<attributes>
@@ -376,7 +379,6 @@
 						<icon>images/incident-closed.png</icon>
 					</item>
 				</highlight_scale>
-				<attribute>status</attribute>
 				<stimuli>
 					<stimulus id="ev_assign" xsi:type="StimulusUserAction"/>
 					<stimulus id="ev_reassign" xsi:type="StimulusUserAction"/>

datamodels/2.x/itop-problem-mgmt/datamodel.itop-problem-mgmt.xml
@@ -21,6 +21,9 @@
                         <attribute id="ref"/>
                     </attributes>
                 </naming>
+                <fields_semantic>
+                    <state_attribute>status</state_attribute>
+                </fields_semantic>
                 <icon>images/problem.png</icon>
                 <reconciliation>
                     <attributes>
@@ -160,7 +163,6 @@
                 </field>
             </fields>
             <lifecycle>
-                <attribute>status</attribute>
                 <stimuli>
                     <stimulus id="ev_assign" xsi:type="StimulusUserAction"/>
                     <stimulus id="ev_reassign" xsi:type="StimulusUserAction"/>

commit 9ea25188ba58f138a59a3e694c1e960396adb04c

N°3925 - Fix state flags not taken into account correctly by a child class (regression from f2ff5a4e)

core/metamodel.class.php
@@ -3083,8 +3083,7 @@ abstract class MetaModel
 							// Set attribute code
 							self::$m_aClassParams[$sPHPClass]['state_attcode'] = self::$m_aClassParams[$sParent]['state_attcode'];
 
-							// Set states
-							self::$m_aStates[$sPHPClass] = self::$m_aStates[$sParent];
+							// Note: Don't set self::$m_aStates[$sPHPClass], it has already been done by self::Init_DefineState()
 						}
 						// - Image attribute
 						$bParentHasImageAttribute = (isset(self::$m_aClassParams[$sParent]['image_attcode']) && !empty(self::$m_aClassParams[$sParent]['image_attcode']));