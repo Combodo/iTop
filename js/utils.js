e29f1825be8a841fc4e094790e51f52e34d253d3
Pierre Goiffon <pierre.goiffon@combodo.com>
2021-11-02 17:05:13 +0100

    N°4367 Fix "redeclaration of const CombodoSanitizer"
    The utils.js can be included more than once in old iTop branches :( This is fixed in 3.0.0 (develop branch)
    
    Also add missing ";"

--- js/utils.js
+++ js/utils.js
@@ -730,8 +730,7 @@ else {
 Dict.S = function (sEntry) {
 	if (sEntry in Dict._entries) {
 		return Dict._entries[sEntry];
-	}
-	else {
+	} else {
 		return sEntry;
 	}
 };
@@ -739,7 +738,7 @@ Dict.Format = function () {
 	var args = Array.from(arguments);
 	args[0] = Dict.S(arguments[0]);
 	return Format(args);
-}
+};
 
 
 
@@ -751,7 +750,7 @@ Dict.Format = function () {
  * @api
  * @since 2.6.5 2.7.6 3.0.0 N°4367
  */
-const CombodoSanitizer = {
+var CombodoSanitizer = {
 	ENUM_SANITIZATION_FILTER_INTEGER: 'integer',
 	ENUM_SANITIZATION_FILTER_STRING: 'string',
 	ENUM_SANITIZATION_FILTER_CONTEXT_PARAM: 'context_param',
@@ -805,4 +804,4 @@ const CombodoSanitizer = {
 			return sDefaultValue;
 		}
 	}
-}
+};
