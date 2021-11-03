e8d314e1f63e312229ac85075d248e4db1b633ca
Pierre Goiffon <pierre.goiffon@combodo.com>
2021-11-03 10:46:59 +0100

    :white_check_mark: N°4367 Fix \privUITransactionFileTest::testIsTransactionValid * change user name for when password policy is active * admin user doesn't exist on Jenkins : create a second user * test UserRights::Login return value * document that we depend on the sample data

--- test/application/privUITransactionFileTest.php
+++ test/application/privUITransactionFileTest.php
@@ -1,31 +1,40 @@
 <?php
 class privUITransactionFileTest extends \Combodo\iTop\Test\UnitTest\ItopDataTestCase
 {
-	const USER_TEST_LOGIN = 'support_test_privUITransaction';
+	/** @var int ID of the "support agent" pofile in the sample data */
+	const SAMPLE_DATA_SUPPORT_PROFILE_ID = 5;
+	const USER1_TEST_LOGIN = 'user1_support_test_privUITransaction';
+	const USER2_TEST_LOGIN = 'user2_support_test_privUITransaction';
 
 	/**
 	 * @throws \SecurityException
+	 * @uses self::SAMPLE_DATA_SUPPORT_PROFILE_ID
+	 * @uses self::USER1_TEST_LOGIN
+	 * @uses self::USER2_TEST_LOGIN
 	 */
-	public function testIsTransactionValid() {
-		$this->markTestSkipped('Still need some work for Jenkins (Token created by support user must be invalid in the admin user context)');
+	public function testIsTransactionValid()
+	{
+		$this->CreateUser(static::USER1_TEST_LOGIN, self::SAMPLE_DATA_SUPPORT_PROFILE_ID);
+		$this->CreateUser(static::USER2_TEST_LOGIN, self::SAMPLE_DATA_SUPPORT_PROFILE_ID);
 
-		$this->CreateUser(static::USER_TEST_LOGIN, 5); // profile:5 is "Support agent"
-
-		// create token in the support user context
-		UserRights::Login(self::USER_TEST_LOGIN);
+		// create token in the user1 context
+		$bUser1Login1 = UserRights::Login(self::USER1_TEST_LOGIN);
+		$this->assertTrue($bUser1Login1, 'Login with user1 throw an error');
 		$sTransactionIdUserSupport = privUITransactionFile::GetNewTransactionId();
 		$bResult = privUITransactionFile::IsTransactionValid($sTransactionIdUserSupport, false);
 		$this->assertTrue($bResult, 'Token created by support user must be valid in the support user context');
 
-		// test token in the admin user context
-		UserRights::Login('admin');
+		// test token in the user2 context
+		$bUser2Login = UserRights::Login(self::USER2_TEST_LOGIN);
+		$this->assertTrue($bUser2Login, 'Login with user2 throw an error');
 		$bResult = privUITransactionFile::IsTransactionValid($sTransactionIdUserSupport, false);
 		$this->assertFalse($bResult, 'Token created by support user must be invalid in the admin user context');
 		$bResult = privUITransactionFile::RemoveTransaction($sTransactionIdUserSupport);
 		$this->assertFalse($bResult, 'Token created by support user cannot be removed in the admin user context');
 
-		// test other methods in the support user context
-		UserRights::Login(self::USER_TEST_LOGIN);
+		// test other methods in the user1 context
+		$bUser1Login2 = UserRights::Login(self::USER1_TEST_LOGIN);
+		$this->assertTrue($bUser1Login2, 'Login with user1 throw an error');
 		$bResult = privUITransactionFile::RemoveTransaction($sTransactionIdUserSupport);
 		$this->assertTrue($bResult, 'Token created by support user must be removed in the support user context');
 	}
