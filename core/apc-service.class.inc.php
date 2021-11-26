f1037147a9f55363ef4e209d894002f45d3d568d
odain <olivier.dain@combodo.com>
2021-11-26 09:17:19 +0100

    N°4125 - phpdoc only coming from pull request Thanks to Hispka and  piRGoif

--- core/apc-service.class.inc.php
+++ core/apc-service.class.inc.php
@@ -8,14 +8,19 @@ class ApcService {
 	public function __construct() {
 	}
 
+	/**
+	 * @param string $function_name
+	 * @return bool
+	 * @see function_exists()
+	 */
 	public function function_exists($function_name) {
 		return function_exists($function_name);
 	}
 
 	/**
-	 * @param $key string|array
+	 * @param string|array $key
 	 * @return mixed
-	 * @see apc_fetch
+	 * @see apc_fetch()
 	 */
 	function apc_fetch($key)
 	{
@@ -27,7 +32,7 @@ class ApcService {
 	 * @param $var
 	 * @param int $ttl
 	 * @return array|bool
-	 * @see apc_store
+	 * @see apc_store()
 	 */
 	function apc_store($key, $var = NULL, $ttl = 0)
 	{
