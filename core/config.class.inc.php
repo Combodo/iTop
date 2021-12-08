b22b25aa5f470b8fa83ca0e1d4e5f17261ec48aa
Pierre Goiffon <pierre.goiffon@combodo.com>
2021-12-08 12:21:50 +0100

    :bulb: Update comment to match regexp
    Co-authored-by: Thomas Casteleyn <thomas.casteleyn@super-visions.com>

--- core/config.class.inc.php
+++ core/config.class.inc.php
@@ -856,7 +856,7 @@ class Config
 			'description' => 'Regular expression to validate/detect the format of an URL (URL attributes and Wiki formatting for Text attributes)',
 			'default' => /** @lang RegExp */
 			'(https?|ftp)\://([a-zA-Z0-9+!*(),;?&=\$_.-]+(\:[a-zA-Z0-9+!*(),;?&=\$_.-]+)?@)?([a-zA-Z0-9-.]{3,})(\:[0-9]{2,5})?(/([a-zA-Z0-9:%+\$_-]\.?)+)*/?(\?[a-zA-Z+&\$_.-][a-zA-Z0-9;:[\]@&%=+/\$_.-]*)?(#[a-zA-Z_.-][a-zA-Z0-9+\$_.-]*)?',
-			// SCHEME.......... USER....................... PASSWORD.......................... HOST/IP........... PORT.......... PATH........................ GET............................................ ANCHOR............................
+			// SCHEME....... USER....................... PASSWORD.......................... HOST/IP........... PORT.......... PATH......................... GET............................................ ANCHOR..........................
 			// Example: http://User:passWord@127.0.0.1:8888/patH/Page.php?arrayArgument[2]=something:blah20#myAnchor
 			// RegExp source: http://www.php.net/manual/fr/function.preg-match.php#93824
 			// Update with N°4515
