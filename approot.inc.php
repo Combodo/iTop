e879ae2f1106f76afbe701c1d003b0cf1d83af21
acognet <anne-catherine.cognet@combodo.com>
2021-12-23 14:14:51 +0100

    Change version number to 3.1.0

--- approot.inc.php
+++ approot.inc.php
@@ -6,6 +6,6 @@ define('APPCONF', APPROOT.'conf/');
 /**
  * iTop framework Version
  */
-define('ITOP_DESIGN_LATEST_VERSION', '3.0');
+define('ITOP_DESIGN_LATEST_VERSION', '3.1');
 
 require_once APPROOT.'bootstrap.inc.php';
