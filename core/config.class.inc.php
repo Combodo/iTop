64e8aa5fee8219cca43e1e0c4a93e471c51e5dc6
Pierre Goiffon <pierre.goiffon@combodo.com>
2022-01-26 10:31:20 +0100

    N°4714 New ITOP_CORE_VERSION constant See following constants PHPDoc for information : * core/config.class.inc.php ITOP_VERSION * core/config.class.inc.php ITOP_CORE_VERSION * setup/itopdesignformat.class.inc.php ITOP_DESIGN_LATEST_VERSION

--- core/config.class.inc.php
+++ core/config.class.inc.php
@@ -27,11 +27,20 @@ define('ITOP_APPLICATION_SHORT', 'iTop');
  * Constant containing the application version
  * Warning: this might be different from iTop core version!
  *
- * @see     ITOP_DESIGN_LATEST_VERSION for iTop core version
- *
- * @used-by utils::GetItopPatchVersion
+ * @see ITOP_CORE_VERSION to get iTop core version
  */
 define('ITOP_VERSION', '2.7.0-dev');
+
+/**
+ * Constant containing the iTop core version, whatever application was built
+ *
+ * Note that in iTop 3.0.0 we used {@see ITOP_DESIGN_LATEST_VERSION} to get core version.
+ * When releasing, both constants should be updated : see `.make/release/update-versions.php` for that !
+ *
+ * @since 2.7.7 3.0.1 3.1.1 N°4714 constant creation
+ * @used-by utils::GetItopPatchVersion
+ */
+define('ITOP_CORE_VERSION', '2.7.7');
 
 define('ITOP_REVISION', 'svn');
 define('ITOP_BUILD_DATE', '$WCNOW$');
