f0715baf7deaef796cf8f64339955e7d621c12da
Pierre Goiffon <pierre.goiffon@combodo.com>
2022-02-01 11:34:09 +0100

    N°4714 move constant from core/config.class.inc.php to approot.inc.php see N°4406

--- approot.inc.php
+++ approot.inc.php
@@ -3,4 +3,17 @@
 define('APPROOT', dirname(__FILE__).'/');
 define('APPCONF', APPROOT.'conf/');
 
+
+/**
+ * Constant containing the iTop core version, whatever application was built
+ *
+ * Note that in iTop 3.0.0 we used {@see ITOP_DESIGN_LATEST_VERSION} to get core version.
+ * When releasing, both constants should be updated : see `.make/release/update-versions.php` for that !
+ *
+ * @since 2.7.7 3.0.1 3.1.0 N°4714 constant creation
+ * @used-by utils::GetItopPatchVersion
+ */
+define('ITOP_CORE_VERSION', '2.7.7');
+
+
 require_once APPROOT.'bootstrap.inc.php';
