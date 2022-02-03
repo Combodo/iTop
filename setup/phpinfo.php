ca1b1854cbaac82fddcc460b9b11ce0d8413b1f3
Pierre Goiffon <pierre.goiffon@combodo.com>
2022-02-03 15:37:44 +0100

    N°4698 if no iTop installation then display a proper message instead of an exception

--- setup/phpinfo.php
+++ setup/phpinfo.php
@@ -17,11 +17,27 @@
  * You should have received a copy of the GNU Affero General Public License
  */
 
+/**
+ * Since N°1934 this page is accessible only to iTop admin users
+ * @since 2.5.2 2.6.1 2.7.0 N°1934 must login as admin user to use
+ *        to check if PHP is up and running, use phpcheck.php !
+ */
 require_once('../approot.inc.php');
 
-require_once(APPROOT.'/application/startup.inc.php');
+try {
+	require_once(APPROOT.'/application/startup.inc.php');
+} catch (Exception $e) {
+	// This means we don't have a valid iTop installation running
+	echo <<<EOF
+No valid installation found, cannot continue !<br>
+If you need to check that PHP is running, use <a href="phpcheck.php">phpcheck.php</a>
+EOF;
+	die(-1);
+}
 
 require_once(APPROOT.'/application/loginwebpage.class.inc.php');
-LoginWebPage::DoLogin(true); // Check user rights and prompt if needed
+LoginWebPage::DoLogin(true); // Check user rights and prompt if needed (N°1934)
+
+/** @noinspection ForgottenDebugOutputInspection */
 phpinfo();
 ?>
