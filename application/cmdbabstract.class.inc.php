Combodo/iTop

commit 0d26211dbed8da6c8ff253e5e9fc7d97a6d11200

Fix error in object details : "Too few arguments to function MenuBlock::GetRenderContent()"

application/cmdbabstract.class.inc.php
@@ -391,7 +391,7 @@ JS
 			$oSingletonFilter = new DBObjectSearch(get_class($this));
 			$oSingletonFilter->AddCondition('id', $this->GetKey(), '=');
 			$oBlock = new MenuBlock($oSingletonFilter, 'details', false);
-			$oActionMenuBlock = $oBlock->GetRenderContent($oPage);
+			$oActionMenuBlock = $oBlock->GetRenderContent($oPage, [], uniqid('', true));
 			$aHeaderBlocks['toolbar'][$oActionMenuBlock->GetId()] = $oActionMenuBlock;
 		}
 

commit 01b94f42feb366134f542a8bd61affafb74bce6f

Fix object's actions menu cannot be open (regression from 0d26211d)

application/cmdbabstract.class.inc.php
@@ -391,7 +391,8 @@ JS
 			$oSingletonFilter = new DBObjectSearch(get_class($this));
 			$oSingletonFilter->AddCondition('id', $this->GetKey(), '=');
 			$oBlock = new MenuBlock($oSingletonFilter, 'details', false);
-			$oActionMenuBlock = $oBlock->GetRenderContent($oPage, [], uniqid('', true));
+			$sActionMenuId = utils::Sanitize(uniqid('', true), '', utils::ENUM_SANITIZATION_FILTER_ELEMENT_IDENTIFIER);
+			$oActionMenuBlock = $oBlock->GetRenderContent($oPage, [], $sActionMenuId);
 			$aHeaderBlocks['toolbar'][$oActionMenuBlock->GetId()] = $oActionMenuBlock;
 		}
 