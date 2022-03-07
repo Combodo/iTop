e347989dcb06d6fd41df3b4c12cf02e91777b4b1
acognet <anne-catherine.cognet@combodo.com>
2022-03-07 12:05:54 +0100

    N°4664 - Core Update : block zip file upload until instance declared OK

--- datamodels/2.x/itop-core-update/dictionaries/fr.dict.itop-core-update.php
+++ datamodels/2.x/itop-core-update/dictionaries/fr.dict.itop-core-update.php
@@ -76,6 +76,7 @@ Dict::Add('FR FR', 'French', 'Français', array(
 	'iTopUpdate:UI:CanCoreUpdate:No' => 'L\'application ne peut pas être mise à jour : %1$s',
 	'iTopUpdate:UI:CanCoreUpdate:Warning' => 'Attention : la mise à jour de l\'application peut échouer : %1$s',
 	'iTopUpdate:UI:CannotUpdateUseSetup' => '<b>Des fichiers modifiés ont été détectés</b>, une mise à jour partielle ne peut pas être effectuée.<br />Suivez la <a href="%2$s"> procedure</a> pour mettre à jour manuellement votre iTop. Vous devez utiliser la page <a href="%1$s">d\'installation</a> pour mettre à jour l\'application.',
+	'iTopUpdate:UI:CheckInProgress'=>'Veuillez patienter pendant la vérification des fichiers',
 
 	// Setup Messages
     'iTopUpdate:UI:SetupMessage:Ready' => 'Prêt pour l\\installation',
--- datamodels/2.x/itop-core-update/dictionaries/zh_cn.dict.itop-core-update.php
+++ datamodels/2.x/itop-core-update/dictionaries/zh_cn.dict.itop-core-update.php
@@ -76,6 +76,7 @@ Dict::Add('ZH CN', 'Chinese', '简体中文', array(
 	'iTopUpdate:UI:CanCoreUpdate:No' => '应用无法升级: %1$s',
 	'iTopUpdate:UI:CanCoreUpdate:Warning' => '警告: 应用升级可能失败: %1$s',
 	'iTopUpdate:UI:CannotUpdateUseSetup' => '<b>Some modified files were detected</b>, a partial update cannot be executed.</br>Follow the <a href="%2$s"> procedure</a> in order to manually upgrade your iTop. You must use the <a href="%1$s">setup</a> to update the application.~~',
+	'iTopUpdate:UI:CheckInProgress'=>'Please wait during integrity check~~',
 
 	// Setup Messages
     'iTopUpdate:UI:SetupMessage:Ready' => '准备开始',
