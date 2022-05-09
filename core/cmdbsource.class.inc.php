b4e623d175b64e2925d96d0b55fe615826f680d4
acognet <anne-catherine.cognet@combodo.com>
2022-05-09 16:08:33 +0200

    Fix "undefined offset" error during "Anonymize" action

--- core/cmdbsource.class.inc.php
+++ core/cmdbsource.class.inc.php
@@ -705,12 +705,11 @@ class CMDBSource
 	private static function Commit()
 	{
 		$aStackTrace = debug_backtrace(DEBUG_BACKTRACE_PROVIDE_OBJECT , 3);
-		if (isset($aStackTrace[2])) {
+		if (array_key_exists(2,$aStackTrace)) {
 			$sCaller = 'From '.$aStackTrace[1]['file'].'('.$aStackTrace[1]['line'].'): '.$aStackTrace[2]['class'].'->'.$aStackTrace[2]['function'].'()';
 		} else {
 			$sCaller = 'From '.$aStackTrace[1]['file'].'('.$aStackTrace[1]['line'].')';
-		}
-		if (!self::IsInsideTransaction()) {
+		}		if (!self::IsInsideTransaction()) {
 			// should not happen !
 			IssueLog::Error("No Transaction COMMIT $sCaller", LogChannels::CMDB_SOURCE);
 			throw new MySQLNoTransactionException('Trying to commit transaction whereas none have been started !', null);
@@ -743,7 +742,7 @@ class CMDBSource
 	private static function Rollback()
 	{
 		$aStackTrace = debug_backtrace(DEBUG_BACKTRACE_PROVIDE_OBJECT , 3);
-		if (isset($aStackTrace[2])) {
+		if (array_key_exists(2,$aStackTrace)) {
 			$sCaller = 'From '.$aStackTrace[1]['file'].'('.$aStackTrace[1]['line'].'): '.$aStackTrace[2]['class'].'->'.$aStackTrace[2]['function'].'()';
 		} else {
 			$sCaller = 'From '.$aStackTrace[1]['file'].'('.$aStackTrace[1]['line'].')';
