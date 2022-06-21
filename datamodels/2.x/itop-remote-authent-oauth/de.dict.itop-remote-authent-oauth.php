9b825cb5298a5587b0718b77bd89d8058cca90d0
Eric Espie <eric.espie@combodo.com>
2022-06-21 16:05:34 +0200

    N°5102 - Allow to send emails using GSuite SMTP and OAuth - Rework

---
+++ datamodels/2.x/itop-remote-authent-oauth/de.dict.itop-remote-authent-oauth.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2013 XXXXX
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('DE DE', 'German', 'Deutsch', [
+	'Menu:CreateMailbox' => 'Create a mailbox...~~',
+	'Menu:RemoteOAuth' => 'OAuth Client~~',
+	'Menu:GenerateTokens' => 'Generate access tokens...~~',
+	'Menu:RegenerateTokens' => 'Regenerate access tokens...~~',
+
+	'itop-remote-authent-oauth/Operation:CreateMailBox/Title' => 'Mailbox creation~~',
+
+	'itop-remote-authent-oauth:UsedForSMTP' => 'This OAuth client is used for SMTP~~',
+	'itop-remote-authent-oauth:TestSMTP' => 'Email send test~~',
+	'itop-remote-authent-oauth:MissingRemoteAuthentOAuth' => 'Missing Oauth client for user name %1$s~~',
+	'itop-remote-authent-oauth:Message:TokenCreated' => 'Access token created~~',
+	'itop-remote-authent-oauth:Message:TokenRecreated' => 'Access token regenerated~~',
+
+	'Class:RemoteAuthentOAuthGoogle' => 'OAuth client for Google~~',
+	'Class:RemoteAuthentOAuthAzure' => 'OAuth client for Microsoft Azure~~',
+]);
+
+//
+// Class: RemoteAuthentOAuth
+//
+
+Dict::Add('DE DE', 'German', 'Deutsch', [
+	'Class:RemoteAuthentOAuth' => 'Oauth Client~~',
+	'Class:RemoteAuthentOAuth/Name' => '%1$s-%%2$s~~',
+	'Class:RemoteAuthentOAuth/Attribute:provider' => 'Provider~~',
+	'Class:RemoteAuthentOAuth/Attribute:provider+' => '~~',
+	'Class:RemoteAuthentOAuth/Attribute:name' => 'Login~~',
+	'Class:RemoteAuthentOAuth/Attribute:name+' => '~~',
+	'Class:RemoteAuthentOAuth/Attribute:scope' => 'Scope~~',
+	'Class:RemoteAuthentOAuth/Attribute:scope+' => '~~',
+	'Class:RemoteAuthentOAuth/Attribute:description' => 'Description~~',
+	'Class:RemoteAuthentOAuth/Attribute:description+' => '~~',
+	'Class:RemoteAuthentOAuth/Attribute:client_id' => 'Client id~~',
+	'Class:RemoteAuthentOAuth/Attribute:client_id+' => '~~',
+	'Class:RemoteAuthentOAuth/Attribute:client_secret' => 'Client secret~~',
+	'Class:RemoteAuthentOAuth/Attribute:client_secret+' => '~~',
+	'Class:RemoteAuthentOAuth/Attribute:refresh_token' => 'Refresh token~~',
+	'Class:RemoteAuthentOAuth/Attribute:refresh_token+' => '~~',
+	'Class:RemoteAuthentOAuth/Attribute:refresh_token_expiration' => 'Refresh token expiration~~',
+	'Class:RemoteAuthentOAuth/Attribute:refresh_token_expiration+' => '~~',
+	'Class:RemoteAuthentOAuth/Attribute:token' => 'Access token~~',
+	'Class:RemoteAuthentOAuth/Attribute:token+' => '~~',
+	'Class:RemoteAuthentOAuth/Attribute:token_expiration' => 'Access token expiration~~',
+	'Class:RemoteAuthentOAuth/Attribute:token_expiration+' => '~~',
+	'Class:RemoteAuthentOAuth/Attribute:redirect_url' => 'Redirect url~~',
+	'Class:RemoteAuthentOAuth/Attribute:redirect_url+' => '~~',
+	'Class:RemoteAuthentOAuth/Attribute:mailbox_list' => 'Mailbox list~~',
+	'Class:RemoteAuthentOAuth/Attribute:mailbox_list+' => '~~',
+]);
+
--- datamodels/2.x/itop-remote-authent-oauth/en.dict.itop-remote-authent-oauth.php
+++ datamodels/2.x/itop-remote-authent-oauth/en.dict.itop-remote-authent-oauth.php
@@ -7,21 +7,21 @@
  */
 
 Dict::Add('EN US', 'English', 'English', [
-	'Menu:CreateMailbox' => 'Create a mailbox',
-	'Menu:RemoteOAuth' => 'Remote OAuth',
-	'Menu:GenerateTokens' => 'Generate tokens',
-	'Menu:RegenerateTokens' => 'Regenerate tokens',
+	'Menu:CreateMailbox' => 'Create a mailbox...',
+	'Menu:RemoteOAuth' => 'OAuth Client',
+	'Menu:GenerateTokens' => 'Generate access tokens...',
+	'Menu:RegenerateTokens' => 'Regenerate access tokens...',
 
 	'itop-remote-authent-oauth/Operation:CreateMailBox/Title' => 'Mailbox creation',
 
-	'itop-remote-authent-oauth:UsedForSMTP' => 'This connection is used for SMTP',
-	'itop-remote-authent-oauth:TestSMTP' => 'Email test',
-	'itop-remote-authent-oauth:MissingRemoteAuthentOAuth' => 'Missing Remote Authentication (OAuth) for user name %1$s',
-	'itop-remote-authent-oauth:Message:TokenCreated' => 'Connection token created',
-	'itop-remote-authent-oauth:Message:TokenRecreated' => 'Connection token regenerated',
+	'itop-remote-authent-oauth:UsedForSMTP' => 'This OAuth client is used for SMTP',
+	'itop-remote-authent-oauth:TestSMTP' => 'Email send test',
+	'itop-remote-authent-oauth:MissingRemoteAuthentOAuth' => 'Missing Oauth client for user name %1$s',
+	'itop-remote-authent-oauth:Message:TokenCreated' => 'Access token created',
+	'itop-remote-authent-oauth:Message:TokenRecreated' => 'Access token regenerated',
 
-	'Class:RemoteAuthentOAuthGoogle' => 'Remote Google Authentication (OAuth)',
-	'Class:RemoteAuthentOAuthAzure' => 'Remote Microsoft Azure Authentication (OAuth)',
+	'Class:RemoteAuthentOAuthGoogle' => 'OAuth client for Google',
+	'Class:RemoteAuthentOAuthAzure' => 'OAuth client for Microsoft Azure',
 ]);
 
 //
@@ -29,7 +29,7 @@ Dict::Add('EN US', 'English', 'English', [
 //
 
 Dict::Add('EN US', 'English', 'English', [
-	'Class:RemoteAuthentOAuth' => 'Remote Authentication (OAuth)',
+	'Class:RemoteAuthentOAuth' => 'Oauth Client',
 	'Class:RemoteAuthentOAuth/Name' => '%1$s-%%2$s',
 	'Class:RemoteAuthentOAuth/Attribute:provider' => 'Provider',
 	'Class:RemoteAuthentOAuth/Attribute:provider+' => '',
@@ -47,10 +47,13 @@ Dict::Add('EN US', 'English', 'English', [
 	'Class:RemoteAuthentOAuth/Attribute:refresh_token+' => '',
 	'Class:RemoteAuthentOAuth/Attribute:refresh_token_expiration' => 'Refresh token expiration',
 	'Class:RemoteAuthentOAuth/Attribute:refresh_token_expiration+' => '',
-	'Class:RemoteAuthentOAuth/Attribute:token' => 'Token',
+	'Class:RemoteAuthentOAuth/Attribute:token' => 'Access token',
 	'Class:RemoteAuthentOAuth/Attribute:token+' => '',
-	'Class:RemoteAuthentOAuth/Attribute:token_expiration' => 'Token expiration',
+	'Class:RemoteAuthentOAuth/Attribute:token_expiration' => 'Access token expiration',
 	'Class:RemoteAuthentOAuth/Attribute:token_expiration+' => '',
 	'Class:RemoteAuthentOAuth/Attribute:redirect_url' => 'Redirect url',
 	'Class:RemoteAuthentOAuth/Attribute:redirect_url+' => '',
+	'Class:RemoteAuthentOAuth/Attribute:mailbox_list' => 'Mailbox list',
+	'Class:RemoteAuthentOAuth/Attribute:mailbox_list+' => '',
 ]);
+
