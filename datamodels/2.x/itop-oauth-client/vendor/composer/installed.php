cb1203fde9bb157eda3fa54a3c3af71eac6e26ce
Benjamin Dalsass <benjamin.dalsass@combodo.com>
2022-06-28 15:09:19 +0200

    Merge 2.7 into Support 3.0 : Update oauth composer

---
+++ datamodels/2.x/itop-oauth-client/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'type' => 'itop-extension',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '1135896b3d084aa9df778bfbb724b7e992534989',
+        'name' => 'combodo/itop-oauth-client',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'combodo/itop-oauth-client' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'type' => 'itop-extension',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '1135896b3d084aa9df778bfbb724b7e992534989',
+            'dev_requirement' => false,
+        ),
+    ),
+);
