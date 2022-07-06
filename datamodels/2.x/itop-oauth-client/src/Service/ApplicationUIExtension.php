c759856a617703d6754cfc6c2e1ceaadc11e84e9
Eric Espie <eric.espie@combodo.com>
2022-07-06 10:10:29 +0200

    N°5102 - Allow to send emails using GSuite SMTP and OAuth - Highlight classes

---
+++ datamodels/2.x/itop-oauth-client/src/Service/ApplicationUIExtension.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @copyright   Copyright (C) 2010-2022 Combodo SARL
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+namespace Combodo\iTop\OAuthClient\Service;
+
+use AbstractApplicationUIExtension;
+use utils;
+
+class ApplicationUIExtension extends AbstractApplicationUIExtension
+{
+
+	public function GetHilightClass($oObject)
+	{
+		// Possible return values are:
+		// HILIGHT_CLASS_CRITICAL, HILIGHT_CLASS_WARNING, HILIGHT_CLASS_OK, HILIGHT_CLASS_NONE
+		$oConfig = utils::GetConfig();
+		$aScopes = $oObject->Get('scope')->GetValues();
+		if ($oObject->Get('status') == 'inactive') {
+			return HILIGHT_CLASS_WARNING;
+		} elseif (in_array('SMTP', $aScopes) && $oConfig->Get('email_transport_smtp.username') == $oObject->Get('name')) {
+			return HILIGHT_CLASS_OK;
+		}
+
+		return HILIGHT_CLASS_NONE;
+	}
+}
