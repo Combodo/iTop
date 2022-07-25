75a931ebc1e261177986d4562afeff49d78d6c14
bdalsass <95754414+bdalsass@users.noreply.github.com>
2022-07-25 14:14:48 +0200

    Update datamodels/2.x/itop-portal-base/portal/src/Form/ObjectFormManager.php
    Co-authored-by: Molkobain <lajarige.guillaume@free.fr>

--- datamodels/2.x/itop-portal-base/portal/src/Form/ObjectFormManager.php
+++ datamodels/2.x/itop-portal-base/portal/src/Form/ObjectFormManager.php
@@ -748,7 +748,7 @@ class ObjectFormManager extends FormManager
 					if (in_array(get_class($oField), array('Combodo\\iTop\\Form\\Field\\LinkedSetField'))) {
 						/** @var \Combodo\iTop\Form\Field\LinkedSetField $oField */
 						if ($this->oFormHandlerHelper !== null) {
-							$oField->SetInformationEndpoint($this->oFormHandlerHelper->getUrlGenerator()->generate('p_object_get_information_json'));
+							$oField->SetInformationEndpoint($this->oFormHandlerHelper->GetUrlGenerator()->generate('p_object_get_information_json'));
 						}
 					}
 					// - Field that require to apply scope on its DM OQL
