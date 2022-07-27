cd861263fb81d6aeb8e0b3db423893e48ad4d529
bdalsass <95754414+bdalsass@users.noreply.github.com>
2022-07-27 08:49:24 +0200

    Update sources/Application/TwigBase/Twig/Extension.php
    Co-authored-by: Molkobain <lajarige.guillaume@free.fr>

--- sources/Application/TwigBase/Twig/Extension.php
+++ sources/Application/TwigBase/Twig/Extension.php
@@ -141,8 +141,8 @@ class Extension
 		// @see https://www.php.net/manual/fr/function.var-export.php
 		$aFilters[] = new TwigFilter('var_export', 'var_export');
 
-		//since 2.7.7 3.0.2 3.1.0 N°4867 "Twig content not allowed" error when use the extkey widget search icon in the user portal
-		//overwrite native twig filter : disable use of 'system' filter
+		// @since 2.7.7 3.0.2 3.1.0 N°4867 "Twig content not allowed" error when use the extkey widget search icon in the user portal
+		// Overwrite native twig filter: disable use of 'system' filter
 		$aFilters[] = new TwigFilter('filter', function ($array, $arrow) {
 			if ($arrow == 'system') {
 				return json_encode($array);
