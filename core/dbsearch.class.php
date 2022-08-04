bb861aa262cfb82fea525fc6325e450668501722
acognet <anne-catherine.cognet@combodo.com>
2022-08-04 16:26:21 +0200

    N°3024 - Archiving Service Families (or any class with no child) with combodo-archive-manual

--- core/dbsearch.class.php
+++ core/dbsearch.class.php
@@ -1623,7 +1623,7 @@ abstract class DBSearch
 		$oSet = new DBObjectSet($this);
 		if (MetaModel::IsStandaloneClass($sClass))
 		{
-			$oSet->OptimizeColumnLoad(array($this->GetClassAlias() => array('')));
+			$oSet->OptimizeColumnLoad(array($this->GetClassAlias() => array()));
 			$aIds = array($sClass => $oSet->GetColumnAsArray('id'));
 		}
 		else
