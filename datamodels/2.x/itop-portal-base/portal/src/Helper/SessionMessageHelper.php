b154952869b67e89367fd6cbf53d6164346a10ce
Benjamin Dalsass <benjamin.dalsass@combodo.com>
2022-08-30 14:36:26 +0200

    N°3390 - Upgrade from Symfony 3.4 to Symfony 5.4

--- datamodels/2.x/itop-portal-base/portal/src/Helper/SessionMessageHelper.php
+++ datamodels/2.x/itop-portal-base/portal/src/Helper/SessionMessageHelper.php
@@ -162,9 +162,11 @@ class SessionMessageHelper implements IteratorAggregate
 					}
 
 					$sMsgMetadata = '';
-					foreach ($aMessageData['metadata'] as $sMetadatumName => $sMetadatumValue)
-					{
-						$sMsgMetadata .= 'data-'.str_replace('_', '-', $sMetadatumName).'="'.utils::HtmlEntities($sMetadatumValue).'" ';
+					// Protection for missing metadata entry when session messages are created from cmdbAbstractObject
+					if (isset($aMessageData['metadata'])) {
+						foreach ($aMessageData['metadata'] as $sMetadatumName => $sMetadatumValue) {
+							$sMsgMetadata .= 'data-'.str_replace('_', '-', $sMetadatumName).'="'.utils::HtmlEntities($sMetadatumValue).'" ';
+						}
 					}
 					$aObjectMessages[] = array('css_classes' => $sMsgClass, 'message' => $aMessageData['message'], 'metadata' => $sMsgMetadata);
 					$aRanks[] = $aMessageData['rank'];
