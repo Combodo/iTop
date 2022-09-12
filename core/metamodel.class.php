eb7a30bf022f1338a52c379e775a8149b0eb2f1b
acognet <anne-catherine.cognet@combodo.com>
2022-09-12 17:12:12 +0200

    N°4342 - Generic bulk deletion function with memory limit handling - change to easier reading

--- core/metamodel.class.php
+++ core/metamodel.class.php
@@ -7174,7 +7174,8 @@ abstract class MetaModel
 		$bExecuteQuery = true;
 
 		while ($bExecuteQuery) {
-			$oSet = new DBObjectSet($oFilter, array(), array(), null, $iMaxChunkSize);
+			$oSet = new DBObjectSet($oFilter);
+			$oSet->SetLimit($iMaxChunkSize);
 			$oSet->OptimizeColumnLoad(array($sTargetClass => array('finalclass')));
 			$aIdToClass = $oSet->GetColumnAsArray('finalclass', true);
 
