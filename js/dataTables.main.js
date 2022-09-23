6d0f02048a2f8de615781ab248b2cc8bd5b24cc1
bdalsass <95754414+bdalsass@users.noreply.github.com>
2022-09-23 10:50:25 +0200

    Update js/dataTables.main.js
    Co-authored-by: Molkobain <lajarige.guillaume@free.fr>

--- js/dataTables.main.js
+++ js/dataTables.main.js
@@ -88,6 +88,7 @@ function getMultipleSelectionParams(listId)
  * @param iColumnTargetIndex
  * @returns {*}
  * @since 3.1.0
+ * @since 3.1.0
  */
 function getRowActionsColumnDefinition(sTableId, iColumnTargetIndex = -1)
 {
