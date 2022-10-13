d9539f9d01d23c335b3805fb755e975bb86cda61
Molkobain <lajarige.guillaume@free.fr>
2022-10-13 19:55:44 +0200

    N°5608 - Add comments to main autoloader

--- test/php-unit-tests/unittestautoload.php
+++ test/php-unit-tests/unittestautoload.php
@@ -1,5 +1,9 @@
 <?php
-include('vendor/autoload.php');
 
-include('ItopTestCase.php');
-include('ItopDataTestCase.php');
+// Main autoload, this is the one to use in the PHPUnit configuration
+// It is customized to include both
+// - Vendors
+require_once 'vendor/autoload.php';
+// - Custom test case PHP classes
+require_once 'ItopTestCase.php';
+require_once 'ItopDataTestCase.php';
