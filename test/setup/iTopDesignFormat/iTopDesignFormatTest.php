0cf8d731bb13051307a1a41a4f2493a541a85333
Pierre Goiffon <pierre.goiffon@combodo.com>
2022-11-08 15:59:14 +0100

    Rename iTopDesignFormat test class

--- test/setup/iTopDesignFormat/iTopDesignFormatTest.php
+++ test/setup/iTopDesignFormat/iTopDesignFormatTest.php
@@ -8,14 +8,12 @@ use iTopDesignFormat;
 
 
 /**
- * Class iTopDesignFormatTest
- *
  * @covers iTopDesignFormat
  *
  * @since 2.7.0 N°2586
  * @package Combodo\iTop\Test\UnitTest\Setup
  */
-class TestForITopDesignFormatClass extends ItopTestCase
+class iTopDesignFormatTest extends ItopTestCase
 {
 	protected function setUp(): void
 	{
