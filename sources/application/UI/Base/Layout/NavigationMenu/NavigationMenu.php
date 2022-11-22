02e63fff64e1f1dc8e10dbdaa8b8cf244889d681
Molkobain <lajarige.guillaume@free.fr>
2022-11-22 13:28:02 +0100

    Add PHPDoc

--- sources/application/UI/Base/Layout/NavigationMenu/NavigationMenu.php
+++ sources/application/UI/Base/Layout/NavigationMenu/NavigationMenu.php
@@ -289,6 +289,10 @@ class NavigationMenu extends UIBlock implements iKeyboardShortcut
 		}
 	}
 
+	/**
+	 * @return True if the silo selection is enabled, false otherwise
+	 * @since 3.1.0
+	 */
 	public function IsSiloSelectionEnabled() : bool {
 		return MetaModel::GetConfig()->Get('navigation_menu.show_organization_filter');
 	}
