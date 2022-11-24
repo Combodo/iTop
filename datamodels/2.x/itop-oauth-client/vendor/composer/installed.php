22111bf66772f4d58dde348901a017792f7b9ed6
Eric Espie <eric.espie@combodo.com>
2022-11-24 14:32:51 +0100

    N°5611 - Fix missing composer files in itop-oauth-client

---
+++ datamodels/2.x/itop-oauth-client/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'type' => 'itop-extension',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'd292a6b0c340330c11c3de163e8004ed16f0c451',
+        'name' => 'combodo/itop-oauth-client',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'combodo/itop-oauth-client' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'type' => 'itop-extension',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'd292a6b0c340330c11c3de163e8004ed16f0c451',
+            'dev_requirement' => false,
+        ),
+    ),
+);
