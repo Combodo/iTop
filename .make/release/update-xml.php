025f8b9dc37a5ea3960b556c15a8d6796d8195e2
Pierre Goiffon <pierre.goiffon@combodo.com>
2022-12-07 17:31:21 +0100

    :hammer: N°5633 update-xml.php : if no version passed, assuming current version

--- .make/release/update-xml.php
+++ .make/release/update-xml.php
@@ -9,9 +9,13 @@
  *
  * Usage :
  * `php .make\release\update-xml.php "1.7"`
+ * `php .make\release\update-xml.php`
+ *
+ * If no parameter provided then the current XML version will be used as target version
  *
  * @since 2.7.0 simple version change using regexp (not doing conversion)
  * @since 3.1.0 N°5405 now does a real conversion
+ * @since 3.1.0 N°5633 allow to use without parameter
  ******************************************************************************/
 
 
@@ -23,10 +27,12 @@ require_once (__DIR__.DIRECTORY_SEPARATOR.'update.classes.inc.php');
 
 if (count($argv) === 1)
 {
-	echo '/!\ You must pass the new version as parameter';
-	exit(1);
+	echo '/!\ No version passed: assuming target XML version is current XML version ('.ITOP_DESIGN_LATEST_VERSION.")\n";
+	$sVersionLabel = ITOP_DESIGN_LATEST_VERSION;
+} else {
+	$sVersionLabel = $argv[1];
 }
-$sVersionLabel = $argv[1];
+
 if (empty($sVersionLabel))
 {
 	echo 'Version passed as parameter is empty !';
