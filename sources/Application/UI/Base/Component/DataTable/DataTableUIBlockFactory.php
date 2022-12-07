9defedb862f277e3e1132d82a4860e52ce941284
Benjamin Dalsass <benjamin.dalsass@combodo.com>
2022-12-07 15:31:19 +0100

    N°5073 - Issue, row actions listeners lost on refresh

--- sources/Application/UI/Base/Component/DataTable/DataTableUIBlockFactory.php
+++ sources/Application/UI/Base/Component/DataTable/DataTableUIBlockFactory.php
@@ -214,7 +214,7 @@ class DataTableUIBlockFactory extends AbstractUIBlockFactory
 				array_key_exists('tooltip', $aAction) ? Dict::S($aAction['tooltip']) : '',
 				array_key_exists('name', $aAction) ? $aAction['name'] : 'undefined'
 			);
-			$oButton->SetDataAttributes(['action-id' => $iKey, 'tooltip-append-to' => 'body']);
+			$oButton->SetDataAttributes(['action-id' => $iKey, 'table-id' => $oTable->GetId()]);
 			$oToolbar->AddSubBlock($oButton);
 		}
 
