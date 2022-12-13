432579657c12738b5b13da0a0f3c6da95886635e
Eric Espie <eric.espie@combodo.com>
2022-12-13 10:47:50 +0100

    N°4756 - Ease extensibility for CRUD operations : Event Service - cleanup

--- sources/Application/Service/EventHelper.php
+++ sources/Application/Service/EventHelper.php
@@ -8,39 +8,10 @@ namespace Combodo\iTop\Service;
 
 use IssueLog;
 use LogChannels;
-use SetupUtils;
 use utils;
 
 class EventHelper
 {
-
-	public static function GetCachedClasses($sModuleName, callable $ListBuilder)
-	{
-		$aClasses = [];
-		$sCacheFileName = '';
-
-		if (!utils::IsDevelopmentEnvironment()) {
-			// Try to read from cache
-			$sCacheFileName = utils::GetCachePath()."EventsClassList/$sModuleName.php";
-			if (is_file($sCacheFileName)) {
-				$aClasses = include $sCacheFileName;
-			}
-		}
-
-		if (empty($aClasses)) {
-			$aClasses = call_user_func($ListBuilder);
-
-			if (!utils::IsDevelopmentEnvironment() && !empty($aClasses)) {
-				// Save to cache
-				$sCacheContent = "<?php\n\nreturn ".var_export($aClasses, true).";";
-				SetupUtils::builddir(dirname($sCacheFileName));
-				file_put_contents($sCacheFileName, $sCacheContent);
-			}
-		}
-
-		return $aClasses;
-	}
-
 	public static function Trace($sMessage)
 	{
 		IssueLog::Trace($sMessage, LogChannels::EVENT_SERVICE);
