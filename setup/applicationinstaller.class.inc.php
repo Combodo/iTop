Combodo/iTop

commit 15d3201a40a5c2cb914c8ff21c87e7162a6f8522

N°2013 Setup : fix crash on compilation if initial DB info were wrong
Use case :
- having in the iTop config file wrong DB parameters (unreachable server for example)
- launch the setup in update mode
- fix the DB config in the wizard

The PR #351 solved the errors that were happening in the following steps.
But we still had an error during compilation, caused by the maintenance mode activation.

setup/applicationinstaller.class.inc.php
@@ -286,8 +286,9 @@ class ApplicationInstaller
 						}
 					}
 
+					$aParamValues = $this->oParams->GetParamForConfigArray();
 					self::DoCompile($aSelectedModules, $sSourceDir, $sExtensionDir, $sTargetDir, $sTargetEnvironment,
-						$bUseSymbolicLinks);
+						$bUseSymbolicLinks, $aParamValues);
 
 					$aResult = array(
 						'status' => self::OK,
@@ -510,7 +511,22 @@ class ApplicationInstaller
 	}
 
 
-	protected static function DoCompile($aSelectedModules, $sSourceDir, $sExtensionDir, $sTargetDir, $sEnvironment, $bUseSymbolicLinks = null)
+	/**
+	 * @param array $aSelectedModules
+	 * @param string $sSourceDir
+	 * @param string $sExtensionDir
+	 * @param string $sTargetDir
+	 * @param string $sEnvironment
+	 * @param boolean $bUseSymbolicLinks
+	 * @param array $aParamValues
+	 *
+	 * @return void
+	 * @throws \ConfigException
+	 * @throws \CoreException
+	 *
+	 * @since 3.1.0 N°2013 added the aParamValues param
+	 */
+	protected static function DoCompile($aSelectedModules, $sSourceDir, $sExtensionDir, $sTargetDir, $sEnvironment, $bUseSymbolicLinks = null, $aParamValues = [])
 	{
 		SetupLog::Info("Compiling data model.");
 
@@ -520,7 +536,7 @@ class ApplicationInstaller
 
 		if (empty($sSourceDir) || empty($sTargetDir)) {
 			throw new Exception("missing parameter source_dir and/or target_dir");
-		}		
+		}
 
 		$sSourcePath = APPROOT.$sSourceDir;
 		$aDirsToScan = array($sSourcePath);
@@ -554,6 +570,7 @@ class ApplicationInstaller
 			{
 				$oConfig = null;
 			}
+			$oConfig->UpdateFromParams($aParamValues);
 			SetupUtils::EnterMaintenanceMode($oConfig);
 		}
 

core/config.class.inc.php
@@ -2473,6 +2473,8 @@ class Config
 	 * @param string|null $sModulesDir
 	 * @param bool $bPreserveModuleSettings
 	 *
+	 * @return void
+	 *
 	 * @throws \Exception
 	 * @throws \CoreException
 	 */

commit 43dd0b7df8afb764581ee052ce94c9ac197d9529

N°2013 Fix regression when installing from scratch Error was "Call to a member function UpdateFromParams() on null" Regression introduced by 15d3201a

setup/applicationinstaller.class.inc.php
@@ -565,15 +565,16 @@ class ApplicationInstaller
 		if (($sEnvironment == 'production') && !$bIsAlreadyInMaintenanceMode)
 		{
 			$sConfigFilePath = utils::GetConfigFilePath($sEnvironment);
-			if (is_file($sConfigFilePath))
-			{
+			if (is_file($sConfigFilePath)) {
 				$oConfig = new Config($sConfigFilePath);
-			}
-			else
-			{
+			} else {
 				$oConfig = null;
 			}
-			$oConfig->UpdateFromParams($aParamValues);
+
+			if (false === is_null($oConfig)) {
+				$oConfig->UpdateFromParams($aParamValues);
+			}
+
 			SetupUtils::EnterMaintenanceMode($oConfig);
 		}
 