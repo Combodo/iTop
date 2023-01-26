0c909907c63d86a2577e6f7039c7e484ffe32c85
tacsaby <87334811+tacsaby@users.noreply.github.com>
2023-01-26 13:26:47 +0100

    Update hu.dict.itop-attachments.php
    datamodels to hungarian

--- datamodels/2.x/itop-attachments/dictionaries/hu.dict.itop-attachments.php
+++ datamodels/2.x/itop-attachments/dictionaries/hu.dict.itop-attachments.php
@@ -21,26 +21,24 @@
  * along with iTop. If not, see <http://www.gnu.org/licenses/>
  */
 Dict::Add('HU HU', 'Hungarian', 'Magyar', array(
-	'Attachments:TabTitle_Count' => 'Attachments (%1$d)~~',
-	'Attachments:EmptyTabTitle' => 'Attachments~~',
-	'Attachments:FieldsetTitle' => 'Attachments~~',
-	'Attachments:DeleteBtn' => 'Delete~~',
-	'Attachments:History_File_Added' => 'Attachment %1$s added.~~',
-	'Attachments:History_File_Removed' => 'Attachment %1$s removed.~~',
-	'Attachments:AddAttachment' => 'Add attachment: ~~',
-	'Attachments:UploadNotAllowedOnThisSystem' => 'File upload in NOT allowed on this system.~~',
-	'Attachment:Max_Go' => '(Maximum file size: %1$s Go)~~',
-	'Attachment:Max_Mo' => '(Maximum file size: %1$s Mo)~~',
-	'Attachment:Max_Ko' => '(Maximum file size: %1$s Ko)~~',
-	'Attachments:NoAttachment' => 'No attachment. ~~',
-	'Attachments:PreviewNotAvailable' => 'Preview not available for this type of attachment.~~',
-	'Attachments:Error:FileTooLarge' => 'File is too large to be uploaded. %1$s~~',
-	'Attachments:Error:UploadedFileEmpty' => 'The received file is empty and cannot be attached.
-Either you have pushed an empty file,
-or ask your iTop administrator if the iTop server disk is full.~~',
-	'Attachments:Render:Icons' => 'Display as icons~~',
-	'Attachments:Render:Table' => 'Display as list~~',
-	'UI:Attachments:DropYourFileHint' => 'Drop files anywhere in this area~~',
+	'Attachments:TabTitle_Count' => 'Mellékletek (%1$d)',
+	'Attachments:EmptyTabTitle' => 'Mellékletek',
+	'Attachments:FieldsetTitle' => 'Mellékletek',
+	'Attachments:DeleteBtn' => 'Törlés',
+	'Attachments:History_File_Added' => '%1$s melléklet hozzáadva',
+	'Attachments:History_File_Removed' => '%1$s melléklet eltávolítva',
+	'Attachments:AddAttachment' => 'Melléklet hozzáadása: ',
+	'Attachments:UploadNotAllowedOnThisSystem' => 'A fájlfeltöltés nem engedélyezett ezen a rendszeren',
+	'Attachment:Max_Go' => '(Maximum fájlméret: %1$s Go)',
+	'Attachment:Max_Mo' => '(Maximum fájlméret: %1$s Mo)',
+	'Attachment:Max_Ko' => '(Maximum fájlméret: %1$s Ko)',
+	'Attachments:NoAttachment' => 'Nincs melléklet. ',
+	'Attachments:PreviewNotAvailable' => 'Az előnézet nem érhető el ilyen típusú melléklethez',
+	'Attachments:Error:FileTooLarge' => 'Túl nagy a fájl a feltöltéshez. %1$s',
+	'Attachments:Error:UploadedFileEmpty' => 'A kapott fájl üres, ezért nem csatolható. Vagy egy üres fájlt tolt be, vagy kérdezze meg a rendszergazdát, hátha az iTop szerver lemeze telt meg.',
+	'Attachments:Render:Icons' => 'Mutassa ikonként',
+	'Attachments:Render:Table' => 'Mutassa listaként',
+	'UI:Attachments:DropYourFileHint' => 'Húzza a fájlokat erre a területre',
 ));
 
 //
@@ -48,40 +46,40 @@ or ask your iTop administrator if the iTop server disk is full.~~',
 //
 
 Dict::Add('HU HU', 'Hungarian', 'Magyar', array(
-	'Class:Attachment' => 'Attachment~~',
+	'Class:Attachment' => 'Mellékletek',
 	'Class:Attachment+' => '~~',
-	'Class:Attachment/Attribute:expire' => 'Expire~~',
+	'Class:Attachment/Attribute:expire' => 'Lejárat',
 	'Class:Attachment/Attribute:expire+' => '~~',
-	'Class:Attachment/Attribute:temp_id' => 'Temporary id~~',
+	'Class:Attachment/Attribute:temp_id' => 'Átmeneti azonosító',
 	'Class:Attachment/Attribute:temp_id+' => '~~',
-	'Class:Attachment/Attribute:item_class' => 'Item class~~',
+	'Class:Attachment/Attribute:item_class' => 'Elem osztály',
 	'Class:Attachment/Attribute:item_class+' => '~~',
-	'Class:Attachment/Attribute:item_id' => 'Item~~',
+	'Class:Attachment/Attribute:item_id' => 'Elem',
 	'Class:Attachment/Attribute:item_id+' => '~~',
-	'Class:Attachment/Attribute:item_org_id' => 'Item organization~~',
+	'Class:Attachment/Attribute:item_org_id' => 'Elem szervezet',
 	'Class:Attachment/Attribute:item_org_id+' => '~~',
-	'Class:Attachment/Attribute:contents' => 'Contents~~',
+	'Class:Attachment/Attribute:contents' => 'Tartalomjegyzék',
 	'Class:Attachment/Attribute:contents+' => '~~',
 ));
 
 
 Dict::Add('HU HU', 'Hungarian', 'Magyar', array(
-	'Attachments:File:Thumbnail' => 'Icon~~',
-	'Attachments:File:Name' => 'File name~~',
-	'Attachments:File:Date' => 'Upload date~~',
-	'Attachments:File:Uploader' => 'Uploaded by~~',
-	'Attachments:File:Size' => 'Size~~',
-	'Attachments:File:MimeType' => 'Type~~',
+	'Attachments:File:Thumbnail' => 'Ikon',
+	'Attachments:File:Name' => 'Fájlnév',
+	'Attachments:File:Date' => 'Feltöltés dátum',
+	'Attachments:File:Uploader' => 'Feltöltötte ',
+	'Attachments:File:Size' => 'Méret',
+	'Attachments:File:MimeType' => 'Típus',
 ));
 //
 // Class: Attachment
 //
 
 Dict::Add('HU HU', 'Hungarian', 'Magyar', array(
-	'Class:Attachment/Attribute:creation_date' => 'Creation date~~',
+	'Class:Attachment/Attribute:creation_date' => 'Létrehozás dátum',
 	'Class:Attachment/Attribute:creation_date+' => '~~',
-	'Class:Attachment/Attribute:user_id' => 'User id~~',
+	'Class:Attachment/Attribute:user_id' => 'Felhasználói azonosító',
 	'Class:Attachment/Attribute:user_id+' => '~~',
-	'Class:Attachment/Attribute:contact_id' => 'Contact id~~',
+	'Class:Attachment/Attribute:contact_id' => 'Kapcsolattartó azonosító',
 	'Class:Attachment/Attribute:contact_id+' => '~~',
 ));
