002e52cc3e1f0b955b10a1772bdc0ec4cd820ae6
Molkobain <lajarige.guillaume@free.fr>
2023-01-30 17:21:41 +0100

    N°5929 - Fix parse error due to not closed array from bbea9782

--- dictionaries/ui/layouts/page-content/hu.dictionary.itop.page-content.php
+++ dictionaries/ui/layouts/page-content/hu.dictionary.itop.page-content.php
@@ -5,3 +5,4 @@
  */
 Dict::Add('HU HU', 'Hungarian', 'Magyar', [
 	'UIBlock:Error:CannotGetBlocks' => 'Nem tudta lekérni a blokkokat a "%1$s" tartalmi területről, mivel úgy tűnik, hogy létezik a "%2$s" oldal tartalmához.',
+]);
