569045f67540f8c1bd2183fd9533bc27e9cfa880
vdumas <vincent.dumas@combodo.com>
2023-02-09 17:30:17 +0100

    N°5946 - Error in a french translation - user preference

--- dictionaries/ui/pages/preferences/fr.dictionary.itop.preferences.php
+++ dictionaries/ui/pages/preferences/fr.dictionary.itop.preferences.php
@@ -21,7 +21,7 @@ Dict::Add('FR FR', 'French', 'Français', array(
 	'UI:Preferences:Title'                                             => 'Préférences',
 	'UI:Preferences:UserInterface:Title'                               => 'Interface utilisateur',
 	'UI:Preferences:General:Title'                                     => 'Général',
-	'UI:Preferences:General:Theme'                                     => 'Thême',
+	'UI:Preferences:General:Theme'                                     => 'Thème',
 	'UI:Preferences:General:Theme:DefaultThemeLabel'                   => '%1$s (défaut)',
 	'UI:Preferences:Lists:Title'                                       => 'Listes',
 	'UI:Preferences:RichText:Title'                                    => 'Éditeur texte riche',
