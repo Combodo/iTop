df0dd7b260ffae01307373e3efb2eecd2405a601
Molkobain <lajarige.guillaume@free.fr>
2023-02-13 14:58:26 +0100

    Update datamodels/2.x/itop-portal-base/portal/src/Form/ObjectFormManager.php

--- datamodels/2.x/itop-portal-base/portal/src/Form/ObjectFormManager.php
+++ datamodels/2.x/itop-portal-base/portal/src/Form/ObjectFormManager.php
@@ -1164,8 +1164,8 @@ class ObjectFormManager extends FormManager
 				$aContext = [
 					'origin'    => __CLASS__.'::'.__METHOD__,
 					'obj_class' => get_class($this->oObject),
+					'obj_key' => $bIsNew ? 'NULL' : $this->oObject->GetKey(),
 				];
-				$aContext['obj_key'] = $bIsNew ? 'NULL' : $this->oObject->GetKey();
 				ExceptionLog::LogException($e, $aContext);
 				$bExceptionLogged = true;
 
