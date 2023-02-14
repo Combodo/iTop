46520535bd24b9377999563f0826dd15cef638cc
Pierre Goiffon <pierre.goiffon@combodo.com>
2023-02-14 09:14:40 +0100

    :bulb: Test comment

--- tests/php-unit-tests/unitary-tests/application/cmdbAbstractObjectTest.php
+++ tests/php-unit-tests/unitary-tests/application/cmdbAbstractObjectTest.php
@@ -7,6 +7,7 @@ class cmdbAbstractObjectTest extends ItopDataTestCase {
 		$aLinkModificationsStack = $this->GetNonPublicStaticProperty(cmdbAbstractObject::class, 'aLinkModificationsStack');
 		$this->assertSame([], $aLinkModificationsStack);
 
+		// lnkPersonToTeam:1 is sample data with : team_id=39 ; person_id=9 ; role_id=3
 		$oLinkPersonToTeam1 = MetaModel::GetObject(lnkPersonToTeam::class, 1);
 		$oLinkPersonToTeam1->Set('role_id', 1);
 		$oLinkPersonToTeam1->DBWrite();
