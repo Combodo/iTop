1483ea26068ecfcca31b592b821993ea2848c810
acognet <anne-catherine.cognet@combodo.com>
2023-03-02 11:54:19 +0100

    N°541 - GraphBar labels too long: unreadable >> increase graph size

--- application/displayblock.class.inc.php
+++ application/displayblock.class.inc.php
@@ -1009,6 +1009,7 @@ EOF
 				$iTotalCount = 0;
 				$aValues = array();
 				$aURLs = array();
+				$iMaxNbCharInLabel = 0;
 				foreach ($aRes as $iRow => $aRow)
 				{
 					$sValue = $aRow['grouped_by_1'];
@@ -1016,7 +1017,10 @@ EOF
 					$aGroupBy[(int)$iRow] = (int) $aRow[$sFctVar];
 					$iTotalCount += $aRow['_itop_count_'];
 					$aValues[] = array('label' => html_entity_decode(strip_tags($sHtmlValue), ENT_QUOTES, 'UTF-8'), 'label_html' => $sHtmlValue, 'value' => (int) $aRow[$sFctVar]);
-					
+					if ($iMaxNbCharInLabel < strlen($sValue)) {
+						$iMaxNbCharInLabel = strlen($sValue);
+					}
+
 					// Build the search for this subset
 					$oSubsetSearch = $this->m_oFilter->DeepClone();
 					$oCondition = new BinaryExpression($oGroupByExp, '=', new ScalarExpression($sValue));
@@ -1039,7 +1043,8 @@ EOF
 				$sJson = json_encode($aValues);
 				$oPage->add_ready_script(
 <<<EOF
-
+var iSizeOfChar = (200 + 6 * $iMaxNbCharInLabel);
+$('#my_chart_$sId').height(iSizeOfChar + 'px');
 var chart = c3.generate({
     bindto: d3.select('#my_chart_$sId'),
     data: {
@@ -1107,8 +1112,15 @@ EOF
 				}
 				$sJSColumns = json_encode($aColumns);
 				$sJSNames = json_encode($aNames);
+				$iNbLineToAddForName = 0;
+				if( count($aNames)>50) {
+					//increase size of the graph in order to have a maximum of 5 columns of legend
+					$iNbLineToAddForName = ceil ( count($aNames) / 5) - 10;
+				}
 				$oPage->add_ready_script(
 <<<EOF
+var iSizeOfChar = (200 + 20 * $iNbLineToAddForName);
+$('#my_chart_$sId').height(iSizeOfChar + 'px');
 var chart = c3.generate({
     bindto: d3.select('#my_chart_$sId'),
     data: {
