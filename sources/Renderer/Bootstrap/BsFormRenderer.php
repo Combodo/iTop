9023e338a5c60f7c95740d73204844fcfbee5e0b
Molkobain <lajarige.guillaume@free.fr>
2023-03-09 10:42:36 +0100

    N°6041 - Introduce dynamic register of fields and renderers in Bootstrap form renderers

--- sources/Renderer/Bootstrap/BsFormRenderer.php
+++ sources/Renderer/Bootstrap/BsFormRenderer.php
@@ -30,38 +30,4 @@ use Combodo\iTop\Form\Form;
 class BsFormRenderer extends FormRenderer
 {
 	const DEFAULT_RENDERER_NAMESPACE = 'Combodo\\iTop\\Renderer\\Bootstrap\\FieldRenderer\\';
-
-	/**
-	 * Default constructor
-	 * 
-	 * @param \Combodo\iTop\Form\Form $oForm
-	 */
-	public function __construct(Form $oForm = null)
-	{
-		parent::__construct($oForm);
-		$this->AddSupportedField('HiddenField', 'BsSimpleFieldRenderer');
-		$this->AddSupportedField('LabelField', 'BsSimpleFieldRenderer');
-		$this->AddSupportedField('PasswordField', 'BsSimpleFieldRenderer');
-        $this->AddSupportedField('StringField', 'BsSimpleFieldRenderer');
-        $this->AddSupportedField('UrlField', 'BsSimpleFieldRenderer');
-        $this->AddSupportedField('EmailField', 'BsSimpleFieldRenderer');
-        $this->AddSupportedField('PhoneField', 'BsSimpleFieldRenderer');
-		$this->AddSupportedField('TextAreaField', 'BsSimpleFieldRenderer');
-		$this->AddSupportedField('CaseLogField', 'BsSimpleFieldRenderer');
-		$this->AddSupportedField('SelectField', 'BsSimpleFieldRenderer');
-		$this->AddSupportedField('MultipleSelectField', 'BsSimpleFieldRenderer');
-		$this->AddSupportedField('RadioField', 'BsSimpleFieldRenderer');
-		$this->AddSupportedField('CheckboxField', 'BsSimpleFieldRenderer');
-		$this->AddSupportedField('SubFormField', 'BsSubFormFieldRenderer');
-		$this->AddSupportedField('SelectObjectField', 'BsSelectObjectFieldRenderer');
-		$this->AddSupportedField('LinkedSetField', 'BsLinkedSetFieldRenderer');
-		$this->AddSupportedField('SetField', 'BsSetFieldRenderer');
-		$this->AddSupportedField('TagSetField', 'BsSetFieldRenderer');
-		$this->AddSupportedField('DateTimeField', 'BsSimpleFieldRenderer');
-		$this->AddSupportedField('DurationField', 'BsSimpleFieldRenderer');
-		$this->AddSupportedField('FileUploadField', 'BsFileUploadFieldRenderer');
-        $this->AddSupportedField('BlobField', 'BsSimpleFieldRenderer');
-        $this->AddSupportedField('ImageField', 'BsSimpleFieldRenderer');
-	}
-
 }
