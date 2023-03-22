Combodo/iTop

commit fc974917088ae67ef4eb27ef041dfce55ed24433

N°6105 - Cleanup unnecessary use of dirname(__FILE__) (#453)

datamodels/2.x/itop-tickets/module.itop-tickets.php
@@ -94,10 +94,10 @@ class TicketsInstaller extends ModuleInstallerAPI
 				$sLang = str_replace(' ', '_', strtolower($oConfiguration->GetDefaultLanguage()));
 			}
 
-			$sFileName = dirname(__FILE__)."/data/{$sLang}.data.itop-tickets.xml";
+			$sFileName = __DIR__."/data/{$sLang}.data.itop-tickets.xml";
 			SetupLog::Info("Searching file: $sFileName");
 			if (!file_exists($sFileName)) {
-				$sFileName = dirname(__FILE__)."/data/en_us.data.itop-tickets.xml";
+				$sFileName = __DIR__."/data/en_us.data.itop-tickets.xml";
 			}
 			SetupLog::Info("Loading file: $sFileName");
 			$oDataLoader->StartSession($oMyChange);

application/utils.inc.php
@@ -1753,7 +1753,6 @@ SQL;
 	 * it at all (losing the direct access to the page) :
 	 *
 	 * ```php
-	 * if (!defined('__DIR__')) define('__DIR__', dirname(__FILE__));
 	 * require_once(__DIR__.'/../../approot.inc.php');
 	 * ```
 	 *

approot.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-define('APPROOT', dirname(__FILE__).'/');
+define('APPROOT', __DIR__.'/');
 define('APPCONF', APPROOT.'conf/');
 
 /**

datamodels/2.x/itop-backup/ajax.backup.php
@@ -17,7 +17,6 @@
  * You should have received a copy of the GNU Affero General Public License
  */
 
-if (!defined('__DIR__')) define('__DIR__', dirname(__FILE__));
 if (!defined('APPROOT')) require_once(__DIR__.'/../../approot.inc.php');
 require_once(APPROOT.'/application/application.inc.php');
 
@@ -166,7 +165,7 @@ JS
 			require_once(APPROOT."setup/runtimeenv.class.inc.php");
 			require_once(APPROOT.'/application/utils.inc.php');
 			require_once(APPROOT.'/setup/backup.class.inc.php');
-			require_once(dirname(__FILE__).'/dbrestore.class.inc.php');
+			require_once(__DIR__.'/dbrestore.class.inc.php');
 
 			$sEnvironment = utils::ReadParam('environment', 'production', false, 'raw_data');
 			try

datamodels/2.x/itop-backup/backup.php
@@ -17,7 +17,6 @@
  * You should have received a copy of the GNU Affero General Public License
  */
 
-if (!defined('__DIR__')) define('__DIR__', dirname(__FILE__));
 if (!defined('APPROOT'))
 {
 	if (file_exists(__DIR__.'/../../approot.inc.php'))
@@ -155,4 +154,4 @@ function ExecuteMainOperation($oP){
 	}
 }
 
-require_once(dirname(__FILE__).'/common.cli-execution.php');
+require_once(__DIR__.'/common.cli-execution.php');

datamodels/2.x/itop-backup/common.cli-execution.php
@@ -17,7 +17,6 @@
  * You should have received a copy of the GNU Affero General Public License
  */
 
-if (!defined('__DIR__')) define('__DIR__', dirname(__FILE__));
 if (!defined('APPROOT'))
 {
 	if (file_exists(__DIR__.'/../../approot.inc.php'))

datamodels/2.x/itop-backup/restore.php
@@ -17,7 +17,6 @@
  * You should have received a copy of the GNU Affero General Public License
  */
 
-if (!defined('__DIR__')) define('__DIR__', dirname(__FILE__));
 if (!defined('APPROOT'))
 {
 	if (file_exists(__DIR__.'/../../approot.inc.php'))
@@ -32,7 +31,7 @@ if (!defined('APPROOT'))
 require_once(APPROOT.'application/application.inc.php');
 require_once(APPROOT.'core/log.class.inc.php');
 require_once(APPROOT.'application/startup.inc.php');
-require_once(dirname(__FILE__).'/dbrestore.class.inc.php');
+require_once(__DIR__.'/dbrestore.class.inc.php');
 
 /**
  * @since 3.0.0 N°4227 new class to handle iTop restore manually via a CLI command
@@ -146,4 +145,4 @@ function ExecuteMainOperation($oP){
 	}
 }
 
-require_once(dirname(__FILE__).'/common.cli-execution.php');
+require_once(__DIR__.'/common.cli-execution.php');

datamodels/2.x/itop-backup/status.php
@@ -27,9 +27,6 @@ use Combodo\iTop\Application\UI\Base\Layout\UIContentBlock;
 use Combodo\iTop\Application\UI\Base\UIBlock;
 use Combodo\iTop\Renderer\BlockRenderer;
 
-if (!defined('__DIR__')) {
-	define('__DIR__', dirname(__FILE__));
-}
 if (!defined('APPROOT')) {
 	require_once(__DIR__.'/../../approot.inc.php');
 }

commit 43ecac7e6a302a402ca605d1e92b074c51c060e6

N°6119 Fix fresh install crashing on profile creation Regression brought by fc97491 / #453

datamodels/2.x/itop-tickets/module.itop-tickets.php
@@ -94,10 +94,10 @@ class TicketsInstaller extends ModuleInstallerAPI
 				$sLang = str_replace(' ', '_', strtolower($oConfiguration->GetDefaultLanguage()));
 			}
 
-			$sFileName = __DIR__."/data/{$sLang}.data.itop-tickets.xml";
+			$sFileName = dirname(__FILE__)."/data/{$sLang}.data.itop-tickets.xml";
 			SetupLog::Info("Searching file: $sFileName");
 			if (!file_exists($sFileName)) {
-				$sFileName = __DIR__."/data/en_us.data.itop-tickets.xml";
+				$sFileName = dirname(__FILE__)."/data/en_us.data.itop-tickets.xml";
 			}
 			SetupLog::Info("Loading file: $sFileName");
 			$oDataLoader->StartSession($oMyChange);