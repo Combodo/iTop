04a80fcd3983ebf66c88be5545d0158326a571f4
Molkobain <lajarige.guillaume@free.fr>
2023-03-24 22:25:58 +0100

    N°6055 - Fix undefined offset error in synchro_exec.php (#465)
    Co-authored-by: odain <olivier.dain@combodo.com>

--- synchro/synchro_import.php
+++ synchro/synchro_import.php
@@ -449,6 +449,7 @@ try
 		if ($sInputColumn === 'primary_key')
 		{
 			$iPrimaryKeyCol = $iFieldId;
+			$aIsBinaryToTransform[$iFieldId] = false;
 			continue;
 		}
 		if (!array_key_exists($sInputColumn, $aColumns))
