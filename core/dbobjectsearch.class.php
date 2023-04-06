2907efde4ba53ead6c797acad37d8b5a404ca2fd
Eric Espie <eric.espie@combodo.com>
2023-04-06 11:07:05 +0200

    N°6151 - Portal rules: source_oql containing UNION not allowed

--- core/dbobjectsearch.class.php
+++ core/dbobjectsearch.class.php
@@ -1,21 +1,8 @@
 <?php
-// Copyright (c) 2010-2023 Combodo SARL
-//
-//   This file is part of iTop.
-//
-//   iTop is free software; you can redistribute it and/or modify
-//   it under the terms of the GNU Affero General Public License as published by
-//   the Free Software Foundation, either version 3 of the License, or
-//   (at your option) any later version.
-//
-//   iTop is distributed in the hope that it will be useful,
-//   but WITHOUT ANY WARRANTY; without even the implied warranty of
-//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//   GNU Affero General Public License for more details.
-//
-//   You should have received a copy of the GNU Affero General Public License
-//   along with iTop. If not, see <http://www.gnu.org/licenses/>
-//
+/*
+ * @copyright   Copyright (C) 2010-2023 Combodo SARL
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
 
 /** @internal Dev hack for disabling some query build optimizations (Folding/Merging) */
 define('ENABLE_OPT', true);
@@ -2064,7 +2051,7 @@ class DBObjectSearch extends DBSearch
 	 * @param $sAttCode
 	 * @return \FunctionExpression|mixed|null
 	 * @throws \CoreException
-*/
+	*/
 	static public function GetPolymorphicExpression($sClass, $sAttCode)
 	{
 		$oExpression = ExpressionCache::GetCachedExpression($sClass, $sAttCode);
@@ -2140,7 +2127,7 @@ class DBObjectSearch extends DBSearch
 		return $oExpression;
 	}
 
-	public function ListParameters()
+	function GetExpectedArguments(): array
 	{
 		return $this->GetCriteria()->ListParameters();
 	}
