a24d50d7e37e245dc16da3ecd3a93b92c7956c03
Pierre Goiffon <pierre.goiffon@combodo.com>
2023-08-11 15:19:48 +0200

    :white_check_mark: DBBackupDataTest::testPrepareFilesToBackup : fix cleanup not done on failing test

--- tests/php-unit-tests/unitary-tests/setup/DBBackupDataTest.php
+++ tests/php-unit-tests/unitary-tests/setup/DBBackupDataTest.php
@@ -32,35 +32,33 @@ class DBBackupDataTest extends ItopDataTestCase
 				file_put_contents(APPROOT.'/'.$sExtraFile, 'Hello World!');
 			}
 		}
-		
-		if ($bUnsafeFileException)
-		{
-			$this->expectExceptionMessage("Backup: Aborting, resource '$sExtraFile'. Considered as UNSAFE because not inside the iTop directory.");
-		}
-		$aFiles = $this->InvokeNonPublicMethod('DBBackup', 'PrepareFilesToBackup', $oBackup, [APPROOT.'/conf/production/config-itop.php', $sTmpDir, true]);
-		SetupUtils::rrmdir($sTmpDir);
-		$aExpectedFiles = [
-			$sTmpDir.'/config-itop.php',
-		];
-		foreach($aExtraFiles as $sRelFile => $bExists)
-		{
-			if ($bExists)
-			{
-				$aExpectedFiles[] = $sTmpDir.'/'.$sRelFile;
+
+		try {
+			if ($bUnsafeFileException) {
+				$this->expectExceptionMessage("Backup: Aborting, resource '$sExtraFile'. Considered as UNSAFE because not inside the iTop directory.");
+			}
+			$aFiles = $this->InvokeNonPublicMethod('DBBackup', 'PrepareFilesToBackup', $oBackup, [APPROOT . '/conf/production/config-itop.php', $sTmpDir, true]);
+			SetupUtils::rrmdir($sTmpDir);
+			$aExpectedFiles = [
+				$sTmpDir . '/config-itop.php',
+			];
+			foreach ($aExtraFiles as $sRelFile => $bExists) {
+				if ($bExists) {
+					$aExpectedFiles[] = $sTmpDir . '/' . $sRelFile;
+				}
+			}
+		} finally {
+			// Cleanup
+			foreach ($aExtraFiles as $sExtraFile => $bExists) {
+				if ($bExists) {
+					unlink(APPROOT . '/' . $sExtraFile);
+				}
 			}
 		}
+
 		sort($aFiles);
 		sort($aExpectedFiles);
 		$this->assertEquals($aFiles, $aExpectedFiles);
-
-		// Cleanup
-		foreach($aExtraFiles as $sExtraFile => $bExists)
-		{
-			if ($bExists)
-			{
-				unlink(APPROOT.'/'.$sExtraFile);
-			}
-		}
 	}
 	
 	function prepareFilesToBackupProvider()
