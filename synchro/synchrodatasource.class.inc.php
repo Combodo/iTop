818734c22c42099d93f9c74bc247f866e4c1d659
Thomas Casteleyn <thomas.casteleyn@super-visions.com>
2023-09-01 12:17:05 +0200

    Document SynchroAttribute columns to be created in the data synchro table

--- synchro/synchrodatasource.class.inc.php
+++ synchro/synchrodatasource.class.inc.php
@@ -764,6 +764,7 @@ EOF
 		$oAttribute->Set('reconcile', 0);
 		$oAttribute->Set('update', 0);
 		$oAttribute->Set('update_policy', 'master_locked');
+		$oAttribute->Set('import_columns', implode(',', array_keys($oAttDef->GetImportColumns())));
 
 		return $oAttribute;
 	}
@@ -1445,6 +1446,13 @@ class SynchroAttribute extends cmdbAbstractObject
 			'allowed_values' => new ValueSetEnum('master_locked,master_unlocked,write_if_empty'),
 			'sql' => 'update_policy',
 			'default_value' => 'master_locked',
+			'is_null_allowed' => false,
+			'depends_on' => array(),
+		)));
+		MetaModel::Init_AddAttribute(new AttributeString('import_columns', array(
+			'allowed_values' => null,
+			'sql' => 'import_columns',
+			'default_value' => null,
 			'is_null_allowed' => false,
 			'depends_on' => array(),
 		)));
