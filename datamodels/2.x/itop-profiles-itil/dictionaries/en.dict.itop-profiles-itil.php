7443fdd5259ada764b11084fcd55f612593059e1
odain <olivier.dain@combodo.com>
2023-09-05 13:11:44 +0200

    5324-userfriendly messages

---
+++ datamodels/2.x/itop-profiles-itil/dictionaries/en.dict.itop-profiles-itil.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) Combodo SARL 2022
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('EN US', 'English', 'English', array(
+	'Class:User/NonStandaloneProfileWarning' => 'Profile %1$s cannot be standalone. You should add other profiles to user %2$s otherwise you may encounter access issue with this user.',
+	'Class:User/NonStandaloneProfileWarning-ReparationMessage' => 'Profile %1$s cannot be standalone. User %2$s has been completed with another profile: %3$s.',
+));
+
