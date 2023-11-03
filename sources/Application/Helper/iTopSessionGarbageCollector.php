6cd6daaf54a2eab027255543cafd43e4501a6940
odain <olivier.dain@combodo.com>
2023-11-03 23:14:52 +0100

    6901-add phpdoc + cleanup unahtentified sessions after 1 minute

--- sources/Application/Helper/iTopSessionGarbageCollector.php
+++ sources/Application/Helper/iTopSessionGarbageCollector.php
@@ -9,6 +9,11 @@ class iTopSessionGarbageCollector implements \iBackgroundProcess
 		return 60 * 1; // seconds
 	}
 
+	/**
+	 * @param int $iTimeLimit
+	 *
+	 * @return string
+	 */
 	public function Process($iTimeLimit)
 	{
 		$max_lifetime = ini_get('session.gc_maxlifetime') ?? 60;
