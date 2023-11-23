af6d9e57437bfe62053513486b84af0e91161f89
Pierre Goiffon <pierre.goiffon@combodo.com>
2023-11-23 11:34:38 +0100

    Revert "Fix depreciation on Request::HEADER_X_FORWARDED_ALL constant in index.php"
    This reverts commit be4daef79a330e4a391c402147767172397ffe23.

--- datamodels/2.x/itop-portal-base/portal/public/index.php
+++ datamodels/2.x/itop-portal-base/portal/public/index.php
@@ -29,6 +29,12 @@ $oContext2 = new ContextTag('Portal:'.$_ENV['PORTAL_ID']);
 
 $oKPI = new ExecutionKPI();
 
+// Note: Manually refactored ternary condition to be PHP 5.x compatible
+if ($trustedProxies = isset($_SERVER['TRUSTED_PROXIES']) ? $_SERVER['TRUSTED_PROXIES'] : (isset($_ENV['TRUSTED_PROXIES']) ? $_ENV['TRUSTED_PROXIES'] : false)) {
+	Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
+}
+
+// Note: Manually refactored ternary condition to be PHP 5.x compatible
 if ($trustedHosts = isset($_SERVER['TRUSTED_HOSTS']) ? $_SERVER['TRUSTED_HOSTS'] : (isset($_ENV['TRUSTED_HOSTS']) ? $_ENV['TRUSTED_HOSTS'] : false)) {
 	Request::setTrustedHosts([$trustedHosts]);
 }
