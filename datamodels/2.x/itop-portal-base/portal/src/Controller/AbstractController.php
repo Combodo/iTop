f0ce09180947b3fcdd013d86978155b6e62cc289
bdalsass <95754414+bdalsass@users.noreply.github.com>
2023-11-24 08:55:21 +0100

    N°6933 - Symfony 6.4 - Remove deprecated calls (#577)
    * N°6933 - SF 6.4 - Remove deprecated calls
    - BrowseBrickController (services access)
    
    * N°6933 - SF 6.4 - Remove deprecated calls
    - AggregatePageBrickController (services access)
    
    * N°6933 - SF 6.4 - Remove deprecated calls
    - CreateBrickController (services access)
    
    * N°6933 - SF 6.4 - Remove deprecated calls
    - ManageBrickController (services access)
    
    * N°6933 - SF 6.4 - Remove deprecated calls
    - ObjectController (services access)
    
    * N°6933 - SF 6.4 - Remove deprecated calls
    - SessionMessageController (services access)
    
    * N°6933 - SF 6.4 - Remove deprecated calls
    - UserProfileBrickController (services access)
    
    * N°6933 - SF 6.4 - Remove deprecated calls
    - AbstractController (remove getSubscribedServices override, add setter for router service injection)
    
    * Fix SessionMessageController Call to a member function AddMessage() on null
    
    * Fix depreciation on Request::HEADER_X_FORWARDED_ALL constant in index.php
    
    Chose to move the protection to the yaml config instead as it is possible since Symfony 5.2
    
    Also removed default support for the $_SERVER['TRUSTED_PROXIES'], seems too untrusty. Use TRUSTED_PROXIES env var instead !
    
    See https://jolicode.com/blog/symfony-reverse-proxies-and-ip-protection
    
    * :pencil2: N°6933 - SF 6.4 - Remove deprecated calls
    - AbstractController (typo)
    
    * N°6933 - SF 6.4 - Remove deprecated calls
    - CreateBrickController (remove return type)
    
    * Revert "Fix depreciation on Request::HEADER_X_FORWARDED_ALL constant in index.php"
    
    This reverts commit be4daef79a330e4a391c402147767172397ffe23.
    
    * Update datamodels/2.x/itop-portal-base/portal/src/Controller/AbstractController.php
    
    Co-authored-by: Molkobain <lajarige.guillaume@free.fr>
    
    * Update datamodels/2.x/itop-portal-base/portal/src/Controller/BrowseBrickController.php
    
    Co-authored-by: Molkobain <lajarige.guillaume@free.fr>
    
    * Update datamodels/2.x/itop-portal-base/portal/src/Controller/BrowseBrickController.php
    
    Co-authored-by: Molkobain <lajarige.guillaume@free.fr>
    
    * Update datamodels/2.x/itop-portal-base/portal/src/Controller/ObjectController.php
    
    Co-authored-by: Molkobain <lajarige.guillaume@free.fr>
    
    * Update datamodels/2.x/itop-portal-base/portal/src/Controller/AggregatePageBrickController.php
    
    Co-authored-by: Molkobain <lajarige.guillaume@free.fr>
    
    * Update datamodels/2.x/itop-portal-base/portal/src/Controller/UserProfileBrickController.php
    
    Co-authored-by: Molkobain <lajarige.guillaume@free.fr>
    
    * Update datamodels/2.x/itop-portal-base/portal/src/Controller/UserProfileBrickController.php
    
    Co-authored-by: Molkobain <lajarige.guillaume@free.fr>
    
    * Update datamodels/2.x/itop-portal-base/portal/src/Controller/UserProfileBrickController.php
    
    Co-authored-by: Molkobain <lajarige.guillaume@free.fr>
    
    * Update datamodels/2.x/itop-portal-base/portal/src/Controller/SessionMessageController.php
    
    Co-authored-by: Molkobain <lajarige.guillaume@free.fr>
    
    * Update datamodels/2.x/itop-portal-base/portal/src/Controller/BrowseBrickController.php
    
    Co-authored-by: Molkobain <lajarige.guillaume@free.fr>
    
    * N°6933 - SF 6.4 - Remove deprecated calls
    - ALl controllers (php doc and parameters names)
    
    ---------
    
    Co-authored-by: Pierre Goiffon <pierre.goiffon@combodo.com>
    Co-authored-by: Molkobain <lajarige.guillaume@free.fr>

--- datamodels/2.x/itop-portal-base/portal/src/Controller/AbstractController.php
+++ datamodels/2.x/itop-portal-base/portal/src/Controller/AbstractController.php
@@ -21,6 +21,8 @@
 namespace Combodo\iTop\Portal\Controller;
 
 use \Symfony\Bundle\FrameworkBundle\Controller\AbstractController as SymfonyAbstractController;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Contracts\Service\Attribute\Required;
 
 /**
  * Class AbstractController
@@ -32,32 +34,16 @@ use \Symfony\Bundle\FrameworkBundle\Controller\AbstractController as SymfonyAbst
 abstract class AbstractController extends SymfonyAbstractController
 {
 	/**
-	 * Return services needed inside controllers.
-	 * Allow access to service via $controller->get(`service_name`).
+	 * @var \Symfony\Component\Routing\RouterInterface symfony router
 	 *
-	 * Improvement: Use service dependency injection
-	 *
-	 * @return array array of service injected to controllers
-	 * @since 3.1.0
-	 *
+	 * @since 3.2.0 N°6933
 	 */
-	public static function getSubscribedServices(): array
+	private RouterInterface $oRouter;
+
+	#[Required]
+	public function setRouter(RouterInterface $oRouter): void
 	{
-		return array_merge(parent::getSubscribedServices(), [
-			'brick_collection'        => 'Combodo\iTop\Portal\Brick\BrickCollection',
-			'request_manipulator'     => 'Combodo\iTop\Portal\Helper\RequestManipulatorHelper',
-			'scope_validator'         => 'Combodo\iTop\Portal\Helper\ScopeValidatorHelper',
-			'security_helper'         => 'Combodo\iTop\Portal\Helper\SecurityHelper',
-			'context_manipulator'     => 'Combodo\iTop\Portal\Helper\ContextManipulatorHelper',
-			'navigation_rule_helper'  => 'Combodo\iTop\Portal\Helper\NavigationRuleHelper',
-			'ui_extensions_helper'    => 'Combodo\iTop\Portal\Helper\UIExtensionsHelper',
-			'lifecycle_validator'     => 'Combodo\iTop\Portal\Helper\LifecycleValidatorHelper',
-			'url_generator'           => 'router',
-			'object_form_handler'     => 'Combodo\iTop\Portal\Helper\ObjectFormHandlerHelper',
-			'browse_brick'            => 'Combodo\iTop\Portal\Helper\BrowseBrickHelper',
-			'brick_controller_helper' => 'Combodo\iTop\Portal\Helper\BrickControllerHelper',
-			'session_message_helper'  => 'Combodo\iTop\Portal\Helper\SessionMessageHelper',
-		]);
+		$this->oRouter = $oRouter;
 	}
 
 	/**
@@ -76,7 +62,7 @@ abstract class AbstractController extends SymfonyAbstractController
 	 */
 	protected function ForwardToRoute($sRouteName, $aRouteParams, $aQueryParameters, $bPreserveDefaultRouteParams = true)
 	{
-		$oRouteCollection = $this->get('router')->getRouteCollection();
+		$oRouteCollection = $this->oRouter->getRouteCollection();
 		$aRouteDefaults = $oRouteCollection->get($sRouteName)->getDefaults();
 
 		if ($bPreserveDefaultRouteParams) {
@@ -113,7 +99,7 @@ abstract class AbstractController extends SymfonyAbstractController
 	 */
 	protected function GetControllerNameFromRoute($sRouteName)
 	{
-		$oRouteCollection = $this->get('router')->getRouteCollection();
+		$oRouteCollection = $this->oRouter->getRouteCollection();
 		$aRouteDefaults = $oRouteCollection->get($sRouteName)->getDefaults();
 
 		return $aRouteDefaults['_controller'];
