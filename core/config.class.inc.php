0c467e71156092f78ba67e82832df95efd6bdc34
peckpeck <benoit.peccatte@normation.com>
2023-12-14 10:08:37 +0100

    N°7067 - Add setting to change default "password change" URL (#540)
    * Add a setting to change default "password change" URL
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: Molkobain <lajarige.guillaume@free.fr>

--- core/config.class.inc.php
+++ core/config.class.inc.php
@@ -889,6 +889,14 @@ class Config
 			'source_of_value' => '',
 			'show_in_conf_sample' => false,
 		],
+    'forgot_password.url' => [
+			'type'                => 'string',
+			'description'         => 'Set this value to your "forgot password" service URL if it should be handled out of '.ITOP_APPLICATION_SHORT.'. Note that it will apply to all users (iTop users, LDAP users, ...)',
+			'default'             => '',
+			'value'               => '',
+			'source_of_value'     => '',
+			'show_in_conf_sample' => false,
+		],
 		'deadline_format' => [
 			'type' => 'string',
 			'description' => 'The format used for displaying "deadline" attributes: any string with the following placeholders: $date$, $difference$',
