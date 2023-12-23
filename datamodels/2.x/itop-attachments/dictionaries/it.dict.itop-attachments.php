9e6981d4cefc2410a7281379451aefa9680228ba
DarkNight97boss <132572493+DarkNight97boss@users.noreply.github.com>
2023-12-23 20:53:18 +0100

    Update it.dict.itop-attachments.php

--- datamodels/2.x/itop-attachments/dictionaries/it.dict.itop-attachments.php
+++ datamodels/2.x/itop-attachments/dictionaries/it.dict.itop-attachments.php
@@ -21,26 +21,26 @@
  * along with iTop. If not, see <http://www.gnu.org/licenses/>
  */
 Dict::Add('IT IT', 'Italian', 'Italiano', array(
-	'Attachments:TabTitle_Count' => 'Attachments (%1$d)~~',
-	'Attachments:EmptyTabTitle' => 'Attachments~~',
-	'Attachments:FieldsetTitle' => 'Attachments~~',
-	'Attachments:DeleteBtn' => 'Delete~~',
-	'Attachments:History_File_Added' => 'Attachment %1$s added.~~',
-	'Attachments:History_File_Removed' => 'Attachment %1$s removed.~~',
-	'Attachments:AddAttachment' => 'Add attachment: ~~',
-	'Attachments:UploadNotAllowedOnThisSystem' => 'File upload in NOT allowed on this system.~~',
-	'Attachment:Max_Go' => '(Maximum file size: %1$s GB)~~',
-	'Attachment:Max_Mo' => '(Maximum file size: %1$s MB)~~',
-	'Attachment:Max_Ko' => '(Maximum file size: %1$s KB)~~',
-	'Attachments:NoAttachment' => 'No attachment. ~~',
-	'Attachments:PreviewNotAvailable' => 'Preview not available for this type of attachment.~~',
-	'Attachments:Error:FileTooLarge' => 'File is too large to be uploaded. %1$s~~',
-	'Attachments:Error:UploadedFileEmpty' => 'The received file is empty and cannot be attached.
-Either you have pushed an empty file,
-or ask your '.ITOP_APPLICATION_SHORT.' administrator if the '.ITOP_APPLICATION_SHORT.' server disk is full.~~',
-	'Attachments:Render:Icons' => 'Display as icons~~',
-	'Attachments:Render:Table' => 'Display as list~~',
-	'UI:Attachments:DropYourFileHint' => 'Drop files anywhere in this area~~',
+	'Attachments:TabTitle_Count' => 'Allegati (%1$d)~~',
+	'Attachments:EmptyTabTitle' => 'Allegati~~',
+	'Attachments:FieldsetTitle' => 'Allegati~~',
+	'Attachments:DeleteBtn' => 'Elimina~~',
+	'Attachments:History_File_Added' => 'Allegato %1$s aggiunto.~~',
+	'Attachments:History_File_Removed' => 'Allegato %1$s rimosso.~~',
+	'Attachments:AddAttachment' => 'Aggiungi allegato: ~~',
+	'Attachments:UploadNotAllowedOnThisSystem' => 'Caricamento file NON consentito su questo sistema.~~',
+	'Attachment:Max_Go' => '(Dimensione massima del file: %1$s GB)~~',
+	'Attachment:Max_Mo' => '(Dimensione massima del file: %1$s MB)~~',
+	'Attachment:Max_Ko' => '(Dimensione massima del file: %1$s KB)~~',
+	'Attachments:NoAttachment' => 'Nessun allegato. ~~',
+	'Attachments:PreviewNotAvailable' => 'Anteprima non disponibile per questo tipo di allegato.~~',
+	'Attachments:Error:FileTooLarge' => 'Il file è troppo grande per essere caricato. %1$s~~',
+	'Attachments:Error:UploadedFileEmpty' => 'Il file ricevuto è vuoto e non può essere allegato.
+	O hai inviato un file vuoto,
+	o chiedi al tuo amministratore di '.ITOP_APPLICATION_SHORT.' se il disco del server '.ITOP_APPLICATION_SHORT.' è pieno.~~',
+	'Attachments:Render:Icons' => 'Visualizza come icone~~',
+	'Attachments:Render:Table' => 'Visualizza come lista~~',
+	'UI:Attachments:DropYourFileHint' => 'Rilascia i file ovunque in quest\'area~~',
 ));
 
 //
@@ -48,43 +48,33 @@ or ask your '.ITOP_APPLICATION_SHORT.' administrator if the '.ITOP_APPLICATION_S
 //
 
 Dict::Add('IT IT', 'Italian', 'Italiano', array(
-	'Class:Attachment' => 'Attachment~~',
-	'Class:Attachment+' => '~~',
-	'Class:Attachment/Attribute:expire' => 'Expire~~',
-	'Class:Attachment/Attribute:expire+' => '~~',
-	'Class:Attachment/Attribute:temp_id' => 'Temporary id~~',
-	'Class:Attachment/Attribute:temp_id+' => '~~',
-	'Class:Attachment/Attribute:item_class' => 'Item class~~',
-	'Class:Attachment/Attribute:item_class+' => '~~',
-	'Class:Attachment/Attribute:item_id' => 'Item~~',
-	'Class:Attachment/Attribute:item_id+' => '~~',
-	'Class:Attachment/Attribute:item_org_id' => 'Item organization~~',
-	'Class:Attachment/Attribute:item_org_id+' => '~~',
-	'Class:Attachment/Attribute:contents' => 'Contents~~',
-	'Class:Attachment/Attribute:contents+' => '~~',
+	'Class:Attachment' => 'Allegato~~',
+	'Class:Attachment/Attribute:expire' => 'Scadenza~~',
+	'Class:Attachment/Attribute:temp_id' => 'ID temporaneo~~',
+	'Class:Attachment/Attribute:item_class' => 'Classe dell\'oggetto~~',
+	'Class:Attachment/Attribute:item_id' => 'Oggetto~~',
+	'Class:Attachment/Attribute:item_org_id' => 'Organizzazione dell\'oggetto~~',
+	'Class:Attachment/Attribute:contents' => 'Contenuti~~',
 ));
 
 
 Dict::Add('IT IT', 'Italian', 'Italiano', array(
-	'Attachments:File:Thumbnail' => 'Icon~~',
-	'Attachments:File:Name' => 'File name~~',
-	'Attachments:File:Date' => 'Upload date~~',
-	'Attachments:File:Uploader' => 'Uploaded by~~',
-	'Attachments:File:Size' => 'Size~~',
-	'Attachments:File:MimeType' => 'Type~~',
-	'Attachments:File:DownloadsCount' => 'Downloads~~',
+	'Attachments:File:Thumbnail' => 'Icona~~',
+	'Attachments:File:Name' => 'Nome del file~~',
+	'Attachments:File:Date' => 'Data di caricamento~~',
+	'Attachments:File:Uploader' => 'Caricato da~~',
+	'Attachments:File:Size' => 'Dimensione~~',
+	'Attachments:File:MimeType' => 'Tipo~~',
+	'Attachments:File:DownloadsCount' => 'Download~~',
 ));
 //
 // Class: Attachment
 //
 
 Dict::Add('IT IT', 'Italian', 'Italiano', array(
-	'Class:Attachment/Attribute:creation_date' => 'Creation date~~',
-	'Class:Attachment/Attribute:creation_date+' => '~~',
-	'Class:Attachment/Attribute:user_id' => 'User id~~',
-	'Class:Attachment/Attribute:user_id+' => '~~',
-	'Class:Attachment/Attribute:contact_id' => 'Contact id~~',
-	'Class:Attachment/Attribute:contact_id+' => '~~',
+	'Class:Attachment/Attribute:creation_date' => 'Data di creazione~~',
+	'Class:Attachment/Attribute:user_id' => 'ID utente~~',
+	'Class:Attachment/Attribute:contact_id' => 'ID contatto~~',
 ));
 
 //
@@ -92,6 +82,6 @@ Dict::Add('IT IT', 'Italian', 'Italiano', array(
 //
 
 Dict::Add('IT IT', 'Italian', 'Italiano', array(
-	'Class:TriggerOnAttachmentDownload' => 'Trigger (on object\'s attachment download)~~',
-	'Class:TriggerOnAttachmentDownload+' => 'Trigger on object\'s attachment download of [a child class of] the given class~~',
+	'Class:TriggerOnAttachmentDownload' => 'Trigger (al download di un allegato dell\'oggetto)~~',
+	'Class:TriggerOnAttachmentDownload+' => 'Trigger al download di un allegato di un oggetto di [una sottoclasse di] la classe data~~',
 ));
