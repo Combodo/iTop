3cb007e96d701485de944a0f9dd318f6e396586b
Pierre Goiffon <pierre.goiffon@combodo.com>
2024-01-22 15:37:00 +0100

    Fix utilsTest::testGetAbsoluteUrlAppRootPersistency as we are now using a static class attribute

--- tests/php-unit-tests/unitary-tests/application/utilsTest.php
+++ tests/php-unit-tests/unitary-tests/application/utilsTest.php
@@ -248,6 +248,9 @@ class utilsTest extends ItopTestCase
 	 */
 	public function testGetAbsoluteUrlAppRootPersistency($bBehindReverseProxy,$bForceTrustProxy1 ,$sExpectedAppRootUrl1,$bForceTrustProxy2 , $sExpectedAppRootUrl2,$bForceTrustProxy3 , $sExpectedAppRootUrl3)
 	{
+		// resetting static property for each test pass
+		$this->SetNonPublicStaticProperty(utils::class, 'sAbsoluteUrlAppRootCache', null);
+
 		utils::GetConfig()->Set('behind_reverse_proxy', $bBehindReverseProxy);
 		utils::GetConfig()->Set('app_root_url', '');
 
