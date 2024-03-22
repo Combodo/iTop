52106fe48cc36020063025ab5511f6b8f1e86c0f
Molkobain <lajarige.guillaume@free.fr>
2024-03-22 14:21:44 +0100

    N°7331 - Remove unnecessary files

--- node_modules/c3/src/ua.js
+++
@@ -1,8 +0,0 @@
-c3_chart_internal_fn.isSafari = function () {
-    var ua = window.navigator.userAgent;
-    return ua.indexOf('Safari') >= 0 && ua.indexOf('Chrome') < 0;
-};
-c3_chart_internal_fn.isChrome = function () {
-    var ua = window.navigator.userAgent;
-    return ua.indexOf('Chrome') >= 0;
-};
