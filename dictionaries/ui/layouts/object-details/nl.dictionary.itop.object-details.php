c9a1dbb350ad7b12735b8a9055732a579e28c524
Anne-Cath <anne-catherine.cognet@combodo.com>
2024-03-26 11:29:15 +0100

    nl tranlation

--- dictionaries/ui/layouts/object-details/nl.dictionary.itop.object-details.php
+++ dictionaries/ui/layouts/object-details/nl.dictionary.itop.object-details.php
@@ -9,5 +9,5 @@ Dict::Add('NL NL', 'Dutch', 'Nederlands', [
 	'UI:Layout:ObjectDetails:KeyboardShortcut:NewObject' => 'Maak een nieuw object (van dezelfde klasse als het getoonde object)',
 	'UI:Layout:ObjectDetails:KeyboardShortcut:SaveObject' => 'Bewaar huidig object',
 	'UI:Layout:ObjectDetails:New:Modal:Title' => 'Creation of an object~~',
-	'UI:Layout:ObjectDetails:DatamodelSchemaLink:Tooltip' => 'Class data model schema~~',
+	'UI:Layout:ObjectDetails:DatamodelSchemaLink:Tooltip' => 'Objecten schema van deze klasse',
 ]);
