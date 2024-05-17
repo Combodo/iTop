8823068608ff5b385a639773a808a9fd1bf4a6c9
Romain Quetiez <romain.quetiez@combodo.com>
2024-05-17 15:26:53 +0200

    N°4459 - Sodium default encrypted key invalid in the CI (workaround)

--- tests/php-unit-tests/unitary-tests/core/querybuilderexpressions/QueryBuilderExpressionsTest.php
+++ tests/php-unit-tests/unitary-tests/core/querybuilderexpressions/QueryBuilderExpressionsTest.php
@@ -14,6 +14,14 @@ class QueryBuilderExpressionsTest extends \Combodo\iTop\Test\UnitTest\ItopCustom
 		return __DIR__."/Delta/all-attributes.xml";
 	}
 
+	protected function setUp(): void
+	{
+		parent::setUp();
+
+		// Workaround to cope with inconsistent settings in itop-config files from the CI
+		\AttributeEncryptedString::$sKey = "6eb9d9afa3ee0fbcebe622a33bf57aaeafb7c37998fd24c403c2522c2d60117f";
+	}
+
 	public function testICanWriteAndReadAnyTypeOfAttribute()
 	{
 		$oTagA = \MetaModel::NewObject(\TagSetFieldData::GetTagDataClassName('TestObject', 'tagset'), [
