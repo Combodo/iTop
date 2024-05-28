Combodo/iTop

commit a642ad6db94b858033af4cf07fcc52827d9ac629

N°2039 - Refresh messages without waiting for the providers TTL to avoid news not being visible even though they have been created

js/components/newsroom-menu.js
@@ -45,10 +45,14 @@ $(function()
 
 			// Important: For now, the popover menu is manually instantiated even though the PHP NewsroomMenu class inherits PopoverMenu because the jQuery widget doesn't. We might refactor this in the future.
 			$(me.element).popover_menu({'toggler': this.js_selectors.menu_toggler});
-			$(this.js_selectors.menu_toggler).on('click', function (oEvent) {
+			$(this.js_selectors.menu_toggler).off('click').on('click', function (oEvent) {
 				var oEventTarget = $(oEvent.target);
 				var aEventTargetPos = oEventTarget.position();
 				var aEventTargetOffset = oEventTarget.offset();
+
+				// N°2039 - When opening the menu, refresh messages without waiting for the providers TTL to avoid news not being visible even though they have been created
+				me.clearCache();
+				me._getAllMessages();
 
 				$iHeight = Math.abs(aEventTargetOffset.top-100);
 				$(me.element).css({

commit afc40d9ccad8c2d3a684917f362525c2de44d0f1

N°2039 - Fix newsroom menu not opening due to regression in a642ad6d

js/components/newsroom-menu.js
@@ -43,9 +43,14 @@ $(function()
 		{
 			var me = this;
 
+			// Check if popover menu is already initialized
+			if ($(this.js_selectors.menu_toggler).hasClass('ibo-is-loaded') === true) {
+				return;
+			}
+
 			// Important: For now, the popover menu is manually instantiated even though the PHP NewsroomMenu class inherits PopoverMenu because the jQuery widget doesn't. We might refactor this in the future.
 			$(me.element).popover_menu({'toggler': this.js_selectors.menu_toggler});
-			$(this.js_selectors.menu_toggler).off('click').on('click', function (oEvent) {
+			$(this.js_selectors.menu_toggler).on('click', function (oEvent) {
 				var oEventTarget = $(oEvent.target);
 				var aEventTargetPos = oEventTarget.position();
 				var aEventTargetOffset = oEventTarget.offset();