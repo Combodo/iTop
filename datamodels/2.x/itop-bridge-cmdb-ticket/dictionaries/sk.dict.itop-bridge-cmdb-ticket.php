18d7510a43e2c0e4fa5620c6e2c5343f1277312c
Timothee <timothee.callet@combodo.com>
2024-06-04 15:50:22 +0200

    Remove duplicates in SK dictionaries

--- datamodels/2.x/itop-bridge-cmdb-ticket/dictionaries/sk.dict.itop-bridge-cmdb-ticket.php
+++ datamodels/2.x/itop-bridge-cmdb-ticket/dictionaries/sk.dict.itop-bridge-cmdb-ticket.php
@@ -1,61 +1,35 @@
 <?php
-// Copyright (C) 2010-2024 Combodo SAS
-//
-//   This file is part of iTop.
-//
-//   iTop is free software; you can redistribute it and/or modify
-//   it under the terms of the GNU Affero General Public License as published by
-//   the Free Software Foundation, either version 3 of the License, or
-//   (at your option) any later version.
-//
-//   iTop is distributed in the hope that it will be useful,
-//   but WITHOUT ANY WARRANTY; without even the implied warranty of
-//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//   GNU Affero General Public License for more details.
-//
-//   You should have received a copy of the GNU Affero General Public License
-//   along with iTop. If not, see <http://www.gnu.org/licenses/>
 /**
-* @author       Benjamin Planque <benjamin.planque@combodo.com>
-* @copyright   Copyright (C) 2010-2024 Combodo SAS
-* @license     http://opensource.org/licenses/AGPL-3.0
-*/
-//////////////////////////////////////////////////////////////////////
-// Note: The classes have been grouped by categories: bizmodel
-//////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////
-// Classes in 'bizmodel'
-//////////////////////////////////////////////////////////////////////
-//
-//
-// Class: lnkFunctionalCIToTicket
-//
-Dict::Add('SK SK', 'Slovak', 'Slovenčina', array(
-	'Class:lnkFunctionalCIToTicket' => 'väzba - Komponent / Ticket',
-	'Class:lnkFunctionalCIToTicket+' => '',
-	'Class:lnkFunctionalCIToTicket/Name' => '%1$s / %2$s~~',
-	'Class:lnkFunctionalCIToTicket/Attribute:ticket_id' => 'Ticket',
-	'Class:lnkFunctionalCIToTicket/Attribute:ticket_id+' => '',
-	'Class:lnkFunctionalCIToTicket/Attribute:ticket_ref' => 'Referencia na Ticket',
-	'Class:lnkFunctionalCIToTicket/Attribute:ticket_ref+' => '',
-	'Class:lnkFunctionalCIToTicket/Attribute:ticket_title' => 'Ticket title~~',
-	'Class:lnkFunctionalCIToTicket/Attribute:ticket_title+' => '~~',
-	'Class:lnkFunctionalCIToTicket/Attribute:functionalci_id' => 'Komponent',
-	'Class:lnkFunctionalCIToTicket/Attribute:functionalci_id+' => '',
-	'Class:lnkFunctionalCIToTicket/Attribute:functionalci_name' => 'Názov CI',
-	'Class:lnkFunctionalCIToTicket/Attribute:functionalci_name+' => '',
-	'Class:lnkFunctionalCIToTicket/Attribute:impact' => 'Dopad',
-	'Class:lnkFunctionalCIToTicket/Attribute:impact+' => '',
-	'Class:lnkFunctionalCIToTicket/Attribute:impact_code' => 'Impact~~',
-	'Class:lnkFunctionalCIToTicket/Attribute:impact_code/Value:manual' => 'Added manually~~',
-	'Class:lnkFunctionalCIToTicket/Attribute:impact_code/Value:computed' => 'Computed~~',
-	'Class:lnkFunctionalCIToTicket/Attribute:impact_code/Value:not_impacted' => 'Not impacted~~',
-));
-
-//
-// Class: FunctionalCI
-//
-Dict::Add('SK SK', 'Slovak', 'Slovenčina', array(
+ * Localized data
+ *
+ * @copyright Copyright (C) 2010-2024 Combodo SAS
+ * @license    https://opensource.org/licenses/AGPL-3.0
+ * 
+ */
+/**
+ * @author Benjamin Planque <benjamin.planque@combodo.com>
+ *
+ */
+Dict::Add('SK SK', 'Slovak', 'Slovenčina', [
 	'Class:FunctionalCI/Attribute:tickets_list' => 'Tickety',
 	'Class:FunctionalCI/Attribute:tickets_list+' => 'All the tickets for this configuration item~~',
-));
+	'Class:lnkFunctionalCIToTicket' => 'väzba - Komponent / Ticket',
+	'Class:lnkFunctionalCIToTicket+' => '~~',
+	'Class:lnkFunctionalCIToTicket/Attribute:functionalci_id' => 'Komponent',
+	'Class:lnkFunctionalCIToTicket/Attribute:functionalci_id+' => '~~',
+	'Class:lnkFunctionalCIToTicket/Attribute:functionalci_name' => 'Názov CI',
+	'Class:lnkFunctionalCIToTicket/Attribute:functionalci_name+' => '~~',
+	'Class:lnkFunctionalCIToTicket/Attribute:impact' => 'Dopad',
+	'Class:lnkFunctionalCIToTicket/Attribute:impact+' => '~~',
+	'Class:lnkFunctionalCIToTicket/Attribute:impact_code' => 'Impact~~',
+	'Class:lnkFunctionalCIToTicket/Attribute:impact_code/Value:computed' => 'Computed~~',
+	'Class:lnkFunctionalCIToTicket/Attribute:impact_code/Value:manual' => 'Added manually~~',
+	'Class:lnkFunctionalCIToTicket/Attribute:impact_code/Value:not_impacted' => 'Not impacted~~',
+	'Class:lnkFunctionalCIToTicket/Attribute:ticket_id' => 'Ticket',
+	'Class:lnkFunctionalCIToTicket/Attribute:ticket_id+' => '~~',
+	'Class:lnkFunctionalCIToTicket/Attribute:ticket_ref' => 'Referencia na Ticket',
+	'Class:lnkFunctionalCIToTicket/Attribute:ticket_ref+' => '~~',
+	'Class:lnkFunctionalCIToTicket/Attribute:ticket_title' => 'Ticket title~~',
+	'Class:lnkFunctionalCIToTicket/Attribute:ticket_title+' => '~~',
+	'Class:lnkFunctionalCIToTicket/Name' => '%1$s / %2$s~~',
+]);
