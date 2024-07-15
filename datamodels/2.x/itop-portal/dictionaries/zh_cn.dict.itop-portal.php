602910d606af636e7dd3cbe2074904704c2c4d78
Dejin Bie <bdejin@hotmail.com>
2024-07-15 23:54:02 +0800

    add missing translation in itop-portal.php

--- datamodels/2.x/itop-portal/dictionaries/zh_cn.dict.itop-portal.php
+++ datamodels/2.x/itop-portal/dictionaries/zh_cn.dict.itop-portal.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Localized data
  *
@@ -11,19 +12,19 @@
  *
  */
 Dict::Add('ZH CN', 'Chinese', '简体中文', [
-	'Brick:Portal:ClosedRequests:Title' => '已关闭的工单',
-	'Brick:Portal:ListAllRequests:Tab' => 'On-going and closed~~',
-	'Brick:Portal:ListAllRequests:Title' => 'All requests~~',
-	'Brick:Portal:ListAllRequests:Title+' => '<p>View all requests regardless of their status.</p>~~',
-	'Brick:Portal:NewRequest:Title' => '新建工单',
-	'Brick:Portal:NewRequest:Title+' => '<p>需要帮助?</p><p>选择子服务, 然后提交工单给我们的支持团队.</p>',
-	'Brick:Portal:OngoingRequests:Tab:OnGoing' => '正在处理',
-	'Brick:Portal:OngoingRequests:Tab:Resolved' => '已解决',
-	'Brick:Portal:OngoingRequests:Title' => '正在处理的工单',
-	'Brick:Portal:OngoingRequests:Title+' => '<p>跟踪正在处理的工单.</p><p>查询进度, 添加评论, 添加附件, 确认解决方案.</p>',
-	'Brick:Portal:SearchInAllRequests:Title' => 'Search in all requests~~',
-	'Brick:Portal:SearchInAllRequests:Title+' => '<p>Regardless of their status.</p>~~',
-	'Brick:Portal:UserProfile:Title' => '我的设置',
-	'Page:DefaultTitle' => ITOP_APPLICATION_SHORT.' - 用户门户',
-	'portal:itop-portal' => '标准门户',
+    'Brick:Portal:ClosedRequests:Title' => '已关闭的工单',
+    'Brick:Portal:ListAllRequests:Tab' => '处理中和已关闭共担',
+    'Brick:Portal:ListAllRequests:Title' => '全部工单',
+    'Brick:Portal:ListAllRequests:Title+' => '<p>查看所有工单无论其为何种状态.</p>',
+    'Brick:Portal:NewRequest:Title' => '新建工单',
+    'Brick:Portal:NewRequest:Title+' => '<p>需要帮助?</p><p>选择子服务, 然后提交工单给我们的支持团队.</p>',
+    'Brick:Portal:OngoingRequests:Tab:OnGoing' => '正在处理',
+    'Brick:Portal:OngoingRequests:Tab:Resolved' => '已解决',
+    'Brick:Portal:OngoingRequests:Title' => '正在处理的工单',
+    'Brick:Portal:OngoingRequests:Title+' => '<p>跟踪正在处理的工单.</p><p>查询进度, 添加评论, 添加附件, 确认解决方案.</p>',
+    'Brick:Portal:SearchInAllRequests:Title' => '在所有工单中搜索',
+    'Brick:Portal:SearchInAllRequests:Title+' => '<p>无论其为何种状态.</p>',
+    'Brick:Portal:UserProfile:Title' => '我的设置',
+    'Page:DefaultTitle' => ITOP_APPLICATION_SHORT . ' - 用户门户',
+    'portal:itop-portal' => '标准门户',
 ]);
