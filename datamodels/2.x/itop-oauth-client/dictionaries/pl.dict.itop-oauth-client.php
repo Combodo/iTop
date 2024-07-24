6bc63d31168a9785fad36b928f990acc2bcf4493
DudekArtur <80252112+DudekArtur@users.noreply.github.com>
2024-07-24 09:45:36 +0200

    Add/Modify Polish translate files

--- datamodels/2.x/itop-oauth-client/dictionaries/pl.dict.itop-oauth-client.php
+++ datamodels/2.x/itop-oauth-client/dictionaries/pl.dict.itop-oauth-client.php
@@ -10,62 +10,62 @@
  *
  */
 Dict::Add('PL PL', 'Polish', 'Polski', [
-	'Class:OAuthClient' => 'OAuth Client~~',
-	'Class:OAuthClient/Attribute:client_id' => 'Client id~~',
-	'Class:OAuthClient/Attribute:client_id+' => 'A long string of characters provided by your OAuth2 provider~~',
-	'Class:OAuthClient/Attribute:client_secret' => 'Client secret~~',
-	'Class:OAuthClient/Attribute:client_secret+' => 'Another long string of characters provided by your OAuth2 provider~~',
-	'Class:OAuthClient/Attribute:description' => 'Description~~',
-	'Class:OAuthClient/Attribute:description+' => '~~',
-	'Class:OAuthClient/Attribute:mailbox_list' => 'Mailbox list~~',
-	'Class:OAuthClient/Attribute:mailbox_list+' => '~~',
-	'Class:OAuthClient/Attribute:name' => 'Login~~',
-	'Class:OAuthClient/Attribute:name+' => 'In general, this is your email address~~',
-	'Class:OAuthClient/Attribute:provider' => 'Provider~~',
-	'Class:OAuthClient/Attribute:provider+' => '~~',
-	'Class:OAuthClient/Attribute:redirect_url' => 'Redirect url~~',
-	'Class:OAuthClient/Attribute:redirect_url+' => 'This url must be copied in the OAuth2 configuration of the provider
-Erase the field to recalculate default value~~',
-	'Class:OAuthClient/Attribute:refresh_token' => 'Refresh token~~',
-	'Class:OAuthClient/Attribute:refresh_token+' => '~~',
-	'Class:OAuthClient/Attribute:refresh_token_expiration' => 'Refresh token expiration~~',
-	'Class:OAuthClient/Attribute:refresh_token_expiration+' => '~~',
-	'Class:OAuthClient/Attribute:status' => 'Status~~',
-	'Class:OAuthClient/Attribute:status+' => 'After creation, use the action “Generate access token” to be able to use this OAuth client~~',
+	'Class:OAuthClient' => 'Klient OAuth',
+	'Class:OAuthClient/Attribute:client_id' => 'Id klienta',
+	'Class:OAuthClient/Attribute:client_id+' => 'Długi ciąg znaków dostarczony przez dostawcę protokołu OAuth2',
+	'Class:OAuthClient/Attribute:client_secret' => 'Sekretny ciąg',
+	'Class:OAuthClient/Attribute:client_secret+' => 'Kolejny długi ciąg znaków dostarczony przez dostawcę OAuth2',
+	'Class:OAuthClient/Attribute:description' => 'Opis',
+	'Class:OAuthClient/Attribute:description+' => '',
+	'Class:OAuthClient/Attribute:mailbox_list' => 'Skrzynki pocztowe',
+	'Class:OAuthClient/Attribute:mailbox_list+' => '',
+	'Class:OAuthClient/Attribute:name' => 'Login',
+	'Class:OAuthClient/Attribute:name+' => 'Ogólnie rzecz biorąc, jest to Twój adres e-mail',
+	'Class:OAuthClient/Attribute:provider' => 'Dostawca',
+	'Class:OAuthClient/Attribute:provider+' => '',
+	'Class:OAuthClient/Attribute:redirect_url' => 'Adres URL przekierowania',
+	'Class:OAuthClient/Attribute:redirect_url+' => 'Ten adres URL należy skopiować w konfiguracji OAuth2 dostawcy
+Usuń pole, aby ponownie obliczyć wartość domyślną',
+	'Class:OAuthClient/Attribute:refresh_token' => 'Token odświeżania',
+	'Class:OAuthClient/Attribute:refresh_token+' => '',
+	'Class:OAuthClient/Attribute:refresh_token_expiration' => 'Czas wygaśnięcia tokena odświeżania',
+	'Class:OAuthClient/Attribute:refresh_token_expiration+' => '',
+	'Class:OAuthClient/Attribute:status' => 'Status',
+	'Class:OAuthClient/Attribute:status+' => 'Po utworzeniu użyj akcji „Generuj token dostępu”, aby móc korzystać z tego klienta OAuth',
 	'Class:OAuthClient/Attribute:status/Value:
               active
               
                 $ibo-lifecycle-active-state-primary-color
                 $ibo-lifecycle-active-state-secondary-color
 	' => '
+              aktywny
+              
+                $ibo-lifecycle-active-state-primary-color
+                $ibo-lifecycle-active-state-secondary-color
+                
+              
+            ',
+	'Class:OAuthClient/Attribute:status/Value:
               active
               
                 $ibo-lifecycle-active-state-primary-color
                 $ibo-lifecycle-active-state-secondary-color
                 
               
-            ~~',
-	'Class:OAuthClient/Attribute:status/Value:
-              active
-              
-                $ibo-lifecycle-active-state-primary-color
-                $ibo-lifecycle-active-state-secondary-color
-                
-              
-            +' => '~~',
+            +' => '',
 	'Class:OAuthClient/Attribute:status/Value:
               inactive
               
                 $ibo-lifecycle-inactive-state-primary-color
                 $ibo-lifecycle-inactive-state-secondary-color
 	' => '
-              inactive
+              nieaktywny
               
                 $ibo-lifecycle-inactive-state-primary-color
                 $ibo-lifecycle-inactive-state-secondary-color
                 
               
-            ~~',
+            ',
 	'Class:OAuthClient/Attribute:status/Value:
               inactive
               
@@ -73,166 +73,166 @@ Erase the field to recalculate default value~~',
                 $ibo-lifecycle-inactive-state-secondary-color
                 
               
-            +' => '~~',
-	'Class:OAuthClient/Attribute:status/Value:active' => 'Access token generated~~',
-	'Class:OAuthClient/Attribute:status/Value:inactive' => 'No Access token~~',
-	'Class:OAuthClient/Attribute:token' => 'Access token~~',
-	'Class:OAuthClient/Attribute:token+' => '~~',
-	'Class:OAuthClient/Attribute:token_expiration' => 'Access token expiration~~',
-	'Class:OAuthClient/Attribute:token_expiration+' => '~~',
-	'Class:OAuthClientAzure' => 'OAuth client for Microsoft Azure~~',
-	'Class:OAuthClientAzure/Attribute:advanced_scope' => 'Advanced scope~~',
-	'Class:OAuthClientAzure/Attribute:advanced_scope+' => 'As soon as you enter something here it takes precedence on the “Scope” selection which is then ignored~~',
-	'Class:OAuthClientAzure/Attribute:scope' => 'Scope~~',
-	'Class:OAuthClientAzure/Attribute:scope+' => 'Usually default selection is appropriate~~',
+            +' => '',
+	'Class:OAuthClient/Attribute:status/Value:active' => 'Wygenerowano token dostępu',
+	'Class:OAuthClient/Attribute:status/Value:inactive' => 'Brak tokena dostępu',
+	'Class:OAuthClient/Attribute:token' => 'Token dostępu',
+	'Class:OAuthClient/Attribute:token+' => '',
+	'Class:OAuthClient/Attribute:token_expiration' => 'Wygaśnięcie tokenu dostępu',
+	'Class:OAuthClient/Attribute:token_expiration+' => '',
+	'Class:OAuthClientAzure' => 'Klient OAuth dla Microsoft Azure',
+	'Class:OAuthClientAzure/Attribute:advanced_scope' => 'Zaawansowany zakres',
+	'Class:OAuthClientAzure/Attribute:advanced_scope+' => 'Gdy tylko coś tu wpiszesz, będzie to miało pierwszeństwo przed wyborem „Zakres”, który zostanie następnie zignorowany',
+	'Class:OAuthClientAzure/Attribute:scope' => 'Zakres',
+	'Class:OAuthClientAzure/Attribute:scope+' => 'Zwykle odpowiedni jest wybór domyślny',
 	'Class:OAuthClientAzure/Attribute:scope/Value:
               IMAP
 	' => '
               IMAP
-            ~~',
+            ',
 	'Class:OAuthClientAzure/Attribute:scope/Value:
               IMAP
-            +' => '~~',
+            +' => '',
 	'Class:OAuthClientAzure/Attribute:scope/Value:
               SMTP
 	' => '
               SMTP
-            ~~',
+            ',
 	'Class:OAuthClientAzure/Attribute:scope/Value:
               SMTP
-            +' => '~~',
-	'Class:OAuthClientAzure/Attribute:scope/Value:IMAP' => 'IMAP~~',
-	'Class:OAuthClientAzure/Attribute:scope/Value:IMAP+' => '~~',
-	'Class:OAuthClientAzure/Attribute:scope/Value:SMTP' => 'SMTP~~',
-	'Class:OAuthClientAzure/Attribute:scope/Value:SMTP+' => '~~',
-	'Class:OAuthClientAzure/Attribute:tenant' => 'Tenant~~',
-	'Class:OAuthClientAzure/Attribute:tenant+' => 'Tenant ID of the configured application. For multi-tenant application, use "common".~~',
-	'Class:OAuthClientAzure/Attribute:used_for_smtp' => 'Used for SMTP~~',
-	'Class:OAuthClientAzure/Attribute:used_for_smtp+' => 'At least one OAuth client must have this flag to “Yes”, if you want iTop to use it for sending mails~~',
+            +' => '',
+	'Class:OAuthClientAzure/Attribute:scope/Value:IMAP' => 'IMAP',
+	'Class:OAuthClientAzure/Attribute:scope/Value:IMAP+' => '',
+	'Class:OAuthClientAzure/Attribute:scope/Value:SMTP' => 'SMTP',
+	'Class:OAuthClientAzure/Attribute:scope/Value:SMTP+' => '',
+	'Class:OAuthClientAzure/Attribute:tenant' => 'Dzierżawa',
+	'Class:OAuthClientAzure/Attribute:tenant+' => 'Identyfikator dzierżawy skonfigurowanej aplikacji. W przypadku aplikacji z wieloma dzierżawcami użyj „wspólnego”.',
+	'Class:OAuthClientAzure/Attribute:used_for_smtp' => 'Używany do SMTP',
+	'Class:OAuthClientAzure/Attribute:used_for_smtp+' => 'Przynajmniej jeden klient OAuth musi mieć tę flagę ustawioną na „Tak”, jeśli chcesz, aby iTop używał jej do wysyłania wiadomości e-mail',
 	'Class:OAuthClientAzure/Attribute:used_for_smtp/Value:
               no
 	' => '
-              no
-            ~~',
+              nie
+            ',
 	'Class:OAuthClientAzure/Attribute:used_for_smtp/Value:
               no
-            +' => '~~',
+            +' => '',
 	'Class:OAuthClientAzure/Attribute:used_for_smtp/Value:
               yes
 	' => '
-              yes
-            ~~',
+              tak
+            ',
 	'Class:OAuthClientAzure/Attribute:used_for_smtp/Value:
               yes
-            +' => '~~',
-	'Class:OAuthClientAzure/Attribute:used_for_smtp/Value:no' => 'No~~',
-	'Class:OAuthClientAzure/Attribute:used_for_smtp/Value:yes' => 'Yes~~',
-	'Class:OAuthClientAzure/Attribute:used_scope' => 'Used scope~~',
-	'Class:OAuthClientAzure/Attribute:used_scope+' => '~~',
+            +' => '',
+	'Class:OAuthClientAzure/Attribute:used_for_smtp/Value:no' => 'Nie',
+	'Class:OAuthClientAzure/Attribute:used_for_smtp/Value:yes' => 'Tak',
+	'Class:OAuthClientAzure/Attribute:used_scope' => 'Użyty zakres',
+	'Class:OAuthClientAzure/Attribute:used_scope+' => '',
 	'Class:OAuthClientAzure/Attribute:used_scope/Value:
               advanced
 	' => '
-              advanced
-            ~~',
+              zaawansowany
+            ',
 	'Class:OAuthClientAzure/Attribute:used_scope/Value:
               advanced
-            +' => '~~',
+            +' => '',
 	'Class:OAuthClientAzure/Attribute:used_scope/Value:
               simple
 	' => '
-              simple
-            ~~',
+              prosty
+            ',
 	'Class:OAuthClientAzure/Attribute:used_scope/Value:
               simple
-            +' => '~~',
-	'Class:OAuthClientAzure/Attribute:used_scope/Value:advanced' => 'Advanced~~',
-	'Class:OAuthClientAzure/Attribute:used_scope/Value:advanced+' => '~~',
-	'Class:OAuthClientAzure/Attribute:used_scope/Value:simple' => 'Simple~~',
-	'Class:OAuthClientAzure/Attribute:used_scope/Value:simple+' => '~~',
-	'Class:OAuthClientAzure/Name' => '%1$s (%2$s)~~',
-	'Class:OAuthClientGoogle' => 'OAuth client for Google~~',
-	'Class:OAuthClientGoogle/Attribute:advanced_scope' => 'Advanced scope~~',
-	'Class:OAuthClientGoogle/Attribute:advanced_scope+' => 'As soon as you enter something here it takes precedence on the “Scope” selection which is then ignored~~',
-	'Class:OAuthClientGoogle/Attribute:scope' => 'Scope~~',
-	'Class:OAuthClientGoogle/Attribute:scope+' => 'Usually default selection is appropriate~~',
+            +' => '',
+	'Class:OAuthClientAzure/Attribute:used_scope/Value:advanced' => 'Zaawansowany',
+	'Class:OAuthClientAzure/Attribute:used_scope/Value:advanced+' => '',
+	'Class:OAuthClientAzure/Attribute:used_scope/Value:simple' => 'Prosty',
+	'Class:OAuthClientAzure/Attribute:used_scope/Value:simple+' => '',
+	'Class:OAuthClientAzure/Name' => '%1$s (%2$s)',
+	'Class:OAuthClientGoogle' => 'Klient OAuth dla Google',
+	'Class:OAuthClientGoogle/Attribute:advanced_scope' => 'Zaawansowany zakres',
+	'Class:OAuthClientGoogle/Attribute:advanced_scope+' => 'Gdy tylko coś tu wpiszesz, będzie to miało pierwszeństwo przed wyborem „Zakres”, który zostanie następnie zignorowany',
+	'Class:OAuthClientGoogle/Attribute:scope' => 'Zakres',
+	'Class:OAuthClientGoogle/Attribute:scope+' => 'Zwykle odpowiedni jest wybór domyślny',
 	'Class:OAuthClientGoogle/Attribute:scope/Value:
               IMAP
 	' => '
               IMAP
-            ~~',
+            ',
 	'Class:OAuthClientGoogle/Attribute:scope/Value:
               IMAP
-            +' => '~~',
+            +' => '',
 	'Class:OAuthClientGoogle/Attribute:scope/Value:
               SMTP
 	' => '
               SMTP
-            ~~',
+            ',
 	'Class:OAuthClientGoogle/Attribute:scope/Value:
               SMTP
-            +' => '~~',
-	'Class:OAuthClientGoogle/Attribute:scope/Value:IMAP' => 'IMAP~~',
-	'Class:OAuthClientGoogle/Attribute:scope/Value:IMAP+' => '~~',
-	'Class:OAuthClientGoogle/Attribute:scope/Value:SMTP' => 'SMTP~~',
-	'Class:OAuthClientGoogle/Attribute:scope/Value:SMTP+' => '~~',
-	'Class:OAuthClientGoogle/Attribute:used_for_smtp' => 'Used for SMTP~~',
-	'Class:OAuthClientGoogle/Attribute:used_for_smtp+' => 'At least one OAuth client must have this flag to “Yes”, if you want iTop to use it for sending mails~~',
+            +' => '',
+	'Class:OAuthClientGoogle/Attribute:scope/Value:IMAP' => 'IMAP',
+	'Class:OAuthClientGoogle/Attribute:scope/Value:IMAP+' => '',
+	'Class:OAuthClientGoogle/Attribute:scope/Value:SMTP' => 'SMTP',
+	'Class:OAuthClientGoogle/Attribute:scope/Value:SMTP+' => '',
+	'Class:OAuthClientGoogle/Attribute:used_for_smtp' => 'Używany do SMTP',
+	'Class:OAuthClientGoogle/Attribute:used_for_smtp+' => 'Przynajmniej jeden klient OAuth musi mieć tę flagę ustawioną na „Tak”, jeśli chcesz, aby iTop używał jej do wysyłania wiadomości e-mail',
 	'Class:OAuthClientGoogle/Attribute:used_for_smtp/Value:
               no
 	' => '
-              no
-            ~~',
+              nie
+            ',
 	'Class:OAuthClientGoogle/Attribute:used_for_smtp/Value:
               no
-            +' => '~~',
+            +' => '',
 	'Class:OAuthClientGoogle/Attribute:used_for_smtp/Value:
               yes
 	' => '
-              yes
-            ~~',
+              tak
+            ',
 	'Class:OAuthClientGoogle/Attribute:used_for_smtp/Value:
               yes
-            +' => '~~',
-	'Class:OAuthClientGoogle/Attribute:used_for_smtp/Value:no' => 'No~~',
-	'Class:OAuthClientGoogle/Attribute:used_for_smtp/Value:yes' => 'Yes~~',
-	'Class:OAuthClientGoogle/Attribute:used_scope' => 'Used scope~~',
-	'Class:OAuthClientGoogle/Attribute:used_scope+' => '~~',
+            +' => '',
+	'Class:OAuthClientGoogle/Attribute:used_for_smtp/Value:no' => 'Nie',
+	'Class:OAuthClientGoogle/Attribute:used_for_smtp/Value:yes' => 'Tak',
+	'Class:OAuthClientGoogle/Attribute:used_scope' => 'Użyty zakres',
+	'Class:OAuthClientGoogle/Attribute:used_scope+' => '',
 	'Class:OAuthClientGoogle/Attribute:used_scope/Value:
               advanced
 	' => '
-              advanced
-            ~~',
+              zaawansowany
+            ',
 	'Class:OAuthClientGoogle/Attribute:used_scope/Value:
               advanced
-            +' => '~~',
+            +' => '',
 	'Class:OAuthClientGoogle/Attribute:used_scope/Value:
               simple
 	' => '
-              simple
-            ~~',
+              prosty
+            ',
 	'Class:OAuthClientGoogle/Attribute:used_scope/Value:
               simple
-            +' => '~~',
-	'Class:OAuthClientGoogle/Attribute:used_scope/Value:advanced' => 'Advanced~~',
-	'Class:OAuthClientGoogle/Attribute:used_scope/Value:advanced+' => '~~',
-	'Class:OAuthClientGoogle/Attribute:used_scope/Value:simple' => 'Simple~~',
-	'Class:OAuthClientGoogle/Attribute:used_scope/Value:simple+' => '~~',
-	'Class:OAuthClientGoogle/Name' => '%1$s (%2$s)~~',
-	'Menu:CreateMailbox' => 'Create a mailbox...~~',
-	'Menu:GenerateTokens' => 'Generate access token...~~',
-	'Menu:OAuthClient' => 'OAuth client~~',
-	'Menu:OAuthClient+' => '~~',
-	'Menu:RegenerateTokens' => 'Regenerate access token...~~',
-	'OAuthClient:Name/UseForSMTPMustBeUnique' => 'The combination Login (%1$s) and Use for SMTP (%2$s) has already be used for OAuth Client~~',
-	'OAuthClient:baseinfo' => 'Base Information~~',
-	'OAuthClient:scope' => 'Scope~~',
-	'itop-oauth-client/Operation:CreateMailBox/Title' => 'Mailbox creation~~',
-	'itop-oauth-client:Message:MissingToken' => 'Generate access token before using this OAuth client~~',
-	'itop-oauth-client:Message:RegenerateToken' => 'Regenerate access token to to take into account the changes~~',
-	'itop-oauth-client:Message:TokenCreated' => 'Access token created~~',
-	'itop-oauth-client:Message:TokenError' => 'Access token not generated due to server error~~',
-	'itop-oauth-client:Message:TokenRecreated' => 'Access token regenerated~~',
-	'itop-oauth-client:MissingOAuthClient' => 'Missing Oauth client for user name %1$s~~',
-	'itop-oauth-client:TestSMTP' => 'Email send test~~',
-	'itop-oauth-client:UsedForSMTP' => 'This OAuth client is used for SMTP~~',
+            +' => '',
+	'Class:OAuthClientGoogle/Attribute:used_scope/Value:advanced' => 'Zaawansowany',
+	'Class:OAuthClientGoogle/Attribute:used_scope/Value:advanced+' => '',
+	'Class:OAuthClientGoogle/Attribute:used_scope/Value:simple' => 'Prosty',
+	'Class:OAuthClientGoogle/Attribute:used_scope/Value:simple+' => '',
+	'Class:OAuthClientGoogle/Name' => '%1$s (%2$s)',
+	'Menu:CreateMailbox' => 'Utwórz skrzynkę pocztową...',
+	'Menu:GenerateTokens' => 'Wygeneruj token dostępu...',
+	'Menu:OAuthClient' => 'Klient OAuth',
+	'Menu:OAuthClient+' => '',
+	'Menu:RegenerateTokens' => 'Wygeneruj ponownie token dostępu...',
+	'OAuthClient:Name/UseForSMTPMustBeUnique' => 'Kombinacja Loginu (%1$s) i SMTP (%2$s) była już użyta dla klienta OAuth',
+	'OAuthClient:baseinfo' => 'Informacje podstawowe',
+	'OAuthClient:scope' => 'Zakres',
+	'itop-oauth-client/Operation:CreateMailBox/Title' => 'Tworzenie skrzynki pocztowej',
+	'itop-oauth-client:Message:MissingToken' => 'Wygeneruj token dostępu przed użyciem tego klienta OAuth',
+	'itop-oauth-client:Message:RegenerateToken' => 'Wygeneruj ponownie token dostępu, aby uwzględnić zmiany',
+	'itop-oauth-client:Message:TokenCreated' => 'Utworzono token dostępu',
+	'itop-oauth-client:Message:TokenError' => 'Token dostępu nie został wygenerowany z powodu błędu serwera',
+	'itop-oauth-client:Message:TokenRecreated' => 'Token dostępu został wygenerowany ponownie',
+	'itop-oauth-client:MissingOAuthClient' => 'Brak klienta Oauth dla nazwy użytkownika %1$s',
+	'itop-oauth-client:TestSMTP' => 'Test wysyłania e-maili',
+	'itop-oauth-client:UsedForSMTP' => 'Ten klient OAuth jest używany do SMTP',
 ]);
