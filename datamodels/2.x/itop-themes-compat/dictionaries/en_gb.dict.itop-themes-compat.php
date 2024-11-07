b7feaed90a0651aaf8125c0fb5fb323ac9dd165c
Lars Kaltefleiter <lars.kaltefleiter@itomig.de>
2024-11-07 16:41:19 +0100

    Create en-gb dict files

---
+++ datamodels/2.x/itop-themes-compat/dictionaries/en_gb.dict.itop-themes-compat.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2013 XXXXX
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('EN GB', 'British English', 'British English', array(
+	'theme:light-grey' => 'Light Grey (deprecated)',
+));
