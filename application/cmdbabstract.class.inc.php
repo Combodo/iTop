bb05fc86e65c907e1410eafde9ae5cdb26369a12
Anne-Cath <anne-catherine.cognet@combodo.com>
2025-09-15 16:11:54 +0200

    N°6071 - Prefill Tagset in transition displayed but not saved

--- application/cmdbabstract.class.inc.php
+++ application/cmdbabstract.class.inc.php
@@ -3983,6 +3983,27 @@ HTML;
 				$sTagSetJson = utils::ReadPostedParam("attr_{$sFormPrefix}{$sAttCode}", null, 'raw_data');
 				if ($sTagSetJson !== null) { // bulk modify, direct linked set not handled
 					$value = json_decode($sTagSetJson, true);
+					if ($this->IsNew()) {
+						if ($value['orig_value'] != '' ) {
+							foreach ($value['orig_value'] as $val) {
+								if (!in_array($val, $value['removed'])) {
+									$value['added'][] = $val;
+								}
+							}
+						}
+					} else {
+						$acurrentValues = $this->Get($sAttCode)->GetValues();
+						foreach ($value['orig_value'] as $val) {
+							if ( !in_array($val, $acurrentValues) && !in_array($val, $value['removed'])  && !in_array($val, $value['added'])  ) {
+								$value['added'][] = $val;
+							}
+						}
+						foreach ($acurrentValues as $val) {
+							if ( !in_array($val, $value['orig_value']) && !in_array($val, $value['removed'])  && !in_array($val, $value['added'])  ) {
+								$value['removed'][] = $val;
+							}
+						}
+					}
 				}
 				break;
 
