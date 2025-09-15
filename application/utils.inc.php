21a11cf1b780c1f1cc6fa6afa7e1eca3ac7d9837
jf-cbd <jimmy.fayolle@combodo.com>
2025-09-15 11:21:51 +0200

    N°8123 - Improve on mention data parsing

--- application/utils.inc.php
+++ application/utils.inc.php
@@ -3022,7 +3022,6 @@ TXT
 	 * Note: Only works for backoffice URLs for now
 	 *
 	 * @param string $sText Text containing the mentioned objects to be found
-	 * @param string $sFormat {@uses static::ENUM_TEXT_FORMAT_HTML, ...}
 	 *
 	 * @return array Array of object classes / IDs for the ones found in $sText
 	 *
@@ -3036,16 +3035,20 @@ TXT
 	 */
 	public static function GetMentionedObjectsFromText(string $sText): array
 	{
-		$aMentionedObjects = [];
-		$aMentionMatches = [];
-		$sText = html_entity_decode($sText);
-
 		$aMentionAllowedClasses = MetaModel::GetConfig()->Get('mentions.allowed_classes');
-		preg_match_all('/<a\s*([^>]*)data-object-class="([^"]*)"\s.*data-object-key="([^"]*)"\s*([^>]*)>(.*)<\/a>/Ui', $sText, $aMentionMatches);
-		foreach ($aMentionMatches[0] as $iMatchIdx => $sCompleteMatch) {
-			$sMatchedClass = $aMentionMatches[2][$iMatchIdx];
-			$sMatchedId = $aMentionMatches[3][$iMatchIdx];
-			$sMatchedName = $aMentionMatches[5][$iMatchIdx];
+		$aMentionedObjects = [];
+
+		$oDom = new \DOMDocument();
+		libxml_use_internal_errors(true); // to keep processing even in case of "invalid" HTML, cf. testGetMentionedObjectsFromText
+		$oDom->loadHTML($sText);
+
+		$oXpath = new \DOMXPath($oDom);
+		$oNodes = $oXpath->query('//a[@data-object-class and @data-object-key]');
+
+		foreach ($oNodes as $oNode) {
+			$sObjClass = $oNode->getAttribute('data-object-class');
+			$sObjId = $oNode->getAttribute('data-object-key');
+			$sText = $oNode->getText();
 
 			// Ensure that what we found is actually configured as a mention
 			$sMentionPrefix = array_search($sMatchedClass, $aMentionAllowedClasses);
@@ -3070,12 +3073,12 @@ TXT
 			}
 
 			// Prepare array for matched class if not already present
-			if (!array_key_exists($sMatchedClass, $aMentionedObjects)) {
-				$aMentionedObjects[$sMatchedClass] = [];
+			if (!array_key_exists($sObjClass, $aMentionedObjects)) {
+				$aMentionedObjects[$sObjClass] = [];
 			}
 			// Add matched ID if not already there
-			if (!in_array($sMatchedId, $aMentionedObjects[$sMatchedClass])) {
-				$aMentionedObjects[$sMatchedClass][] = $sMatchedId;
+			if (!in_array($sObjId, $aMentionedObjects[$sObjClass])) {
+				$aMentionedObjects[$sObjClass][] = $sObjId;
 			}
 		}
 
