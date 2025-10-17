cdfded766fdb2d1cb291b7eb135801564388faf6
Benjamin Dalsass <benjamin.dalsass@combodo.com>
2025-10-17 09:03:45 +0200

    N°8772 - Form dependencies manager implementation

---
+++ sources/Forms/FormBuilder/ResolvedFormType.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * @copyright   Copyright (C) 2010-2025 Combodo SARL
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+namespace Combodo\iTop\Forms\FormBuilder;
+
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\Form\ResolvedFormType as SymfonyResolvedFormType;
+use Symfony\Component\Form\ResolvedFormTypeInterface;
+
+class ResolvedFormType extends SymfonyResolvedFormType implements ResolvedFormTypeInterface
+{
+	protected function newBuilder(string $name, ?string $dataClass, FormFactoryInterface $factory, array $options): FormBuilderInterface
+	{
+		$builder = parent::newBuilder($name, $dataClass, $factory, $options);
+		return new FormBuilder($builder);
+	}
+}
---
+++ sources/Forms/FormType/OqlType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Combodo\iTop\Forms\FormType;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class OqlType extends AbstractType
+{
+	public function getParent(): string
+	{
+		return TextType::class;
+	}
+
+	public function configureOptions(OptionsResolver $resolver): void
+	{
+		$resolver->setDefault('outputs', array(
+			'selected_class' => function($oData) {
+				if($oData === null)
+					return null;
+				// extract selected class
+				preg_match('/SELECT\s+(\w+)/', $oData, $aMatches);
+				return $aMatches[1] ?? null;
+			}
+		));
+	}
+
+}
