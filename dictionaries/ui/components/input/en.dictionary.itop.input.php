e02fc9cb44af57b88c0f01da560b33e5811cd18f
Eric Espie <eric.espie@combodo.com>
2025-11-14 16:14:34 +0100

    Merge dicos...

--- dictionaries/ui/components/input/en.dictionary.itop.input.php
+++ dictionaries/ui/components/input/en.dictionary.itop.input.php
@@ -20,9 +20,10 @@
 
 // Input
 Dict::Add('EN US', 'English', 'English', [
-	'UI:Component:Input:ChangeNotAllowed' => 'This change is not allowed',
-	'UI:Component:Input:Password:DoesNotMatch' => 'Passwords do not match',
-	'UI:Component:Input:Set:MinimumItems' => 'Minimum %1$s item(s) required',
+		'UI:Component:Input:ChangeNotAllowed'      => 'This change is not allowed',
+		'UI:Component:Input:Password:DoesNotMatch' => 'Passwords do not match',
+		'UI:Component:Input:Set:MinimumItems'      => 'Minimum %1$s item(s) required',
 
-	'UI:Component:Input:Select:Select_item' => 'Select an item...'
-));
+		'UI:Component:Input:Select:Select_item' => 'Select an item...',
+	]
+);
