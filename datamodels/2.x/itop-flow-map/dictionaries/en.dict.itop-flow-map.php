de396ac2cf6418dde1ca03ff6bf558262abbeec2
v-dumas <vincent.dumas@combodo.com>
2025-12-15 12:28:36 +0100

    N°7771 - Add CMDB flow map

---
+++ datamodels/2.x/itop-flow-map/dictionaries/en.dict.itop-flow-map.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Module combodo-flow-map
+ *
+ * @copyright   Copyright (C) 2013 XXXXX
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('EN US', 'English', 'English', array(
+ 
+    'Relation:flow/Description'                               => 'Flow maps',
+    'Relation:flow/DownStream'                                => 'Impacts flow...',
+    'Relation:flow/UpStream'                                  => 'Depends flow...',
+
+	'Class:Flow'                                              => 'Flow',
+	'Class:Flow+'                                             => 'For application flow for example',
+	'Class:Flow/Name'                                         =>'%1$s %2$s %3$s',
+	'Class:Flow/Attribute:source_id'                          => 'Source',
+	'Class:Flow/Attribute:source_id+'                         => 'Source Ci of the flow',
+	'Class:Flow/Attribute:source_name'                        => 'Source name',
+	'Class:Flow/Attribute:source_name+'                       => 'Name of the source Ci of the flow',
+	'Class:Flow/Attribute:destination_id'                     => 'Destination',
+	'Class:Flow/Attribute:destination_id+'                    => 'Destination Ci for the flow',
+	'Class:Flow/Attribute:destination_name'                   => 'Destination name',
+	'Class:Flow/Attribute:destination_name+'                  => 'Name of the destination CI for the flow',
+	'Class:Flow/Attribute:type_id'                            => 'Flow type',
+	'Class:Flow/Attribute:type_id+'                           => 'Flow type such as : http, https, ftp..., will appear in the friendlyname',
+	'Class:Flow/Attribute:description'                        => 'Description',
+	'Class:Flow/Attribute:description+'                       => 'Flow description, will appear in the summary card',
+	'Class:Flow/Attribute:source_id_friendlyname'             => 'source_id_friendlyname',
+	'Class:Flow/Attribute:source_id_friendlyname+'            => 'Full name',
+	'Class:Flow/Attribute:source_id_finalclass_recall'        => 'source_id->CI sub-class',
+	'Class:Flow/Attribute:source_id_finalclass_recall+'       => 'Name of the final class',
+	'Class:Flow/Attribute:source_id_obsolescence_flag'        => 'source_id->Obsolete',
+	'Class:Flow/Attribute:source_id_obsolescence_flag+'       => 'Computed dynamically on other attributes',
+	'Class:Flow/Attribute:destination_id_friendlyname'        => 'destination_id_friendlyname',
+	'Class:Flow/Attribute:destination_id_friendlyname+'       => 'Full name',
+	'Class:Flow/Attribute:destination_id_finalclass_recall'   => 'destination_id->CI sub-class',
+	'Class:Flow/Attribute:destination_id_finalclass_recall+'  => 'Name of the final class',
+	'Class:Flow/Attribute:destination_id_obsolescence_flag'   => 'destination_id->Obsolete',
+	'Class:Flow/Attribute:destination_id_obsolescence_flag+'  => 'Computed dynamically on other attributes', 
+    'Class:Flow/Attribute:status'                             => 'Status',
+    'Class:Flow/Attribute:status+'                            => '',
+    'Class:Flow/Attribute:status/Value:active'                => 'Active',
+    'Class:Flow/Attribute:status/Value:inactive'              => 'Inactive',
+    'Class:Flow/Attribute:org_id'                             => 'Organization',
+    'Class:Flow/Attribute:org_id+'                            => '',	
+));
+?>
---
+++ datamodels/2.x/itop-flow-map/dictionaries/fr.dict.itop-flow-map.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Module combodo-flow-map
+ *
+ * @copyright   Copyright (C) 2013 XXXXX
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('FR FR', 'French', 'Français', array(
+ 
+    'Relation:flow/Description'                              => 'Carte des fluxs',
+    'Relation:flow/DownStream'                               => 'Impacte flux...',
+    'Relation:flow/UpStream'                                 => 'Dépend de flux...',
+
+	'Class:Flow'                                             => 'Flux',
+	'Class:Flow+'                                            => 'Pour les fluxs applicatifs par exemple',
+	'Class:Flow/Name'                                        =>'%1$s %2$s %3$s',
+	'Class:Flow/Attribute:source_id'                         => 'Source',
+	'Class:Flow/Attribute:source_id+'                        => 'Ci source du flux',
+	'Class:Flow/Attribute:source_name'                       => 'Nom de la source ',
+	'Class:Flow/Attribute:source_name+'                      => 'Nom du CI à la source du flux',
+	'Class:Flow/Attribute:destination_id'                    => 'Destination',
+	'Class:Flow/Attribute:destination_id+'                   => 'Ci destinataire du flux',
+	'Class:Flow/Attribute:destination_name'                  => 'Nom du destinataire',
+	'Class:Flow/Attribute:destination_name+'                 => 'Nom du Ci destinataire du flux',
+	'Class:Flow/Attribute:type_id'                           => 'Type de flux',
+	'Class:Flow/Attribute:type_id+'                          => 'Type de flux : http, https, ftp..., apparaitra dans le nom commun',
+	'Class:Flow/Attribute:description'                       => 'Description',
+	'Class:Flow/Attribute:description+'                      => 'Description du flux, apparaitra dans les informations résumées ',
+	'Class:Flow/Attribute:source_id_friendlyname'            => 'source_id_friendlyname',
+	'Class:Flow/Attribute:source_id_friendlyname+'           => 'Nom complet',
+	'Class:Flow/Attribute:source_id_finalclass_recall'       => 'source_id->CI sub-class',
+	'Class:Flow/Attribute:source_id_finalclass_recall+'      => 'Classe finale',
+	'Class:Flow/Attribute:source_id_obsolescence_flag'       => 'source_id->Obsolete',
+	'Class:Flow/Attribute:source_id_obsolescence_flag+'      => 'Computed dynamically on other attributes',
+	'Class:Flow/Attribute:destination_id_friendlyname'       => 'destination_id_friendlyname',
+	'Class:Flow/Attribute:destination_id_friendlyname+'      => 'Nom complet',
+	'Class:Flow/Attribute:destination_id_finalclass_recall'  => 'destination_id->CI sub-class',
+	'Class:Flow/Attribute:destination_id_finalclass_recall+' => 'Classe finale',
+	'Class:Flow/Attribute:destination_id_obsolescence_flag'  => 'destination_id->Obsolete',
+	'Class:Flow/Attribute:destination_id_obsolescence_flag+' => 'Computed dynamically on other attributes',
+    'Class:Flow/Attribute:status'                            => 'Etat',
+    'Class:Flow/Attribute:status+'                           => '',
+    'Class:Flow/Attribute:status/Value:active'               => 'Actif',
+    'Class:Flow/Attribute:status/Value:inactive'             => 'Inactif',
+    'Class:Flow/Attribute:org_id'                            => 'Organisation',
+    'Class:Flow/Attribute:org_id+'                           => '',		
+	
+));
+?>
