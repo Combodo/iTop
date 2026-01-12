51f0eea8e3a5288ca76a70fd53124ac3e15cfde0
odain <olivier.dain@combodo.com>
2026-01-12 13:27:40 +0100

    enhance test sdk: add GetTemporaryFilePath
    # Conflicts:
    #	tests/php-unit-tests/src/BaseTestCase/ItopTestCase.php

--- tests/php-unit-tests/src/BaseTestCase/ItopTestCase.php
+++ tests/php-unit-tests/src/BaseTestCase/ItopTestCase.php
@@ -731,6 +731,18 @@ abstract class ItopTestCase extends KernelTestCase
 	}
 
 	/**
+	 * Return a temporary file path. that will be cleaned up by tearDown()
+	 * @return string: temporary file path
+	 */
+	public function GetTemporaryFilePath(string $sPrefix = "test"): string
+	{
+
+		$sPath = tempnam(sys_get_temp_dir(), $sPrefix);
+		$this->aFileToClean[] = $sPath;
+		return $sPath;
+	}
+
+	/**
 	 * @param $sXML
 	 *
 	 * @return false|string
