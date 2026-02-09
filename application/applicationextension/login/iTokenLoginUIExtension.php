d92bdf7b89dc19744370d6d4084fb62728859643
odain <olivier.dain@combodo.com>
2026-02-09 23:05:33 +0100

    new iTokenLoginUIExtension interface

---
+++ application/applicationextension/login/iTokenLoginUIExtension.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Login page extensibility
+ *
+ * @api
+ * @package UIExtensibilityAPI
+ * @since 3.x.x
+ */
+interface iTokenLoginUIExtension
+{
+    /**
+     * @return array
+     * @api
+     */
+    public function GetTokenInfo() : array;
+
+	/**
+	 * @return array
+	 * @api
+	 */
+	public function GetUserLogin(array $aTokenInfo) : string;
+}
