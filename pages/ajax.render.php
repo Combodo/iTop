4ac5269b7cabc3b177443c3f87f2755abdb3ca23
Stephen Abello <stephen.abello@combodo.com>
2026-02-19 16:07:10 +0100

    N°8549 - Update inline images secret

--- pages/ajax.render.php
+++ pages/ajax.render.php
@@ -2136,7 +2136,7 @@ EOF
 							$oAttachment->Set('item_class', $sObjClass);
 							$oAttachment->SetDefaultOrgId();
 							$oAttachment->Set('contents', $oDoc);
-							$oAttachment->Set('secret', sprintf('%06x', mt_rand(0, 0xFFFFFF))); // something not easy to guess
+                            $oAttachment->Set('secret', bin2hex(random_bytes(16))); // 128 bits of entropy, cryptographically secure
 							$iAttId = $oAttachment->DBInsert();
 
 							$aResult['uploaded'] = 1;
