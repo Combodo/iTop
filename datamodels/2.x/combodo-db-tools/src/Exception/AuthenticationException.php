51041e9fb49f2e01dfbf867ee4da01eff0a7eb02
Molkobain <lajarige.guillaume@free.fr>
2026-02-24 20:52:01 +0100

    N°8604 N°8605 - Add autoloader and dedicated classes for binaries utils

---
+++ datamodels/2.x/combodo-db-tools/src/Exception/AuthenticationException.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * @copyright   Copyright (C) 2010-2026 Combodo SAS
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+namespace Combodo\iTop\DBTools\Exception;
+
+class AuthenticationException extends \Exception
+{
+
+}
