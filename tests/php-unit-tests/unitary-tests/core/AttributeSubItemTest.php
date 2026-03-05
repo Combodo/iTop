872f75721390bd14e94db75ed7a556e0a53c24ac
Anne-Cath <anne-catherine.cognet@combodo.com>
2026-03-05 13:59:38 +0100

    tests

---
+++ tests/php-unit-tests/unitary-tests/core/AttributeSubItemTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Combodo\iTop\Test\UnitTest\Core;
+
+use AttributeDate;
+use AttributeDateTime;
+use Change;
+use Combodo\iTop\Test\UnitTest\ItopDataTestCase;
+use DateTime;
+use MetaModel;
+use UserRequest;
+
+class AttributeSubItemTest extends ItopDataTestCase
+{
+	public const CREATE_TEST_ORG = true;
+
+	/**
+	 * @param string $sAttCode
+	 * @param string $sVerb
+	 * @param string $sExpectedValue
+	 *
+	 * @return void
+	 */
+	public function testGetForTemplate()
+    {
+        $aUserRequestCustomParams = [
+            'title' => "Test DisplayStopwatch",
+        ];
+        $oUserRequest = $this->CreateUserRequest(456, $aUserRequestCustomParams);
+
+	    $iStartDate =  time() - 200;
+	    $oStopwatch = $oUserRequest->Get('ttr');
+        $oStopwatch->DefineThreshold(100, $iStartDate);
+        $oUserRequest->Set('ttr', $oStopwatch);
+
+		$sValue = $oUserRequest->Get('ttr_escalation_deadline');
+	    $oAttDef = MetaModel::GetAttributeDef(get_class($oUserRequest), 'ttr_escalation_deadline');
+
+	 /*   self::assertEquals('Missed by 3 min', MetaModel::ApplyParams('$this->title$', ['this' => $oUserRequest]));
+	    self::assertEquals('Missed by 3 min', MetaModel::ApplyParams('$this->ttr_escalation_deadline$', ['this' => $oUserRequest]));
+	    self::assertEquals('Missed by 3 min', MetaModel::ApplyParams('$this->html(ttr_escalation_deadline)$', ['this' => $oUserRequest]));*/
+
+
+	    self::assertEquals('Missed by 3 min', $oAttDef->GetForTemplate($sValue, 'html', $oUserRequest));
+	    $oDateTime = new DateTime();
+	    $oDateTime->setTimestamp($iStartDate);
+	    $sDate = $oDateTime->format(AttributeDateTime::GetFormat());
+	    self::assertEquals($sDate, $oAttDef->GetForTemplate($sValue, 'label', $oUserRequest));
+	    self::assertEquals('Missed by 3 min', $oAttDef->GetForTemplate($sValue, 'text', $oUserRequest));
+	    self::assertEquals($iStartDate, $oAttDef->GetForTemplate($sValue, '', $oUserRequest));
+    }
+}
