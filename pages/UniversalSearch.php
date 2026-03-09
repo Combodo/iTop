9792358aeaf504a1b3f5134de4d93621c230d84a
lenaick.moreira <lenaick.moreira@gmail.com>
2026-03-09 16:32:30 +0100

    Remove debug OQL filter in HTML comments of the universal search page

--- pages/UniversalSearch.php
+++ pages/UniversalSearch.php
@@ -107,10 +107,6 @@ if ($oFilter != null) {
 	$sPageId = "ui-search-".$oFilter->GetClass();
 	$sLabel = MetaModel::GetName($oFilter->GetClass());
 	$oP->SetBreadCrumbEntry($sPageId, $sLabel, '', '', 'fas fa-search', iTopWebPage::ENUM_BREADCRUMB_ENTRY_ICON_TYPE_CSS_CLASSES);
-
-	// Menu node
-	$sFilter = utils::EscapeHtml($oFilter->ToOQL());
-	$oP->add("\n<!-- $sFilter -->\n");
 }
 $oP->add("</div>\n");
 $oP->output();
