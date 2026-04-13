1222507205c30d8058237483333aa0c6b11fb03a
odain <olivier.dain@combodo.com>
2026-04-13 11:48:18 +0200

    N°9500 - Designer - Can't mtp - OQL errors

--- setup/moduleinstaller.class.inc.php
+++ setup/moduleinstaller.class.inc.php
@@ -17,7 +17,7 @@
 //   You should have received a copy of the GNU Affero General Public License
 //   along with iTop. If not, see <http://www.gnu.org/licenses/>
 
-require_once(APPROOT.'setup/setuppage.class.inc.php');
+//require_once(APPROOT.'setup/setuppage.class.inc.php');
 
 /**
  * Class ModuleInstaller
