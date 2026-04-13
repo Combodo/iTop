3086ad58b072a45050702c5b5e6783eb8754f17f
odain <olivier.dain@combodo.com>
2026-04-13 09:36:48 +0200

    N°8806 - typo

--- setup/runtimeenv.class.inc.php
+++ setup/runtimeenv.class.inc.php
@@ -1622,7 +1622,7 @@ class RunTimeEnvironment
 		$aExtensionDirs = [];
 		foreach ($this->GetExtensionMap()->GetAllExtensions() as $oExtension) {
 			if ($oExtension->bMarkedAsChosen && is_dir($oExtension->sSourceDir)) {
-				$aExtensionDirs [] = $oExtension->sSourceDir;
+				$aExtensionDirs []= $oExtension->sSourceDir;
 			}
 		}
 
