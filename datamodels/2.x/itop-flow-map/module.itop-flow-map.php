e662370c328ffa455c472ce0d143d66cc495bdd7
Vincent Dumas <42336698+v-dumas@users.noreply.github.com>
2026-05-06 11:28:21 +0200

    N°7771 New module for DataFlow management (#905)

---
+++ datamodels/2.x/itop-flow-map/module.itop-flow-map.php
@@ -0,0 +1,50 @@
+<?php
+
+//
+// iTop module definition file
+//
+
+SetupWebPage::AddModule(
+	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
+	'itop-flow-map/3.3.0',
+	[
+		// Identification
+		//
+		'label' => 'Map applications data flows',
+		'category' => 'business',
+
+		// Setup
+		//
+		'dependencies' => [
+			'itop-config-mgmt/3.2.0',
+		],
+		'mandatory' => false,
+		'visible' => true,
+
+		// Components
+		//
+		'datamodel' => [
+
+		],
+		'webservice' => [
+
+		],
+		'data.struct' => [
+			'data/en_us.data.itop-flow-map.xml',
+		],
+		'data.sample' => [
+			// add your sample data XML files here,
+		],
+
+		// Documentation
+		//
+		'doc.manual_setup' => '', // hyperlink to manual setup documentation, if any
+		'doc.more_information' => '', // hyperlink to more information, if any
+
+		// Default settings
+		//
+		'settings' => [
+			// Module specific settings go here, if any
+		],
+	]
+);
