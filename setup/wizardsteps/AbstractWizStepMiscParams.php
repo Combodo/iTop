509d1c6caae184e18076f845880e4a06c8308f2b
Timmy38 <timothee.callet@combodo.com>
2026-05-28 11:16:24 +0200

    N°9172 Modify ext mgmt labels

--- setup/wizardsteps/AbstractWizStepMiscParams.php
+++ setup/wizardsteps/AbstractWizStepMiscParams.php
@@ -47,7 +47,7 @@ abstract class AbstractWizStepMiscParams extends WizardStep
 
 		$oPage->add('<div id="prefix_option" class="collapsable-options">');
 		$oPage->add('<span data-role="setup-collapsable-options--toggler"><label style="font-weight: normal;">Advanced parameters</label></span>');
-		$oPage->add('<div class="" style="display:none">');
+		$oPage->add('<div class="" style="'.(mb_strlen($sChecked) === 0 ? 'display:none' : '').'">');
 		$oPage->add('<input id="force-uninstall" type="checkbox"'.$sChecked.' name="force-uninstall"><label for="force-uninstall">&nbsp;Unlock any extension uninstallation</label>');
 		$oPage->add('<div class="message message-warning">This could result in data corruption and application crashes.</div>');
 		$oPage->add('</div>');
