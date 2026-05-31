bdc0077fa1a7ad4a9e569dfeadb68226cb044e64
purple grape <purplegrape4@gmail.com>
2026-05-31 19:20:22 +0800

    Update zh_cn.dict.itop-config.php

--- datamodels/2.x/itop-config/dictionaries/zh_cn.dict.itop-config.php
+++ datamodels/2.x/itop-config/dictionaries/zh_cn.dict.itop-config.php
@@ -21,9 +21,10 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with iTop. If not, see <http://www.gnu.org/licenses/>
  */
+
 Dict::Add('ZH CN', 'Chinese', '简体中文', [
-	'Menu:ConfigFileEditor' => 'Plain text editor~~',
-	'Menu:ConfigEditor' => '编辑配置文件',
+
+	'Menu:ConfigFileEditor' => '纯文本编辑器',
 	'itop-config/Operation:Edit/Title' => '配置文件编辑器',
 	'config-edit-intro' => '编辑配置文件时请务必格外小心.',
 	'config-apply' => '应用',
@@ -34,7 +35,7 @@ Dict::Add('ZH CN', 'Chinese', '简体中文', [
 	'config-no-change' => '没有变化: 配置文件将保持不变.',
 	'config-reverted' => '配置文件已恢复.',
 	'config-parse-error' => '第%2$d行: %1$s.<br/>配置文件尚未更新.',
-	'config-current-line' => '正在编辑第%1$s行',
+	'config-current-line' => '正在编辑第 %1$s 行',
 	'config-saved-warning-db-password' => '保存成功, 但因为数据库密码中包含不支持的字符, 配置文件备份不会成功.',
 	'config-error-transaction' => '错误: 无效的事务编号. 配置<b>没有</b>被更新.',
 	'config-error-file-changed' => '错误: 配置文件在您打开以后已被更改, 无法保存. 请刷新并再次保存.',
