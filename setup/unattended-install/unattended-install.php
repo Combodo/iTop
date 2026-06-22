52f52000a69bde0a9c243605dbc223b9eb5536a0
odain-cbd <56586767+odain-cbd@users.noreply.github.com>
2026-06-22 17:24:05 +0200

    Update setup/unattended-install/unattended-install.php
    Co-authored-by: greptile-apps[bot] <165735046+greptile-apps[bot]@users.noreply.github.com>

--- setup/unattended-install/unattended-install.php
+++ setup/unattended-install/unattended-install.php
@@ -7,7 +7,7 @@ function fatalHandler()
 {
 	$error = error_get_last();
 	if ($error) {
-		if ($error['type'] === E_ERROR) {
+		if ($error['type'] & (E_ERROR | E_PARSE | E_CORE_ERROR | E_COMPILE_ERROR | E_USER_ERROR)) {
 			// fatal error has occured
 			echo "PHP Fatal captured: {$error["message"]}";
 			SetupLog::Error("Fatal error during setup", null, $error);
