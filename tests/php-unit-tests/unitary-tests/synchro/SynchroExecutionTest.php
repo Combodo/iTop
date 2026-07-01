c7d3283848c0e81fdb1e32b05d2c6c4f0bac4400
odain <olivier.dain@combodo.com>
2026-07-01 11:33:33 +0200

    N°9746 - handle timechange during synchro execution + cleanup m_bIsImportPhaseDateKnown

--- tests/php-unit-tests/unitary-tests/synchro/SynchroExecutionTest.php
+++ tests/php-unit-tests/unitary-tests/synchro/SynchroExecutionTest.php
@@ -1,10 +1,46 @@
 <?php
 
-use Combodo\iTop\Test\UnitTest\ItopTestCase;
+use Combodo\iTop\Test\UnitTest\ItopDataTestCase;
 
-class SynchroDataSourceTest extends ItopTestCase
+class SynchroExecutionTest extends ItopDataTestCase
 {
-	public function () {
+	private function InitAndGetTZ($sTZ = null): string
+	{
+		$sTZ = $sTZ ?? 'Europe/Paris';
+		MetaModel::GetConfig()->Set('timezone', $sTZ);
+		return $sTZ;
+	}
 
-}
+	public function testGetDateMinusRetentionWithTimeChange()
+	{
+		$sTZ = $this->InitAndGetTZ();
+		$oObj = new SynchroExecution($this->createMock(SynchroDataSource::class));
+		$oDate = DateTime::createFromFormat('Y-m-d H:i:s', "2026-03-29 03:30:01", new DateTimeZone($sTZ));
+
+		$oObj->ExactlySubtractSeconds($oDate, 3600);
+		//03h30 minus 1hours => 03h minus 30mn => 03h
+		//                   => 03h with timechange => 2h
+		//                   => 02 minus 30mn => 01h30
+		$this->assertEquals("2026-03-29 01:30:01", $oDate->Format('Y-m-d H:i:s'));
+	}
+
+	public function testGetDateMinusRetentionWithTimeChangeAndUTC()
+	{
+		$sTZ = $this->InitAndGetTZ('UTC');
+		$oObj = new SynchroExecution($this->createMock(SynchroDataSource::class));
+		$oDate = DateTime::createFromFormat('Y-m-d H:i:s', "2026-03-29 03:30:01", new DateTimeZone($sTZ));
+
+		$oObj->ExactlySubtractSeconds($oDate, 3600);
+		$this->assertEquals("2026-03-29 02:30:01", $oDate->Format('Y-m-d H:i:s'));
+	}
+
+	public function testGetDateMinusRetention()
+	{
+		$sTZ = $this->InitAndGetTZ();
+		$oObj = new SynchroExecution($this->createMock(SynchroDataSource::class));
+		$oDate = DateTime::createFromFormat('Y-m-d H:i:s', "2026-04-01 03:30:01", new DateTimeZone($sTZ));
+
+		$oObj->ExactlySubtractSeconds($oDate, 3600);
+		$this->assertEquals("2026-04-01 02:30:01", $oDate->Format('Y-m-d H:i:s'));
+	}
 }
