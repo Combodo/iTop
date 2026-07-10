eacb7f34016acd51d2a4f21ca92a7925712f9643
Vincent Dumas <42336698+v-dumas@users.noreply.github.com>
2026-07-10 14:21:56 +0200

    N°9553 - Allow loading of localized sample and structural data based on iTop default language
    * Add English, French and German structural and sample data in iTop Community
    
    * Add helper methods to allow :
      * Checking of supported module version format
      * Finding a localized file name based on a provided en_us file name and a language
      * Loading a file if the module upgrade crossed a given version

--- setup/runtimeenv.class.inc.php
+++ setup/runtimeenv.class.inc.php
@@ -1102,7 +1102,7 @@ class RunTimeEnvironment
 	{
 		$oConfig = MetaModel::GetConfig();
 		$oConfig->Set('access_mode', ACCESS_FULL);
-
+		$sDefaultLanguage = $oConfig->GetDefaultLanguage();
 		$oDataLoader = new XMLDataLoader();
 
 		CMDBObject::SetCurrentChangeFromParams("Initialization from XML files for the selected modules ");
@@ -1148,7 +1148,7 @@ class RunTimeEnvironment
 		// in the current database
 		foreach ($aPreviouslyLoadedFiles as $sFileRelativePath) {
 			$sFileName = APPROOT.$sFileRelativePath;
-			SetupLog::Info("Loading file: $sFileName (just to get the keys mapping)");
+			SetupLog::Info("Loading file: $sFileName (just to build & cache the keys mapping)");
 			if (!file_exists($sFileName)) {
 				throw(new Exception("File $sFileName does not exist"));
 			}
@@ -1164,7 +1164,7 @@ class RunTimeEnvironment
 			if (!file_exists($sFileName)) {
 				throw(new Exception("File $sFileName does not exist"));
 			}
-
+			$sFileName = ModuleInstallerAPI::GetLocalizedFileName($sDefaultLanguage, $sFileName);
 			$oDataLoader->LoadFile($sFileName);
 			$sResult = sprintf("loading of %s done.", basename($sFileName));
 			SetupLog::Info($sResult);
