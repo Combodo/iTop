2bbc7bc2f372af751642619e19c2965f5a2b19bb
Eric Espie <eric.espie@combodo.com>
2026-07-15 09:08:28 +0200

    N°4692 - Enable parallelization of multiple CRON jobs

---
+++ sources/Service/Cron/CronLog.php
@@ -0,0 +1,71 @@
+<?php
+
+/*
+ * @copyright   Copyright (C) 2010-2022 Combodo SARL
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+namespace Combodo\iTop\Service\Cron;
+
+use LogAPI;
+use Page;
+use utils;
+
+/**
+ * @since 3.1.0
+ */
+class CronLog extends LogAPI
+{
+	public static int $iProcessNumber = 0;
+	private static int $iDebugLevel = 0;
+	private static ?Page $oP = null;
+
+	public const CHANNEL_DEFAULT = 'Cron';
+	/**
+	 * @inheritDoc
+	 *
+	 * As this object is used during setup, without any conf file available, customizing the level can be done by changing this constant !
+	 */
+	public const LEVEL_DEFAULT = self::LEVEL_INFO;
+
+	protected static $m_oFileLog = null;
+
+	public static function Log($sLevel, $sMessage, $sChannel = null, $aContext = []): void
+	{
+		$sMessage = 'cron'.str_pad(static::$iProcessNumber, 3).$sMessage;
+		parent::Log($sLevel, $sMessage, $sChannel, $aContext);
+	}
+
+	public static function Debug($sMessage, $sChannel = null, $aContext = []): void
+	{
+		if (self::$iDebugLevel > 0 && self::$oP) {
+			self::$oP->p('cron'.str_pad(static::$iProcessNumber, 3).$sMessage);
+		}
+		parent::Debug($sMessage, $sChannel, $aContext);
+	}
+
+	public static function Trace($sMessage, $sChannel = null, $aContext = []): void
+	{
+		if (self::$iDebugLevel > 1 && self::$oP) {
+			self::$oP->p('cron'.str_pad(static::$iProcessNumber, 3).$sMessage);
+		}
+		parent::Trace($sMessage, $sChannel, $aContext);
+	}
+
+	public static function SetDebug(Page $oP, int $iDebugLevel): void
+	{
+		self::$oP = $oP;
+		self::$iDebugLevel = $iDebugLevel;
+	}
+
+	public static function GetDebugClassName($sTaskClass): string
+	{
+		if (utils::StartsWith($sTaskClass, 'Combodo\\iTop\\Service\\')) {
+			return substr($sTaskClass, strlen('Combodo\\iTop\\Service\\'));
+		}
+		if (utils::StartsWith($sTaskClass, 'Combodo\\iTop\\')) {
+			return substr($sTaskClass, strlen('Combodo\\iTop\\'));
+		}
+		return $sTaskClass;
+	}
+}
