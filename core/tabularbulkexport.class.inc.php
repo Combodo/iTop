6a0b2bdfc76b1b4fde28335cb71dcc08490b9e58
Benjamin DALSASS <benjamin.dalsass@combodo.com>
2026-07-16 07:32:50 +0200

    N°8860 - Régression Export CSV clé externe, plus de critères

--- core/tabularbulkexport.class.inc.php
+++ core/tabularbulkexport.class.inc.php
@@ -8,9 +8,13 @@
 use Combodo\iTop\Application\UI\Base\Component\Input\InputUIBlockFactory;
 use Combodo\iTop\Application\UI\Base\Layout\UIContentBlockUIBlockFactory;
 use Combodo\iTop\Application\WebPage\WebPage;
+use Combodo\iTop\Core\AttributeDefinition\AttributeExternalField;
 use Combodo\iTop\Core\AttributeDefinition\AttributeExternalKey;
+use Combodo\iTop\Core\AttributeDefinition\AttributeFriendlyName;
 use Combodo\iTop\Core\AttributeDefinition\AttributeHierarchicalKey;
+use Combodo\iTop\Core\AttributeDefinition\AttributeLinkedSet;
 use Combodo\iTop\Core\AttributeDefinition\AttributeStopWatch;
+use Combodo\iTop\Core\AttributeDefinition\AttributeSubItem;
 
 /**
  * Bulk export: Tabular export: abstract base class for all "tabular" exports.
