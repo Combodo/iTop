fcb8098bae50a0812b7e2039ed3162a24108626f
purple grape <purplegrape4@gmail.com>
2026-08-08 20:02:15 +0800

    Update zh_cn.dictionary.itop.welcome-popup.php

--- dictionaries/ui/application/welcome-popup/zh_cn.dictionary.itop.welcome-popup.php
+++ dictionaries/ui/application/welcome-popup/zh_cn.dictionary.itop.welcome-popup.php
@@ -14,43 +14,29 @@ Dict::Add('ZH CN', 'Chinese', '简体中文', [
 
 // Message
 Dict::Add('ZH CN', 'Chinese', '简体中文', [
-	'UI:WelcomePopup:Message:320_01_Welcome:Title' => '欢迎使用 '.ITOP_APPLICATION_SHORT.' 3.2',
-	'UI:WelcomePopup:Message:320_01_Welcome:Description' => '<div>恭喜, 您已登录 '.ITOP_APPLICATION.' '.ITOP_VERSION_NAME.'!</div>
+	'UI:WelcomePopup:Message:330_01_Welcome:Title' => '欢迎使用 '.ITOP_APPLICATION_SHORT.' 3.3',
+	'UI:WelcomePopup:Message:330_01_Welcome:Description' => '<div>我们很高兴推出最新版本, 带来了一系列更新和改进!</div>
 <br>
-<div>很高兴向你宣布这个新版本. </div>
-<div>新增了新闻室等新功能, '.ITOP_APPLICATION_SHORT.' 3.2 还包含了关键的安全补丁, 增强的可访问性以及其它重要的改进，旨在为您提供更好的稳定性和安全性.</div>
+<div>除了多因子认证等全新功能之外, 此版本还搭载了关键安全更新, 首批 AI 组件, 以及多项优化升级, 旨在带来更流畅稳定的使用体验.</div>
 <br>
-<div>发现 '.ITOP_APPLICATION_SHORT.' 所有令人兴奋的新功能，并通过我们的新欢迎弹窗保持与重要通知的同步！</div>
-<div>我们希望你能像我们一样，从构思到创造，全程享受这个版本！</div>
+<div>希望您能尽情享受这个版本, 就像我们构建它时一样.</div>
+<div>别忘了调整您的 iTop 偏好设置, 以满足您的个性化体验.</div>',
+	'UI:WelcomePopup:Message:330_02_AIFoundations:Title' => '为 AI 奠定基础',
+	'UI:WelcomePopup:Message:330_02_AIFoundations:Description' => '<div>本次版本更新推出了首批基础组件, 用于支撑我们即将上线的多个 AI 驱动的新功能.</div>
 <br>
-<div>定制您的 '.ITOP_APPLICATION_SHORT.' 偏好设置，可以获得个性化的使用体验.</div>',
-	'UI:WelcomePopup:Message:320_02_Newsroom:Title' => '向新闻室说 "Hello" ',
-	'UI:WelcomePopup:Message:320_02_Newsroom:Description' => '<div>告别杂乱的收件箱，用 <a href="%1$s" target="_blank">'.ITOP_APPLICATION_SHORT.' 新闻室</a>迎接个性化的告警!</div>
-<div>新闻室允许您轻松管理平台内的通知,因此您可以掌握重要更新而无需频繁查收电子邮件.通过将消息标记为已读或未读,并自动删除旧通知,您可以完全控制您的通知.</div>
+<div>我们已实现核心 API ,它将支持未来的增强功能，为新的智能功能和更丰富的体验铺平道路.</div>
 <br>
-<div>今天就试试,简化您的 '.ITOP_APPLICATION_SHORT.' 沟通体验！</div>',
-	'UI:WelcomePopup:Message:320_03_NotificationsCenter:Title' => '通知中心',
-	'UI:WelcomePopup:Message:320_03_NotificationsCenter:Description' => '<div>由于我们知道您的信息摄入量已经达到最大限度,现在您可以轻松选择如何接收通知 - 通过电子邮件、聊天,甚至新闻室功能</div>
-<div>您不想接收某种类型的警报?使用这些高级自定义功能,您可以根据需要轻松定制体验.</div>
+<div>敬请拭目以待,这仅仅是好戏的开始.</div>',
+	'UI:WelcomePopup:Message:330_03_CMDB:Title' => '增强版 CMDB',
+	'UI:WelcomePopup:Message:330_03_CMDB:Description' => '<div>此版本对数据模型进行了多项改进，以更好地适配持续变化的业务需求.</div>
 <br>
-<div>通过新闻室或您的偏好设置访问您的<a href="%1$s" target="_blank">通知中心</a>,避免所有通信渠道的信息过载！</div>',
-	'UI:WelcomePopup:Message:320_05_A11yThemes:Title' => ITOP_APPLICATION_SHORT.' UI 的亲和性',
-	'UI:WelcomePopup:Message:320_05_A11yThemes:Description' => '<div>为了确保 '.ITOP_APPLICATION_SHORT.' 的亲和性,我们的团队一直在开发<a href="%1$s" target="_blank">新的后台主题</a>.符合 WCAG 标准,这些 UI 主题可以帮助视力障碍用户更容易的使用:
-<ul>
-	<li><b>色盲主题:</b> 设计用于帮助色盲用户,此主题实际上分为两个子主题以适应特定情况:</li>
-		<ul>
-			<li>一个适用于红绿色盲和绿色色盲</li>	
-			<li>另一个适用于黄蓝色盲</li>	
-		</ul>
-		<br>
-	<li><b>高对比度主题:</b> 增加对比度以允许用户更容易区分屏幕上的不同元素,并避免依赖颜色方案传递信息.它可以帮助从色盲到弱视等不同病理的用户.</li>
-</ul>
-</div>',
-	'UI:WelcomePopup:Message:320_04_PowerfulNotifications_AdminOnly:Title' => '强大的通知',
-	'UI:WelcomePopup:Message:320_04_PowerfulNotifications_AdminOnly:Description' => '<div>'.ITOP_APPLICATION_SHORT.' 的新闻室为您提供了一种新的方法,可以 <a href="%1$s" target="_blank"><b>自动化</b> 基于事件的告警</a> 并支持重复设置, 因此您可以轻松设置适合您的规则. </div>
-<div>我们的<b>基于优先级的通知排序</b>确保重要消息优先展示,同时,我们的 URL 自定义选项允许您将收件人引导到正确的位置.</div>
+<div>我们引入了容器管理, 改进的工作流管理, 更丰富的类描述, 以及生命周期管理 (End of Life / End of Support). 服务管理菜单也经过重新设计, 界面更加清晰易用, 部分配置项中还新增了图片和 Logo.</div>
 <br>
-<div>支持<b>多语言</b>,您现在可以完全控制通知显示.</div>
+<div>这些更新旨在带来更有条理、直观易懂的使用体验.</div>',
+	'UI:WelcomePopup:Message:330_04_MFA:Title' => '使用 MFA 增强安全',
+	'UI:WelcomePopup:Message:330_04_MFA:Description' => '<div>多因子认证(MFA) 现在已完全集成进入 iTop.</div>
 <br>
-<div>现在就配置它,看看您的警报流程可以变得多么高效！</div>',
+<div>MFA, 现已直接集成至核心使用流程, 在强化整体安全防护能力的同时, 保障用户获得更流畅统一的使用体验.</div>
+<br>
+<div>我们建议您检查账户设置并开启多因子认证，以获得更强的账户安全防护.</div>',
 ]);
