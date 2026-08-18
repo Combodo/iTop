ca62f746e33e8ab5818181b9e8361eceb9f8e491
odain <olivier.dain@combodo.com>
2026-08-18 17:04:03 +0200

    N°9875 - Error deprecated on component version object - use ZipArchive boilerplate methods

--- sources/Application/TwigBase/Controller/Controller.php
+++ sources/Application/TwigBase/Controller/Controller.php
@@ -646,9 +646,7 @@ abstract class Controller extends AbstractController
 	 */
 	final protected function ZipDownloadRemoveFile(array $aFiles, string $sDownloadArchiveName, bool $bUnlinkFiles = false): void
 	{
-		$sArchiveFileFullPath = tempnam(SetupUtils::GetTmpDir(), 'itop_download-').'.zip';
-		$oArchive = new ZipArchive();
-		$oArchive->open($sArchiveFileFullPath, ZipArchive::CREATE);
+		list($sArchiveFileFullPath, $oArchive) = utils::ZipArchiveOpenWithTempNam(SetupUtils::GetTmpDir(), 'itop_download-');
 		foreach ($aFiles as $sFile) {
 			$oArchive->addFile($sFile, basename($sFile));
 		}
