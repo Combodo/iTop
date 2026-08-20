be5a5b0da1c468945a712cdb3aee33979098821c
Eric Espie <eric.espie@combodo.com>
2026-08-20 15:07:06 +0200

    N°9954 - Login-basic incompatible with OAuth2 server

--- application/loginbasic.class.inc.php
+++ application/loginbasic.class.inc.php
@@ -5,7 +5,7 @@ use Combodo\iTop\Application\Helper\Session;
 /**
  * Class LoginBasic
  *
- * @copyright   Copyright (C) 2010-2024 Combodo SAS
+ * @copyright   Copyright (C) 2010-2026 Combodo SAS
  * @license     http://opensource.org/licenses/AGPL-3.0
  */
 
@@ -24,11 +24,7 @@ class LoginBasic extends AbstractLoginFSMExtension
 	protected function OnModeDetection(&$iErrorCode)
 	{
 		if (!Session::IsSet('login_mode')) {
-			if (isset($_SERVER['HTTP_AUTHORIZATION']) && !empty($_SERVER['HTTP_AUTHORIZATION'])) {
-				Session::Set('login_mode', 'basic');
-			} elseif (isset($_SERVER['REDIRECT_HTTP_AUTHORIZATION']) && !empty($_SERVER['REDIRECT_HTTP_AUTHORIZATION'])) {
-				Session::Set('login_mode', 'basic');
-			} elseif (isset($_SERVER['PHP_AUTH_USER'])) {
+			if (!is_null($this->GetAuthUserAndPassword())) {
 				Session::Set('login_mode', 'basic');
 			}
 		}
@@ -86,38 +82,46 @@ class LoginBasic extends AbstractLoginFSMExtension
 		return LoginWebPage::LOGIN_FSM_CONTINUE;
 	}
 
-	private function GetAuthUserAndPassword()
+	/**
+	 * @return array|null [$sAuthUser, $sAuthPwd]
+	 */
+	private function GetAuthUserAndPassword(): ?array
 	{
-		$sAuthUser = '';
-		$sAuthPwd = null;
 		$sAuthorization = '';
 		if (isset($_SERVER['HTTP_AUTHORIZATION']) && !empty($_SERVER['HTTP_AUTHORIZATION'])) {
 			$sAuthorization = $_SERVER['HTTP_AUTHORIZATION'];
 		} elseif (isset($_SERVER['REDIRECT_HTTP_AUTHORIZATION']) && !empty($_SERVER['REDIRECT_HTTP_AUTHORIZATION'])) {
 			$sAuthorization = $_SERVER['REDIRECT_HTTP_AUTHORIZATION'];
+		} elseif (isset($_SERVER['PHP_AUTH_USER'])) {
+			$sAuthUser = $_SERVER['PHP_AUTH_USER'];
+			// Unfortunately, the RFC is not clear about the encoding...
+			// IE and FF supply the user and password encoded in ISO-8859-1 whereas Chrome provides them encoded in UTF-8
+			// So let's try to guess if it's an UTF-8 string or not... fortunately all encodings share the same ASCII base
+			if (!LoginWebPage::LooksLikeUTF8($sAuthUser)) {
+				// Does not look like and UTF-8 string, try to convert it from iso-8859-1 to UTF-8
+				// Supposed to be harmless in case of a plain ASCII string...
+				$sAuthUser = iconv('iso-8859-1', 'utf-8', $sAuthUser);
+			}
+			$sAuthPwd = $_SERVER['PHP_AUTH_PW'];
+			if (!LoginWebPage::LooksLikeUTF8($sAuthPwd)) {
+				// Does not look like and UTF-8 string, try to convert it from iso-8859-1 to UTF-8
+				// Supposed to be harmless in case of a plain ASCII string...
+				$sAuthPwd = iconv('iso-8859-1', 'utf-8', $sAuthPwd);
+			}
+
+			return [$sAuthUser, $sAuthPwd];
 		}
 
-		if (!empty($sAuthorization)) {
-			list($sAuthUser, $sAuthPwd) = explode(':', base64_decode(substr($sAuthorization, 6)));
-		} else {
-			if (isset($_SERVER['PHP_AUTH_USER'])) {
-				$sAuthUser = $_SERVER['PHP_AUTH_USER'];
-				// Unfortunately, the RFC is not clear about the encoding...
-				// IE and FF supply the user and password encoded in ISO-8859-1 whereas Chrome provides them encoded in UTF-8
-				// So let's try to guess if it's an UTF-8 string or not... fortunately all encodings share the same ASCII base
-				if (!LoginWebPage::LooksLikeUTF8($sAuthUser)) {
-					// Does not look like and UTF-8 string, try to convert it from iso-8859-1 to UTF-8
-					// Supposed to be harmless in case of a plain ASCII string...
-					$sAuthUser = iconv('iso-8859-1', 'utf-8', $sAuthUser);
-				}
-				$sAuthPwd = $_SERVER['PHP_AUTH_PW'];
-				if (!LoginWebPage::LooksLikeUTF8($sAuthPwd)) {
-					// Does not look like and UTF-8 string, try to convert it from iso-8859-1 to UTF-8
-					// Supposed to be harmless in case of a plain ASCII string...
-					$sAuthPwd = iconv('iso-8859-1', 'utf-8', $sAuthPwd);
-				}
-			}
+		if (empty($sAuthorization)) {
+			return null;
 		}
-		return [$sAuthUser, $sAuthPwd];
+
+		// Decode without Bearer
+		$sDecodedAuthorisation = base64_decode(substr($sAuthorization, 6));
+		if (str_contains($sDecodedAuthorisation, ':')) {
+			return explode(':', $sDecodedAuthorisation);
+		}
+
+		return null;
 	}
 }
