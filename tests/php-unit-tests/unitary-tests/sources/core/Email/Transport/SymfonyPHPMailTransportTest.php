2c8febceefcabace5703af3f887e507d9a679490
jf-cbd <jf-cbd@users.noreply.github.com>
2026-08-24 11:04:14 +0200

    Add unit test

---
+++ tests/php-unit-tests/unitary-tests/sources/core/Email/Transport/SymfonyPHPMailTransportTest.php
@@ -0,0 +1,22 @@
+<?php
+
+use Combodo\iTop\Core\Email\Transport\SymfonyPHPMailTransport;
+use Combodo\iTop\Test\UnitTest\ItopTestCase;
+use Symfony\Component\Mime\Email;
+
+class SymfonyPHPMailTransportTest extends ItopTestCase
+{
+	public function testPrepareToReturnsEmptyStringWhenToHeaderIsMissing(): void
+	{
+		$oEmail = new Email()
+			->from('sender@example.com')
+			->cc('cc1@example.com', 'cc2@example.com')
+			->text('Body');
+
+		$oTransport = new SymfonyPHPMailTransport();
+
+		$oTransport->prepareTo($oEmail);
+		$this->assertTrue(true); // if no error is thrown, the test passes
+
+	}
+}
