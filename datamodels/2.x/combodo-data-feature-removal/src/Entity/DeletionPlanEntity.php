467774deaaf8f90f5541ac816ec271db462bbd04
odain <olivier.dain@combodo.com>
2026-08-24 19:31:42 +0200

    N°9949 - phpstan code cleanup

--- datamodels/2.x/combodo-data-feature-removal/src/Entity/DeletionPlanEntity.php
+++ datamodels/2.x/combodo-data-feature-removal/src/Entity/DeletionPlanEntity.php
@@ -12,17 +12,12 @@ class DeletionPlanEntity
 	public readonly DeletionPlanItem $oDelete;
 	public readonly DeletionPlanItem $oUpdate;
 	public readonly DeletionPlanItem $oIssue;
-
-	/**
-	 * @param \Combodo\iTop\DataFeatureRemoval\Entity\DeletionPlanItem $oDelete
-	 * @param \Combodo\iTop\DataFeatureRemoval\Entity\DeletionPlanItem $oUpdate
-	 * @param \Combodo\iTop\DataFeatureRemoval\Entity\DeletionPlanItem $oIssue
-	 */
+	
 	public function __construct()
 	{
-		$this->oDelete = $oDelete ?? new DeletionPlanItem();
-		$this->oUpdate = $oUpdate ?? new DeletionPlanItem();
-		$this->oIssue = $oIssue ?? new DeletionPlanItem();
+		$this->oDelete = new DeletionPlanItem();
+		$this->oUpdate = new DeletionPlanItem();
+		$this->oIssue = new DeletionPlanItem();
 	}
 
 	public function TotalCount(): int
