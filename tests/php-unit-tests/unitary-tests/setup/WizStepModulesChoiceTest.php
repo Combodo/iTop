8272273fb1700b744749b5ffaab8cbbaee02ac11
Timmy38 <timothee.callet@combodo.com>
2026-09-01 14:42:59 +0200

    N°10005 Fix phpunit labels

--- tests/php-unit-tests/unitary-tests/setup/WizStepModulesChoiceTest.php
+++ tests/php-unit-tests/unitary-tests/setup/WizStepModulesChoiceTest.php
@@ -718,7 +718,7 @@ class WizStepModulesChoiceTest extends ItopTestCase
 					'remote' => false,
 				],
 			],
-			'#node17 - An installed non uninstallable extension should be checked and disabled when the "disable uninstallation check" flag is not set, even if it is not selected' => [
+			'#node13 - An installed non uninstallable extension should be checked and disabled when the "disable uninstallation check" flag is not set, even if it is not selected' => [
 				'aExtensionsOnDiskOrDb' => [
 					'itop-ext1' => [
 						'installed' => true,
@@ -741,7 +741,7 @@ class WizStepModulesChoiceTest extends ItopTestCase
 					'remote' => false,
 				],
 			],
-			'#node18 - An installed but not selected extension with missing dependencies without force uninstall should be checked and disabled, even if it is not selected' => [
+			'#node6 - An installed extension with missing dependencies without force uninstall should be checked and disabled, even if it is not selected' => [
 				'aExtensionsOnDiskOrDb' => [
 					'itop-ext1' => [
 						'installed' => true,
